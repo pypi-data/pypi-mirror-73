aaeee9f76ffd89585f40af726b691f01728650ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:04 +0000

    Add package distributions-t/0.1/distributions_t-0.1.tar.gz

---
+++ packages/distributions-t/distributions_t-0.1.tar.gz/distributions_t-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_t',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_t'],
+      zip_safe=False)
