055054c68e989204b94ac469684387e54408ecd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:25 +0000

    Add package windowsapps/0.0.3/windowsapps-0.0.3.tar.gz

---
+++ packages/windowsapps/windowsapps-0.0.3.tar.gz/windowsapps-0.0.3/windowsapps/__init__.py
@@ -0,0 +1,25 @@
+from json import loads
+from subprocess import check_output, call
+
+def get_apps():
+	apps = loads(check_output(['powershell.exe','Get-StartApps|convertto-json'],shell=False))
+	names = {}
+	for each in apps:
+		names.update({each['Name']:each['AppID']})
+	return names
+
+def find_app(app_name):
+	apps = get_apps()
+	for each in sorted(apps,key=len):
+		if app_name.upper() in each.upper():
+			return each,apps[each]
+
+def open_app(app_name):
+	app = find_app(app_name)
+	if app == None:
+		raise ValueError('Application not found!')
+	else:
+		call("powershell.exe start 'shell:AppsFolder\%s'"%app[1],shell=False)
+
+if __name__ == "__main__":
+    open_app("Calculator")
