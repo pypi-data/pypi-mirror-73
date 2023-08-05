626d0c91dd690c90d386a72d2b42e0217fb832dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:20 +0000

    Add package distributions-akash-v123/0.1/distributions_akash_v123-0.1.tar.gz

---
+++ packages/distributions-akash-v123/distributions_akash_v123-0.1.tar.gz/distributions_akash_v123-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_akash_v123',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_akash_v123'],
+      zip_safe=False)
