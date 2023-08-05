913ae6186bef07bd9ab9a736134a6adb7a68ab30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:21 +0000

    Add package python-bugzilla/2.5.0/python-bugzilla-2.5.0.tar.gz

---
+++ packages/python-bugzilla/python-bugzilla-2.5.0.tar.gz/python-bugzilla-2.5.0/bugzilla/apiversion.py
@@ -0,0 +1,8 @@
+#
+# Copyright (C) 2014 Red Hat Inc.
+#
+# This work is licensed under the GNU GPLv2 or later.
+# See the COPYING file in the top-level directory.
+
+version = "2.5.0"
+__version__ = version
---
+++ packages/python-bugzilla/python-bugzilla-2.5.0.tar.gz/python-bugzilla-2.5.0/examples/apikey.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#
+# This work is licensed under the GNU GPLv2 or later.
+# See the COPYING file in the top-level directory.
+
+# apikey.py: Demostrate prompting for API key and passing it to Bugzilla
+# pylint: disable=undefined-variable
+
+from __future__ import print_function
+import sys
+
+import bugzilla
+
+# Don't worry, changing things here is fine, and won't send any email to
+# users or anything. It's what landfill.bugzilla.org is for!
+URL = "https://landfill.bugzilla.org/bugzilla-5.0-branch/xmlrpc.cgi"
+
+print("You can get an API key at:\n "
+      "    https://landfill.bugzilla.org/bugzilla-5.0-branch/userprefs.cgi")
+print("This is a test site, so no harm will come!\n")
+
+if sys.version_info[0] >= 3:
+    api_key = input("Enter Bugzilla API Key: ")  # noqa
+else:
+    api_key = raw_input("Enter Bugzilla API Key: ")  # noqa
+
+# API key usage assumes the API caller is storing the API key; if you would
+# like to use one of the login options that stores credentials on-disk for
+# command-line usage, use tokens or cookies.
+bzapi = bugzilla.Bugzilla(URL, api_key=api_key)
+assert bzapi.logged_in
