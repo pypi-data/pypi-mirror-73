dee8c9c03e05581de4fa0527ac2d7ae24062003e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:47 +0000

    Add package rest-framework-roles/0.4.0/rest_framework_roles-0.4.0.tar.gz

---
+++ packages/rest-framework-roles/rest_framework_roles-0.4.0.tar.gz/rest_framework_roles-0.4.0/setup.py
@@ -0,0 +1,62 @@
+import os
+import sys
+import re
+import subprocess
+import shlex
+
+try:
+    from setuptools import setup
+    from setuptools.command.install import install
+except ImportError:
+    from distutils.core import setup
+    from distutils.command.install import install
+
+
+VERSION = '0.4.0'
+
+
+def get_tag_version():
+    cmd = 'git tag --points-at HEAD'
+    versions = subprocess.check_output(shlex.split(cmd)).splitlines()
+    if len(versions) != 1:
+        sys.exit(f"Trying to get tag via git: Expected excactly one tag, got {len(versions)}")
+    version = versions[0].decode()
+    if re.match('^v[0-9]', version):
+        version = version[1:]
+    return version
+
+
+class VerifyVersionCommand(install):
+    """ Custom command to verify that the git tag matches our version """
+    description = 'verify that the git tag matches our version'
+
+    def run(self):
+        if get_tag_version() != VERSION:
+            sys.exit(f"Git tag: {tag} does not match the version of this app: {VERSION}")
+
+
+setup(
+    name='rest_framework_roles',
+    version=VERSION,
+    description='Role-based permissions for Django REST Framework and vanilla Django.',
+    author='Johan Hanssen Seferidis',
+    author_email='manossef@gmail.com',
+    packages=['rest_framework_roles'],
+    url='https://pypi.org/project/rest-framework-roles/',
+    license='LICENSE',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=[],
+    python_requires='>=3',
+    keywords=[
+        'permissions',
+        'roles',
+    ],
+    classifiers=[
+        'Framework :: Django',
+        'Topic :: Security',
+    ],
+    cmdclass={
+        'verify': VerifyVersionCommand,
+    },
+)
