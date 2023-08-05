0aaffbf7ff63b614fe96101e192d81770db9a7b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:31 +0000

    Add package honeybee-radiance/1.20.4/honeybee_radiance-1.20.4-py2.py3-none-any.whl

---
+++ packages/honeybee-radiance/honeybee_radiance-1.20.4-py2.py3-none-any.whl/honeybee_radiance/cli/__init__.py
@@ -0,0 +1,30 @@
+"""honeybee-radiance commands which will be added to honeybee command line interface."""
+
+try:
+    import click
+except ImportError:
+    raise ImportError(
+        'click is not installed. Try `pip install . [cli]` command.'
+    )
+
+from honeybee.cli import main
+from .translate import translate
+from .lib import lib
+from .sky import sky
+from .grid import grid
+from .sunpath import sunpath
+
+# command group for all radiance extension commands.
+@click.group(help='honeybee radiance commands.')
+def radiance():
+    pass
+
+# add sub-commands to radiance
+radiance.add_command(translate)
+radiance.add_command(lib)
+radiance.add_command(sky)
+radiance.add_command(grid)
+radiance.add_command(sunpath)
+
+# add radiance sub-commands to honeybee CLI
+main.add_command(radiance)
