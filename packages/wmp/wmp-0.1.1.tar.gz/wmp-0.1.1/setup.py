8a2fa925a3dc8a9af92609731dd1533546234a3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:04 +0000

    Add package wmp/0.1.1/wmp-0.1.1.tar.gz

---
+++ packages/wmp/wmp-0.1.1.tar.gz/wmp-0.1.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wmp",
+    version="0.1.1",
+    author="Frederick Corpuz",
+    author_email="fcorpuz@wesleyan.edu",
+    description="Module for face detection & recognition",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://wmp-face.readthedocs.io",
+    packages=setuptools.find_packages(),
+    package_data={"wmp": ["datasets/*.jpg", "datasets/*.mp4", "models/*.dat"]},
+    install_requires=["dlib>=19.20.0", "numpy>=1.19.0", "Pillow>=7.2.0"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
