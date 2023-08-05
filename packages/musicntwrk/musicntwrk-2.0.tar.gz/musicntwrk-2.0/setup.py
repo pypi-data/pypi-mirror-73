63a5fd03d39fb39a5e71480ceea58cb2daf4a726
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:38:34 +0000

    Add package musicntwrk/2.0/musicntwrk-2.0.tar.gz

---
+++ packages/musicntwrk/musicntwrk-2.0.tar.gz/musicntwrk-2.0/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+from distutils.core import setup
+import os
+
+data = os.path.join('src','data')
+harmony = os.path.join('src','harmony')
+ml_utils = os.path.join('src','ml_utils')
+networks = os.path.join('src','networks')
+plotting = os.path.join('src','plotting')
+timbre = os.path.join('src','timbre')
+utils = os.path.join('src','utils')
+
+extras = {
+	'with_MPI': ['mpi4py']
+}
+
+setup(name='musicntwrk',
+	version='2.0',
+	description='music as data, data as music',
+	author='Marco Buongiorno Nardelli',
+	author_email='mbn@unt.edu',
+	platforms='OS independent',
+	url='https://www.materialssoundmusic.com',
+	packages=['musicntwrk', 'musicntwrk.data','musicntwrk.harmony','musicntwrk.ml_utils','musicntwrk.networks','musicntwrk.plotting',
+			  'musicntwrk.timbre','musicntwrk.utils'],
+	package_dir={'musicntwrk':'src'},
+	install_requires=['numpy','scipy','pandas','python-louvain','networkx','music21','librosa','numba==0.48','pyo',
+					  'matplotlib','tensorflow','powerlaw','vpython','wget','PySimpleGUI','pydub'],
+	extras_require=extras
+	)
