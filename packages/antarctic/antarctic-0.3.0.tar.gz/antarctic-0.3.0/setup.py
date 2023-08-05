1d8bdbff71dc4a2d1b5b0ae4bb8fb404eac5e5b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:17 +0000

    Add package antarctic/0.3.0/antarctic-0.3.0.tar.gz

---
+++ packages/antarctic/antarctic-0.3.0.tar.gz/antarctic-0.3.0/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+from antarctic import __version__ as version
+
+# read the contents of your README file
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='antarctic',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version=version,
+    packages=find_packages(include=["antarctic*"]),
+    author='Thomas Schmelzer',
+    author_email='thomas.schmelzer@gmail.com',
+    url='https://github.com/tschm/antarctic',
+    description='Storing Pandas Data in a MongoDB database',
+    install_requires=['pandas>=1.0.5', 'pymongo>=3.10.1', 'mongoengine>=0.19.1', 'pyarrow>=0.17.1'],
+    license='MIT'
+)
