62b2fcc15cbd1961e18ff4f3ef2152e42fd4789e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:01 +0000

    Add package snapchat-dl/0.2.7/snapchat_dl-0.2.7-py2.py3-none-any.whl

---
+++ packages/snapchat-dl/snapchat_dl-0.2.7-py2.py3-none-any.whl/snapchat_dl/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for Snapchat Downloader."""
+
+__author__ = """Aakash Gajjar"""
+__email__ = "skyqutip@gmail.com"
+__version__ = "0.2.7"
+
+from snapchat_dl.snapchat_dl import SnapchatDL
+
+__all__ = ["SnapchatDL"]
