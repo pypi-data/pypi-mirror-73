3bd696dfe87052cd0921790fcd05ab2d2f1fbf46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:58 +0000

    Add package distributionstry/0.1/distributionstry-0.1.tar.gz

---
+++ packages/distributionstry/distributionstry-0.1.tar.gz/distributionstry-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributionstry',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributionstry'],
+      zip_safe=False)
