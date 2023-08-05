a7ad82c1aafa3ba4760b7034cb29cda403efa68f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:34 +0000

    Add package GnB-distributions-project/0.1/GnB_distributions_project-0.1.tar.gz

---
+++ packages/GnB-distributions-project/GnB_distributions_project-0.1.tar.gz/GnB_distributions_project-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='GnB_distributions_project',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['GnB_distributions_project'],
+      author = 'Desmond Webb',
+      author_email = 'datasciencedjw@gmail.com',
+      zip_safe=False)
