f2943b425322b6df0803b8a63f2b5608ffd29b18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:06 +0000

    Add package gb-julio-distributions/0.1/gb_julio_distributions-0.1.tar.gz

---
+++ packages/gb-julio-distributions/gb_julio_distributions-0.1.tar.gz/gb_julio_distributions-0.1/README.md
@@ -0,0 +1,3 @@
+Package for usage of Gaussian and Binomial Distribution.
+
+Take a txt file for input with the data
---
+++ packages/gb-julio-distributions/gb_julio_distributions-0.1.tar.gz/gb_julio_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='gb_julio_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      author='Julio Mastrodomenico',
+      packages=['gb_julio_distributions'],
+      zip_safe=False)
