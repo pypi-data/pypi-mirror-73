d61c7d2864c9efa56beb5f5a9d1148493e5276b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:22 +0000

    Add package gitlas/0.2.3/gitlas-0.2.3-py3-none-any.whl

---
+++ packages/gitlas/gitlas-0.2.3-py3-none-any.whl/gitlas/__init__.py
@@ -0,0 +1,12 @@
+"""
+
+ Name        : gitlas\n
+ Author      : Abhi-1U <https://github.com/Abhi-1U> \n
+ Description : A minimalist git log Statistics library\n
+ Encoding    : UTF-8\n
+ Version     : 0.2.03\n
+ Build       : 0.2.03/05-07-2020\n
+
+"""
+
+from gitlas.core import AnalyzeDataOverall,Log,JSONExport
