2a92280d1ac56dfa6e88d104ad1162bf1c80d671
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:18 +0000

    Add package vivek-ssm-distributions/0.1/vivek_ssm_distributions-0.1.tar.gz

---
+++ packages/vivek-ssm-distributions/vivek_ssm_distributions-0.1.tar.gz/vivek_ssm_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='vivek_ssm_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['vivek_ssm_distributions'],
+      zip_safe=False)
