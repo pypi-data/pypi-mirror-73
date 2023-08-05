f14cd8a03b2e04f48c6e61b78a4d9e2fa731b352
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:41 +0000

    Add package CryptoBaker/0.7.1/CryptoBaker-0.7.1.tar.gz

---
+++ packages/CryptoBaker/CryptoBaker-0.7.1.tar.gz/CryptoBaker-0.7.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+import os
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setuptools.setup(name="CryptoBaker",
+    version="0.7.1",
+    description="Crypto Bakery",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    url="https://github.com/AgeOfMarcus/CryptoBaker",
+    author="AgeOfMarcus",
+    author_email="marcus@marcusweinberger.com",
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+    install_requires=['hexdump'],
+)
