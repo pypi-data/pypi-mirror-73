4522c2b44b536577adbb403846cec6f93e105149
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:50 +0000

    Add package exif/0.11.2/exif-0.11.2.tar.gz

---
+++ packages/exif/exif-0.11.2.tar.gz/exif-0.11.2/exif/__init__.py
@@ -0,0 +1,6 @@
+"""Read and modify image EXIF metadata using Python."""
+
+from exif._constants import (
+    ColorSpace, ExposureMode, ExposureProgram, MeteringMode, Orientation, ResolutionUnit, Saturation, SceneCaptureType,
+    SensingMethod, Sharpness, WhiteBalance)
+from exif._image import Image
