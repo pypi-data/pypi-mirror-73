89390eb1a5305756091a1d6aebca8b23e3659b77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:21 +0000

    Add package dist-prop-ability2/1.0/dist_prop_ability2-1.0.tar.gz

---
+++ packages/dist-prop-ability2/dist_prop_ability2-1.0.tar.gz/dist_prop_ability2-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dist_prop_ability2',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['dist_prop_ability'],
+      Author = "Anieno Adeyemi",
+      Author_email = "anienoidaresit@gmail.com",
+      zip_safe=False)
