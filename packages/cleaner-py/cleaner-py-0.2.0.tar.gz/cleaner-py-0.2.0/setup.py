6d9637d334cc9aee861884857dd06ead894f1e46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:06 +0000

    Add package cleaner-py/0.2.0/cleaner-py-0.2.0.tar.gz

---
+++ packages/cleaner-py/cleaner-py-0.2.0.tar.gz/cleaner-py-0.2.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['cleaner', 'cleaner.commands']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['cleo>=0.8.1,<0.9.0', 'clikit>=0.6.2,<0.7.0', 'pyyaml>=5.3.1,<6.0.0']
+
+entry_points = \
+{'console_scripts': ['poetry = cleaner.commands:main']}
+
+setup_kwargs = {
+    'name': 'cleaner-py',
+    'version': '0.2.0',
+    'description': "Check for virtual environments and packages that you haven't used recently and clean them.",
+    'long_description': None,
+    'author': 'Schumi543',
+    'author_email': '12729148+Schumi543@users.noreply.github.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
