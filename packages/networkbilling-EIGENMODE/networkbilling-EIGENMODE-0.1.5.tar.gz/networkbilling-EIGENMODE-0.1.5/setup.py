63cc762e5af1bf8e848d8fc0aa97a798afad62ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:14 +0000

    Add package networkbilling-EIGENMODE/0.1.5/networkbilling-EIGENMODE-0.1.5.tar.gz

---
+++ packages/networkbilling-EIGENMODE/networkbilling-EIGENMODE-0.1.5.tar.gz/networkbilling-EIGENMODE-0.1.5/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="networkbilling-EIGENMODE",
+    version="0.1.5",
+    author="Eric Sheppard",
+    author_email="eric@eigenmo.de",
+    description="Parse and Verify Network Bill Files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/eigenmo-de/network-py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'python-dateutil',
+        'pydantic',
+    ],
+    python_requires='>=3.8',
+)
