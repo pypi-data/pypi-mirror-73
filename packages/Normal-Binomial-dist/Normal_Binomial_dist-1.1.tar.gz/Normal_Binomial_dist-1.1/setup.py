a2e60e6b3b5903d0f967a62c10433fb0e9c5e398
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:17 +0000

    Add package Normal-Binomial-dist/1.1/Normal_Binomial_dist-1.1.tar.gz

---
+++ packages/Normal-Binomial-dist/Normal_Binomial_dist-1.1.tar.gz/Normal_Binomial_dist-1.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='Normal_Binomial_dist',
+      version='1.1',
+      description='Gaussian distributions',
+      packages=['Normal_Binomial_dist'],
+      author='Pranav',
+      zip_safe=False)
