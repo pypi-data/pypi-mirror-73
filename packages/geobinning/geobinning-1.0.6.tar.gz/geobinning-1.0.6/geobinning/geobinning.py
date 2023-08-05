fc63c453c6e99b0b1cb1d4df66246ad110fd5628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:13 +0000

    Add package geobinning/1.0.6/geobinning-1.0.6.tar.gz

---
+++ packages/geobinning/geobinning-1.0.6.tar.gz/geobinning-1.0.6/geobinning/geobinning.py
@@ -0,0 +1,13 @@
+import matplotlib.path as mpltPath
+
+def geobin(geojson):
+    id = [];bins = []
+    for i,feature in enumerate(geojson['features']):
+        coords = geojson['geometry']['coordinates'][0]
+        if (len(coords)>2):
+            path = mpltPath.Path(coords)
+            inside = path.contains_points(points)
+            cnt = np.count_nonzero(inside)
+            id.append(feature['id'])
+            bins.append(cnt)
+    return pd.DataFrame(list(zip(id, bins)),columns=['id','bins'])
