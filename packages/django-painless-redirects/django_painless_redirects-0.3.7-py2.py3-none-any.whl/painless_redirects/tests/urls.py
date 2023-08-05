7f98cc0e0a39c2cd6c5ea61df1172b5a182b70a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:21 +0000

    Add package django-painless-redirects/0.3.7/django_painless_redirects-0.3.7-py2.py3-none-any.whl

---
+++ packages/django-painless-redirects/django_painless_redirects-0.3.7-py2.py3-none-any.whl/painless_redirects/tests/urls.py
@@ -0,0 +1,10 @@
+"""URLs to run the tests."""
+from django.conf.urls import url
+from django.contrib import admin
+
+
+admin.autodiscover()
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+]
