723b45e9ca2a9acc2c08064558a37c1e2db83008
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:57 +0000

    Add package aa-freight/1.1.4/aa-freight-1.1.4.tar.gz

---
+++ packages/aa-freight/aa-freight-1.1.4.tar.gz/aa-freight-1.1.4/freight/__init__.py
@@ -0,0 +1,4 @@
+default_app_config = "freight.apps.FreightConfig"
+
+__version__ = "1.1.4"
+__title__ = "Freight"
---
+++ packages/aa-freight/aa-freight-1.1.4.tar.gz/aa-freight-1.1.4/freight/auth_hooks.py
@@ -0,0 +1,34 @@
+from allianceauth.services.hooks import MenuItemHook, UrlHook
+from allianceauth import hooks
+
+from . import urls
+from .app_settings import FREIGHT_APP_NAME
+
+
+class ExampleMenuItem(MenuItemHook):
+    """ This class ensures only authorized users will see the menu entry """
+
+    def __init__(self):
+        # setup menu entry for sidebar
+        MenuItemHook.__init__(
+            self,
+            FREIGHT_APP_NAME,
+            "fas fa-truck fa-fw",
+            "freight:index",
+            navactive=["freight:index"],
+        )
+
+    def render(self, request):
+        if request.user.has_perm("freight.basic_access"):
+            return MenuItemHook.render(self, request)
+        return ""
+
+
+@hooks.register("menu_item_hook")
+def register_menu():
+    return ExampleMenuItem()
+
+
+@hooks.register("url_hook")
+def register_urls():
+    return UrlHook(urls, "freight", r"^freight/")
