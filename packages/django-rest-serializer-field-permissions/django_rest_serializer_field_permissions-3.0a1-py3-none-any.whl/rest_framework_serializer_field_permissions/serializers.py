517ea3dac386962220e2b35f4a60ff3fce5b5dca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:40 +0000

    Add package django-rest-serializer-field-permissions/3.0a1/django_rest_serializer_field_permissions-3.0a1-py3-none-any.whl

---
+++ packages/django-rest-serializer-field-permissions/django_rest_serializer_field_permissions-3.0a1-py3-none-any.whl/rest_framework_serializer_field_permissions/serializers.py
@@ -0,0 +1,34 @@
+"""
+Drop in serializer mixins.
+"""
+
+from rest_framework_serializer_field_permissions.middleware import RequestMiddleware
+
+
+class FieldPermissionSerializerMixin(object):
+    """
+    Mixin to your serializer class as follows:
+
+        class PersonSerializer(FieldPermissionSerializerMixin, serializers.ModelSerializer):
+
+            family_names = fields.CharField(permission_classes=(IsAuthenticated(), ))
+            given_names = fields.CharField(permission_classes=(IsAuthenticated(), ))
+    """
+
+    @property
+    def fields(self):
+        """
+        Supercedes drf's serializers.ModelSerializer's fields property
+        :return: a set of permission-scrubbed fields
+        """
+        ret = super(FieldPermissionSerializerMixin, self).fields
+        request = RequestMiddleware.request
+
+        forbidden_field_names = [
+            field_name for field_name, field in ret.items() if hasattr(field, 'check_permission') and (not field.check_permission(request))
+        ]
+
+        for field_name in forbidden_field_names:
+            ret.pop(field_name)
+
+        return ret
