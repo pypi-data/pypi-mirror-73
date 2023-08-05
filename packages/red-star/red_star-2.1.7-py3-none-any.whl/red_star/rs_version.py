b672dfd2c8d417470f17cf944ebb34898828dd49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:37 +0000

    Add package red-star/2.1.7/red_star-2.1.7-py3-none-any.whl

---
+++ packages/red-star/red_star-2.1.7-py3-none-any.whl/red_star/rs_version.py
@@ -0,0 +1,15 @@
+from collections import namedtuple
+__all__ = ['version_tuple', 'version']
+
+
+class VersionInfo(namedtuple("VersionInfo", 'major minor patch releaselevel')):
+    def as_string(self):
+        verstr = f"{self.major}.{self.minor}.{self.patch}"
+        if self.releaselevel != "release":
+            verstr += f"-{version_tuple.releaselevel}"
+        return verstr
+
+
+version_tuple = VersionInfo(major=2, minor=1, patch=7, releaselevel="release")
+
+version = version_tuple.as_string()
