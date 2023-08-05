7b024e344eeebd4a9d74c7836aa01f19a47e22a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:32 +0000

    Add package fawkes/0.0.5/fawkes-0.0.5.tar.gz

---
+++ packages/fawkes/fawkes-0.0.5.tar.gz/fawkes-0.0.5/fawkes/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# @Date    : 2020-07-01
+# @Author  : Shawn Shan (shansixiong@cs.uchicago.edu)
+# @Link    : https://www.shawnshan.com/
+
+
+__version__ = '0.0.5'
+
+from .detect_faces import create_mtcnn, run_detect_face
+from .differentiator import FawkesMaskGeneration
+from .protection import main
+from .utils import load_extractor, init_gpu, select_target_label, dump_image, reverse_process_cloaked, Faces, get_file
+
+__all__ = (
+    '__version__',
+    'FawkesMaskGeneration', 'load_extractor',
+    'init_gpu',
+    'select_target_label', 'dump_image', 'reverse_process_cloaked',
+    'Faces', 'get_file', 'main',
+)
