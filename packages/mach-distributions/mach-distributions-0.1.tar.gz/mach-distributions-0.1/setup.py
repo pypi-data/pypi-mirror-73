6e4c795bda6fd580c186ed2458930e16ffc0a934
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:19 +0000

    Add package mach-distributions/0.1/mach-distributions-0.1.tar.gz

---
+++ packages/mach-distributions/mach-distributions-0.1.tar.gz/mach-distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='mach-distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['mach-distributions'],
+      zip_safe=False)
