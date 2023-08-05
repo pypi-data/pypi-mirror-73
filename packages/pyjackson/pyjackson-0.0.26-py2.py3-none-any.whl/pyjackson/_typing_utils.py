e1155f7511774fda68c7e5bd66f537e254ca50b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:17 +0000

    Add package pyjackson/0.0.26/pyjackson-0.0.26-py2.py3-none-any.whl

---
+++ packages/pyjackson/pyjackson-0.0.26-py2.py3-none-any.whl/pyjackson/_typing_utils.py
@@ -0,0 +1,52 @@
+import sys
+import warnings
+
+_major, _minor, *_ = sys.version_info
+
+if _major != 3 or _minor < 5:
+    raise Exception('Pyjackson works only with python version >= 3.5')
+
+if _minor < 7:
+    from ._typing_utils35 import (is_generic35 as is_generic,
+                                  is_mapping35 as is_mapping,
+                                  get_collection_type35 as get_collection_type,
+                                  resolve_forward_ref35 as resolve_forward_ref,
+                                  resolve_inner_forward_refs35 as resolve_inner_forward_refs,
+                                  is_tuple35 as is_tuple,
+                                  get_generic_origin35 as get_generic_origin)
+
+    if _minor == 5:
+        from ._typing_utils35 import is_union35 as is_union, is_collection35 as is_collection
+    else:
+        from ._typing_utils36 import is_union36 as is_union, is_collection36 as is_collection
+
+elif _minor <= 8:
+    from ._typing_utils37 import (is_collection37 as is_collection,
+                                  is_generic37 as is_generic,
+                                  is_mapping37 as is_mapping,
+                                  is_union37 as is_union,
+                                  get_collection_type37 as get_collection_type,
+                                  resolve_forward_ref37 as resolve_forward_ref,
+                                  resolve_inner_forward_refs37 as resolve_inner_forward_refs,
+                                  is_tuple37 as is_tuple,
+                                  get_generic_origin37 as get_generic_origin)
+
+else:
+    warnings.warn('Pyjackson was not tested for python version > 3.8')
+
+
+def is_generic_or_union(cls):
+    return is_generic(cls) or is_union(cls)
+
+
+__all__ = [
+    'resolve_inner_forward_refs',
+    'is_collection',
+    'is_generic',
+    'is_mapping', 'is_union',
+    'get_collection_type',
+    'resolve_forward_ref',
+    'is_tuple',
+    'get_generic_origin',
+    'is_generic_or_union'
+]
