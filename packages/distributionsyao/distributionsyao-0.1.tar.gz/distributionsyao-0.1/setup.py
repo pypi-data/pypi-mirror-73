522deb02d3e1e45f0e7dba6397fd64e9a5a319c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:09 +0000

    Add package distributionsyao/0.1/distributionsyao-0.1.tar.gz

---
+++ packages/distributionsyao/distributionsyao-0.1.tar.gz/distributionsyao-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributionsyao',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributionsyao'],
+      zip_safe=False)
