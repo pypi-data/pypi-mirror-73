cee9089c4fc18de9a4357b78f72ed15a14bdc7b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:54 +0000

    Add package django-microblog/0.1.2/django_microblog-0.1.2-py3-none-any.whl

---
+++ packages/django-microblog/django_microblog-0.1.2-py3-none-any.whl/microblog/admin.py
@@ -0,0 +1,30 @@
+from django import forms
+from django.contrib import admin
+
+from .models import Category, Post
+
+
+class PostFormAdmin(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = '__all__'
+        widgets = {
+            'content': forms.Textarea(attrs={
+                'class': 'vLargeTextField fullwidth',
+                'rows': 32,
+            })
+        }
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    form = PostFormAdmin
+    list_display = ['title', 'slug', 'category', 'is_visible', 'weight', 'created']
+    list_filter = ['is_visible', 'category']
+    list_editable = ['weight']
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    form = PostFormAdmin
+    list_display = ['title', 'created']
