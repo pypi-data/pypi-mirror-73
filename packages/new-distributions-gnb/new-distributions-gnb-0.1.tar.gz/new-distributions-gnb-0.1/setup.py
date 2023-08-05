b136fa9100d9d6db73fb23692d56f34e9e469360
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:06 +0000

    Add package new-distributions-gnb/0.1/new-distributions-gnb-0.1.tar.gz

---
+++ packages/new-distributions-gnb/new-distributions-gnb-0.1.tar.gz/new-distributions-gnb-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='new-distributions-gnb',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['new-distributions-gnb'],
+      author= 'Indra Fransiskus Alam',
+      author_email= 'cleverdarkness@gmail.com',
+      zip_safe=False)
