4c629c67421f3e7689e84a23970e03a437624b9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:23 +0000

    Add package quickrepo/0.0.7/quickrepo-0.0.7.tar.gz

---
+++ packages/quickrepo/quickrepo-0.0.7.tar.gz/quickrepo-0.0.7/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+import os
+
+version = '0.0.7'
+current = os.path.abspath(os.path.dirname(__file__))
+# Getting long description
+with open(os.path.join(current, 'README.md'), 'r') as f:
+    readme = f.read()
+
+setup(
+    name='quickrepo',
+    version=version,
+    author='Boris Kayi & Herve Musangwa',
+    author_email='boriskayienzo@gmail.com',
+    description=
+    'Command-line tool to automate git and github repository creation.',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    python_requires='>=3.6',
+    install_requires=[
+        'Click',
+        'PyGithub',
+        'gitpython',
+        'colorama',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points='''
+    [console_scripts]
+    quickrepo=quickrepo:cli
+    ''',
+)
