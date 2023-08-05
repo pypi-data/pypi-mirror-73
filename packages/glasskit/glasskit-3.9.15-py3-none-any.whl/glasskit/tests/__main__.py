6a77016f5f410c0644513b64ff90466f271660bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:45 +0000

    Add package glasskit/3.9.15/glasskit-3.9.15-py3-none-any.whl

---
+++ packages/glasskit/glasskit-3.9.15-py3-none-any.whl/glasskit/tests/__main__.py
@@ -0,0 +1,9 @@
+import unittest
+from .test_base_model import TestBaseModel
+from .test_storable_model import TestStorableModel
+from .test_sharded_model import TestShardedModel
+from .test_submodel import TestStorableSubmodel, TestShardedSubmodel
+from .test_validators import TestValidators
+
+if __name__ == '__main__':
+    unittest.main()
