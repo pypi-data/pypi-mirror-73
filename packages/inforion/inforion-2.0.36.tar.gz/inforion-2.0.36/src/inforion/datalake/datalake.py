3b22712786024627976d66d784f9f87e4f068c3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:13:21 +0000

    Add package inforion/2.0.36/inforion-2.0.36.tar.gz

---
+++ packages/inforion/inforion-2.0.36.tar.gz/inforion-2.0.36/src/inforion/datalake/datalake.py
@@ -0,0 +1,39 @@
+import requests
+from inforion.ionapi.model import inforlogin
+
+
+def get_v1_payloads_list(filter=None, sort=None, page=None, records=None):
+    url = inforlogin.base_url() + "/IONSERVICES/datalakeapi/v1/payloads/list"
+    headers = inforlogin.header()
+    payload = {}
+
+    if filter is not None:
+        payload["filter"] = filter
+
+    if sort is not None:
+        payload["sort"] = sort
+
+    if page is not None:
+        payload["page"] = page
+
+    if records is not None:
+        payload["records"] = records
+
+    res = requests.get(url, headers=headers, params=payload)
+    return res
+
+
+def get_v1_payloads_stream_by_id(dl_id):
+    url = inforlogin.base_url() + "/IONSERVICES/datalakeapi/v1/payloads/streambyid"
+    headers = inforlogin.header()
+    payload = {"datalakeId": dl_id}
+    res = requests.get(url, headers=headers, params=payload)
+    return res
+
+
+def delete_v1_purge_id(ids):
+    url = inforlogin.base_url() + "/IONSERVICES/datalakeapi/v1/purge/ids"
+    headers = inforlogin.header()
+    payload = {"id": ids}
+    res = requests.delete(url, headers=headers, params=payload)
+    return res
