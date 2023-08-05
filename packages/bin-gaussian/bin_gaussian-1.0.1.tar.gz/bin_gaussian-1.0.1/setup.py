6378d2468134f832c04b72679e390487b8abbe8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:49 +0000

    Add package bin-gaussian/1.0.1/bin_gaussian-1.0.1.tar.gz

---
+++ packages/bin-gaussian/bin_gaussian-1.0.1.tar.gz/bin_gaussian-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(name='bin_gaussian',
+      version='1.0.1',
+      author = "oluwatobi ojo",
+      author_email = 'oluwatobiojo453@gmail.com',
+      description='Gaussian distributions and binomial distributions',
+      packages=['bin_gaussian'],
+      zip_safe=False)
+            
