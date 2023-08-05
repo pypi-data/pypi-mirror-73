de8319cf05d65c2bfc60ab257604e2751a1825d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:19 +0000

    Add package muldistri/1.3/muldistri-1.3.tar.gz

---
+++ packages/muldistri/muldistri-1.3.tar.gz/muldistri-1.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='muldistri',
+        version='1.3',
+        description='Gaussian and binomial distributions',
+        packages=['muldistri'],
+        author= 'AndresGarciaEscalante',
+        author_email = 'andres_ge@hotmail.es',
+      zip_safe=False)
