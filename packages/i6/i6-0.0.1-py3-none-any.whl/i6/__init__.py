efca17a576c4635f9f285a28ce2fec9812f0f4c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:01 +0000

    Add package i6/0.0.1/i6-0.0.1-py3-none-any.whl

---
+++ packages/i6/i6-0.0.1-py3-none-any.whl/i6/__init__.py
@@ -0,0 +1,18 @@
+"""
+    i6
+
+    A standardized collection of python libs and tools.
+
+    Example:
+    ```
+    import i6
+    
+    i6.cli().run()
+    ```
+
+    :copyright: 2020 kruserr
+    :license: MIT
+"""
+
+
+from i6.cli import cli
