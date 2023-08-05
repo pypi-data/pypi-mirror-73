514e243a22b8424d010b75db32c8949ad87d79f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:09 +0000

    Add package pxpy/1.0a26/pxpy-1.0a26.tar.gz

---
+++ packages/pxpy/pxpy-1.0a26.tar.gz/pxpy-1.0a26/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setuptools
+
+with open("README.md", "r") as fh:
+      long_description = fh.read()
+
+setuptools.setup(
+      name='pxpy',
+      version='1.0a26',
+      author = 'Nico Piatkowski',
+      author_email = 'nico.piatkowski@gmail.com',
+      description = 'discrete pairwise undirected graphical models',
+      long_description=long_description,
+      url = 'https://www.randomfields.org/px',
+      packages=['pxpy','pxpy.test'],
+      package_data={'pxpy': ['lib/libpx.so','lib/libpx_dbg.so','lib/libpx.dll','data/sin44','data/5_14.mod','data/5_14_F.mod']},
+      classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "License :: Free for non-commercial use",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence"
+      ],
+      python_requires='>=3.5'
+)
