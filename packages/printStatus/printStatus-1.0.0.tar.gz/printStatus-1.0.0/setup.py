d19397b0135111b6679cbccc8d325da8f86c13e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:16 +0000

    Add package printStatus/1.0.0/printStatus-1.0.0.tar.gz

---
+++ packages/printStatus/printStatus-1.0.0.tar.gz/printStatus-1.0.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+from setuptools import find_packages
+
+def readme():
+    with open('README') as file:
+        return(file.read())
+
+setup(name='printStatus',
+      version='1.0.0',
+      description='Display a progress bar and the status of a job in the console',
+      long_description=readme(),
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 3',
+      ],
+      url='https://abittner.gitlab.io/home/',
+      author='Adrian Bittner',
+      author_email='adrian.bittner@eso.org',
+      license='Other/Proprietary License',
+      packages=find_packages(),
+      install_requires=[],
+      python_requires='>=3',
+      include_package_data=True,
+      zip_safe=False)
