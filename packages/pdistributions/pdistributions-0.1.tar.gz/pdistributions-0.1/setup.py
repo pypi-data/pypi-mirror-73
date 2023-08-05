bf25ac79de419b6fafe03bcd545c6f67380dc470
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:45 +0000

    Add package pdistributions/0.1/pdistributions-0.1.tar.gz

---
+++ packages/pdistributions/pdistributions-0.1.tar.gz/pdistributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pdistributions',
+      version='0.1',
+      description='Gaussian distributions1',
+      packages=['pdistributions'],
+      author = 'Muhammad Liman Dahiru',
+      author_email = 'mlimand76@gmail.com',
+      zip_safe=False)
