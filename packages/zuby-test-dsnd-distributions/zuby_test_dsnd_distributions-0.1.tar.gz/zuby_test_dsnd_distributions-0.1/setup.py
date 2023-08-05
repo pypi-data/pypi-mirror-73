9efe38f525b19c36bf4fdf4c003958f30cb26b28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:59 +0000

    Add package zuby-test-dsnd-distributions/0.1/zuby_test_dsnd_distributions-0.1.tar.gz

---
+++ packages/zuby-test-dsnd-distributions/zuby_test_dsnd_distributions-0.1.tar.gz/zuby_test_dsnd_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='zuby_test_dsnd_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['zuby_test_dsnd_distributions'],
+      author='Zubair Abubakar',
+      author_email='zuby52@gmail.com',
+      zip_safe=False)
