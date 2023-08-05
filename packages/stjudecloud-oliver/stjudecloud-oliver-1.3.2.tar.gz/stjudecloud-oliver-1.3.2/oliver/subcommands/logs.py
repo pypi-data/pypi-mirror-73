a76cffd19fd68722d80e07435d1a1aecca29b14b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:23 +0000

    Add package stjudecloud-oliver/1.3.2/stjudecloud-oliver-1.3.2.tar.gz

---
+++ packages/stjudecloud-oliver/stjudecloud-oliver-1.3.2.tar.gz/stjudecloud-oliver-1.3.2/oliver/subcommands/logs.py
@@ -0,0 +1,86 @@
+import argparse
+
+from typing import Any, Dict
+
+from ..lib import api, errors, reporting
+
+
+async def call(args: Dict[str, Any], cromwell: api.CromwellAPI) -> None:
+    """Execute the subcommand.
+
+    Args:
+        args (Dict): Arguments parsed from the command line.
+    """
+
+    logs = await cromwell.get_workflows_logs(args["workflow-id"])
+    results = []
+
+    for name, call in logs["calls"].items():
+        for process in call:
+            attempt = process.get("attempt")
+            shard = process.get("shardIndex")
+
+            # TODO: experimental, this code can be removed in the future if no
+            # runtime errors are raised. If they are raised, we'll need to
+            # further flesh out how Cromwell is reporting results.
+            if not attempt:
+                errors.report(
+                    "Expected key is missing! The code needs to be updated, please contact the author!",
+                    fatal=True,
+                    exitcode=errors.ERROR_UNEXPECTED_RESPONSE,
+                )
+
+            stdout = process.get("stdout", "")
+            stderr = process.get("stderr", "")
+            results.append(
+                {
+                    "Call Name": name,
+                    "Attempt": attempt,
+                    "Shard": shard,
+                    "Log Name": "stdout",
+                    "Location": stdout,
+                }
+            )
+            results.append(
+                {
+                    "Call Name": name,
+                    "Attempt": attempt,
+                    "Shard": shard,
+                    "Log Name": "stderr",
+                    "Location": stderr,
+                }
+            )
+
+    if args.get("output_prefix"):
+        for result in results:
+            result["Location"] = args["output_prefix"] + result["Location"]
+
+    if args.get("call_name"):
+        results = list(filter(lambda r: args["call_name"] in r["Call Name"], results))
+
+    reporting.print_dicts_as_table(results)
+
+
+def register_subparser(
+    subparser: argparse._SubParsersAction,  # pylint: disable=protected-access
+) -> argparse.ArgumentParser:
+    """Registers a subparser for the current command.
+
+    Args:
+        subparser (argparse._SubParsersAction): Subparsers action.
+    """
+
+    subcommand = subparser.add_parser(
+        "logs", aliases=["l"], help="Find all reported logs for a given workflow."
+    )
+    subcommand.add_argument("workflow-id", help="Cromwell workflow ID.")
+    subcommand.add_argument(
+        "-c", "--call-name", help="Call name from the Cromwell workflow instance."
+    )
+    subcommand.add_argument(
+        "--grid-style",
+        help="Any valid `tablefmt` for python-tabulate.",
+        default="fancy_grid",
+    )
+    subcommand.set_defaults(func=call)
+    return subcommand
