7ee99261e4c559863752b3a74c05091a118bbe58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:59 +0000

    Add package ifstate/0.7.0/ifstate-0.7.0.tar.gz

---
+++ packages/ifstate/ifstate-0.7.0.tar.gz/ifstate-0.7.0/libifstate/__init__.py
@@ -0,0 +1,192 @@
+from libifstate.exception import LinkDuplicate
+from libifstate.link.base import Link
+from libifstate.address import Addresses
+from libifstate.routing import Tables, Rules
+from libifstate.parser import Parser
+from libifstate.util import logger, ipr, LogStyle
+from libifstate.exception import LinkCircularLinked, LinkNoConfigFound, ParserValidationError
+from ipaddress import ip_network, ip_interface
+from jsonschema import validate, ValidationError
+import pkgutil
+import re
+import json
+
+__version__ = "0.7.0"
+
+class IfState():
+    def __init__(self):
+        logger.debug('IfState {}'.format(__version__))
+        self.links = {}
+        self.addresses = {}
+        self.ignore = {}
+        self.tables = None
+        self.rules = None
+    
+    def update(self, ifstates):
+        # check config schema
+        schema = json.loads(pkgutil.get_data("libifstate", "../schema/ifstate.conf.schema.json"))
+        try:
+            validate(ifstates, schema)
+        except ValidationError as ex:
+            if len(ex.path) > 0:
+                path = ["$"]
+                for i, p in enumerate(ex.path):
+                    if type(p) == int:
+                        path.append("[{}]".format(p))
+                    else:
+                        path.append(".")
+                        path.append(p)
+
+                detail = "{}: {}".format("".join(path),ex.message)
+            else:
+                detail = ex.message
+            raise ParserValidationError(detail)
+
+        # add interfaces from config
+        for ifstate in ifstates['interfaces']:
+            name = ifstate['name']
+            if name in self.links:
+                raise LinkDuplicate()
+            if 'link' in ifstate:
+                self.links[name] = Link(name, **ifstate['link'])
+            else:
+                self.links[name] = None
+
+            if 'addresses' in ifstate:
+                self.addresses[name] = Addresses(name, ifstate['addresses'])
+            else:
+                self.addresses[name] = None
+
+        # add routing from config
+        if 'routing' in ifstates:
+            if 'routes' in ifstates['routing']:
+                if self.tables is None:
+                    self.tables = Tables()
+                for route in ifstates['routing']['routes']:
+                    self.tables.add(route)
+
+            if 'rules' in ifstates['routing']:
+                if self.rules is None:
+                    self.rules = Rules()
+                for rule in ifstates['routing']['rules']:
+                    self.rules.add(rule)
+
+        # add ignore list items
+        self.ignore.update(ifstates['ignore'])
+
+    def apply(self):
+        self.ipaddr_ignore = set()
+        for ip in self.ignore.get('ipaddr', []):
+            self.ipaddr_ignore.add( ip_network(ip) )
+
+        if not any(not x is None for x in self.links.values()):
+            logger.error("DANGER: Not a single link config has been found!")
+            raise LinkNoConfigFound()
+
+        logger.info('configuring interface links')
+
+        applied = []
+        while len(applied) < len(self.links):
+            last = len(applied)
+            for name, link in self.links.items():
+                if link is None:
+                    logger.debug('skipped due to no link settings', extra={'iface': name})
+                    applied.append(name)
+                else:
+                    dep = link.depends()
+                    if dep is None or dep in applied:
+                        link.apply()
+                        applied.append(name)
+            if last == len(applied):
+                raise LinkCircularLinked()
+
+        for link in ipr.get_links():
+            name = link.get_attr('IFLA_IFNAME')
+            # skip links on ignore list
+            if not name in self.links and not any(re.match(regex, name) for regex in self.ignore.get('ifname', [])):
+                info = link.get_attr('IFLA_LINKINFO')
+                # remove virtual interface
+                if info is not None:
+                    kind = info.get_attr('IFLA_INFO_KIND')
+                    logger.info('del', extra={'iface': name, 'style': LogStyle.DEL})
+                    ipr.link('set', index=link.get('index'), state='down')
+                    ipr.link('del', index=link.get('index'))
+                # shutdown physical interfaces
+                else:
+                    if link.get('state') == 'down':
+                        logger.warning('orphan', extra={'iface': name, 'style': LogStyle.OK})
+                    else:
+                        logger.warning('orphan', extra={'iface': name, 'style': LogStyle.CHG})
+                        ipr.link('set', index=link.get('index'), state='down')
+
+        if any(not x is None for x in self.addresses.values()):
+            logger.info("\nconfiguring interface ip addresses...")
+            # add empty objects for unhandled interfaces
+            for link in ipr.get_links():
+                name = link.get_attr('IFLA_IFNAME')
+                # skip links on ignore list
+                if not name in self.addresses and not any(re.match(regex, name) for regex in self.ignore.get('ifname', [])):
+                    self.addresses[name] = Addresses(name, [])
+
+            for name, addresses in self.addresses.items():
+                if addresses is None:
+                    logger.debug('skipped due to no address settings', extra={'iface': name})
+                else:
+                    addresses.apply(self.ipaddr_ignore)
+        else:
+            logger.info("\nno interface ip addressing to be applied")
+
+        if not self.tables is None:
+            self.tables.apply(self.ignore.get('routes', []))
+
+        if not self.rules is None:
+            self.rules.apply(self.ignore.get('rules', []))
+
+    def show(self):
+        self.ipaddr_ignore = set()
+        for ip in Parser._default_ifstates.get('ignore').get('ipaddr'):
+            self.ipaddr_ignore.add( ip_network(ip) )
+
+        ifs_links = []
+        for ipr_link in ipr.get_links():
+            name = ipr_link.get_attr('IFLA_IFNAME')
+            # skip links on ignore list
+            if not any(re.match(regex, name) for regex in Parser._default_ifstates['ignore'].get('ifname', [])):
+                ifs_link = {
+                        'name': name,
+                        'addresses': [],
+                        'link': {
+                            'state': ipr_link['state'],
+                        },
+                }
+
+                for addr in ipr.get_addr(index=ipr_link['index']):
+                    ip = ip_interface(addr.get_attr('IFA_ADDRESS') + '/' + str(addr['prefixlen']))
+                    if not any(ip in net for net in self.ipaddr_ignore):
+                        ifs_link['addresses'].append(ip.with_prefixlen)
+
+                info = ipr_link.get_attr('IFLA_LINKINFO')
+                if info is not None:
+                    kind = info.get_attr('IFLA_INFO_KIND')
+                    ifs_link['link']['kind'] = kind
+
+                    data = info.get_attr('IFLA_INFO_DATA')
+                    # unsupported link type, fallback to raw encoding
+                    if type(data) == str:
+                        ifs_link['link']["info_data"] = data
+                    elif data is not None:
+                        for k, v in data['attrs']:
+                            ifs_link['link'][ipr_link.nla2name(k)] = v
+                else:
+                    ifs_link['link']['kind'] = 'physical'
+                    addr = ipr_link.get_attr('IFLA_ADDRESS')
+                    if not addr is None:
+                        ifs_link['link']['address'] = addr
+
+                ifs_links.append(ifs_link)
+
+        routing = {
+            'routes': Tables().show_routes(Parser._default_ifstates['ignore']['routes']),
+        }
+
+        return { **Parser._default_ifstates, **{'interfaces': ifs_links, 'routing': routing}}
---
+++ packages/ifstate/ifstate-0.7.0.tar.gz/ifstate-0.7.0/libifstate/link/base.py
@@ -0,0 +1,155 @@
+from libifstate.util import logger, ipr, LogStyle
+from libifstate.exception import LinkTypeUnknown
+from abc import ABC, abstractmethod
+
+class Link(ABC):
+    _nla_prefix = 'IFLA_'
+    _classes = {}
+
+    def __new__(cls, *args, **kwargs):
+        cname = cls.__name__
+        if cname == Link.__name__:
+            cname = "{}Link".format(kwargs["kind"].lower().capitalize())
+
+        for c in Link.__subclasses__():
+            if c.__name__ == cname:
+                return super().__new__(c)
+
+        return super().__new__(GenericLink)
+        #raise LinkTypeUnknown()
+
+    def __init__(self, name, **kwargs):
+        self.cap_create = True
+        self.settings = {
+            'ifname': name,
+        }
+        self.settings.update(kwargs)
+        self.attr_map = {
+            'kind': ['IFLA_LINKINFO', 'IFLA_INFO_KIND'],
+        }
+        self.attr_idx = ['link', 'master', 'gre_link', 'ip6gre_link', 'vxlan_link', 'xfrm_link']
+        self.idx = None
+
+        if 'address' in self.settings:
+            self.idx = next(iter(ipr.link_lookup(
+                address=self.settings['address'])), None)
+        if self.idx is None:
+            self.idx = next(iter(ipr.link_lookup(ifname=name)), None)
+
+    def _drill_attr(self, data, keys):
+        key = keys[0]
+        d = data.get_attr(key)
+
+        if d is not None:
+            if len(keys) > 1:
+                return self._drill_attr(d, keys[1:])
+            else:
+                return d
+        else:
+            return None
+
+    def get_if_attr(self, key):
+        if key == "state":
+            return self.iface['state']
+
+        if key in self.attr_map:
+            return self._drill_attr(self.iface, self.attr_map[key])
+
+        nla = self.name2nla(key)
+        ret = self.iface.get_attr(nla)
+        if not ret is None:
+            return ret
+
+        info = self.iface.get_attr('IFLA_LINKINFO')
+        if not info is None:
+            ret = info.get_attr(nla)
+            if not ret is None:
+                return ret
+
+            info = info.get_attr('IFLA_INFO_DATA')
+            if not info is None:
+                ret = info.get_attr(nla)
+                if not ret is None:
+                    return ret
+
+        return None
+
+    def apply(self):
+        # lookup for attributes requiring a interface index
+        for attr in self.attr_idx:
+            if attr in self.settings:
+                self.settings[attr] = next(iter(ipr.link_lookup(ifname=self.settings[attr])), self.settings[attr])
+
+        if self.idx is not None:
+            self.iface = next(iter(ipr.get_links(self.idx)), None)
+
+            # check for ifname collisions
+            idx = next(iter(ipr.link_lookup(ifname=self.settings['ifname'])), None)
+            if idx is not None and idx != self.idx:
+                ipr.link('set', index=idx, state='down')
+                ipr.link('set', index=idx, ifname='{}!'.format(self.settings['ifname']))
+
+            if self.cap_create and self.get_if_attr('kind') != self.settings['kind']:
+                self.recreate()
+            else:
+                self.update()
+        else:
+            self.create()
+
+    def create(self, oper="add"):
+        logger.info(oper, extra={'iface': self.settings['ifname'], 'style': LogStyle.CHG})
+
+        logger.debug("ip link add: {}".format( " ".join("{}={}".format(k, v) for k,v in self.settings.items()) ))
+        ipr.link('add', **(self.settings))
+
+    def recreate(self):
+        logger.debug('has wrong link kind %s, removing', self.settings['kind'], extra={'iface': self.settings['ifname']})
+        ipr.link('del', index=self.idx)
+        self.idx = None
+        self.create("replace")
+
+    def update(self):
+        logger.debug('checking', extra={'iface': self.settings['ifname']})
+
+        old_state = self.iface['state']
+        has_changes = False
+        for setting in self.settings.keys():
+            logger.debug('  %s: %s => %s', setting, self.get_if_attr(
+                setting), self.settings[setting], extra={'iface': self.settings['ifname']})
+            if setting != 'kind' or self.cap_create:
+                has_changes |= self.get_if_attr(setting) != self.settings[setting]
+
+        if has_changes:
+            logger.debug('needs to be configured', extra={'iface': self.settings['ifname']})
+            if old_state:
+                logger.debug('shutting down', extra={'iface': self.settings['ifname']})
+                ipr.link('set', index=self.idx, state='down')
+                if not 'state' in self.settings:
+                    self.settings['state'] = 'up'
+
+            logger.info('change', extra={'iface': self.settings['ifname'], 'style': LogStyle.CHG})
+            ipr.link('set', index=self.idx, **(self.settings))
+        else:
+            logger.info('ok', extra={'iface': self.settings['ifname'], 'style': LogStyle.OK})
+
+    def depends(self):
+        return None
+
+    @classmethod
+    def name2nla(self, name):
+        '''
+        Convert human-friendly name into NLA name
+
+        Example: address -> IFLA_ADDRESS
+
+        Requires self.prefix to be set
+        '''
+        name = name.upper()
+        if name.find(self._nla_prefix) == -1:
+            name = "%s%s" % (self._nla_prefix, name)
+        return name
+
+
+class GenericLink(Link):
+    def __init__(self, name, **kwargs):
+        super().__init__(name, **kwargs)
