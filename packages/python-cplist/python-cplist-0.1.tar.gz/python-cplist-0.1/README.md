29f3d8a29aaf34bc6ce88e67fbc6bf0f0d8ba5b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:45 +0000

    Add package python-cplist/0.1/python-cplist-0.1.tar.gz

---
+++ packages/python-cplist/python-cplist-0.1.tar.gz/python-cplist-0.1/README.md
@@ -0,0 +1,12 @@
+# CPList
+
+A CPython parser/serializer for Apple's PropertyList format, using [libplist](https://github.com/libimobiledevice/libplist), with improved performance over Python's standard library `plistlib`.
+
+
+## Why?
+This is around 3-4x faster than CPython's PList implementation.
+
+## Bugs
+
+None so far
+
---
+++ packages/python-cplist/python-cplist-0.1.tar.gz/python-cplist-0.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+cplist = setuptools.Extension('cplist', sources=['plist.c'], libraries=['plist'], extra_compile_args=['-std=c99'], extra_objects=['/usr/local/lib/libplist.a'])
+
+setuptools.setup(
+        name='python-cplist',
+        author="Jonathan Goren",
+        author_email="jonagn@gmail.com",
+        description="CPython property list parser",
+        version="0.1",
+        ext_modules = [cplist],
+        )
