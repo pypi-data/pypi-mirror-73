8321f6c636e601a744f8b593a286977c2cb2462d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:58 +0000

    Add package bi-dist/0.1/bi_dist-0.1.tar.gz

---
+++ packages/bi-dist/bi_dist-0.1.tar.gz/bi_dist-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='bi_dist',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['bi_dist'],
+      author_email = 'jnyfaah@gmaail.com',
+      author = 'jennifer chukwu',
+      zip_safe=False)
