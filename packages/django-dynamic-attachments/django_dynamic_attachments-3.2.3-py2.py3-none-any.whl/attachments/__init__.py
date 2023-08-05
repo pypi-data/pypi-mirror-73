e0669f64c2c01a24a1e77261917897fcd9a7cdbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:51 +0000

    Add package django-dynamic-attachments/3.2.3/django_dynamic_attachments-3.2.3-py2.py3-none-any.whl

---
+++ packages/django-dynamic-attachments/django_dynamic_attachments-3.2.3-py2.py3-none-any.whl/attachments/__init__.py
@@ -0,0 +1,8 @@
+__version_info__ = (3, 2, 3)
+__version__ = '.'.join(str(i) for i in __version_info__)
+
+
+def session(*args, **kwargs):
+    # Expose utils.session without importing utils from __init__ at module level.
+    from .utils import session as attachment_session
+    return attachment_session(*args, **kwargs)
