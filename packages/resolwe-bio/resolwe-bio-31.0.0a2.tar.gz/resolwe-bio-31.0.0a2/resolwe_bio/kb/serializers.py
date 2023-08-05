0fecff20c371bde4fc24b111104bbc43bd06ec9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:10 +0000

    Add package resolwe-bio/31.0.0a2/resolwe-bio-31.0.0a2.tar.gz

---
+++ packages/resolwe-bio/resolwe-bio-31.0.0a2.tar.gz/resolwe-bio-31.0.0a2/resolwe_bio/kb/serializers.py
@@ -0,0 +1,32 @@
+""".. Ignore pydocstyle D400.
+
+===========
+Serializers
+===========
+
+"""
+from rest_framework import serializers
+
+from resolwe.rest.serializers import SelectiveFieldMixin
+
+from .models import Feature, Mapping
+
+
+class FeatureSerializer(SelectiveFieldMixin, serializers.ModelSerializer):
+    """Serializer for feature."""
+
+    class Meta:
+        """Serializer configuration."""
+
+        model = Feature
+        fields = "__all__"
+
+
+class MappingSerializer(SelectiveFieldMixin, serializers.ModelSerializer):
+    """Serializer for mapping."""
+
+    class Meta:
+        """Serializer configuration."""
+
+        model = Mapping
+        fields = "__all__"
