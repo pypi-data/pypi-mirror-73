2e76a6599692b158c13822824279319cc18dfa81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:03 +0000

    Add package robotframework-browser/0.6.0/robotframework-browser-0.6.0.tar.gz

---
+++ packages/robotframework-browser/robotframework-browser-0.6.0.tar.gz/robotframework-browser-0.6.0/Browser/keywords/browser_control.py
@@ -0,0 +1,107 @@
+from typing import Optional
+
+from robot.api import logger  # type: ignore
+from robot.utils.robottime import timestr_to_secs  # type: ignore
+from robotlibcore import keyword  # type: ignore
+
+from ..generated.playwright_pb2 import Request
+
+
+class Control:
+    def __init__(self, library):
+        self.library = library
+
+    @property
+    def playwright(self):
+        return self.library.playwright
+
+    @keyword
+    def open_browser(self, url=None, browser="Chromium", headless: bool = True):
+        """Opens a new browser instance.
+
+        If ``url`` is provided, navigates there.
+
+        The optional ``browser`` argument specifies which browser to use. The
+        supported browsers are listed in the table below. The browser names
+        are case-insensitive.
+        |   = Browser =   |        = Name(s) =        |
+        | Firefox         | firefox                   |
+        | Chromium        | chromium                  |
+        | WebKit          | webkit                    |
+
+        """
+        browser_ = browser.lower().strip()
+        if browser_ not in self.library.SUPPORTED_BROWSERS:
+            raise ValueError(
+                f"{browser} is not supported, "
+                f'it should be one of: {", ".join(self.library.SUPPORTED_BROWSERS)}'
+            )
+        with self.playwright.grpc_channel() as stub:
+            response = stub.OpenBrowser(
+                Request().openBrowser(
+                    url=url or "", browser=browser_, headless=headless
+                )
+            )
+            logger.info(response.log)
+
+    @keyword
+    def close_browser(self):
+        """Closes the current browser."""
+        with self.playwright.grpc_channel() as stub:
+            response = stub.CloseBrowser(Request.Empty())
+            logger.info(response.log)
+
+    @keyword
+    def go_forward(self):
+        """Navigates to the next page in history."""
+        with self.playwright.grpc_channel() as stub:
+            response = stub.GoForward(Request.Empty())
+            logger.info(response.log)
+
+    @keyword
+    def go_back(self):
+        """Navigates to the previous page in history."""
+        with self.playwright.grpc_channel() as stub:
+            response = stub.GoBack(Request.Empty())
+            logger.info(response.log)
+
+    @keyword
+    def go_to(self, url: str):
+        """Navigates to the given ``url``."""
+        with self.playwright.grpc_channel() as stub:
+            response = stub.GoTo(Request().goTo(url=url))
+            logger.info(response.log)
+
+    @keyword
+    def take_page_screenshot(self, path: Optional[str] = None):
+        """Takes screenshot of the current window and saves it to ``path``.
+
+        The default path is the Robot Framework output directory.
+        """
+        if path is None:
+            path = self.library.get_screenshot_path
+        logger.info(f"Taking screenshot into ${path}")
+        with self.playwright.grpc_channel() as stub:
+            response = stub.Screenshot(Request().screenshot(path=path))
+            logger.info(response.log)
+
+    @keyword
+    def set_timeout(self, timeout: str):
+        """Sets the timeout used by most input and getter keywords.
+
+        Technically this is the timeout of current playwright context.
+        """
+        parsed_timeout = float(timestr_to_secs(timeout)) * 1000
+        with self.playwright.grpc_channel() as stub:
+            response = stub.SetTimeout(Request().timeout(timeout=parsed_timeout))
+            logger.info(response.log)
+
+    @keyword
+    def add_style_tag(self, content: str):
+        """Adds a <style type="text/css"> tag with the content.
+
+        ``content``: Raw CSS content to be injected into frame.
+        """
+        with self.playwright.grpc_channel() as stub:
+            response = stub.AddStyleTag(Request().addStyleTag(content=content))
+            logger.info(response.log)
