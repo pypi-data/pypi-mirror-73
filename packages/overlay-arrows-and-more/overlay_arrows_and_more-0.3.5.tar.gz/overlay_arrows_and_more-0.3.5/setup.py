35c7c533b36f9739d318c307982d17f79568c782
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:32 +0000

    Add package overlay-arrows-and-more/0.3.5/overlay_arrows_and_more-0.3.5.tar.gz

---
+++ packages/overlay-arrows-and-more/overlay_arrows_and_more-0.3.5.tar.gz/overlay_arrows_and_more-0.3.5/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+import overlay_arrows_and_more
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='overlay_arrows_and_more',
+    version= overlay_arrows_and_more.__version__,
+    packages=['overlay_arrows_and_more'],
+    url='https://github.com/beuaaa/overlay_arrows_and_more',
+    license='MIT',
+    author='david pratmarty',
+    author_email='david.pratmarty@gmail.com',
+    description='overlay arrows and more',
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    install_requires=['pywinauto', 'enum34', 'setuptools'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Environment :: Win32 (MS Windows)',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: User Interfaces',
+        'Topic :: Software Development :: Testing',
+    ]
+)
