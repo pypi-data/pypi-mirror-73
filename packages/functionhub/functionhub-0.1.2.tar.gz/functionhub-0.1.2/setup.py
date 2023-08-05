d7c2e7be3b6c7cb178638483ac1484d00d25e6a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:37 +0000

    Add package functionhub/0.1.2/functionhub-0.1.2.tar.gz

---
+++ packages/functionhub/functionhub-0.1.2.tar.gz/functionhub-0.1.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='functionhub',
+    version='0.1.2',
+    author="naesheim",
+    description="A client for CloudStash.io interaction",
+    url="https://cloudstash.io",
+    packages=find_packages(),
+    install_requires=[
+        'Click','requests','chardet','urllib3'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    entry_points='''
+        [console_scripts]
+        fuhub=functionhub.main:fuhub
+    '''
+)
