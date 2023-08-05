d550fcd2b06a664e6043af07988b2d4823b8ab1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:28 +0000

    Add package python-active-directory/1.0.5/python_active_directory-1.0.5-cp37-cp37m-manylinux2010_x86_64.whl

---
+++ packages/python-active-directory/python_active_directory-1.0.5-cp37-cp37m-manylinux2010_x86_64.whl/activedirectory/core/exception.py
@@ -0,0 +1,17 @@
+#
+# This file is part of Python-AD. Python-AD is free software that is made
+# available under the MIT license. Consult the file "LICENSE" that is
+# distributed together with this file for the exact licensing terms.
+#
+# Python-AD is copyright (c) 2007 by the Python-AD authors. See the file
+# "AUTHORS" for a complete overview.
+
+from __future__ import absolute_import
+import ldap
+
+
+class Error(Exception):
+    pass
+
+
+LDAPError = ldap.LDAPError
