b0c6e978137f1de990ff2461def95a7d64e82ea7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:54 +0000

    Add package mr-distributions-test-mr/0.1/mr-distributions-test-mr-0.1.tar.gz

---
+++ packages/mr-distributions-test-mr/mr-distributions-test-mr-0.1.tar.gz/mr-distributions-test-mr-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='mr-distributions-test-mr',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['mr-distributions-test-mr'],
+      zip_safe=False)
