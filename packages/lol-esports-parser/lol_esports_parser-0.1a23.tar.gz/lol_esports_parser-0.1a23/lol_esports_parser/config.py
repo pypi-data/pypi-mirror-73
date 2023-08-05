4609356c56af86b05334ac36e7e7701c723bbd34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:44 +0000

    Add package lol-esports-parser/0.1a23/lol_esports_parser-0.1a23.tar.gz

---
+++ packages/lol-esports-parser/lol_esports_parser-0.1a23.tar.gz/lol_esports_parser-0.1a23/lol_esports_parser/config.py
@@ -0,0 +1,24 @@
+import json
+import os
+
+# Configuration for endpoints retries
+MAX_RETRIES = os.environ.get("LOL_ESPORTS_PARSER_MAX_RETRIES") or 2
+RETRY_DELAY = os.environ.get("LOL_ESPORTS_PARSER_RETRY_DELAY") or 10  # Time to sleep on errors (in seconds)
+
+# Required for pro endpoints
+riot_username = os.environ["RIOT_USERNAME"]
+riot_password = os.environ["RIOT_PASSWORD"]
+
+# Required for amateur games played on live servers
+api_key = os.environ.get("RIOT_API_KEY")
+
+# Local configuration files
+config_folder = os.path.join(os.path.expanduser("~"), ".config", "lol_esports_parser")
+endpoints_location = os.path.join(config_folder, "endpoints.json")
+
+if not os.path.exists(config_folder):
+    os.makedirs(config_folder)
+    raise FileNotFoundError(f"Please create {endpoints_location}.")
+
+with open(endpoints_location) as file:
+    endpoints = json.load(file)
