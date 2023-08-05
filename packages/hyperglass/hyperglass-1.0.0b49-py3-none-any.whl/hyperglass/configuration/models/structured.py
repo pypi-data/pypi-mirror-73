93dc70cf1b879530f3ac9374f179fcaa63f2e61e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:39:40 +0000

    Add package hyperglass/1.0.0b49/hyperglass-1.0.0b49-py3-none-any.whl

---
+++ packages/hyperglass/hyperglass-1.0.0b49-py3-none-any.whl/hyperglass/configuration/models/structured.py
@@ -0,0 +1,30 @@
+"""Structured data configuration variables."""
+
+# Standard Library
+from typing import List
+
+# Third Party
+from pydantic import StrictStr, constr
+
+# Project
+from hyperglass.models import HyperglassModel
+
+
+class StructuredCommunities(HyperglassModel):
+    """Control structured data response for BGP communties."""
+
+    mode: constr(regex=r"(permit|deny)") = "deny"
+    items: List[StrictStr] = []
+
+
+class StructuredRpki(HyperglassModel):
+    """Control structured data response for RPKI state."""
+
+    mode: constr(regex=r"(router|external)") = "router"
+
+
+class Structured(HyperglassModel):
+    """Control structured data responses."""
+
+    communities: StructuredCommunities = StructuredCommunities()
+    rpki: StructuredRpki = StructuredRpki()
