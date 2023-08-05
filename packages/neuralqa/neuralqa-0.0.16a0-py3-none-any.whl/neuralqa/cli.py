3c0b517c0e70b699ca2099d2171a35c67eca00df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:50 +0000

    Add package neuralqa/0.0.16a0/neuralqa-0.0.16a0-py3-none-any.whl

---
+++ packages/neuralqa/neuralqa-0.0.16a0-py3-none-any.whl/neuralqa/cli.py
@@ -0,0 +1,36 @@
+import click
+from neuralqa.server import _run_server
+from neuralqa.utils import cli_args
+from neuralqa.utils import import_case_data, ConfigParser
+
+
+@click.group()
+@click.version_option()
+def cli():
+    pass
+
+
+@cli.command()
+@cli_args.HOST
+@cli_args.PORT
+@cli_args.INDEX_HOST
+@cli_args.INDEX_PORT
+@cli_args.CONFIG_PATH
+def test(host, port, index_host, index_port, config_path):
+    config = ConfigParser(config_path)
+    print(config.config["reader"]["models"])
+    # import_case_data()
+
+
+@cli.command()
+@cli_args.HOST
+@cli_args.PORT
+@cli_args.INDEX_HOST
+@cli_args.INDEX_PORT
+@cli_args.CONFIG_PATH
+def ui(host, port, index_host, index_port, config_path):
+    _run_server(host, port, index_host, index_port, config_path)
+
+
+if __name__ == '__main__':
+    cli()
