3718d11db24f9ac9527b089f7fc8398c61e9d90d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:19 +0000

    Add package lonny-aws-procs/0.1.7/lonny_aws_procs-0.1.7.tar.gz

---
+++ packages/lonny-aws-procs/lonny_aws_procs-0.1.7.tar.gz/lonny_aws_procs-0.1.7/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['lonny_aws_procs']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['boto3>=1.14.16,<2.0.0']
+
+setup_kwargs = {
+    'name': 'lonny-aws-procs',
+    'version': '0.1.7',
+    'description': '',
+    'long_description': None,
+    'author': 'tlonny',
+    'author_email': 't@lonny.io',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
