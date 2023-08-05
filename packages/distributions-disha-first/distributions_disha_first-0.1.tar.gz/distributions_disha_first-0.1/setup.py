2f7f42091cd05073d991c2a67ed9cdf60ca057e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:23 +0000

    Add package distributions-disha-first/0.1/distributions_disha_first-0.1.tar.gz

---
+++ packages/distributions-disha-first/distributions_disha_first-0.1.tar.gz/distributions_disha_first-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_disha_first',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_disha_first'],
+      zip_safe=False)
