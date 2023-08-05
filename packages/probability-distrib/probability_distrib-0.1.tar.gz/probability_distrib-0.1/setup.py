caaaafe3880215b379e70bbba0f33bc083dd4bc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:10 +0000

    Add package probability-distrib/0.1/probability_distrib-0.1.tar.gz

---
+++ packages/probability-distrib/probability_distrib-0.1.tar.gz/probability_distrib-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='probability_distrib',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['probability_distrib'],
+      zip_safe=False)
