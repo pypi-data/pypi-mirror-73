2e5592f1c89f205ce24cba1e8f723f72c8cb1ce3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:26 +0000

    Add package abita-distributions/0.1/abita_distributions-0.1.tar.gz

---
+++ packages/abita-distributions/abita_distributions-0.1.tar.gz/abita_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='abita_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['abita_distributions'],
+      author='Abita Ann Augustine',
+      author_email='abitaaugustine@gmail.com',
+      zip_safe=False)
