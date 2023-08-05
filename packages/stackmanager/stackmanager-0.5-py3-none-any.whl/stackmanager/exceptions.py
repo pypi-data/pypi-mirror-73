d1ac3dae7ac1933f425ceab081e655644af8137e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:45 +0000

    Add package stackmanager/0.5/stackmanager-0.5-py3-none-any.whl

---
+++ packages/stackmanager/stackmanager-0.5-py3-none-any.whl/stackmanager/exceptions.py
@@ -0,0 +1,13 @@
+class ValidationError(Exception):
+    """Exception thrown when configuration fails validation"""
+    pass
+
+
+class StackError(Exception):
+    """Exception thrown when there is an AWS Error managing a CloudFormation stack"""
+    pass
+
+
+class TransferError(Exception):
+    """Exception thrown when there is an AWS Error transferring files to S3"""
+    pass
