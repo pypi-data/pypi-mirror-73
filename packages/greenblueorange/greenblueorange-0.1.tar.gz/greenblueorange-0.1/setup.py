cc7f7e81798985be675ce45d594de5bec17fef02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:30 +0000

    Add package greenblueorange/0.1/greenblueorange-0.1.tar.gz

---
+++ packages/greenblueorange/greenblueorange-0.1.tar.gz/greenblueorange-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='greenblueorange',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['greenblueorange'],
+      zip_safe=False)
