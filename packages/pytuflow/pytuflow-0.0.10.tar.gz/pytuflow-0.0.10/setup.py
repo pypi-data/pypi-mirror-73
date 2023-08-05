1d6979d6b8f120543b80a85cec96d2129c8ce667
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:54 +0000

    Add package pytuflow/0.0.10/pytuflow-0.0.10.tar.gz

---
+++ packages/pytuflow/pytuflow-0.0.10.tar.gz/pytuflow-0.0.10/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pytuflow",
+    version="0.0.10",
+    author="tuflowsupport",
+    author_email="support@tuflow.com",
+    description="Package for scripting TUFLOW time series results.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TUFLOW-Support/PyTuflow",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['numpy', 'matplotlib', 'netCDF4'],
+)
