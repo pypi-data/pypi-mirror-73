6a71de4290e6c172481773a0433a3e3837676aac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:58 +0000

    Add package pylatexenc/2.6/pylatexenc-2.6.tar.gz

---
+++ packages/pylatexenc/pylatexenc-2.6.tar.gz/pylatexenc-2.6/pylatexenc/version.py
@@ -0,0 +1,42 @@
+#
+# The MIT License (MIT)
+# 
+# Copyright (c) 2019 Philippe Faist
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
+
+#
+# Self-note: Don't forget to
+#
+#   1) bump version number here and update change log (doc/changes.rst)
+#
+#   2) git commit any remaining changes
+#
+#   3) " git tag vX.X -am '<message>' "
+#
+#   4) " git push --tags "
+#
+#   5) on github.com, fill in release details with a summary of changes etc.
+#   
+#   6) upload package to PyPI (twine upload dist/pylatexenc-X.X.tar.gz -r realpypi)
+#
+
+version_str = "2.6"
