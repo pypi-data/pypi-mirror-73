3f5bd4a860df7b5793692ab30a1b23ec6ef2e481
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:44 +0000

    Add package pyblueprint/0.0.2/pyblueprint-0.0.2.tar.gz

---
+++ packages/pyblueprint/pyblueprint-0.0.2.tar.gz/pyblueprint-0.0.2/README.rst
@@ -0,0 +1,17 @@
+=============
+*pyblueprint*
+=============
+
+.. image:: https://img.shields.io/pypi/v/pyblueprint
+
+.. image:: https://img.shields.io/github/license/veltzer/pyblueprint
+
+.. image:: https://img.shields.io/badge/code%20style-black-000000.svg
+
+project website: https://veltzer.github.io/pyblueprint
+
+author: Mark Veltzer
+
+version: 0.0.2
+
+
---
+++ packages/pyblueprint/pyblueprint-0.0.2.tar.gz/pyblueprint-0.0.2/pyblueprint/blueprint.py
@@ -0,0 +1,34 @@
+"""
+This is the module of pyblueprint
+"""
+import svgwrite
+import svgwrite.text
+import svgwrite.shapes
+
+
+class Palette:
+    def __init__(self):
+        self.text_color = 'blue'
+
+
+class BluePrint:
+    def __init__(self):
+        self.dwg = svgwrite.Drawing(size=(100, 100))
+        self.palette = Palette()
+
+    def background(self):
+        self.dwg.add(svgwrite.shapes.Rect())
+
+    def text(self, text: str, color: str = None) -> None:
+        if color is None:
+            color = self.palette.text_color
+        self.dwg.add(svgwrite.text.Text(text=text, color=color))
+
+    def rectangle(self) -> None:
+        self.dwg.add(svgwrite.shapes.Rect())
+
+    def square(self) -> None:
+        self.dwg.add(svgwrite.shapes.Rect())
+
+    def save(self, filename: str) -> None:
+        self.dwg.saveas(filename=filename)
