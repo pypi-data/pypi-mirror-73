5b3c0028bc517f771ce8dc4415820f9951d89cb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:16 +0000

    Add package demoben/0.1.4/demoben-0.1.4.tar.gz

---
+++ packages/demoben/demoben-0.1.4.tar.gz/demoben-0.1.4/demoben/demoben.py
@@ -0,0 +1,9 @@
+class Demo(object):
+    """docstring for Demo"""
+
+    def __init__(self, arg):
+        super(Demo, self).__init__()
+        self.arg = arg
+
+    def yu(self):
+        print(self.arg)
