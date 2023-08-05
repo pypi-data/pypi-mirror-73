c864a9beb110fd8e12d4e591dfc01a8d6c03f97e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:38:18 +0000

    Add package pyopentdb/0.0.4/pyopentdb-0.0.4.tar.gz

---
+++ packages/pyopentdb/pyopentdb-0.0.4.tar.gz/pyopentdb-0.0.4/pyopentdb/__init__.py
@@ -0,0 +1,14 @@
+from pyopentdb.api import OpenTDBClient
+from pyopentdb.enum import (
+    QuestionType,
+    Difficulty,
+    Category,
+    CategoryItem,
+    ResponseCode,
+)
+from pyopentdb.exc import APIError, QuestionError
+from pyopentdb.model import Question
+
+__author__ = "Andrew Mickael"
+__version__ = "0.0.4"
+__description__ = "Python interface for the Open Trivia DB"
