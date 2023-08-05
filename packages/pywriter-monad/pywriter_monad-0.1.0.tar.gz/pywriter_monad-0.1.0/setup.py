bc3b367d6a7317225f9692432a52f33cd0c9148d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:35:29 +0000

    Add package pywriter-monad/0.1.0/pywriter_monad-0.1.0.tar.gz

---
+++ packages/pywriter-monad/pywriter_monad-0.1.0.tar.gz/pywriter_monad-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['writer']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['toolz>=0.10.0,<0.11.0', 'typing-extensions>=3.7.4,<4.0.0']
+
+setup_kwargs = {
+    'name': 'pywriter-monad',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Yohei Tamura',
+    'author_email': 'tamuhey@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
