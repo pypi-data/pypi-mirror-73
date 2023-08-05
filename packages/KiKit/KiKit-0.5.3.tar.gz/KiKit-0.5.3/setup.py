c0ed7093f0a8f32d4366db3659416013a5db9e45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:10 +0000

    Add package KiKit/0.5.3/KiKit-0.5.3.tar.gz

---
+++ packages/KiKit/KiKit-0.5.3.tar.gz/KiKit-0.5.3/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="KiKit",
+    version="0.5.3",
+    author="Jan Mrázek",
+    author_email="email@honzamrazek.cz",
+    description="Automation for KiCAD boards",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yaqwsx/KiKit",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "numpy",
+        "shapely",
+        "click",
+        "markdown2",
+        "pybars3",
+        "solidpython"
+    ],
+    zip_safe=False,
+    include_package_data=True,
+    entry_points = {
+        "console_scripts": [
+            "kikit=kikit.ui:cli"
+        ],
+    }
+)
