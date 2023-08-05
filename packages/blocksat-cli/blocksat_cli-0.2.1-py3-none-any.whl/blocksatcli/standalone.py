8497e7183fdf0d4f63b0387ec7a8e293e9f5178a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:38 +0000

    Add package blocksat-cli/0.2.1/blocksat_cli-0.2.1-py3-none-any.whl

---
+++ packages/blocksat-cli/blocksat_cli-0.2.1-py3-none-any.whl/blocksatcli/standalone.py
@@ -0,0 +1,40 @@
+"""Standalone Demodulator"""
+from argparse import ArgumentDefaultsHelpFormatter
+from . import rp, firewall, defs, config
+
+
+def subparser(subparsers):
+    p = subparsers.add_parser('standalone',
+                              description="Configure host to receive \
+                              data from standalone DVB-S2 demodulator",
+                              help='Configure host to receive from \
+                              standalone DVB-S2 demodulator',
+                              formatter_class=ArgumentDefaultsHelpFormatter)
+    p.add_argument('-i', '--interface',
+                    default=None,
+                    help='Network interface connected to the standalone \
+                    demodulator')
+    p.add_argument('-y', '--yes', default=False, action='store_true',
+                   help="Default to answering Yes to configuration prompts")
+    p.set_defaults(func=cfg_standalone)
+    return p
+
+def cfg_standalone(args):
+    """Configurations for standalone DVB demodulator
+    """
+    # User info
+    user_info = config.read_cfg_file(args.cfg, args.cfg_dir)
+
+    if 'netdev' not in user_info['setup']:
+        assert(args.interface is not None), \
+            ("Please specify the network interface through option "
+             "\"-i/--interface\"")
+
+    interface = args.interface if (args.interface is not None) else \
+                user_info['setup']['netdev']
+
+    rp.set_filters([interface], prompt=(not args.yes))
+    firewall.configure([interface], defs.src_ports, igmp=True,
+                       prompt=(not args.yes))
+
+
