5f5941197d29919891731016d58c4689db5005f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:30 +0000

    Add package cuitools/1.7.2/cuitools-1.7.2.tar.gz

---
+++ packages/cuitools/cuitools-1.7.2.tar.gz/cuitools-1.7.2/README.md
@@ -0,0 +1,13 @@
+# CUITOOLS!
+## Warning! This library will not be updated anymore! If you are looking for a similar library, I recommend using the sophisticated prompt toolkit!
+## (The author's cszp uses features only found in this library, so cuitools is still used as a dependent library.)
+### install
+```pip3 install cuitools```
+
+### help
+
+[Here!](https://github.com/kumitatepazuru/cuitools/wiki)
+
+### Bugs, features you want to add
+
+[Here!](https://github.com/kumitatepazuru/cuitools/issues)
---
+++ packages/cuitools/cuitools-1.7.2.tar.gz/cuitools-1.7.2/setup.py
@@ -0,0 +1,36 @@
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+
+from setuptools import setup, find_packages
+
+try:
+    with open('README.md') as f:
+        readme = f.read()
+except IOError:
+    readme = ''
+
+
+def _requires_from_file(filename):
+    return open(filename).read().splitlines()
+
+
+setup(
+    name="cuitools",
+    version="1.7.2",
+    url='https://github.com/kumitatepazuru/cuitools',
+    author='kumitatepazuru',
+    author_email='teltelboya18@gmail.com',
+    maintainer='kumitatepazuru',
+    maintainer_email='teltelboya18@gmail.com',
+    description='Provides CUI drawing support libraries.',
+    long_description=readme,
+    packages=find_packages(),
+    install_requires=_requires_from_file('requirements.txt'),
+    long_description_content_type='text/markdown',
+    license="MIT",
+    classifiers=[
+        'Programming Language :: Python :: 3 :: Only',
+        'License :: OSI Approved :: MIT License',
+    ]
+)
