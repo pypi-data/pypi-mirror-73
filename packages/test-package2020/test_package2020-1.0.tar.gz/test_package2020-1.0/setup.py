87d16f2d567c1646188c06e4c38003b0e2d4022c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:34 +0000

    Add package test-package2020/1.0/test_package2020-1.0.tar.gz

---
+++ packages/test-package2020/test_package2020-1.0.tar.gz/test_package2020-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='test_package2020',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
