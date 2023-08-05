53494c67c2138412679e90e272aec9550b81604a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:41 +0000

    Add package prettyqt/0.76.0/prettyqt-0.76.0-py2.py3-none-any.whl

---
+++ packages/prettyqt/prettyqt-0.76.0-py2.py3-none-any.whl/prettyqt/core/url.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+"""
+
+import pathlib
+
+from qtpy import QtCore
+
+
+class Url(QtCore.QUrl):
+
+    # def __str__(self):
+    #     return self.absolutePath()
+
+    def to_path(self) -> pathlib.Path:
+        return pathlib.Path(self.path())
+
+    def is_local_file(self) -> bool:
+        return self.isLocalFile()
