3b132235e9fbbdb59408a1f2314121e79856fe11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:23 +0000

    Add package Medeina/2.1/Medeina-2.1.tar.gz

---
+++ packages/Medeina/Medeina-2.1.tar.gz/Medeina-2.1/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Medeina",
+    version="2.01",
+    author="Daniel Davies",
+    author_email="dd16785@bristol.ac.uk",
+    description="A automated solution for building of cumulative interacion web",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    url="https://github.com/Daniel-Davies/Medeina",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords=["Food Webs", "Species networks", "Cumulative Networks"],
+    install_requires=[
+        "pycountry",
+        "taxon_parser",
+        "networkx",
+        "numpy",
+        "msgpack",
+        "EcoNameTranslator",
+        "requests",
+        "pandas",
+    ],
+)
