f087a8dc400517f048fddfe17c68a80ee32f9fc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:22 +0000

    Add package gaussdist99/0.2/gaussdist99-0.2.tar.gz

---
+++ packages/gaussdist99/gaussdist99-0.2.tar.gz/gaussdist99-0.2/README.md
@@ -0,0 +1,7 @@
+This is a package to use Gaussian Distributions and Binomial Distributions
+
+# Example Package
+
+This is a simple example package. You can use
+[Github-flavored Markdown](https://guides.github.com/features/mastering-markdown/)
+to write your content.
---
+++ packages/gaussdist99/gaussdist99-0.2.tar.gz/gaussdist99-0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='gaussdist99',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['gaussdist99'],
+      url='https://github.com/Kenneth-Edwards',  # Optional
+      author='Kenneth',  # Optional
+      author_email='kenneth.nbt@gmail.com',  # Optional
+      zip_safe=False)
