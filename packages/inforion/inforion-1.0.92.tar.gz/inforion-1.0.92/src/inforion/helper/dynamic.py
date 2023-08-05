8ea795ffc906a1067a299e9ef2c89619a08fb598
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:18 +0000

    Add package inforion/1.0.92/inforion-1.0.92.tar.gz

---
+++ packages/inforion/inforion-1.0.92.tar.gz/inforion-1.0.92/src/inforion/helper/dynamic.py
@@ -0,0 +1,11 @@
+from urllib.parse import urlparse
+
+
+def url_change(url):
+
+    if url.find("ionapi") > 0:
+        result = "https://mingle-sso.eu1.inforcloudsuite.com"
+
+        return result
+    else:
+        return url
