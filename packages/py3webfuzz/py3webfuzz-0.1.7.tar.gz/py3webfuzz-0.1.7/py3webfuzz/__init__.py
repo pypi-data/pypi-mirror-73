3a076fde58521446204e118ee4c4ec22d1a5887f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:53 +0000

    Add package py3webfuzz/0.1.7/py3webfuzz-0.1.7.tar.gz

---
+++ packages/py3webfuzz/py3webfuzz-0.1.7.tar.gz/py3webfuzz-0.1.7/py3webfuzz/__init__.py
@@ -0,0 +1,16 @@
+__version__ = "0.1.7"
+
+___name__ = """ 
+ :::====  :::===  :::  ===  === :::===  :::=======  :::===== :::  === :::===== :::=====
+ :::  ===     === :::  ===  === :::     ::: === === :::      :::  ===      ===      ===
+ =======   =====  ===  ===  ===  =====  === === === ======   ===  ===    ===      ===  
+ ===          ===  ===========      === ===     === ===      ===  ===  ===      ===    
+ ===      ======    ==== ====   ======  ===     === ===       ======  ======== ========  \n\n"""
+
+__package__ = "py3webfuzz"
+
+# Classes the user will have access through the package interface
+from py3webfuzz.fuzzdb import *
+from py3webfuzz.utils import *
+from py3webfuzz.encoderFuncs import *
+from py3webfuzz import ___name__, __package__, __version__
