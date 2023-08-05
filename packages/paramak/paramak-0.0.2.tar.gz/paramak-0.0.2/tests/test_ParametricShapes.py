c0fd7150134ec2a77d594ee1ed97e108ecf8ea7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:33 +0000

    Add package paramak/0.0.2/paramak-0.0.2.tar.gz

---
+++ packages/paramak/paramak-0.0.2.tar.gz/paramak-0.0.2/tests/test_ParametricShapes.py
@@ -0,0 +1,210 @@
+
+import os
+import unittest
+from pathlib import Path
+
+import pytest
+
+from paramak import *
+
+class test_ConstantThicknessArcV(unittest.TestCase):
+    def test_BlanketConstantThickness_creation(self):
+        """creates blanket from parametric shape and checks a solid is created"""
+
+        test_shape = ConstantThicknessArcV(                
+                         inner_lower_point=(300,-200),
+                         inner_mid_point=(500,0),
+                         inner_upper_point=(300,200),
+                         thickness=20,
+                         rotation_angle = 180
+        )
+
+        assert test_shape.solid is not None
+        assert test_shape.volume > 1000
+
+class test_BlanketConstantThickness(unittest.TestCase):
+    def test_BlanketConstantThickness_creation(self):
+        """creates blanket from parametric shape and checks a solid is created"""
+
+        test_shape = BlanketConstantThickness(
+            major_radius=300,
+            minor_radius=50,
+            triangularity=0.5,
+            elongation=2,
+            thickness=200,
+            stop_angle=90,
+            start_angle=270,
+            offset_from_plasma=30,
+        )
+
+        assert test_shape.solid is not None
+        assert test_shape.volume > 1000
+
+
+class test_PoloidalFieldCoilCase(unittest.TestCase):
+    def test_PoloidalFieldCoilCase_creation(self):
+        """creates a poloidal field coil from parametric shape and checks a solid is created"""
+        test_shape = PoloidalFieldCoilCase(
+            casing_thickness=5, coil_height=50, coil_width=50, center_point=(1000, 500)
+        )
+
+        assert test_shape.solid is not None
+        assert test_shape.volume > 1000
+
+
+class test_Plasma(unittest.TestCase):
+    def test_plasma_elongation_type(self):
+        """creates a plasma object and checks elongation is type float"""
+
+        test_plasma = Plasma()
+
+        assert type(test_plasma.elongation) == float
+
+        def test_plasma_elongation_min_setting():
+            """checks ValueError is raised when an elongation < 0 is specified"""
+
+            test_plasma.elongation = -1
+
+        self.assertRaises(ValueError, test_plasma_elongation_min_setting)
+
+        def test_plasma_elongation_max_setting():
+            """checks ValueError is raised when an elongation > 4 is specified"""
+
+            test_plasma.elongation = 400
+
+        self.assertRaises(ValueError, test_plasma_elongation_max_setting)
+
+    def test_export_plasma_source(self):
+        """checks that export_stp() exports plasma stp file"""
+
+        test_plasma = Plasma()
+
+        os.system("rm plasma.stp")
+
+        test_plasma.export_stp("plasma.stp")
+
+        assert Path("plasma.stp").exists() == True
+        os.system("rm plasma.stp")
+
+
+# class test_DivertorBlock(unittest.TestCase):
+# def test_DivertorBlock_creation(self):
+#         test_shape = DivertorBlock(major_radius = 300,
+#                                    minor_radius = 50,
+#                                    triangularity = 0.5,
+#                                    elongation = 2,
+#                                    thickness = 50,
+#                                    stop_angle = 120,
+#                                    offset_from_plasma = 20,
+#                                    start_x_value = 10)
+
+#         assert test_shape.solid is not None
+#         assert test_shape.volume > 1000
+
+
+class test_CenterColumnShieldFlatTopHyperbola(unittest.TestCase):
+    def test_CenterColumnShieldFlatTopHyperbola_creation(self):
+        """creates a CenterColumnShieldFlatTopHyperbola object and checks a solid is created"""
+
+        test_shape = CenterColumnShieldFlatTopHyperbola(
+            height=500,
+            arc_height=300,
+            inner_radius=50,
+            mid_radius=100,
+            outer_radius=150,
+        )
+
+        assert test_shape.solid is not None
+        assert test_shape.volume > 1000
+
+
+class test_CenterColumnShieldPlasmaHyperbola(unittest.TestCase):
+    def test_CenterColumnShieldPlasmaHyperbola_creation(self):
+        """creates a CenterColumnShieldPlasmaHyperbola object and checks a solid is created"""
+
+        test_shape = CenterColumnShieldPlasmaHyperbola(
+            inner_radius=50, height=800, mid_offset=40, edge_offset=30
+        )
+
+        assert test_shape.solid is not None
+        assert test_shape.volume > 1000
+
+
+class test_CenterColumnShieldHyperbola(unittest.TestCase):
+    def test_CenterColumnShieldHyperbola_creation(self):
+        """creates a CenterColumnShieldHyperbola object and checks a solid is created"""
+
+        test_shape = CenterColumnShieldHyperbola(
+            height=100, inner_radius=50, mid_radius=80, outer_radius=100
+        )
+
+        assert test_shape.solid is not None
+        assert test_shape.volume > 1000
+
+
+class test_PoloidalFieldCoil(unittest.TestCase):
+    def test_PoloidalFieldCoil_creation(self):
+        """creates a PoloidalFieldCoil object and checks a solid is created"""
+
+        test_shape = PoloidalFieldCoil(height=50, width=60, center_point=(1000, 500))
+
+        assert test_shape.solid is not None
+        assert test_shape.volume > 1000
+
+
+class test_CenterColumnShieldCylinder(unittest.TestCase):
+    def test_CenterColumnShieldCylinder_creation(self):
+        """creates a CenterColumnShieldCylinder object and checks a solid is created"""
+
+        test_shape = CenterColumnShieldCylinder(
+            height=600, inner_radius=100, outer_radius=200
+        )
+
+        assert test_shape.solid is not None
+        assert test_shape.volume > 1000
+
+    def test_volume_CenterColumnShieldCylinder(self):
+        """creates a CenterColumnShieldCylinder object and checks that the volume is correct"""
+
+        test_shape = CenterColumnShieldCylinder(
+            rotation_angle=360,
+            height=15,
+            inner_radius=5,
+            outer_radius=10,
+            azimuth_placement_angle=0,
+        )
+
+        assert test_shape.volume == pytest.approx(3534.29)
+
+        test_shape = CenterColumnShieldCylinder(
+            rotation_angle=180,
+            height=15,
+            inner_radius=5,
+            outer_radius=10,
+            azimuth_placement_angle=95,
+        )
+
+        assert test_shape.volume == pytest.approx(3534.29 / 2.0)
+
+    def test_export_stp_CenterColumnShieldCylinder(self):
+        """checks that export_stp() can export an stp file of a CenterColumnShieldCylinder object"""
+
+        test_shape = CenterColumnShieldCylinder(
+            rotation_angle=360,
+            height=15,
+            inner_radius=5,
+            outer_radius=10,
+            azimuth_placement_angle=0,
+        )
+
+        os.system("rm center_column_shield.stp")
+
+        test_shape.export_stp("center_column_shield.stp")
+
+        assert Path("center_column_shield.stp").exists() == True
+
+        os.system("rm center_column_shield.stp")
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ packages/paramak/paramak-0.0.2.tar.gz/paramak-0.0.2/tests/test_Reactor.py
@@ -0,0 +1,188 @@
+
+import os
+from pathlib import Path
+import unittest
+import json
+
+from paramak import *
+
+
+class test_object_properties(unittest.TestCase):
+    def test_reactor_creation_with_default_properties(self):
+        """creates a Reactor object and checks that it has \
+                no default properties"""
+
+        test_reactor = Reactor()
+
+        assert test_reactor is not None
+
+    def test_adding_component_to_reactor(self):
+        """creates a Reactor object and checks that shapes \
+                can be added to it"""
+
+        test_shape = RotateStraightShape(points=[(0, 0), (0, 20), (20, 20)])
+        test_shape.rotation_angle = 360
+        test_shape.create_solid()
+        test_reactor = Reactor()
+        assert len(test_reactor) == 0
+        test_reactor.add_shape(test_shape)
+        assert len(test_reactor) == 1
+
+    def test_Graveyard_exists(self):
+        """checks that make_graveyard() creates a graveyard \
+                using the shapes in the Reactor object"""
+
+        test_shape = RotateStraightShape(points=[(0, 0), (0, 20), (20, 20)])
+        test_shape.rotation_angle = 360
+        test_shape.create_solid()
+        test_reactor = Reactor()
+        test_reactor.add_shape(test_shape)
+        test_reactor.make_graveyard()
+
+        assert type(test_reactor.graveyard) == Shape
+
+    def test_exported_graveyard_creates_stp_file(self):
+        """checks that export_graveyard() creates stp file in the \
+                specified location"""
+
+        test_shape = RotateStraightShape(points=[(0, 0), (0, 20), (20, 20)])
+        test_shape.rotation_angle = 360
+        os.system("rm my_graveyard.stp")
+        os.system("rm Graveyard.stp")
+        test_shape.stp_filename = "test_shape.stp"
+        test_reactor = Reactor()
+        test_reactor.add_shape(test_shape)
+
+        test_reactor.export_graveyard()
+        test_reactor.export_graveyard(filename="my_graveyard.stp")
+
+        for filepath in ["Graveyard.stp", "my_graveyard.stp"]:
+            assert Path(filepath).exists() is True
+            os.system("rm " + filepath)
+
+
+    def test_exported_stp_files_exist(self):
+        """checks that export_stp() creates stp file in the \
+                specified location"""
+
+        test_shape = RotateStraightShape(points=[(0, 0), (0, 20), (20, 20)])
+        test_shape.rotation_angle = 360
+        os.system("rm test_reactor/test_shape.stp")
+        os.system("rm test_reactor/Graveyard.stp")
+        test_shape.stp_filename = "test_shape.stp"
+        test_reactor = Reactor()
+        test_reactor.add_shape(test_shape)
+
+        test_reactor.export_stp(output_folder="test_reactor")
+
+        for filepath in ["test_reactor/test_shape.stp", "test_reactor/Graveyard.stp"]:
+            assert Path(filepath).exists() is True
+            os.system("rm " + filepath)
+
+    def test_neutronics_desscription(self):
+        def test_neutronics_description_without_material_tag():
+            """checks that a ValueError is raised when the neutronics description \
+                        is exported without material_tag"""
+
+            test_shape = RotateStraightShape(points=[(0, 0), (0, 20), (20, 20)])
+            test_shape.rotation_angle = 360
+            test_shape.stp_filename = "test.stp"
+            test_reactor = Reactor()
+            test_reactor.add_shape(test_shape)
+            neutronics_description = test_reactor.neutronics_description()
+
+        self.assertRaises(ValueError, test_neutronics_description_without_material_tag)
+
+        def test_neutronics_description_without_stp_filename():
+            """checks that a ValueError is raised when the neutronics description \
+                        is exported without stp_filename"""
+
+            test_shape = RotateStraightShape(points=[(0, 0), (0, 20), (20, 20)])
+            test_shape.rotation_angle = 360
+            test_shape.material_tag = "test_material"
+            test_reactor = Reactor()
+            test_reactor.add_shape(test_shape)
+            neutronics_description = test_reactor.neutronics_description()
+
+        self.assertRaises(ValueError, test_neutronics_description_without_stp_filename)
+
+    def test_neutronics_description_without_plasma(self):
+        """checks that the neutronics description is exported with correct \
+                material_tag and filename"""
+
+        test_shape = RotateStraightShape(points=[(0, 0), (0, 20), (20, 20)])
+        test_shape.rotation_angle = 360
+        test_shape.material_tag = "test_material"
+        test_shape.stp_filename = "test.stp"
+        test_reactor = Reactor()
+        test_reactor.add_shape(test_shape)
+        neutronics_description = test_reactor.neutronics_description()
+
+        assert len(neutronics_description) == 2
+        assert "filename" in neutronics_description[0].keys()
+        assert "material" in neutronics_description[0].keys()
+        assert neutronics_description[0]["material"] == "test_material"
+        assert neutronics_description[0]["filename"] == "test.stp"
+        assert neutronics_description[1]["material"] == "Graveyard"
+        assert neutronics_description[1]["filename"] == "Graveyard.stp"
+
+    def test_export_neutronics_description(self):
+        """checks that the neutronics description is exported to a json file with \
+                the correct material name and filename"""
+
+        os.system("rm manifest_test.json")
+
+        test_shape = RotateStraightShape(points=[(0, 0), (0, 20), (20, 20)])
+        test_shape.rotation_angle = 360
+        test_shape.material_tag = "test_material"
+        test_shape.stp_filename = "test.stp"
+        test_reactor = Reactor()
+        test_reactor.add_shape(test_shape)
+        returned_filename = test_reactor.export_neutronics_description(
+            filename="manifest_test.json"
+        )
+        with open("manifest_test.json") as json_file:
+            neutronics_description = json.load(json_file)
+
+        assert returned_filename == "manifest_test.json"
+        assert Path("manifest_test.json").exists() is True
+        assert len(neutronics_description) == 2
+        assert "filename" in neutronics_description[0].keys()
+        assert "material" in neutronics_description[0].keys()
+        assert neutronics_description[0]["material"] == "test_material"
+        assert neutronics_description[0]["filename"] == "test.stp"
+        assert neutronics_description[1]["material"] == "Graveyard"
+        assert neutronics_description[1]["filename"] == "Graveyard.stp"
+        os.system("rm manifest_test.json")
+
+    def test_export_2d_image(self):
+        """checks that export_2d_image() exports a png file with \
+                the correct filename"""
+
+        os.system("rm 2d_test_image.png")
+        test_shape = RotateStraightShape(points=[(0, 0), (0, 20), (20, 20)])
+        test_shape.rotation_angle = 360
+        test_reactor = Reactor()
+        test_reactor.add_shape(test_shape)
+        returned_filename = test_reactor.export_2d_image(filename="2d_test_image.png")
+
+        assert Path(returned_filename).exists() is True
+        os.system("rm 2d_test_image.png")
+
+    def test_export_html(self):
+        """checks that export_html() exports a html file with \
+                the correct filename"""
+
+        os.system("rm test_html.html")
+        test_shape = RotateStraightShape(points=[(0, 0), (0, 20), (20, 20)])
+        test_shape.rotation_angle = 360
+        test_reactor = Reactor()
+        test_reactor.add_shape(test_shape)
+        test_reactor.export_html(filename="test_html.html")
+
+        assert Path("test_html.html").exists() is True
+        os.system("rm test_html.html")
+
+
+if __name__ == "__main__":
+    unittest.main()
