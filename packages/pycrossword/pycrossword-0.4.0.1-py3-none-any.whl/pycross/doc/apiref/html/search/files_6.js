63c733df01a6b58aa49fbe2ac33fe0d512dc2e5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:53 +0000

    Add package pycrossword/0.4.0.1/pycrossword-0.4.0.1-py3-none-any.whl

---
+++ packages/pycrossword/pycrossword-0.4.0.1-py3-none-any.whl/pycross/doc/apiref/html/search/files_6.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['pluginbase_2epy_1670',['pluginbase.py',['../pluginbase_8py.html',1,'']]],
+  ['pluginmanager_2epy_1671',['pluginmanager.py',['../pluginmanager_8py.html',1,'']]]
+];
