dbcd04eb1e3ccbdda83f759ff209392193627a9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:29 +0000

    Add package socli/4.2/socli-4.2.tar.gz

---
+++ packages/socli/socli-4.2.tar.gz/socli-4.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+from codecs import open
+
+with open('README.md') as f:
+    longd = f.read()
+
+setup(
+    name='socli',
+    include_package_data=True,
+    packages=["socli"],
+    entry_points = {"console_scripts": ['socli = socli.socli:main']},
+    python_requires='>=3.5.0',
+    install_requires=['BeautifulSoup4','requests','colorama','Py-stackExchange', 'urwid'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    requires=['BeautifulSoup4','requests','colorama','PyStackExchange', 'urwid'],
+    version='4.2',
+    url='http://www.github.com/gautamkrishnar/socli',
+    keywords="stack overflow cli",
+    license='BSD',
+    author='Gautam krishna R',
+    author_email='r.gautamkrishna@gmail.com',
+    description='Stack overflow commnand line interface. SoCLI allows you to search and browse stack overflow from the terminal.',
+    long_description="\n\n"+longd,
+    long_description_content_type='text/markdown'
+)
