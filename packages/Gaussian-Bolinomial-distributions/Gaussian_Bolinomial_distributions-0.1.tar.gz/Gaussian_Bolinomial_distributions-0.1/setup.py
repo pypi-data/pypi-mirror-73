1a7ce51c4d48d4a3e75d1d1d86ce8e6c8350c472
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:44 +0000

    Add package Gaussian-Bolinomial-distributions/0.1/Gaussian_Bolinomial_distributions-0.1.tar.gz

---
+++ packages/Gaussian-Bolinomial-distributions/Gaussian_Bolinomial_distributions-0.1.tar.gz/Gaussian_Bolinomial_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Gaussian_Bolinomial_distributions',
+      version='0.1',
+      description='Gaussian and bolinomial distributions',
+      packages=['Gaussian_Bolinomial_distributions'],
+      zip_safe=False)
