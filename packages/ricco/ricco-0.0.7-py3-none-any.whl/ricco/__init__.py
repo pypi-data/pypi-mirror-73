bbb8bfb4124751a8246920167671a0a9ad1b0613
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:38:36 +0000

    Add package ricco/0.0.7/ricco-0.0.7-py3-none-any.whl

---
+++ packages/ricco/ricco-0.0.7-py3-none-any.whl/ricco/__init__.py
@@ -0,0 +1,14 @@
+__version__ = '0.0.7'
+
+
+import numpy as np
+
+from ricco.util import add
+
+from ricco.util import csv2shp
+from ricco.util import pinyin
+from ricco.util import rdf
+from ricco.util import shp2csv
+from ricco.util import valid_check
+from ricco.gis_tools import circum_pio_num_geo_aoi
+from ricco.gis_tools import mark_tags_df
