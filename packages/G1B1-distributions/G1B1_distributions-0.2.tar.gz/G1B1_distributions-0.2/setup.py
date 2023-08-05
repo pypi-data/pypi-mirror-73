31e8a48fdd5b730c9193cc7853b9d7c23fdd77f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:19 +0000

    Add package G1B1-distributions/0.2/G1B1_distributions-0.2.tar.gz

---
+++ packages/G1B1-distributions/G1B1_distributions-0.2.tar.gz/G1B1_distributions-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='G1B1_distributions',
+      version='0.2',
+      description='Gaussian distributions and Binomial distribution',
+      packages=find_packages(),
+      zip_safe=False)
