b38a2b21886f408283df6632004014110d591a17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:43 +0000

    Add package gcloud-rest-auth/3.3.0/gcloud-rest-auth-3.3.0.tar.gz

---
+++ packages/gcloud-rest-auth/gcloud-rest-auth-3.3.0.tar.gz/gcloud-rest-auth-3.3.0/tests/integration/conftest.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+from future import standard_library
+standard_library.install_aliases()
+import os
+
+import pytest
+
+
+@pytest.fixture(scope='module')  # type: ignore
+def creds()       :
+    return os.environ['GOOGLE_APPLICATION_CREDENTIALS']
+
+
+@pytest.fixture(scope='module')  # type: ignore
+def project()       :
+    return 'dialpad-oss'
