96c3a486c03ae13bb56254b4161143e324a78d21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:36 +0000

    Add package ppbank_cli/1.3/ppbank_cli-1.3.tar.gz

---
+++ packages/ppbank_cli/ppbank_cli-1.3.tar.gz/ppbank_cli-1.3/ppbank_cli/test.py
@@ -0,0 +1,11 @@
+import cli
+TESTFILE = '/Users/lhr/@/project-databank/ppbank/apiserver/2-IG-N'
+
+# put_conformers_xyz(TESTFILE_xyz)
+# del_conformers('2-CC-N')
+# put_conformers(TESTFILE)
+# replace_conformers(TESTFILE)
+
+# get_conformers(TESTFILE_get)
+# cli.get_conformers_by_energy(TESTFILE, 2)
+cli.del_conformers("2-CC-N")
