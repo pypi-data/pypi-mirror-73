fc73921a98ba0a5daae10965fcd433e0987456d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:19 +0000

    Add package new-project/0.0.4/new-project-0.0.4.tar.gz

---
+++ packages/new-project/new-project-0.0.4.tar.gz/new-project-0.0.4/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='new-project', # Replace with your own username
+    version='0.0.4',
+    author='Divan Visagie',
+    author_email='me@dvisagie.com',
+    description='Generate new projects from git repositories',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/divanvisagie/new',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    entry_points={
+        'console_scripts': ['new=new.new:main'],
+    },
+    python_requires='>=3.6',
+)
