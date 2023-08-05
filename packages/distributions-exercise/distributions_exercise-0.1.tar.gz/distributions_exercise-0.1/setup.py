c0a486fa39d0e2680cd39427795085f8a445f119
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:52 +0000

    Add package distributions-exercise/0.1/distributions_exercise-0.1.tar.gz

---
+++ packages/distributions-exercise/distributions_exercise-0.1.tar.gz/distributions_exercise-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_exercise',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['distributions_exercise'],
+      zip_safe=False)
