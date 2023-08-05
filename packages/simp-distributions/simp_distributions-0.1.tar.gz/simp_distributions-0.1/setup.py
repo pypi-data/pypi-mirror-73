8b47c4cbb4b989545f49a4b7c6e6a8776e44646c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:11 +0000

    Add package simp-distributions/0.1/simp_distributions-0.1.tar.gz

---
+++ packages/simp-distributions/simp_distributions-0.1.tar.gz/simp_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='simp_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['simp_distributions'],
+      author = 'Timilehin Aderinola',
+      author_email = 'timiderinola@gmail.com',
+      zip_safe=False)
