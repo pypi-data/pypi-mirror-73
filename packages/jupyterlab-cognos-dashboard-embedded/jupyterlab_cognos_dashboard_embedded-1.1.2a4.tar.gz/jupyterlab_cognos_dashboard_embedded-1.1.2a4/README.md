c1c51519f928f35e21791093f86629f377dfde4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:33 +0000

    Add package jupyterlab-cognos-dashboard-embedded/1.1.2a4/jupyterlab_cognos_dashboard_embedded-1.1.2a4.tar.gz

---
+++ packages/jupyterlab-cognos-dashboard-embedded/jupyterlab_cognos_dashboard_embedded-1.1.2a4.tar.gz/jupyterlab_cognos_dashboard_embedded-1.1.2a4/README.md
@@ -0,0 +1,3 @@
+# jupyterlab-cognos-dashboard-embedded [![Build Status](https://travis-ci.org/IBM/jupyterlab-cognos-dashboard-embedded.svg?branch=master)](https://travis-ci.org/IBM/jupyterlab-cognos-dashboard-embedded)
+
+A JupyterLab extension for creating and viewing Cognos Embedded Dashboards.
---
+++ packages/jupyterlab-cognos-dashboard-embedded/jupyterlab_cognos_dashboard_embedded-1.1.2a4.tar.gz/jupyterlab_cognos_dashboard_embedded-1.1.2a4/setup.py
@@ -0,0 +1,43 @@
+import setuptools
+from setupbase import (
+    create_cmdclass, find_packages
+)
+
+data_files_spec = [
+    ('etc/jupyter/jupyter_notebook_config.d',
+     'jupyter-config/jupyter_notebook_config.d',
+     'jupyterlab_cognos_dashboard_embedded.json'),
+]
+
+cmdclass = create_cmdclass(data_files_spec=data_files_spec)
+
+setup_dict = dict(
+    name='jupyterlab_cognos_dashboard_embedded',
+    packages=find_packages(),
+    cmdclass=cmdclass,
+    author='James Reeve',
+    author_email='james.reeve@ibm.com',
+    url='https://github.com/IBM/jupyterlab-cognos-dashboard-embedded',
+    license='Apache 2',
+    platforms="Linux, Mac OS X, Windows",
+    keywords=['Jupyter', 'JupyterLab', 'S3'],
+    python_requires='>=3.5',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+    install_requires=[
+        'notebook',
+        'requests',
+        'traitlets'
+    ]
+)
+
+setuptools.setup(
+    version='1.1.2-alpha4',
+    **setup_dict
+)
