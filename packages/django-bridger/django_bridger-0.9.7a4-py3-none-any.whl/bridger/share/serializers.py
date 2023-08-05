7ac11b2402a8c394ba50a2a5dda1ae68614512b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:04 +0000

    Add package django-bridger/0.9.7a4/django_bridger-0.9.7a4-py3-none-any.whl

---
+++ packages/django-bridger/django_bridger-0.9.7a4-py3-none-any.whl/bridger/share/serializers.py
@@ -0,0 +1,9 @@
+from bridger.serializers import Serializer, CharField, IntegerField, TextField
+
+
+class ShareSerializer(Serializer):
+
+    user_id = IntegerField(label="User ID")
+    widget_endpoint = CharField(label="Widget URL")
+    message = TextField(label="Message", default="Check out this Widget.")
+
