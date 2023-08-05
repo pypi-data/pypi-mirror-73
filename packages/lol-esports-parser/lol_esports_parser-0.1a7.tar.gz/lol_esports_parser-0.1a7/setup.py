de992fe8cec3531a5fa5011593e9f5e481684fec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:41 +0000

    Add package lol-esports-parser/0.1a7/lol_esports_parser-0.1a7.tar.gz

---
+++ packages/lol-esports-parser/lol_esports_parser-0.1a7.tar.gz/lol_esports_parser-0.1a7/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="lol_esports_parser",
+    version="0.1a7",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "requests",
+        "dateparser",
+        "lol-id-tools>=1.4.0",
+        "riot-transmute>=0.1a12",
+        "lol-dto>=0.1a8",
+        "riotwatcher",
+    ],
+    url="https://github.com/mrtolkien/lol_esports_parser",
+    license="MIT",
+    author='Gary "Tolki" Mialaret',
+    author_email="gary.mialaret+pypi@gmail.com",
+    description="A utility to query and transform LoL games from QQ and ACS into the LolGame DTO format.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
