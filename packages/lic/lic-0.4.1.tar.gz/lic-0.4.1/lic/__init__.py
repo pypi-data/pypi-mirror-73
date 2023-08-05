5a547cb98766846af9f09143a1cc70e277eea9e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:43 +0000

    Add package lic/0.4.1/lic-0.4.1.tar.gz

---
+++ packages/lic/lic-0.4.1.tar.gz/lic-0.4.1/lic/__init__.py
@@ -0,0 +1,6 @@
+"""line integral convolution algorithms
+"""
+
+from .lic import lic, run, gen_seed  # noqa: F401
+from .lic import _check_data, _load_npy_data, _load_seed, _contrast  # noqa: F401
+from .lic import __version__  # noqa: F401
