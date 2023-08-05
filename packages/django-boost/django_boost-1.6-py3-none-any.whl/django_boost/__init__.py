5febe70a7e2d3d6b9508fbba02ccb3d2deabdca2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:08 +0000

    Add package django-boost/1.6/django_boost-1.6-py3-none-any.whl

---
+++ packages/django-boost/django_boost-1.6-py3-none-any.whl/django_boost/__init__.py
@@ -0,0 +1,12 @@
+VERSION = (1, 6, 0)
+
+
+def get_version(version=VERSION):
+    if version[2] == 0:
+        return "%s.%s" % version[:2]
+    return "%s.%s.%s" % version
+
+
+__version__ = get_version()
+
+default_app_config = 'django_boost.apps.DjangoBoostConfig'
