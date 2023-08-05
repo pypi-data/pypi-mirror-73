606011fcc1b4e501e281a824075c08c4823b24c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:49 +0000

    Add package dz-distributions/0.1/dz_distributions-0.1.tar.gz

---
+++ packages/dz-distributions/dz_distributions-0.1.tar.gz/dz_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dz_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dz_distributions'],
+      author = 'Daniel Zapata',
+      author_email = 'danielrzapatas@gmail.com',
+      zip_safe=False)
