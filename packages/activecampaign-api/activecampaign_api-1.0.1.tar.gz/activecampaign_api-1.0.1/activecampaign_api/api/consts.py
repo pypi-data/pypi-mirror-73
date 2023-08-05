3f8034df3da401433c56b73d1f65ed7cc342f199
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:13 +0000

    Add package activecampaign-api/1.0.1/activecampaign_api-1.0.1.tar.gz

---
+++ packages/activecampaign-api/activecampaign_api-1.0.1.tar.gz/activecampaign_api-1.0.1/activecampaign_api/api/consts.py
@@ -0,0 +1,17 @@
+from ..version import __version__
+
+USER_AGENT = "ActiveCampaign-Python/" + __version__
+
+
+class API_ENDPOINT(object):
+    EMAIL_ACTIVITIES = 'emailActivities'
+    MESSAGE = 'messages'
+    SEGMENT = 'segments'
+    ADDRESSES = 'addresses'
+    CONTACT = 'contacts'
+    FIELD = 'fields'
+    FIELD_VALUE = 'fieldValues'
+    ACCOUNTS = 'accounts'
+    ACCOUNT_CONTACTS = 'accountContacts'
+    ACCOUNT_CUSTOM_FIELD_META = 'accountCustomFieldMeta'
+    ACCOUNT_CUSTOM_FIELD_DATA = 'accountCustomFieldData'
---
+++ packages/activecampaign-api/activecampaign_api-1.0.1.tar.gz/activecampaign_api-1.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='activecampaign_api',
+    version='1.0.1',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='Yevhen Pechurin',
+    author_email='yevhen.pechurin@gmail.com',
+    description='Api for activecampaign'
+)
