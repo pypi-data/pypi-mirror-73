0e48e63435b046ded069a7b458362a77baf5509d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:19 +0000

    Add package gep-python-coding-challenge/0.0.2/gep_python_coding_challenge-0.0.2.tar.gz

---
+++ packages/gep-python-coding-challenge/gep_python_coding_challenge-0.0.2.tar.gz/gep_python_coding_challenge-0.0.2/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import print_function
+
+from glob import glob
+from os.path import basename
+from os.path import splitext
+
+from setuptools import find_packages
+from setuptools import setup
+
+def read():
+    with open("README.md", "r") as f:
+        return f.read()
+
+
+setup(
+    name='gep_python_coding_challenge',
+    version='0.0.2',
+    license='MIT',
+    description='This package contains the beginner level assignments of the GEP Python Coding Challenge.',
+    long_description=read(),
+    long_description_content_type="text/markdown",
+    author='JcS',
+    author_email='joyce@hey.com',
+    url='https://github.com/Joyce-NL/gep-python-coding-challenge',
+    py_modules=['Problem01', 'Problem02', 'Problem41'],
+    package_dir={'': 'src'},
+    zip_safe=False,
+    classifiers=[
+        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Other/Nonlisted Topic',
+    ],
+    keywords=[
+        'Euler', 'Project Euler'
+    ],
+    python_requires='>=3.0',
+    install_requires=[],
+    extras_require={},
+)
