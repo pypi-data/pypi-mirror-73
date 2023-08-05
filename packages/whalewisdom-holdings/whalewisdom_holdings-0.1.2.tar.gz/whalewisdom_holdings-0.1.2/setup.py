4df1fed4814960f7dd6b20652f77ab1865491c13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:09 +0000

    Add package whalewisdom-holdings/0.1.2/whalewisdom_holdings-0.1.2.tar.gz

---
+++ packages/whalewisdom-holdings/whalewisdom_holdings-0.1.2.tar.gz/whalewisdom_holdings-0.1.2/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="whalewisdom_holdings",
+    version="0.1.2",
+    author="Ben Scholar",
+    author_email="bens@digitalman.com",
+    description="Package for retrieving holdings data from whalewisdom's api",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BBScholar/whalewisdom_holdings",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+    install_requires = [
+        'pandas',
+        'numpy',
+        'pycurl',
+        'certifi',
+        "openpyxl"
+    ]
+)
