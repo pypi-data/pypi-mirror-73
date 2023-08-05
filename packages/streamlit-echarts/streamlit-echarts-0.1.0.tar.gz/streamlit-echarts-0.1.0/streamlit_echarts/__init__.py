6573f56f48badca647eb6820908a417d76741a6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:07 +0000

    Add package streamlit-echarts/0.1.0/streamlit-echarts-0.1.0.tar.gz

---
+++ packages/streamlit-echarts/streamlit-echarts-0.1.0.tar.gz/streamlit-echarts-0.1.0/streamlit_echarts/__init__.py
@@ -0,0 +1,88 @@
+import os
+from typing import Dict
+from typing import Union
+
+import simplejson as json
+from pyecharts.charts.base import Base
+from pyecharts.charts.base import default
+
+import streamlit.components.v1 as components
+
+_RELEASE = True  # on packaging, pass this to True
+
+if not _RELEASE:
+    _component_func = components.declare_component("st_echarts", url="http://localhost:3001",)
+else:
+    parent_dir = os.path.dirname(os.path.abspath(__file__))
+    build_dir = os.path.join(parent_dir, "frontend/build")
+    _component_func = components.declare_component("st_echarts", path=build_dir)
+
+
+def st_echarts(
+    options: Dict,
+    theme: Union[str, Dict] = "",
+    events: Dict[str, str] = None,
+    height: str = "300px",
+    width: str = "100%",
+    renderer: str = "canvas",
+    key: str = None,
+):
+    """Display echarts chart from options dictionary
+    :param options: dictionary of echarts options. JS code should have been wrapped beforehand.
+    :param theme: prebuilt theme as string, or object
+    :param events: dictionary of mouse events to string JS functions. Don't wrap values with JsCode placeholder.
+    :param height: height of div wrapper
+    :param width: width of div wrapper
+    :param renderer: choose canvas or svg
+    :param key: assign a key to prevent component remounting
+    :return: chart
+    """
+    if events is None:
+        events = {}
+    return _component_func(
+        options=options,
+        theme=theme,
+        onEvents={k: JsCode(v).js_code for k, v in events.items()},
+        height=height,
+        width=width,
+        renderer=renderer,
+        key=key,
+        default=None,
+    )
+
+
+def st_pyecharts(
+    chart: Base,
+    theme: Union[str, Dict] = "",
+    events: Dict[str, str] = None,
+    height: str = "300px",
+    width: str = "100%",
+    renderer: str = "canvas",
+    key: str = None,
+):
+    """Display echarts chart from pyecharts instance
+    :param chart: pyecharts instance. JS code should have been wrapped beforehand.
+    :param theme: prebuilt theme as string, or object
+    :param events: dictionary of mouse events to string JS functions. Don't wrap values with JsCode placeholder.
+    :param height: height of div wrapper
+    :param width: width of div wrapper
+    :param renderer: choose canvas or svg
+    :param key: assign a key to prevent component remounting
+    :return: chart
+    """
+    options = json.dumps(chart.get_options(), default=default, ignore_nan=True)
+    return st_echarts(
+        options=json.loads(options),
+        theme=theme,
+        events=events,
+        height=height,
+        width=width,
+        key=key,
+        renderer=renderer,
+    )
+
+
+class JsCode:
+    def __init__(self, js_code: str):
+        js_placeholder = "--x_x--0_0--"
+        self.js_code = f"{js_placeholder}{js_code}{js_placeholder}"
---
+++ packages/streamlit-echarts/streamlit-echarts-0.1.0.tar.gz/streamlit-echarts-0.1.0/streamlit_echarts/frontend/build/precache-manifest.4542c2c02d29a9bf51a3595fb7a4da9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6098f2a711f0c39c5371ff856a7a6efc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0694a9db3fe247c061e5",
+    "url": "./static/css/main.d984dddf.chunk.css"
+  },
+  {
+    "revision": "5522d75429c449bebf16",
+    "url": "./static/js/2.032b8fbb.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "./static/js/2.032b8fbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0694a9db3fe247c061e5",
+    "url": "./static/js/main.881870cd.chunk.js"
+  },
+  {
+    "revision": "6515c66d2a8747a146d578e1c038a822",
+    "url": "./static/js/main.881870cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "177d041b66bb68f23701",
+    "url": "./static/js/runtime-main.34185f1e.js"
+  }
+]);
