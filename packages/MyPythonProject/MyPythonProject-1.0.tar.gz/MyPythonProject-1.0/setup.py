3cec592168ac71f732bdbbc945e8c20bfc6bd77b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:36 +0000

    Add package MyPythonProject/1.0/MyPythonProject-1.0.tar.gz

---
+++ packages/MyPythonProject/MyPythonProject-1.0.tar.gz/MyPythonProject-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='MyPythonProject',
+    version='1.0',
+    packages=[''],
+    url='https://google.com',
+    license='Free',
+    author='deepak',
+    author_email='dd@dd.com',
+    description='Test Python'
+)
