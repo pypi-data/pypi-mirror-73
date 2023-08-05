71ea664b16c107b9cb9d8b55b55f212594d72bd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:49 +0000

    Add package threador/0.1.1/threador-0.1.1.tar.gz

---
+++ packages/threador/threador-0.1.1.tar.gz/threador-0.1.1/threador/contrib.py
@@ -0,0 +1,25 @@
+from config import call_task
+import datetime
+
+
+class Executor:
+    __slots__ = ('tasks',)
+
+    def __init__(self, *args, tasks, **kwargs):
+        self.tasks = tasks or []
+
+    def run(self):
+        _start = datetime.datetime.now()
+        tasks = []
+
+        for task_name, args, kwargs in self.tasks:
+            args = args or ()
+            kwargs = kwargs or {}
+            tasks.append(call_task.delay(task_name, *args, **kwargs))
+
+        while all([i.ready() for i in tasks]):
+            pass
+
+        result = tuple(i.get(timeout=0.0) for i in tasks)
+        print(datetime.datetime.now() - _start)
+        return result
