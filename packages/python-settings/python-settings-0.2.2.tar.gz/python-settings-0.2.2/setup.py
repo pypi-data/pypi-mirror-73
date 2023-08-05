073779d0532c8c55d40df3357a7bf299d6c296ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:07 +0000

    Add package python-settings/0.2.2/python-settings-0.2.2.tar.gz

---
+++ packages/python-settings/python-settings-0.2.2.tar.gz/python-settings-0.2.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='python-settings',
+    version='0.2.2',
+    packages=setuptools.find_packages(),
+    url='https://github.com/charlsagente/python-settings',
+    license='MIT',
+    author='Carlos Perez',
+    author_email='charlsagente@gmail.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    description='This module provides you easy access to your config/settings properties from all your python modules',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Topic :: Utilities',
+        'Programming Language :: Python',
+    ]
+)
