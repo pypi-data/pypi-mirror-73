67431fbb8f5e885d69eeabe359a054fa1ab3acd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:30 +0000

    Add package Flask-IPFS/0.1.0b4/Flask-IPFS-0.1.0b4.tar.gz

---
+++ packages/Flask-IPFS/Flask-IPFS-0.1.0b4.tar.gz/Flask-IPFS-0.1.0b4/flask_ipfs/base.py
@@ -0,0 +1,48 @@
+from abc import (
+    ABC,
+    abstractmethod,
+)
+import json
+from typing import (
+    Dict,
+    List,
+    Union,
+)
+from cid import (
+    CIDv0,
+    CIDv1,
+)
+
+CID = Union[str, CIDv0, CIDv1]
+
+
+class BaseAPI(ABC):
+    @abstractmethod
+    def add(self, raw_str: str) -> CID:
+        pass
+
+    @abstractmethod
+    def get(self, cid: CID) -> str:
+        pass
+
+    def add_json(self, obj: Dict) -> CID:
+        return self.add(json.dumps(obj, sort_keys=True, separators=(",", ":")))
+
+    def get_json(self, cid: CID) -> Dict:
+        return json.loads(self.get(cid))
+
+    @abstractmethod
+    def ls_pins(self) -> List[CID]:
+        pass
+
+    @abstractmethod
+    def add_pin(self, cid: CID):
+        pass
+
+    @abstractmethod
+    def rm_pin(self, cid: CID):
+        pass
+
+    def update_pin(self, old_cid: CID, new_cid: CID):
+        self.add_pin(new_cid)
+        self.rm_pin(old_cid)
