4d2d62ce510de296ab8262d994efc3fe0a6283d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:06 +0000

    Add package sinever/1.0.0/sinever-1.0.0.tar.gz

---
+++ packages/sinever/sinever-1.0.0.tar.gz/sinever-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='sinever',
+    version='1.0.0',
+    description='a test for dist on pypi',
+    author='sinever',
+    author_email='sinever@126.com',
+    url='https://openeuler.org',
+)
