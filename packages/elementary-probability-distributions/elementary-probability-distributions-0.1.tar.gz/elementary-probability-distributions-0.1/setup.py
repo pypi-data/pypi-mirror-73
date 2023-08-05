758f48748076b9f5db615e746ac96559344f5e70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:06 +0000

    Add package elementary-probability-distributions/0.1/elementary-probability-distributions-0.1.tar.gz

---
+++ packages/elementary-probability-distributions/elementary-probability-distributions-0.1.tar.gz/elementary-probability-distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='elementary-probability-distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions'],
+      zip_safe=False)
