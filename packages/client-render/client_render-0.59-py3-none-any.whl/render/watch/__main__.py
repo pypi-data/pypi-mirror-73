1d92101e0430f9842991e56a2c335758a6dc27d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:59 +0000

    Add package client-render/0.59/client_render-0.59-py3-none-any.whl

---
+++ packages/client-render/client_render-0.59-py3-none-any.whl/render/watch/__main__.py
@@ -0,0 +1,40 @@
+import logging
+import time
+from argparse import ArgumentParser
+
+from render import LOG_DIR
+from render.logger import configure_logger
+from render.s3 import AWS_S3
+from render.watch.logs import collect_instance_logs
+from render.watch.screen import capture_screenshot
+from render.watch.stat import log_usage
+from render.watch.window import WindowMgr
+
+logger = logging.getLogger(__name__)
+
+
+def main(args):
+    configure_logger(LOG_DIR / 'watch.log')
+
+    s3 = AWS_S3()
+    w = WindowMgr()
+
+    while True:
+        w.find_window_wildcard(args.window_name)
+        w.foreground()
+        w.maximaze()
+
+        capture_screenshot(s3)
+        log_usage()
+
+        collect_instance_logs(s3)
+
+        logger.debug(f"Waiting {args.delay} seconds")
+        time.sleep(args.delay)
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument("window_name")
+    parser.add_argument("--delay", type=int, default=60)
+    main(parser.parse_args())
