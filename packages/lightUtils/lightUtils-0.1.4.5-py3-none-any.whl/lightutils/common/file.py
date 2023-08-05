828dd830525146307d98fa25463a16b4fc3f29dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:04 +0000

    Add package lightUtils/0.1.4.5/lightUtils-0.1.4.5-py3-none-any.whl

---
+++ packages/lightUtils/lightUtils-0.1.4.5-py3-none-any.whl/lightutils/common/file.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/7/7 8:36
+# @Author  : lightsmile
+# @Software: PyCharm
+
+import os
+from typing import Union, List
+
+
+class NotSpecifiedFileException(Exception):
+    def __init__(self, info: Union[str, List[str]]):
+        if isinstance(info, str):
+            self._info = info
+        else:
+            self._info = '/'.join(info)
+
+    def __str__(self):
+        return "并非指定的文件错误，应该是形如：{}".format(self._info)
+
+
+def check_file(file_path: str, ext: Union[str, List[str]] = None):
+    if not os.path.exists(file_path):
+        raise FileNotFoundError
+    if ext and not file_path.endswith(ext):
+        raise NotSpecifiedFileException('xx.' + ext)
+
+
+def get_file_name(file_path: str):
+    """
+    获取文件名，例如：
+    :param file_path: fuck/fuck/fuck.txt -> fuck
+    :return: 文件名，不含拓展名
+    """
+    _, name = os.path.split(file_path)
+    file_name, _ = os.path.splitext(name)
+    return file_name
+
+
+if __name__ == '__main__':
+    # check_file('fuck.txt', 'txt')
+    # check_file('__init__.py', 'txt')
+    check_file('__init__.py', 'py')
+    print(get_file_name('hello_world.py'))
