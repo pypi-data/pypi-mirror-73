12ddeec7ebbb7d67da2e663fe47f5cb01b239c19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:49 +0000

    Add package example-pkg-jcanode/0.0.1/example-pkg-jcanode-0.0.1.tar.gz

---
+++ packages/example-pkg-jcanode/example-pkg-jcanode-0.0.1.tar.gz/example-pkg-jcanode-0.0.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+from setuptools import setup, find_namespace_packages
+
+
+setuptools.setup(
+    name="example-pkg-jcanode",
+    version="0.0.1",
+    author="Justin Canode",
+    author_email="jcanode@my.gcu.edu",
+    description="A small Neural Network Framework",
+    long_description_content_type="text/markdown",
+    url="https://github.com/jcanode/small_nn",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
