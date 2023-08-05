d28cd859e9b691002513b659f08cf7b143fb98e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:37 +0000

    Add package cfdi-cli/0.0.19/cfdi-cli-0.0.19.tar.gz

---
+++ packages/cfdi-cli/cfdi-cli-0.0.19.tar.gz/cfdi-cli-0.0.19/cfdi/_version.py
@@ -0,0 +1,11 @@
+"""
+Provides cfdi version information.
+"""
+
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update cfdi` to change this file.
+
+from incremental import Version
+
+__version__ = Version('cfdi', 0, 0, 19)
+__all__ = ["__version__"]
