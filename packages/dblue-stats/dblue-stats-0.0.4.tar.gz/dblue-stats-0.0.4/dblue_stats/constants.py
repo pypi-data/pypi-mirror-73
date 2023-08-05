5953cbcbbb4f85e20adb5e1f9922f53b7dc4828f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:05 +0000

    Add package dblue-stats/0.0.4/dblue-stats-0.0.4.tar.gz

---
+++ packages/dblue-stats/dblue-stats-0.0.4.tar.gz/dblue-stats-0.0.4/dblue_stats/constants.py
@@ -0,0 +1,10 @@
+class Constants:
+    # Data types
+    DATA_TYPE_INTEGER = "integer"
+    DATA_TYPE_NUMBER = "number"
+    DATA_TYPE_STRING = "string"
+    DATA_TYPE_BOOLEAN = "boolean"
+
+    # Field types
+    FIELD_TYPE_CATEGORICAL = "categorical"
+    FIELD_TYPE_NUMERICAL = "numerical"
