0c4086cdd3a6654ff8ac262f60da0205aa032a81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:00 +0000

    Add package iospytools/1.0.3/iospytools-1.0.3.tar.gz

---
+++ packages/iospytools/iospytools-1.0.3.tar.gz/iospytools-1.0.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="iospytools",
+    version="1.0.3",
+    author="Merculous",
+    author_email="vycemerculous@gmail.com",
+    description="provides useful tools/commands which are used in iOS research",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/merculous/ios-python-tools",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "iospytools = src.__main__:main",
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+    python_requires='>=3.5',
+)
