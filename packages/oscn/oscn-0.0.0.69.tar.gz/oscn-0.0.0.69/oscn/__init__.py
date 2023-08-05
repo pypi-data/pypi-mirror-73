fb89219f358dd79b84f5ef20777253e93adca3d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:37 +0000

    Add package oscn/0.0.0.69/oscn-0.0.0.69.tar.gz

---
+++ packages/oscn/oscn-0.0.0.69.tar.gz/oscn-0.0.0.69/oscn/__init__.py
@@ -0,0 +1,9 @@
+from . import _meta
+from . import request
+from . import parse
+from . import find
+
+counties = _meta.courts()
+courts = _meta.courts()
+type = _meta.get_type
+types = _meta.all_types
