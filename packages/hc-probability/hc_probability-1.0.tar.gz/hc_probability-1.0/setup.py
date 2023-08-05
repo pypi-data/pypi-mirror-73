5202b150ea8bf7571800aa481c3d85f3fd85f807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:12 +0000

    Add package hc-probability/1.0/hc_probability-1.0.tar.gz

---
+++ packages/hc-probability/hc_probability-1.0.tar.gz/hc_probability-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='hc_probability',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['hc_probability'],
+      author='Uyioghosa Nosa-Ihaza',
+      author_email='ihazauyi@gmail.com',
+      zip_safe=False)
