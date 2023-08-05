9140492c6596526238318a94071e8d0e9594313e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:38:26 +0000

    Add package django-theme-installer/0.1.4/django_theme_installer-0.1.4-py2.py3-none-any.whl

---
+++ packages/django-theme-installer/django_theme_installer-0.1.4-py2.py3-none-any.whl/django_theme_installer-0.1.4.data/scripts/theme_cli.py
@@ -0,0 +1,34 @@
+#!python
+"""
+The client of Django Theme Installer
+Usage:
+    theme_cli.py -n name -s /path/to/html/source/ -c /path/to/djangoproject/static/
+        -t /path/to/djangoproject/templates/
+"""
+from theme_installer.core import ThemeInstaller
+import argparse
+
+parser = argparse.ArgumentParser(description="Install a theme in the specified "
+                                 "Django dirs")
+
+parser.add_argument('-n', '--name', help='The name of the theme')
+parser.add_argument('-s', '--source', help='The source dir of the theme')
+parser.add_argument('-t', '--templates', help='The destination directory '
+                    'of the templates')
+parser.add_argument('-c', '--static', help="The destination directory "
+                    "of the static files")
+parser.add_argument('-p', '--prefix', help="The prefix of the static links "
+                    "in template. Default /static/")
+
+args = parser.parse_args()
+
+if not(args.name and args.source and args.static and args.templates):
+    parser.print_help()
+    import os
+    os._exit(0)
+
+ti = ThemeInstaller(args.name, args.source, args.static, args.templates, 
+                    prefix=args.prefix)
+ti.proceed()
+
+print("Installation done!")
