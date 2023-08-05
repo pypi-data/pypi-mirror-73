a028476cc302e8be1b16bd4acaee953ed203adcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:09 +0000

    Add package django-data-browser/2.1.0/django-data-browser-2.1.0.tar.gz

---
+++ packages/django-data-browser/django-data-browser-2.1.0.tar.gz/django-data-browser-2.1.0/setup.py
@@ -0,0 +1,43 @@
+import os
+
+import setuptools
+
+from data_browser import version
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+
+data_files = []
+for directory in "data_browser/fe_build", "data_browser/templates":
+    for (path, _, filenames) in os.walk(directory):
+        for filename in filenames:
+            data_files.append(os.path.join("..", path, filename))
+
+
+setuptools.setup(
+    name="django-data-browser",
+    version=version,
+    author="Gordon Wrigley",
+    author_email="gordon.wrigley@gmail.com",
+    description=(
+        "A Django app for interactive user friendly browsing "
+        "of a Django projects DB."
+    ),
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/tolomea/django-data-browser",
+    packages=setuptools.find_packages(exclude=["tests"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    package_data={"": data_files},
+    install_requires=[
+        "Django>=2.0",
+        "python-dateutil",
+        'dataclasses; python_version<"3.7"',
+    ],
+)
---
+++ packages/django-data-browser/django-data-browser-2.1.0.tar.gz/django-data-browser-2.1.0/tests/admin.py
@@ -0,0 +1,87 @@
+from django.contrib import admin
+
+from . import models
+
+# admin for perm testing
+
+
+class InlineAdminInline(admin.TabularInline):
+    model = models.InlineAdmin
+    fields = ["name"]
+
+
+@admin.register(models.InAdmin)
+class InAdmin(admin.ModelAdmin):
+    fields = ["name"]
+    inlines = [InlineAdminInline]
+
+
+@admin.register(models.Normal)
+class Normal(admin.ModelAdmin):
+    fields = ["name", "in_admin", "not_in_admin", "inline_admin"]
+
+
+# general admin
+
+
+@admin.register(models.Tag)
+class Tag(admin.ModelAdmin):
+    fields = ["name"]
+
+
+@admin.register(models.Address)
+class Address(admin.ModelAdmin):
+    fields = ["pk", "city", "bob", "fred", "tom"]
+    readonly_fields = ["bob", "fred", "tom"]
+
+    def bob(self, obj):
+        assert obj.street != "bad", obj.street
+        return "bob"
+
+
+class ProductMixin:
+    fields = [
+        "id",
+        "producer",
+        "name",
+        "size",
+        "size_unit",
+        "producer",
+        "is_onsale",
+        "default_sku",
+        "tags",
+        "model_not_in_admin",
+        "onsale",
+        "image",
+        "created_time",
+    ]
+    readonly_fields = ["is_onsale"]
+
+
+class ProductInline(ProductMixin, admin.TabularInline):
+    model = models.Product
+
+
+@admin.register(models.Producer)
+class Producer(admin.ModelAdmin):
+    fields = ["name", "address", "frank"]
+    readonly_fields = ["frank"]
+    inlines = [ProductInline]
+
+    def frank(self, obj):
+        return "frank"
+
+
+class SKUInline(admin.TabularInline):
+    model = models.SKU
+    fields = ["name", "product", "bob"]
+    readonly_fields = ["bob"]
+
+    def bob(self, obj):  # pragma: no cover don't show funcs on inlines test
+        return "bob"
+
+
+@admin.register(models.Product)
+class Product(ProductMixin, admin.ModelAdmin):
+    inlines = [SKUInline]
+    list_display = ["only_in_list_view"]
