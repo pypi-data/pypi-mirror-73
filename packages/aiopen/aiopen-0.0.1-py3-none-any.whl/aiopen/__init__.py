f49c8a40e8e054859e912f99fb6f7aea74100221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:55 +0000

    Add package aiopen/0.0.1/aiopen-0.0.1-py3-none-any.whl

---
+++ packages/aiopen/aiopen-0.0.1-py3-none-any.whl/aiopen/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""aiopen"""
+
+from aiopen._version import VERSION_INFO, VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH, __version__
+
+__all__ = [
+    '__version__',
+    'VERSION_MAJOR',
+    'VERSION_MINOR',
+    'VERSION_PATCH',
+    'VERSION_INFO'
+    ]
