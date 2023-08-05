fac1131541bb71dfe07739b890b59d7f312b5fe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:36 +0000

    Add package multiprocess-logger/0.1/multiprocess_logger-0.1.tar.gz

---
+++ packages/multiprocess-logger/multiprocess_logger-0.1.tar.gz/multiprocess_logger-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup,find_packages
+
+
+
+setup(name='multiprocess_logger',
+      version='0.1',
+      description='一个安全可靠高效的进程间日志处理器  A safe, reliable and efficient inter process log processor',
+      long_description='一个安全可靠高效的进程间日志处理器  A safe, reliable and efficient inter process log processor',
+      url='https://gitee.com',
+      author='liuyancong',
+      author_email='lyc456789@163.com',
+      license='MIT',
+      packages=find_packages(),
+      )
