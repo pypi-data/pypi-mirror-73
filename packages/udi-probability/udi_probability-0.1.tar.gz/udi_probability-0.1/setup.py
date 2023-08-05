b1b7d1ada24b5b778d964187cd514221609325e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:41 +0000

    Add package udi-probability/0.1/udi_probability-0.1.tar.gz

---
+++ packages/udi-probability/udi_probability-0.1.tar.gz/udi_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='udi_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['udi_probability'],
+      author = 'Pranjal Raj',
+      author_email = 'pranjalraj0046@gmail.com',
+      zip_safe=False)
