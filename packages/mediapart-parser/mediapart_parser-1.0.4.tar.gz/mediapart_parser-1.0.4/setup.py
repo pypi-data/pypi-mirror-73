cafab49d96dd30018f9d5095e9baf429bde442b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:40:17 +0000

    Add package mediapart-parser/1.0.4/mediapart_parser-1.0.4.tar.gz

---
+++ packages/mediapart-parser/mediapart_parser-1.0.4.tar.gz/mediapart_parser-1.0.4/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+from os import path
+
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='mediapart_parser',
+    packages=['mediapart_parser'],
+    version='1.0.4',
+    license='MIT',
+    description='Provide tools to parse and download\
+                 Mediapart articles as PDF.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Robin Perice',
+    author_email='robin.perice@protonmail.com',
+    url='https://github.com/r0perice/mediapart-parser',
+    download_url='https://github.com/r0perice/\
+    mediapart-parser/archive/1.0.4.tar.gz',
+    install_requires=[
+          'feedparser',
+          'beautifulsoup4',
+          'requests',
+          'Deprecated',
+      ],
+
+)
