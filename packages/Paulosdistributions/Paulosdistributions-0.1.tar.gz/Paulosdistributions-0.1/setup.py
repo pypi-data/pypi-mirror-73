e839c143544e53de21677c181f458fb6564620a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:28 +0000

    Add package Paulosdistributions/0.1/Paulosdistributions-0.1.tar.gz

---
+++ packages/Paulosdistributions/Paulosdistributions-0.1.tar.gz/Paulosdistributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Paulosdistributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['Paulosdistributions'],
+      author = 'osinowo paul',
+      author_email = 'osinowo62@gmail.com',
+      zip_safe=False)
