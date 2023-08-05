978119d7a80e02c2cd485ca0bc3def32af0a62d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:47 +0000

    Add package rasa-storyteller/0.2.1/rasa-storyteller-0.2.1.tar.gz

---
+++ packages/rasa-storyteller/rasa-storyteller-0.2.1.tar.gz/rasa-storyteller-0.2.1/backend/handlers/AbstractHandler.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+
+
+class AbstractHandler(ABC):
+    @abstractmethod
+    def __init__(self, filename, *args):
+        pass
+
+    @abstractmethod
+    def import_data(self):
+        pass
+
+    @abstractmethod
+    def export_data(self):
+        pass
---
+++ packages/rasa-storyteller/rasa-storyteller-0.2.1.tar.gz/rasa-storyteller-0.2.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="rasa-storyteller",
+    version="0.2.1",
+    author="ezhvsalate",
+    author_email="ezhvsalate@ya.ru",
+    description="A simple GUI utility to create complex stories for RASA chatbots easily.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ezhvsalate/rasa-storyteller",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    install_requires=[
+        "PySimpleGUI==4.22.0",
+        "AnyTree==2.8.0",
+        "markdown-generator==0.1.3",
+        "PyYAML==5.3.1",
+        "Pillow==7.2.0",
+    ],
+    entry_points="""
+        [console_scripts]
+        rasa-storyteller=gui.run:launcher
+    """,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
