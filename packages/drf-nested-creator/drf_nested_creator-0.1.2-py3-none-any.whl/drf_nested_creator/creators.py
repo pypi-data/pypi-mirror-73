7c08a4ac4ee060337db2ba237b08ca48480b809c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:08 +0000

    Add package drf-nested-creator/0.1.2/drf_nested_creator-0.1.2-py3-none-any.whl

---
+++ packages/drf-nested-creator/drf_nested_creator-0.1.2-py3-none-any.whl/drf_nested_creator/creators.py
@@ -0,0 +1,49 @@
+__all__ = ['SerializerCreator', 'ModelCreator']
+
+
+class BaseCreator:
+    def create(self, data):
+        raise NotImplementedError
+
+    def perform_data(self, data):
+        new_data = data.copy()
+        new_data[self.source_obj.__class__.__name__.lower()] = self.source_obj
+        new_data[f'{self.source_obj.__class__.__name__.lower()}_id'] = self.source_obj.id
+        return new_data
+
+
+class SerializerCreator(BaseCreator):
+    def __init__(self, serializer_class, context, **kwargs):
+        self.serializer_class = serializer_class
+        self.context = context
+        self.kwargs = kwargs
+        self.source_obj = None
+
+    def get_serializer(self, *args, **kwargs):
+        return self.serializer_class(*args, **kwargs)
+
+    def create(self, data):
+        data = self.perform_data(data)
+        serializer = self.get_serializer(context=self.context, data=data, many=isinstance(data, list))
+        if serializer.is_valid():
+            obj = serializer.save()
+            return obj
+        raise BaseException(str(serializer.errors))
+
+
+class ModelCreator(BaseCreator):
+    def __init__(self, model):
+        self.model = model
+
+    def create(self, data):
+        data = self.perform_data(data)
+        try:
+            if isinstance(data, list):
+                for d in data:
+                    self.model.objects.create(**d)
+            else:
+                self.model.objects.create(**data)
+            return True
+        except BaseException as e:
+            print(e)
+            return str(e)
