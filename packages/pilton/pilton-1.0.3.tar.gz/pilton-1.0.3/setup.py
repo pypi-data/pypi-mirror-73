120943ba6574276b5495e2702a6400260268a233
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:04 +0000

    Add package pilton/1.0.3/pilton-1.0.3.tar.gz

---
+++ packages/pilton/pilton-1.0.3.tar.gz/pilton-1.0.3/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+        name='pilton',
+        version='1.0.3',
+        py_modules= ['pilton','bcolors'],
+        package_data     = {
+            "": [
+                "*.txt",
+                "*.md",
+                "*.rst",
+                "*.py"
+                ]
+            },
+        license='Creative Commons Attribution-Noncommercial-Share Alike license',
+        long_description=open('README.md').read(),
+        )
