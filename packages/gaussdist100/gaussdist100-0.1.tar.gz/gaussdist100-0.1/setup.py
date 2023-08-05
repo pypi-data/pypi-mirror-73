bdb8db83112fbffa1322c2792183d9f0e45afdcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:27 +0000

    Add package gaussdist100/0.1/gaussdist100-0.1.tar.gz

---
+++ packages/gaussdist100/gaussdist100-0.1.tar.gz/gaussdist100-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='gaussdist100',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['gaussdist100'],
+      url='https://github.com/Kenneth-Edwards',  # Optional
+      author='Kenneth',  # Optional
+      author_email='kenneth.nbt@gmail.com',  # Optional
+      zip_safe=False)
