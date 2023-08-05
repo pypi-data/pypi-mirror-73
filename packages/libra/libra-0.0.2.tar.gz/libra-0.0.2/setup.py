189f94480dbeddedc24c77252454492924d4927d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:03 +0000

    Add package libra/0.0.2/libra-0.0.2.tar.gz

---
+++ packages/libra/libra-0.0.2.tar.gz/libra-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="libra", # Replace with your own username
+    version="0.0.2",
+    author="Palash Shah",
+    author_email="ps9cmk@virginia.edu",
+    description="Fully automated machine learning in one-liners.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Palashio/libra",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
