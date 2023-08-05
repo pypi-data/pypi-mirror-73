456d594a2af2faea81a2708ebb2f704b2c63c33f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:05 +0000

    Add package pilton/1.0.4/pilton-1.0.4.tar.gz

---
+++ packages/pilton/pilton-1.0.4.tar.gz/pilton-1.0.4/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+        name='pilton',
+        version='1.0.4',
+        py_modules= ['pilton','bcolors'],
+        package_data     = {
+            "": [
+                "*.txt",
+                "*.md",
+                "*.rst",
+                "*.py"
+                ]
+            },
+        license='Creative Commons Attribution-Noncommercial-Share Alike license',
+        long_description="Python interface for Parameter Interface Library (PIL)",
+        )
