cc28a1f59223ec9242c41718190b2c20b234cba5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:49 +0000

    Add package kangaroo-build/1.0.11/kangaroo-build-1.0.11.tar.gz

---
+++ packages/kangaroo-build/kangaroo-build-1.0.11.tar.gz/kangaroo-build-1.0.11/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+   name="kangaroo-build",
+   version="1.0.11",
+   description="keep track of your build number, that's it.",
+   long_description="a build tracker",
+   author="Marco Marchesi",
+   author_email="marchesimarco@gmail.com",
+   packages=find_packages(),
+   install_requires=[
+       'PyYAML',
+   ],
+)
