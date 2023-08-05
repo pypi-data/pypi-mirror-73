e787dc883fa29374eac6e45ead5b4e76ea33d41d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:10 +0000

    Add package R07/0.0.2/R07-0.0.2.tar.gz

---
+++ packages/R07/R07-0.0.2.tar.gz/R07-0.0.2/test/test_R07.py
@@ -0,0 +1,21 @@
+# Say Hello
+from R07 import say_hello
+
+def test_R07_no_params():
+    assert say_hello() == "Hello R-07!"
+
+def test_R07_with_params():
+    assert say_hello("Rahul") == "Hello, Rahul!"
+
+'''
+# Downloader
+from R07 import YTDownloader
+# Downloader.YTDown(url = None)
+
+def test_R07_no_params():
+    assert say_hello() == "Hello R-07!"
+
+def test_R07_with_params():
+    assert say_hello("Rahul") == "Hello, Rahul!"
+'''
+
