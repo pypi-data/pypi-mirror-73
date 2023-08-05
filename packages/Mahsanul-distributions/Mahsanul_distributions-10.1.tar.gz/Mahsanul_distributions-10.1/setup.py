0104999e2d1b43a3f9ed058790ac8cdeebcb1eef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:35 +0000

    Add package Mahsanul-distributions/10.1/Mahsanul_distributions-10.1.tar.gz

---
+++ packages/Mahsanul-distributions/Mahsanul_distributions-10.1.tar.gz/Mahsanul_distributions-10.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Mahsanul_distributions',
+      version='10.1',
+      description='Gaussian distributions',
+      packages=['Mahsanul_distributions'],
+      zip_safe=False)
