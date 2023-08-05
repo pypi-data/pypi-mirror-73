af978d8af3ac1b1c838468a3e808328e2b0b2e29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:03 +0000

    Add package scroll/2020.6.26/scroll-2020.6.26-py3-none-any.whl

---
+++ packages/scroll/scroll-2020.6.26-py3-none-any.whl/scroll/__init__.py
@@ -0,0 +1,25 @@
+"""SCROLL: Automatically generates documentation and unit-tests for python programs"""
+__version__ = "2020.6.26"
+
+import sys
+import click
+import runpy
+from scroll.runner import MyContextManager
+from scroll.tracer import Tracer
+from scroll.generators import generate_docs
+import pprint
+
+
+@click.command()
+@click.argument('filename')
+def scroll(filename):
+    """Print FILENAME."""
+    click.echo(filename)
+    with MyContextManager(Tracer) as manager:
+        runpy.run_path(filename, run_name='__main__')
+    sys.settrace(None)
+
+    collected_traces = manager.traces
+    pprint.pprint(collected_traces)
+    # write assets to files
+    generate_docs(collected_traces)
