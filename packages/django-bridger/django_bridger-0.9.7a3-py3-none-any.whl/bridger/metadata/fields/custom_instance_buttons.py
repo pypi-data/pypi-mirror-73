e13ee22031debecee3bbbfcdc83e1510e60fdc46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:08 +0000

    Add package django-bridger/0.9.7a3/django_bridger-0.9.7a3-py3-none-any.whl

---
+++ packages/django-bridger/django_bridger-0.9.7a3-py3-none-any.whl/bridger/metadata/fields/custom_instance_buttons.py
@@ -0,0 +1,43 @@
+from typing import List, Iterable, Dict
+
+from rest_framework.request import Request
+
+from bridger.buttons.bases import ButtonConfig as Button
+from bridger.metadata.mixins import BridgerMetadataMixin
+from bridger.utils.itertools import uniquify_dict_iterable
+from bridger.signals.instance_buttons import add_instance_button
+
+
+class CustomInstanceButtonMetadata(BridgerMetadataMixin):
+    key = "custom_instance_buttons"
+    method_name = "_get_custom_instance_buttons"
+
+
+class CustomInstanceButtonMetadataMixin:
+    def get_custom_list_instance_buttons(self, request: Request, buttons: List) -> List:
+        return buttons
+
+    def get_custom_instance_buttons(self, request: Request, buttons: List) -> List:
+        return buttons
+
+    def _get_custom_instance_buttons(self, request: Request) -> List:
+        if "pk" in self.kwargs:
+            # results = add_instance_button.send(sender=self.__class__)
+            # print(results)
+            buttons = self.get_custom_instance_buttons(request=request, buttons=getattr(self, "CUSTOM_INSTANCE_BUTTONS", []))
+            button_list = list()
+            for button in buttons:
+                button.request = request
+                button_list.append(dict(button))
+            return uniquify_dict_iterable(button_list, "key")
+        else:
+            # results = add_instance_button.send(sender=self.__class__)
+            # print(results)
+            buttons = self.get_custom_list_instance_buttons(
+                request=request, buttons=getattr(self, "CUSTOM_LIST_INSTANCE_BUTTONS", []),
+            )
+            button_list = list()
+            for button in buttons:
+                button.request = request
+                button_list.append(dict(button))
+            return uniquify_dict_iterable(button_list, "key")
---
+++ packages/django-bridger/django_bridger-0.9.7a3-py3-none-any.whl/bridger/serializers/fields/__init__.py
@@ -0,0 +1,22 @@
+from .boolean import BooleanField
+from .choice import ChoiceField
+from .datetime import DateField, DateTimeField, TimeField
+from .fields import (
+    AdditionalResourcesField,
+    DynamicButtonField,
+    HyperlinkField,
+    ReadOnlyField,
+    SerializerMethodField,
+    register_resource,
+    register_dynamic_button,
+)
+from .file import FileField, ImageField
+from .fsm import FSMStatusField
+from .json import JSONField, JSONTextEditorField
+from .list import ListField
+from .number import DecimalField, FloatField, IntegerField
+from .other import RangeSelectField, StarRatingField
+from .primary_key import PrimaryKeyCharField, PrimaryKeyField
+from .related import ListSerializer, PrimaryKeyRelatedField
+from .text import CharField, StringRelatedField, TextField, MarkdownTextField
+from .types import BridgerType, ReturnContentType
