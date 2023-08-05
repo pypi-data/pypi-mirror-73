9c224903404d353515e5313ee30b305f0d59098b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:40 +0000

    Add package bevy/0.2.2/Bevy-0.2.2.tar.gz

---
+++ packages/bevy/Bevy-0.2.2.tar.gz/Bevy-0.2.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['bevy']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'bevy',
+    'version': '0.2.2',
+    'description': 'Simple app framework that helps you build looesly coupled applications.',
+    'long_description': None,
+    'author': 'Zech Zimmerman',
+    'author_email': 'hi@zech.codes',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
