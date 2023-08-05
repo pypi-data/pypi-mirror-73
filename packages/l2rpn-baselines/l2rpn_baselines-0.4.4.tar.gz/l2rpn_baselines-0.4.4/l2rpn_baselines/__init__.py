312ccb47f6c6bb7506b72f8d584c07f56a219755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:51 +0000

    Add package l2rpn-baselines/0.4.4/l2rpn_baselines-0.4.4.tar.gz

---
+++ packages/l2rpn-baselines/l2rpn_baselines-0.4.4.tar.gz/l2rpn_baselines-0.4.4/l2rpn_baselines/__init__.py
@@ -0,0 +1,18 @@
+__all__ = [
+    "Template",
+    "DoubleDuelingDQN",
+    "DoubleDuelingRDQN",
+    "DoNothing",
+    "SliceRDQN",
+    "DeepQSimple",
+    "DuelQSimple",
+    "SAC",
+    # contribution
+    "PandapowerOPFAgent",
+    "Geirina",
+    "AsynchronousActorCritic",
+    # utilitary scripts
+    "utils"
+]
+
+__version__ = "0.4.4"
