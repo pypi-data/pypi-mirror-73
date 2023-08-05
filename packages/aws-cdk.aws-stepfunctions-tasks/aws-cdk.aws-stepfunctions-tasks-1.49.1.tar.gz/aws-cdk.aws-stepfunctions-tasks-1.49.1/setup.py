9e4111f5663d71886545c5fefcab6e4e5366619c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:07 +0000

    Add package aws-cdk.aws-stepfunctions-tasks/1.49.1/aws-cdk.aws-stepfunctions-tasks-1.49.1.tar.gz

---
+++ packages/aws-cdk.aws-stepfunctions-tasks/aws-cdk.aws-stepfunctions-tasks-1.49.1.tar.gz/aws-cdk.aws-stepfunctions-tasks-1.49.1/setup.py
@@ -0,0 +1,73 @@
+import json
+import setuptools
+
+kwargs = json.loads("""
+{
+    "name": "aws-cdk.aws-stepfunctions-tasks",
+    "version": "1.49.1",
+    "description": "Task integrations for AWS StepFunctions",
+    "license": "Apache-2.0",
+    "url": "https://github.com/aws/aws-cdk",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "project_urls": {
+        "Source": "https://github.com/aws/aws-cdk.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "aws_cdk.aws_stepfunctions_tasks",
+        "aws_cdk.aws_stepfunctions_tasks._jsii"
+    ],
+    "package_data": {
+        "aws_cdk.aws_stepfunctions_tasks._jsii": [
+            "aws-stepfunctions-tasks@1.49.1.jsii.tgz"
+        ],
+        "aws_cdk.aws_stepfunctions_tasks": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "jsii>=1.7.0, <2.0.0",
+        "publication>=0.0.3",
+        "aws-cdk.assets==1.49.1",
+        "aws-cdk.aws-batch==1.49.1",
+        "aws-cdk.aws-cloudwatch==1.49.1",
+        "aws-cdk.aws-dynamodb==1.49.1",
+        "aws-cdk.aws-ec2==1.49.1",
+        "aws-cdk.aws-ecr==1.49.1",
+        "aws-cdk.aws-ecr-assets==1.49.1",
+        "aws-cdk.aws-ecs==1.49.1",
+        "aws-cdk.aws-glue==1.49.1",
+        "aws-cdk.aws-iam==1.49.1",
+        "aws-cdk.aws-kms==1.49.1",
+        "aws-cdk.aws-lambda==1.49.1",
+        "aws-cdk.aws-s3==1.49.1",
+        "aws-cdk.aws-sns==1.49.1",
+        "aws-cdk.aws-sqs==1.49.1",
+        "aws-cdk.aws-stepfunctions==1.49.1",
+        "aws-cdk.core==1.49.1",
+        "constructs>=3.0.2, <4.0.0"
+    ],
+    "classifiers": [
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Typing :: Typed",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved"
+    ]
+}
+""")
+
+with open('README.md') as fp:
+    kwargs['long_description'] = fp.read()
+
+
+setuptools.setup(**kwargs)
