c46228aac882c8f09d07d0c8d0455ba6e3562c37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:23 +0000

    Add package bigbharath-probability/1.0/bigbharath_probability-1.0.tar.gz

---
+++ packages/bigbharath-probability/bigbharath_probability-1.0.tar.gz/bigbharath_probability-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='bigbharath_probability',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['bigbharath_probability'],
+      author = 'Bharath C S',
+      author_email ='bharath.sdupuc@gmail.com',
+      zip_safe=False)
