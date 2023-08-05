8fb66943faa6b4a520a2e76c2473488a23105720
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:13:04 +0000

    Add package agate-sql/0.5.5/agate-sql-0.5.5.tar.gz

---
+++ packages/agate-sql/agate-sql-0.5.5.tar.gz/agate-sql-0.5.5/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+install_requires = [
+    'agate>=1.5.0',
+    'sqlalchemy>=1.0.8'
+]
+
+setup(
+    name='agate-sql',
+    version='0.5.5',
+    description='agate-sql adds SQL read/write support to agate.',
+    long_description=open('README.rst').read(),
+    author='Christopher Groskopf',
+    author_email='chrisgroskopf@gmail.com',
+    url='http://agate-sql.readthedocs.org/',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Multimedia :: Graphics',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    packages=[
+        'agatesql'
+    ],
+    install_requires=install_requires
+)
