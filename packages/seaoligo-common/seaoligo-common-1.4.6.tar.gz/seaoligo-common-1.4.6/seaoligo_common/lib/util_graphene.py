443516c9fd5f409f0e84e7ca4238f651757e7a75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:35 +0000

    Add package seaoligo-common/1.4.6/seaoligo-common-1.4.6.tar.gz

---
+++ packages/seaoligo-common/seaoligo-common-1.4.6.tar.gz/seaoligo-common-1.4.6/seaoligo_common/lib/util_graphene.py
@@ -0,0 +1,24 @@
+from graphene import AbstractType, Enum, Int
+from flask import g
+
+
+class Counts(AbstractType):
+    total_count = Int()
+    def resolve_total_count(root, info):
+        return g.total_count
+
+    filtered_count = Int()
+    def resolve_filtered_count(root, info):
+        return g.filtered_count
+
+
+class OrganismSelect(Enum):
+    HUMAN = 'human'
+    MOUSE = 'mouse'
+    # RAT = 'rat'
+
+
+class AssemblySelect(Enum):
+    hg38 = 'hg38'
+    mm10 = 'mm10'
+    # rn6 = 'rn6'
