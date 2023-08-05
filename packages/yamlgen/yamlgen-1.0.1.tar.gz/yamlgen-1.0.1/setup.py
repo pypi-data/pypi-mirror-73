1521a21141daf84cccb427baab21cefd96cdf954
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:56 +0000

    Add package yamlgen/1.0.1/yamlgen-1.0.1.tar.gz

---
+++ packages/yamlgen/yamlgen-1.0.1.tar.gz/yamlgen-1.0.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import pathlib
+
+with open(pathlib.Path(__file__).parent/"Readme.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="yamlgen",
+    version="1.0.1",
+    author="Razaq Kloc",
+    author_email="razaqkor@gmail.com",
+    description="A package to generate yaml file. Convert Json String to Yaml",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/razaqK/yamlgen",
+    packages=find_packages(),
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'yamlgen = yamlgen.__main__:main'
+        ]
+    },
+    install_requires=[
+        'click==7.1.1',
+        'colorama==0.4.3',
+        'pathlib==1.0.1',
+        'pyfiglet==0.8.post1',
+        'PyYAML==5.3.1',
+        'six==1.14.0',
+        'termcolor==1.1.0'
+    ]
+)
