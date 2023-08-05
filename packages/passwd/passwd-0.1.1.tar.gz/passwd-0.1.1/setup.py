86edeafd2f683e0142af654104df395101a435fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:06 +0000

    Add package passwd/0.1.1/passwd-0.1.1.tar.gz

---
+++ packages/passwd/passwd-0.1.1.tar.gz/passwd-0.1.1/setup.py
@@ -0,0 +1,41 @@
+import codecs
+import os.path
+
+import setuptools
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="passwd",
+    version=get_version("passwd/__init__.py"),
+    author="Benjamin Soyka",
+    author_email="bensoyka@icloud.com",
+    description="Assorted utilities for gracefully handling and generating passwords",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bsoyka/passwd",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.6',
+)
