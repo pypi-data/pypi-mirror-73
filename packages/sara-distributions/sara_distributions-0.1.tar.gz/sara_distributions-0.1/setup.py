bc93fade2192181bbc44c20a64ded9c2fb491a78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:50 +0000

    Add package sara-distributions/0.1/sara_distributions-0.1.tar.gz

---
+++ packages/sara-distributions/sara_distributions-0.1.tar.gz/sara_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='sara_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['sara_distributions'],
+      author = 'Sara Jalali',
+      author_email = 'sr.jalali@gmail.com',
+      zip_safe=False)
