f180fc19ded6ead81806f0628bf1b1cff4017e15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:14:06 +0000

    Add package get-age/1.0.0/get_age-1.0.0-py3-none-any.whl

---
+++ packages/get-age/get_age-1.0.0-py3-none-any.whl/age/__main__.py
@@ -0,0 +1,7 @@
+def main():
+    num = int(input('To know your age in 2020, enter your birth year \n'))
+    age=2020-num
+    print("your age is {}".format(age))
+
+if __name__ == '__main__':
+    main()
