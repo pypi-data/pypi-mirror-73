38fb3bfe69b2b30acb27688a2f44cd108119581c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:27 +0000

    Add package Probability-Distr/0.2/Probability_Distr-0.2.tar.gz

---
+++ packages/Probability-Distr/Probability_Distr-0.2.tar.gz/Probability_Distr-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Probability_Distr',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['Probability_Distr'],
+      author = 'Opeyemi Ajala',
+      author_email = 'daelectropel@gmail.com',
+      zip_safe=False)
