b9e9454a35b619f7d8da51dc8607d1d507f5d75c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:34 +0000

    Add package napoleontoolbox/2.5.43/napoleontoolbox-2.5.43.tar.gz

---
+++ packages/napoleontoolbox/napoleontoolbox-2.5.43.tar.gz/napoleontoolbox-2.5.43/napoleontoolbox/utility/date_utility.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import re
+
+def add_datepart(df, fldname, drop=True, time=False):
+    "Helper function that adds columns relevant to a date."
+    # add_datepart(weather, "Date", drop=False)
+    # add_datepart(googletrend, "Date", drop=False)
+    # add_datepart(train, "Date", drop=False)
+    # add_datepart(test, "Date", drop=False)
+    fld = df[fldname]
+    fld_dtype = fld.dtype
+    if isinstance(fld_dtype, pd.core.dtypes.dtypes.DatetimeTZDtype):
+        fld_dtype = np.datetime64
+
+    if not np.issubdtype(fld_dtype, np.datetime64):
+        df[fldname] = fld = pd.to_datetime(fld, infer_datetime_format=True)
+    targ_pre = re.sub('[Dd]ate$', '', fldname)
+    attr = ['Year', 'Month', 'Week', 'Day', 'Hour', 'Dayofweek', 'Dayofyear',
+            'Is_month_end', 'Is_month_start', 'Is_quarter_end', 'Is_quarter_start', 'Is_year_end', 'Is_year_start']
+    if time: attr = attr + ['Hour', 'Minute', 'Second']
+    for n in attr: df[targ_pre + n] = getattr(fld.dt, n.lower())
+    df[targ_pre + 'Elapsed'] = fld.astype(np.int64) // 10 ** 9
+    if drop: df.drop(fldname, axis=1, inplace=True)
+
