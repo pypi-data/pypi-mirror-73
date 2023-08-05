bac6768ac284b635c5a5d7e8fa9e51c7f5502695
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:55 +0000

    Add package my-first-package1234/0.1/my_first_package1234-0.1.tar.gz

---
+++ packages/my-first-package1234/my_first_package1234-0.1.tar.gz/my_first_package1234-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='my_first_package1234',
+      version='0.1',
+      description='Gaussian & Binomial Distributions',
+      packages=['my_first_package1234'],
+      author = 'Maya Screen',
+      zip_safe=False)
