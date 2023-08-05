a65a3d58b5375ca6caa4624ae290763c0dc556db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:19 +0000

    Add package jenbonzhang-test/1.0.2/jenbonzhang_test-1.0.2.tar.gz

---
+++ packages/jenbonzhang-test/jenbonzhang_test-1.0.2.tar.gz/jenbonzhang_test-1.0.2/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+import os
+
+
+
+
+setuptools.setup(
+    name="jenbonzhang_test",
+    version="1.0.2",
+    author="jenbonzhang",
+    license='MIT License',
+    author_email="jenbonzhagn@gmail.com",
+    description="test package",
+    long_description="test package long description",
+    long_description_content_type="text/x-rst",
+    url="https://github.com/jenbonzhang/chinesename",
+    py_modules=['foo9'],
+)
+
