08a0fc9ac21f4d95547fb7a0464d05a8a8717e71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:19 +0000

    Add package odoo13-addon-ddmrp/13.0.1.1.1.dev4/odoo13_addon_ddmrp-13.0.1.1.1.dev4-py3-none-any.whl

---
+++ packages/odoo13-addon-ddmrp/odoo13_addon_ddmrp-13.0.1.1.1.dev4-py3-none-any.whl/odoo/addons/ddmrp/wizards/res_config_settings.py
@@ -0,0 +1,18 @@
+# Copyright 2020 ForgeFlow S.L. (http://www.forgeflow.com)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+from odoo import fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    module_ddmrp_history = fields.Boolean(
+        string="Store historical data from stock buffers",
+    )
+    module_ddmrp_adjustment = fields.Boolean(
+        string="apply adjustments to dynamically alter buffers",
+    )
+    ddmrp_auto_update_nfp = fields.Boolean(
+        related="company_id.ddmrp_auto_update_nfp", readonly=False
+    )
