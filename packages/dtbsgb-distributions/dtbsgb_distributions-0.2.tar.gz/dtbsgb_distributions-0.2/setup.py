385ea7f8a112cea28d9e0692ef2674a8add753fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:53 +0000

    Add package dtbsgb-distributions/0.2/dtbsgb_distributions-0.2.tar.gz

---
+++ packages/dtbsgb-distributions/dtbsgb_distributions-0.2.tar.gz/dtbsgb_distributions-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dtbsgb_distributions',
+      version='0.2',
+      description='Gaussian and Binomial distributions - Course Udacity',
+      packages=['dtbsgb_distributions'],
+      author='Sebastian Buitrago',
+      author_email='sebastian.buitrago@gmail.com',
+      zip_safe=False)
