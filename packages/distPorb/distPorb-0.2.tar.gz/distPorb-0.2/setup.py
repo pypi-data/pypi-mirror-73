69166743b50dc7706ea589678805a50683ecb2ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:16 +0000

    Add package distPorb/0.2/distPorb-0.2.tar.gz

---
+++ packages/distPorb/distPorb-0.2.tar.gz/distPorb-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distPorb',
+      version='0.2',
+      description='Gaussian and binomial distributions',
+      packages=['distPorb'],
+      author='Ruba Dabbas',
+      author_email='uba@al-dabbas.com',
+      zip_safe=False)
