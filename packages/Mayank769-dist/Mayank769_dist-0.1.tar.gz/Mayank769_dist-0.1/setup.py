da9542487c00e94428582f650778504478eff68a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:15 +0000

    Add package Mayank769-dist/0.1/Mayank769_dist-0.1.tar.gz

---
+++ packages/Mayank769-dist/Mayank769_dist-0.1.tar.gz/Mayank769_dist-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Mayank769_dist',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['Mayank769_Dist'],
+      zip_safe=False)
