5c1add5e2ca45d0a15893b96cfb0b97b421423dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:08 +0000

    Add package kora/0.4.2/kora-0.4.2-py3-none-any.whl

---
+++ packages/kora/kora-0.4.2-py3-none-any.whl/kora/update/tornado.py
@@ -0,0 +1,14 @@
+import os
+import pkg_resources
+from time import sleep
+
+
+version = pkg_resources.get_distribution("tornado").version
+# if run again, will not restart
+if version == '4.5.3':
+    os.system("pip install -U tornado")  # to 6.0.4
+
+    print("Runtime is now restarting...")
+    print("You can ignore the error message [Your session crashed for an unknown reason.]")
+    sleep(0.5)
+    os._exit(0)  # restart
