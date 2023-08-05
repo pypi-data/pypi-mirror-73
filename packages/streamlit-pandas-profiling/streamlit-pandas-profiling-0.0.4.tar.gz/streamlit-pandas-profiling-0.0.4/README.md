23506006101ec1f340e13cc7044ec71756d24e4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:49 +0000

    Add package streamlit-pandas-profiling/0.0.4/streamlit-pandas-profiling-0.0.4.tar.gz

---
+++ packages/streamlit-pandas-profiling/streamlit-pandas-profiling-0.0.4.tar.gz/streamlit-pandas-profiling-0.0.4/README.md
@@ -0,0 +1,23 @@
+# Streamlit Pandas Profiling
+
+[Pandas profiling](https://github.com/pandas-profiling/pandas-profiling) component for [Streamlit](https://www.streamlit.io/).
+
+## Installation
+
+In your python virtual environment, run:
+
+```sh
+pip install streamlit-pandas-profiling
+```
+
+## Demo
+
+You can try this component by running:
+
+```sh
+# Install dependencies
+pip install numpy pandas pandas-profiling
+
+# Run demo app
+streamlit run https://raw.githubusercontent.com/Ghasel/streamlit-pandas-profiling/master/examples/streamlit_pandas_profiling_demo.py
+```
---
+++ packages/streamlit-pandas-profiling/streamlit-pandas-profiling-0.0.4.tar.gz/streamlit-pandas-profiling-0.0.4/streamlit_pandas_profiling/frontend/build/precache-manifest.4921d82e38aaa4989c2224b4def9229e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0758b2e7523935b7ba5f6b6497c92b47",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7373ea9f8a49edcb5b44",
+    "url": "./static/js/2.5f41fff6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.5f41fff6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ea4aa8365f0ea1c047f",
+    "url": "./static/js/main.e023b968.chunk.js"
+  },
+  {
+    "revision": "6515c66d2a8747a146d578e1c038a822",
+    "url": "./static/js/main.e023b968.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ca1f16084572af38cc0",
+    "url": "./static/js/runtime-main.f973e952.js"
+  }
+]);
