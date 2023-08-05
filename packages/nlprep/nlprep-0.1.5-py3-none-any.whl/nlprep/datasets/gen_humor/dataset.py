59c940fc7431d8523a7c0ac673c4669787745367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:13 +0000

    Add package nlprep/0.1.5/nlprep-0.1.5-py3-none-any.whl

---
+++ packages/nlprep/nlprep-0.1.5-py3-none-any.whl/nlprep/datasets/gen_humor/dataset.py
@@ -0,0 +1,32 @@
+from nlprep.middleformat import MiddleFormat
+import nlp2
+import csv
+
+try:
+    from phraseg import *
+except ImportError:
+    print("phraseg not install, plz install it from https://github.com/voidful/Phraseg")
+    raise
+
+DATASET_FILE_MAP = {
+    "humor-dev": "https://raw.githubusercontent.com/voidful/CCL2019-Chinese-Humor-Computation/master/task2/task2_development.csv",
+    "humor-train": "https://raw.githubusercontent.com/voidful/CCL2019-Chinese-Humor-Computation/master/task2/task2_train.csv",
+    "humor-test": "https://raw.githubusercontent.com/voidful/CCL2019-Chinese-Humor-Computation/master/task2/task2_test.csv",
+}
+TYPE = "gen"
+
+
+def toMiddleFormat(path):
+    dataset = MiddleFormat(TYPE)
+    phraseg = Phraseg(path)
+    with open(path, encoding='utf8') as csvfile:
+        rows = csv.reader(csvfile)
+        next(rows, None)
+        for row in tqdm(rows):
+            input = [k for k, v in phraseg.extract_sent(row[1], filter=True)]
+            target = nlp2.split_sentence_to_array(row[1], True)
+            if len(input) + len(target) <= 512:
+                input = " ".join(input)
+                target = " ".join(target)
+                dataset.add_data(input, target)
+    return dataset
