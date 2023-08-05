13d9b0e2f4deb816b32b46d8208a3ec9fa2d2375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:19 +0000

    Add package distributions-sale/0.1/distributions_sale-0.1.tar.gz

---
+++ packages/distributions-sale/distributions_sale-0.1.tar.gz/distributions_sale-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_sale',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_sale'],
+      zip_safe=False)
