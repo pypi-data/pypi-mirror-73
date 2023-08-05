2fff4709bddf0f038bdf9d9ab718a3ade38cd77d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:12 +0000

    Add package prob-dist-uda/0.2/prob_dist_uda-0.2.tar.gz

---
+++ packages/prob-dist-uda/prob_dist_uda-0.2.tar.gz/prob_dist_uda-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='prob_dist_uda',
+      version='0.2',
+      description='Gaussian and binomial distributions',
+      packages=['prob_dist_uda'],
+      zip_safe=False)
