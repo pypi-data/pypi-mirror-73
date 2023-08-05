30004ecdf02d589deaff7ebe151443bd7dea14c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:11 +0000

    Add package griffon-client/0.2.3/griffon-client-0.2.3.tar.gz

---
+++ packages/griffon-client/griffon-client-0.2.3.tar.gz/griffon-client-0.2.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['griffon_client']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['python-socketio[asyncio_client]>=4.6.0,<5.0.0']
+
+setup_kwargs = {
+    'name': 'griffon-client',
+    'version': '0.2.3',
+    'description': 'A message queue for connecting workflows',
+    'long_description': None,
+    'author': 'August Brenner',
+    'author_email': 'augustbrenner@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
