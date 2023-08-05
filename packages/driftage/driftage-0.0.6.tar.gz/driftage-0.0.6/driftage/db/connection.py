6198f3dcc8ac649d07e8b58ce9aa51b062da756c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:28 +0000

    Add package driftage/0.0.6/driftage-0.0.6.tar.gz

---
+++ packages/driftage/driftage-0.0.6.tar.gz/driftage-0.0.6/driftage/db/connection.py
@@ -0,0 +1,89 @@
+import pandas as pd
+from datetime import datetime
+from sqlalchemy.engine import Engine
+from sqlalchemy.sql import select
+from aiobreaker import CircuitBreaker
+
+from driftage.db.schema import table
+from driftage.base.conf import getLogger
+
+
+class Connection:
+    def __init__(
+            self,
+            db_engine: Engine,
+            bulk_size: int,
+            circuit_breaker: CircuitBreaker = CircuitBreaker()):
+        """Connects with SQLAlchemy Engine to store and query for
+        data for concept drift datection.
+
+        :param db_engine: SQLAlchemy Engine to use as backend
+        :type db_engine: Engine
+        :param bulk_size: Quantity of data that connection will
+            wait to make bulk insert
+        :type bulk_size: int
+        :param circuit_breaker: Circuit Breaker configuration to
+            connect with Database, defaults to CircuitBreaker()
+        :type circuit_breaker: CircuitBreaker, optional
+        """
+        self._jid = None
+        self._conn = db_engine
+        self._bulk_size = bulk_size
+        self._bulk_df = pd.DataFrame()
+        self.get_between = circuit_breaker(self.get_between)
+        self._insert = circuit_breaker(self._insert)
+        self._logger = getLogger("connection")
+
+    async def _insert(self):
+        """Really insert to database with Pandas.
+        """
+        self._bulk_df.to_sql(
+            name=table.name,
+            con=self._conn,
+            if_exists='append',
+            index=False,
+            method="multi"
+        )
+        self._logger.debug(f"Inserted on table {table.name}")
+        self._bulk_df = pd.DataFrame()
+
+    async def lazy_insert(self, df: pd.DataFrame):
+        """Insert in database if bulk size reached.
+
+        :param df: Data to be inserted
+        :type df: pd.DataFrame
+        """
+        self._bulk_df = pd.concat([self._bulk_df, df])
+        if (len(self._bulk_df.index) >= self._bulk_size):
+            await self._insert()
+
+    async def get_between(
+            self,
+            from_datetime: datetime,
+            to_datetime: datetime) -> pd.DataFrame:
+        """Get data between dates from database.
+
+        :param from_datetime: Start Datetime to search
+        :type from_datetime: datetime
+        :param to_datetime: End Datetime to search
+        :type to_datetime: datetime
+        :return: Data got from date range specified
+        :rtype: pd.DataFrame
+        """
+        selectable = select([table]).where(
+            (table.c.driftage_datetime > str(from_datetime)) &
+            (table.c.driftage_datetime < str(to_datetime))
+        )
+        select_obj = selectable.compile(self._conn,
+                                        compile_kwargs={"literal_binds": True})
+        query = str(select_obj)
+        try:
+            return pd.read_sql_query(
+                sql=query,
+                con=self._conn,
+                parse_dates=[table.c.driftage_datetime.name]
+            )
+            self._logger.debug(f"Query executed: {query}")
+        except Exception as e:
+            self._logger.exception(e)
+            return pd.DataFrame()
---
+++ packages/driftage/driftage-0.0.6.tar.gz/driftage-0.0.6/test/unit/test_connection.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import asyncio
+from datetime import datetime
+from asynctest import TestCase, Mock, CoroutineMock, patch
+from driftage.db.connection import Connection
+from driftage.db.schema import table
+
+
+class TestConnection(TestCase):
+    def setUp(self):
+        self.engine = Mock()
+        self.connection = Connection(
+            self.engine,
+            10
+        )
+
+    async def test_should_not_insert_with_less_than_bulk_size(self):
+        df = pd.DataFrame([1, 2, 3, 4, 5])
+        self.connection._insert = CoroutineMock()
+        await self.connection.lazy_insert(df)
+        self.connection._insert.assert_not_awaited()
+        await self.connection.lazy_insert(df)
+        self.connection._insert.assert_awaited_once_with()
+
+    @patch("driftage.db.connection.pd")
+    @patch("driftage.db.connection.select")
+    async def test_should_get_with_right_query(self, select_mock, pd_mock):
+        from_dt = datetime.utcnow()
+        await asyncio.sleep(1)
+        to_dt = datetime.utcnow()
+        await self.connection.get_between(from_dt, to_dt)
+        select_mock.assert_called_once_with([table])
+        pd_mock.read_sql_query.assert_called_with(
+            con=self.engine,
+            parse_dates=[table.c.driftage_datetime.name],
+            sql=str(select_mock().where().compile())
+        )
