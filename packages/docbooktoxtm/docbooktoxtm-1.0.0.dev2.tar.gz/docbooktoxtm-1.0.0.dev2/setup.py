67726aee6a8281c27497fa32a8d2341ce17e7d1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:38 +0000

    Add package docbooktoxtm/1.0.0.dev2/docbooktoxtm-1.0.0.dev2.tar.gz

---
+++ packages/docbooktoxtm/docbooktoxtm-1.0.0.dev2.tar.gz/docbooktoxtm-1.0.0.dev2/setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['docbooktoxtm']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyGithub>=1.51,<2.0',
+ 'fuzzywuzzy>=0.18.0,<0.19.0',
+ 'lxml>=4.5.1,<5.0.0',
+ 'pydantic>=1.5.1,<2.0.0',
+ 'python-Levenshtein>=0.12.0,<0.13.0',
+ 'requests>=2.24.0,<3.0.0',
+ 'typer[all]>=0.3.0,<0.4.0',
+ 'xmltodict>=0.12.0,<0.13.0']
+
+entry_points = \
+{'console_scripts': ['docbooktoxtm = docbooktoxtm.main:app']}
+
+setup_kwargs = {
+    'name': 'docbooktoxtm',
+    'version': '1.0.0.dev2',
+    'description': '',
+    'long_description': '',
+    'author': "Ryan O'Rourke",
+    'author_email': 'ryan.orourke@welocalize.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
