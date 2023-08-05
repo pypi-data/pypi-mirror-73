e4c5b76eabb9b56135c55c2bc5e6fb21c458e06f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:34 +0000

    Add package clumpy/0.1.0/clumpy-0.1.0-py3-none-any.whl

---
+++ packages/clumpy/clumpy-0.1.0-py3-none-any.whl/clumpy/definition/__init__.py
@@ -0,0 +1,8 @@
+"""
+Texte intro au dossier definition
+"""
+
+from ._layer import LandUseCoverLayer, FeatureLayer, DistanceToVFeatureLayer, TransitionProbabilityLayers
+from . import _transition
+from . import _feature
+from ._case import Case
