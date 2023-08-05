854fff9cb7ceebcf181c467313aae39cdc07b5a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:44 +0000

    Add package ikku/0.1.3/ikku-0.1.3.tar.gz

---
+++ packages/ikku/ikku-0.1.3.tar.gz/ikku-0.1.3/ikku/parser.py
@@ -0,0 +1,13 @@
+from natto import MeCab
+from .node import Node
+
+
+class Parser:
+    def parse(self, text):
+        m = MeCab()
+        results = []
+        for n in m.parse(text, as_nodes=True):
+            tmp = Node(n.surface, n.feature, n.stat)
+            if tmp.analyzable():
+                results.append(tmp)
+        return results
