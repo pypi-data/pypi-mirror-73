7ed2467a721f5d8ecf5758bcf3faca834115a58c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:15 +0000

    Add package distributions-general/1/distributions_general-1.tar.gz

---
+++ packages/distributions-general/distributions_general-1.tar.gz/distributions_general-1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_general',
+      version='1',
+      description='Gaussian distributions',
+      packages=['distributions_general'],
+      zip_safe=False)
