0515ac2e37e76d748ffa7ec64a01943bb617871d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:06 +0000

    Add package control-toolbox/0.0.9/control-toolbox-0.0.9.tar.gz

---
+++ packages/control-toolbox/control-toolbox-0.0.9.tar.gz/control-toolbox-0.0.9/control/__init__.py
@@ -0,0 +1,8 @@
+from .system import TransferFunction, feedback, PID, reduce, StateSpace
+from .rootlocus import rootlocus
+from .bode import bode
+from .StateFeedback import StateFeedback
+from .LQR import LQR
+from .StateObserver import StateObserver
+from .kalman import KalmanFilter
+__version__ = '0.0.9'
---
+++ packages/control-toolbox/control-toolbox-0.0.9.tar.gz/control-toolbox-0.0.9/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+with open('README.md', 'r') as fp:
+    long_description = fp.read()
+
+setup(
+  name = 'control-toolbox',
+  packages = ['control'],
+  version = '0.0.9', 
+  license='MIT',
+  description = 'Python Control System Toolbox',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  author = 'Rushad Mehta',
+  author_email = 'rushadmehta16@gmail.com',
+  url = 'https://control-toolbox.readthedocs.io/',
+  download_url = 'https://github.com/rushad7/control-toolbox/archive/v0.0.9.tar.gz',
+  keywords = ['Control Systems', 'Python Control Toolbox', 'System Simulation'],
+  install_requires=[
+          'numpy',
+          'scipy',
+          'sympy',
+          'matplotlib',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Science/Research',
+    'Topic :: Scientific/Engineering',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
