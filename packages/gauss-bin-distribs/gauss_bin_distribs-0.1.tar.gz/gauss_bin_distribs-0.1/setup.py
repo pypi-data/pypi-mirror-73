5d04092d16b6163fe9e47742d71d8c055460620a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:18 +0000

    Add package gauss-bin-distribs/0.1/gauss_bin_distribs-0.1.tar.gz

---
+++ packages/gauss-bin-distribs/gauss_bin_distribs-0.1.tar.gz/gauss_bin_distribs-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gauss_bin_distribs',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['gauss_bin_distribs'],
+      author='Antonella Bernobich Dean',
+      email='me@aberdean.dev',
+      zip_safe=False)
