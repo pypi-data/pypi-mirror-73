9a406dc33532d2e80d8ae131d6269a48508d4f47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:57 +0000

    Add package dsnd-vinod-probablity/1.1/dsnd_vinod_probablity-1.1.tar.gz

---
+++ packages/dsnd-vinod-probablity/dsnd_vinod_probablity-1.1.tar.gz/dsnd_vinod_probablity-1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+	name="dsnd_vinod_probablity",
+    version="1.1",
+    description="Binomial and gaussian distribution",
+    author="vinodkumar mandora",
+    author_email="vinodmandora143@gmail.com",
+    zip_safe=False
+)
