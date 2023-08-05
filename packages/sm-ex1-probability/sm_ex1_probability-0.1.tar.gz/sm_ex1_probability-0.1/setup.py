eeb919a8a823eac849d6c635c8626a506162630b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:45 +0000

    Add package sm-ex1-probability/0.1/sm_ex1_probability-0.1.tar.gz

---
+++ packages/sm-ex1-probability/sm_ex1_probability-0.1.tar.gz/sm_ex1_probability-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='sm_ex1_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['sm_ex1_probability'],
+      zip_safe=False)
