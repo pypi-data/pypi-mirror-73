8fd533c9d286b4ea811744e563b691617196e8b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:43 +0000

    Add package cool-functions/0.1/cool_functions-0.1.tar.gz

---
+++ packages/cool-functions/cool_functions-0.1.tar.gz/cool_functions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='cool_functions',
+      version='0.1',
+      description='Some Useful Functions',
+      packages=['cool_functions'],
+      Author='John Nikhil Kollanoor',
+      zip_safe=False)
