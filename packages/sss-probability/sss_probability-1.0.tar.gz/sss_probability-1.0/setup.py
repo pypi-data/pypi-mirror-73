87ffd480165e5ade58728a93de52b465635ecddc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:46 +0000

    Add package sss-probability/1.0/sss_probability-1.0.tar.gz

---
+++ packages/sss-probability/sss_probability-1.0.tar.gz/sss_probability-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='sss_probability',
+      version='1.0',
+      description='Probability distributions',
+      packages=['sss_probability'],
+      zip_safe=False)
