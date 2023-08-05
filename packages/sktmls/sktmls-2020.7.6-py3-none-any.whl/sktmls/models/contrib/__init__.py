46039038bf31df026f0b1473685fd0d483ceddbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:39:01 +0000

    Add package sktmls/2020.7.6/sktmls-2020.7.6-py3-none-any.whl

---
+++ packages/sktmls/sktmls-2020.7.6-py3-none-any.whl/sktmls/models/contrib/__init__.py
@@ -0,0 +1,18 @@
+from .default_model import DefaultLightGBMModel, DefaultXGBoostModel
+from .sample_model import SampleModel
+from .sample_rule_model import SampleRuleModel
+from .random_pick_model import RandomPickModel
+from .bnf_loyalty_rule_model import BnfLoyaltyRuleModel
+from .info_unpaid_rule_model import InfoUnpaidRuleModel
+from .mbr_vip_info_rule_model import MbrVipInfoRuleModel
+
+__all__ = [
+    DefaultLightGBMModel,
+    DefaultXGBoostModel,
+    SampleModel,
+    SampleRuleModel,
+    RandomPickModel,
+    BnfLoyaltyRuleModel,
+    InfoUnpaidRuleModel,
+    MbrVipInfoRuleModel,
+]
