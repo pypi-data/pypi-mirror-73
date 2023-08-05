1b4d398f7833e29f791a2091c0f525b67837193b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:08 +0000

    Add package amshan/0.4.1/amshan-0.4.1.tar.gz

---
+++ packages/amshan/amshan-0.4.1.tar.gz/amshan-0.4.1/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="amshan",
+    version="0.4.1",
+    author="Tore Amundsen",
+    author_email="tore@amundsen.org",
+    description="Decode norwegian AMS-smart meter data stream from meter HAN port",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    platforms=["POSIX", "Windows"],
+    url="https://github.com/toreamun/amshan",
+    packages=["amshan"],
+    package_data={"amshan": ["py.typed"]},
+    keywords=[
+        "meter",
+        "han",
+        "ams",
+        "aidon",
+        "kaifa",
+        "kamstrup",
+        "cosem",
+        "hdlc",
+        "fast frame check",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=["construct"],
+    extras_require={"serial": ["pyserial-asyncio>=0.4"],},
+)
