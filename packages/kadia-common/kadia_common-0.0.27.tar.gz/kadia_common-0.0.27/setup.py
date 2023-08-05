5a4139e2330aa6c466fb696755e21076485c99ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:53 +0000

    Add package kadia-common/0.0.27/kadia_common-0.0.27.tar.gz

---
+++ packages/kadia-common/kadia_common-0.0.27.tar.gz/kadia_common-0.0.27/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='kadia_common',
+      version='0.0.27',
+      description='The common code for Kadia voice assistant',
+      author='Andrew Ishutin',
+      author_email='hazmozavr@gmail.com',
+      url='https://github.com/kadia-iva/kadia',
+      packages=['kadia_common'],
+      install_requires=[
+        'pydantic'
+      ]
+     )
