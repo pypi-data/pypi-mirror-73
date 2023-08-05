cef74ba5592ac393f7dc2b7573ae6eb17906bc39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:35 +0000

    Add package Gen-Gaus-Bi-distributions/0.1/Gen_Gaus_Bi_distributions-0.1.tar.gz

---
+++ packages/Gen-Gaus-Bi-distributions/Gen_Gaus_Bi_distributions-0.1.tar.gz/Gen_Gaus_Bi_distributions-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='Gen_Gaus_Bi_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['Gen_Gaus_Bi_distributions'],
+      zip_safe=False,
+      author = "Satoe Yokoyama",
+      
+     )
