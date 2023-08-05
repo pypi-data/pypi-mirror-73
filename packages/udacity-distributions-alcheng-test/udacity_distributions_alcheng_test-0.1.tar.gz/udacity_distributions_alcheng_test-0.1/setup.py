6ef4a7a396d69e0681ac40ee41f01cbca22b190f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:09 +0000

    Add package udacity-distributions-alcheng-test/0.1/udacity_distributions_alcheng_test-0.1.tar.gz

---
+++ packages/udacity-distributions-alcheng-test/udacity_distributions_alcheng_test-0.1.tar.gz/udacity_distributions_alcheng_test-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='udacity_distributions_alcheng_test',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['udacity_distributions_alcheng_test'],
+      zip_safe=False)
