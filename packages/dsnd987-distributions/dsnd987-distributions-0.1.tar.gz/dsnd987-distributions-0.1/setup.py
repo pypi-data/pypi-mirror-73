c02391f3a174aa526568a08e56ecc15c6a8f47d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:40:07 +0000

    Add package dsnd987-distributions/0.1/dsnd987-distributions-0.1.tar.gz

---
+++ packages/dsnd987-distributions/dsnd987-distributions-0.1.tar.gz/dsnd987-distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dsnd987-distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd987-distributions'],
+      zip_safe=False)
