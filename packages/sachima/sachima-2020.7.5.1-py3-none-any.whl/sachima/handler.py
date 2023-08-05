87b378ae9c5a0459dd1b946c130223338e576421
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:24 +0000

    Add package sachima/2020.7.5.1/sachima-2020.7.5.1-py3-none-any.whl

---
+++ packages/sachima/sachima-2020.7.5.1-py3-none-any.whl/sachima/handler.py
@@ -0,0 +1,24 @@
+import importlib
+from sachima.log import logger
+
+
+class ReportsHandler(object):
+    def __init__(self, **kwargs):
+        if isinstance(kwargs["handler"], list):
+            self.handlers = kwargs["handler"]
+        else:
+            self.handlers = [kwargs["handler"]]  # only one in list
+
+    def handle(self, model_in, params):
+        data_in = model_in
+        for handler_str in self.handlers:
+            logger.info("Call handler: " + handler_str)
+            m = importlib.import_module("handler." + handler_str, handler_str)
+            m = importlib.reload(m)
+            # each handler should has run function
+            res = m.run(data_in, params)
+            if res is not None:
+                # the previous handler's result with push to next handler
+                data_in = [res]
+        # return the last handler's result
+        return data_in
