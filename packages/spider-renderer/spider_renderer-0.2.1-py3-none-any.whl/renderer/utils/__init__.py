e6a6053288758f64a60e14898fc910f66fb219ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:43 +0000

    Add package spider-renderer/0.2.1/spider_renderer-0.2.1-py3-none-any.whl

---
+++ packages/spider-renderer/spider_renderer-0.2.1-py3-none-any.whl/renderer/utils/__init__.py
@@ -0,0 +1,12 @@
+'''
+    网络请求、解析等方法的工具库
+'''
+
+from .constants import DATETIME_12, DATETIME_24, HEADERS, WHITESPACE
+from .formlib import (extract_list, extract_string, is_equal, is_table,
+                      parse_form)
+from .htmlib import rm_tags, rm_ws
+from .httplib import download, download_file, retry_get, retry_post, save_html
+from .money import chinese_to_arabic
+from .ocrlib import GeneralOcr
+from .quick import contains, replace_all, replace_ws, split_path
