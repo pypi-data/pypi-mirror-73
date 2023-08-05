cb8f6c460a6b146001c12846493609b714208434
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:20 +0000

    Add package ynd-probability/1.0/ynd_probability-1.0.tar.gz

---
+++ packages/ynd-probability/ynd_probability-1.0.tar.gz/ynd_probability-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+  name = "ynd_probability",
+  version = "1.0",
+  author = "YOUNKAP NINA Duplex",
+  author_email = "nina.younkap@ccaa.pro",
+  description = "Gaussian and Binomial distributions",
+  packages = ["ynd_probability"],
+  zip_safe = False
+)
