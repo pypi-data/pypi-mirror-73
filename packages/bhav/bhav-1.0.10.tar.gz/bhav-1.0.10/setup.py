f4339d4e89f0606a3ee7c20e0afaa72aabe5c533
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:28 +0000

    Add package bhav/1.0.10/bhav-1.0.10.tar.gz

---
+++ packages/bhav/bhav-1.0.10.tar.gz/bhav-1.0.10/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bhav',
+    version='1.0.10',
+    author="Ajay Sharma",
+    author_email="deal.ajay@gmail.com",
+    python_requires='>=3',
+    packages=find_packages(),
+    include_package_data=True,
+    url="https://gitlab.com/sw8fbar/bhav",
+    description="CLI to download End of day Stock data from BSE and NSE",
+    long_description=open("README.md").read(),
+    license="LICENSE.txt",
+    install_requires=[
+        'Click',
+        'requests',
+        'python-dotenv',
+        'PyInquirer',
+        'flask',
+        'pandas',
+        'marshmallow',
+        'flask_cors'
+    ],
+    entry_points={
+        'console_scripts': [
+            'bhav=bhav.main:cli',
+        ]
+    },
+)
