a3f0b570569f20c56903d72af9132b251d937eeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:02 +0000

    Add package markdown-live-preview/0.1.20/markdown-live-preview-0.1.20.tar.gz

---
+++ packages/markdown-live-preview/markdown-live-preview-0.1.20.tar.gz/markdown-live-preview-0.1.20/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+name = "markdown_live_preview"
+
+
+def slurp(path: str) -> str:
+    with open(path) as fd:
+        return fd.read()
+
+
+setup(
+    name="markdown-live-preview",
+    version="0.1.20",
+    description="live web preview of markdown docs",
+    long_description=slurp("README.md"),
+    long_description_content_type="text/markdown",
+    author="ms-jpq",
+    author_email="github@bigly.dog",
+    url="https://github.com/ms-jpq/markdown-live-preview",
+    install_requires=slurp("requirements.txt").splitlines(),
+    packages=[name, name + ".server"],
+    package_data={name: ["js/*"]},
+    scripts=["mlp"],
+)
