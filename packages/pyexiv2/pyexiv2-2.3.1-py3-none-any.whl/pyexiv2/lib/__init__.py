c417ec7a5ff7690a5348326e03a80d2f9040b717
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:55 +0000

    Add package pyexiv2/2.3.1/pyexiv2-2.3.1-py3-none-any.whl

---
+++ packages/pyexiv2/pyexiv2-2.3.1-py3-none-any.whl/pyexiv2/lib/__init__.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import ctypes
+import platform
+
+
+# Check the Python interpreter
+if platform.architecture()[0] != '64bit':
+    raise RuntimeError('pyexiv2 can only run on 64-bit python3 interpreter.')
+py_version = platform.python_version()[:3]
+if py_version not in ['3.5', '3.6', '3.7', '3.8']:
+    raise RuntimeError('pyexiv2 only supports these Python versions: 3.5, 3.6, 3.7, 3.8 , but your version is {} .'.format(py_version))
+
+lib_dir = os.path.dirname(__file__)
+
+# Recognize the system
+sys_name = platform.system() or 'Unknown'
+if sys_name == 'Linux':
+    ctypes.CDLL(os.path.join(lib_dir, 'libexiv2.so')) # import the library at first, otherwise the Python interpreter can not find it.
+    sys.path.append(os.path.join(lib_dir, 'linux64-py{}'.format(py_version.replace('.', ''))))
+    import exiv2api
+elif sys_name == 'Darwin':
+    ctypes.CDLL(os.path.join(lib_dir, 'libexiv2.dylib'))
+    sys.path.append(os.path.join(lib_dir, 'darwin64-py{}'.format(py_version.replace('.', ''))))
+    import exiv2api
+elif sys_name == 'Windows':
+    ctypes.CDLL(os.path.join(lib_dir, 'exiv2.dll'))
+    sys.path.append(os.path.join(lib_dir, 'win64-py{}'.format(py_version.replace('.', ''))))
+    import exiv2api
+else:
+    raise RuntimeError('pyexiv2 can only run on Linux, Darwin or Windows system. But your system is {} .'.format(sys_name))
