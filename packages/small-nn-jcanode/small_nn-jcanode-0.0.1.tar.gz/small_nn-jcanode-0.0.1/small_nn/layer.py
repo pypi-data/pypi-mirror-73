312e4e4dbf59ec156a29eafa9aeef7901e71b5ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:49 +0000

    Add package small-nn-jcanode/0.0.1/small_nn-jcanode-0.0.1.tar.gz

---
+++ packages/small-nn-jcanode/small_nn-jcanode-0.0.1.tar.gz/small_nn-jcanode-0.0.1/small_nn/layer.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+class layer():
+    """docstring for layer."""
+
+    def __init__(self, arg):
+        super(layer, self).__init__()
+        self.arg = arg
+
+def dense():
+    raise NotImplementedError
+
+def conv():
+    raise NotImplementedError
+
+def input():
+    raise NotImplementedError
+
+def output():
+    raise NotImplementedError
---
+++ packages/small-nn-jcanode/small_nn-jcanode-0.0.1.tar.gz/small_nn-jcanode-0.0.1/small_nn/models.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+class models(object):
+    """docstring for model."""
+
+    def __init__(self, arg):
+        super(model, self).__init__()
+        self.arg = arg
+
+def build():
+    raise NotImplementedError
+
+def add(layer):
+    raise NotImplementedError
