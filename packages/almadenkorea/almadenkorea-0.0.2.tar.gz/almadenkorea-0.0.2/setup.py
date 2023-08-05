8e5bb3c31b8a3b6f47b1fab1a7ef7240f4e83405
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:03 +0000

    Add package almadenkorea/0.0.2/almadenkorea-0.0.2.tar.gz

---
+++ packages/almadenkorea/almadenkorea-0.0.2.tar.gz/almadenkorea-0.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='almadenkorea',
+    version='0.0.2',   #20200703
+    description='almaden package',
+    author='tajinbae',
+    author_email='bjin86@gmail.com',
+    url='https://github.com/BaeJin/almadenkorea',
+    download_url='https://github.com/BaeJin/almadenkorea/archive/master.zip',
+    install_requires=['pandas','pymysql'],
+    packages=find_packages(exclude=[]),
+    keywords=['sql'],
+    python_requires='>=3',
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
