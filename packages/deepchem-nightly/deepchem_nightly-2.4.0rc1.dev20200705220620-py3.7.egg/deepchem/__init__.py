dee95d3188cc1c4e120dc085f8c77aa64192a96d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:19 +0000

    Add package deepchem-nightly/2.4.0rc1.dev20200705220620/deepchem_nightly-2.4.0rc1.dev20200705220620-py3.7.egg

---
+++ packages/deepchem-nightly/deepchem_nightly-2.4.0rc1.dev20200705220620-py3.7.egg/deepchem/__init__.py
@@ -0,0 +1,17 @@
+"""
+Imports all submodules
+"""
+__version__ = '2.4.0-rc.1'
+
+import deepchem.data
+import deepchem.feat
+import deepchem.hyper
+import deepchem.metalearning
+import deepchem.metrics
+import deepchem.models
+import deepchem.splits
+import deepchem.trans
+import deepchem.utils
+import deepchem.dock
+import deepchem.molnet
+import deepchem.rl
