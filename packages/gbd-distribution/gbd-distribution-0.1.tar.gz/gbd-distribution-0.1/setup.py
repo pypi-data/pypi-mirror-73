49d74c1fa433d8266083330b96c9250846537dcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:31 +0000

    Add package gbd-distribution/0.1/gbd-distribution-0.1.tar.gz

---
+++ packages/gbd-distribution/gbd-distribution-0.1.tar.gz/gbd-distribution-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gbd-distribution',
+      version='0.1',
+      description='Gaussian distribution',
+      packages=['gbd-distributions'],
+      author = 'Muhammad Liman Dahiru',
+      author_email = 'mlimand76@gmail.com',
+      zip_safe=False)
