875ed05bbcbea5c83097d623f497a74c2d1b5530
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:14 +0000

    Add package wayne-test/0.1/wayne_test-0.1.tar.gz

---
+++ packages/wayne-test/wayne_test-0.1.tar.gz/wayne_test-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='wayne_test',
+      version='0.1',
+      description='Distributions',
+      packages=['wayne-test'],
+      author='Wayne Chim',
+      author_email='myemail@something.com',
+      zip_safe=False)
