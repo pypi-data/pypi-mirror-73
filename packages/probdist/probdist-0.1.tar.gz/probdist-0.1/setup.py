cc1aa93dd0c810fc206d3359735b68537a4c63b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:27 +0000

    Add package probdist/0.1/probdist-0.1.tar.gz

---
+++ packages/probdist/probdist-0.1.tar.gz/probdist-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='probdist',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['probdist'],
+      author ='Rasyid Sulaeman',
+      author_email = 'rasyidrafi@gmail.com',
+      zip_safe=False)
