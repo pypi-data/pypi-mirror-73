2aa4ee4f357020fb60b3364a50516f425ddf730a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:00 +0000

    Add package oyedistributions/0.1/oyedistributions-0.1.tar.gz

---
+++ packages/oyedistributions/oyedistributions-0.1.tar.gz/oyedistributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='oyedistributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['oyedistributions'],
+      zip_safe=False)
