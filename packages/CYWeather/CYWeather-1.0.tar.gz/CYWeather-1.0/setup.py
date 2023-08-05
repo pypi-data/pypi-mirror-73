0a88bc5d4ce5663c1902a6b11690c1d70e7336d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:07 +0000

    Add package CYWeather/1.0/CYWeather-1.0.tar.gz

---
+++ packages/CYWeather/CYWeather-1.0.tar.gz/CYWeather-1.0/setup.py
@@ -0,0 +1,13 @@
+# -*- coding: UTF-8 -*-
+# @Project -> File   ：6.1.py -> setup
+# @Time   ：2020/7/6 19:54
+# @Author ：fjg
+# @software：PyCharm
+from distutils.core import setup
+setup(name='CYWeather',
+      version='1.0',
+      author='fjg',
+      author_email='395987374@qq.com',
+      py_modules=['MyWeather']
+      )
+
