d6bb52df1c093a90607c0fb48274ec12ab0b5d64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:31 +0000

    Add package pro-distributions-func/1.3/pro_distributions_func-1.3.tar.gz

---
+++ packages/pro-distributions-func/pro_distributions_func-1.3.tar.gz/pro_distributions_func-1.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pro_distributions_func',
+      version='1.3',
+      description='Gaussian and Binomial distributions',
+      packages=['pro_distributions_func'],
+      author="Mohamed Makram",
+      author_email = "mohamedmakram202@gmail.com",
+      zip_safe=False)
