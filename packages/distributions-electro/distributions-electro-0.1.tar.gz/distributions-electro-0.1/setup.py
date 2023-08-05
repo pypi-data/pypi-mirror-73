552af10ed59b6b26e9db9f625ad679c7089b5177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:22 +0000

    Add package distributions-electro/0.1/distributions-electro-0.1.tar.gz

---
+++ packages/distributions-electro/distributions-electro-0.1.tar.gz/distributions-electro-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions-electro',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions-electro'],
+      author='Abhay Singh Chauhan',
+      author_email='abhaypsc.5@gmail.com',
+      zip_safe=False)
