98b25b854d0e61aaba8016e467559e302c88463e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:22 +0000

    Add package anusha/0.1/anusha-0.1.tar.gz

---
+++ packages/anusha/anusha-0.1.tar.gz/anusha-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='anusha',
+      version='0.1',
+      description='Gaussian and binomialdistributions',
+      packages=['anusha'],
+      author='Anusha Devulapally',
+      author_email='anushadevulapally2013@gmail.com',
+      zip_safe=False)
