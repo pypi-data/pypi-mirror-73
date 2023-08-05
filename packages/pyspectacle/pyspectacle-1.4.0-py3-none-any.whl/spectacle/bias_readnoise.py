5853be4b3c7cbb358963ca172c6cfa5e07c42e7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:24 +0000

    Add package pyspectacle/1.4.0/pyspectacle-1.4.0-py3-none-any.whl

---
+++ packages/pyspectacle/pyspectacle-1.4.0-py3-none-any.whl/spectacle/bias_readnoise.py
@@ -0,0 +1,55 @@
+"""
+Code relating to bias and read noise, such as loading maps of either.
+"""
+
+import numpy as np
+from . import io
+
+
+def load_bias_map(root, return_filename=False):
+    """
+    Load the bias map located at `root`/calibration/bias.npy.
+    If `return_filename` is True, also return the exact filename used.
+    """
+    filename = root/"calibration/bias.npy"
+    bias_map = np.load(filename)
+    if return_filename:
+        return bias_map, filename
+    else:
+        return bias_map
+
+
+def load_bias_metadata(root, return_filename=False):
+    """
+    Load the bias value from the camera metadata file, and generate a Bayer-
+    tiled map from it
+    If `return_filename` is True, also return the exact filename used.
+    """
+    camera, filename = io.load_metadata(root, return_filename=True)
+    bias_map = camera.generate_bias_map()
+    if return_filename:
+        return bias_map, filename
+    else:
+        return bias_map
+
+
+def load_readnoise_map(root, return_filename=False):
+    """
+    Load the bias map located at `root`/calibration/readnoise.npy
+    If `return_filename` is True, also return the exact filename used.
+    """
+    filename = root/"calibration/readnoise.npy"
+    readnoise_map = np.load(filename)
+    if return_filename:
+        return readnoise_map, filename
+    else:
+        return readnoise_map
+
+
+def correct_bias_from_map(bias_map, data):
+    """
+    Apply a bias correction from a bias map `bias_map` to an array `data`.
+    """
+    data_corrected = data - bias_map
+
+    return data_corrected
