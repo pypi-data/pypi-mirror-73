0317674c8b2f015c23c8b2df765d8afb34e68273
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:21 +0000

    Add package distributions-nk/0.1/distributions_nk-0.1.tar.gz

---
+++ packages/distributions-nk/distributions_nk-0.1.tar.gz/distributions_nk-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_nk',
+      version='0.1',
+      description='Gaussian,Binomial distributions',
+      packages=['distributions_nk'],
+      zip_safe=False)
