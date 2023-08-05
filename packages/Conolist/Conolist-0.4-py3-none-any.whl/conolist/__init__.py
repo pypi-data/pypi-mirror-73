80f110e1b8ae7fa85c3c0975c5f38a7b099ab2da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:49 +0000

    Add package Conolist/0.4/Conolist-0.4-py3-none-any.whl

---
+++ packages/Conolist/Conolist-0.4-py3-none-any.whl/conolist/__init__.py
@@ -0,0 +1,9 @@
+from conolist import menu_initilizer
+
+# use to create command line access to conolist
+def execute_from_command_line():
+    """
+    Given the command-line arguments, figure out which subcommand is being
+    run, create a parser appropriate to that command, and run it.
+    """
+    menu_initilizer()
