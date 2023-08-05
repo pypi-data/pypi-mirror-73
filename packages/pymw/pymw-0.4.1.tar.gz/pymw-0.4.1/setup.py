29063b2e41f52ed5bb09d59e98e75bfc73149e30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:57 +0000

    Add package pymw/0.4.1/pymw-0.4.1.tar.gz

---
+++ packages/pymw/pymw-0.4.1.tar.gz/pymw-0.4.1/setup.py
@@ -0,0 +1,37 @@
+from os.path import abspath, dirname, join
+from re import search, MULTILINE
+
+from setuptools import setup
+
+
+with open('README.rst', 'r') as fh:
+    long_description = fh.read()
+
+here = abspath(dirname(__file__))
+setup(
+    name='pymw',
+    version=search(
+        r"^__version__ = '([^']*)'",
+        open(
+            join(here, 'pymw', '_api.py'),
+            encoding='ascii', errors='ignore').read(),
+        MULTILINE).group(1),
+    author='5j9',
+    author_email='5j9@users.noreply.github.com',
+    description="A thin MediaWiki client using requests.",
+    license='GNU General Public License v3 (GPLv3)',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    url='https://github.com/5j9/pymw',
+    packages=['pymw'],
+    python_requires='>=3.9',
+    install_requires=['requests', 'tomlkit'],
+    tests_require=['pytest'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.9',
+        'Environment :: Web Environment'],
+    zip_safe=True)
