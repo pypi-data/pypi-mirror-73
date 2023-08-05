e14636a32c507e2cef82eb7e083f21f197e7d8f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:39 +0000

    Add package transmission-rpc/3.0.0a3/transmission_rpc-3.0.0a3-py3-none-any.whl

---
+++ packages/transmission-rpc/transmission_rpc-3.0.0a3-py3-none-any.whl/transmission_rpc/error.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2018-2020 Trim21 <i@trim21.me>
+# Copyright (c) 2008-2014 Erik Svensson <erik.public@gmail.com>
+# Licensed under the MIT license.
+
+
+class TransmissionError(Exception):
+    """
+        This exception is raised when there has occurred an error related to
+        communication with Transmission. It is a subclass of Exception.
+    """
+    def __init__(self, message='', original=None):
+        Exception.__init__(self)
+        self.message = message
+        self.original = original
+
+    def __str__(self):
+        if self.original:
+            original_name = type(self.original).__name__
+            return '{} Original exception: {}, "{}"'.format(
+                self.message, original_name, str(self.original)
+            )
+        else:
+            return self.message
+
+
+class TransmissionAuthError(TransmissionError):
+    pass
+
+
+class ServerVersionTooLow(TransmissionError):
+    pass
