9c9d00eb50a92719361752566d6f5fafa071f28c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:38 +0000

    Add package pdynamics/0.2.1/pdynamics-0.2.1.tar.gz

---
+++ packages/pdynamics/pdynamics-0.2.1.tar.gz/pdynamics-0.2.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pdynamics",
+    version="0.2.1",
+    author="Weifeng Jiang",
+    author_email="jiangweifeng@live.com",
+    description="Python connector for Microsoft Dynamics 365, supports AD, IFD and Online",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jiangweifeng/pdynamics",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
