d57b3b1e7cb39b1a78d11f5f153bbe347589c397
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:10 +0000

    Add package ts3audiobot-api/0.0.3/ts3audiobot-api-0.0.3.tar.gz

---
+++ packages/ts3audiobot-api/ts3audiobot-api-0.0.3.tar.gz/ts3audiobot-api-0.0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ts3audiobot-api",
+    version="0.0.3",
+    author="EverHype Systems",
+    author_email="abajrami@everhype-systems.eu",
+    description="API Wrapper for TS3AudioBot",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/EverHype-Systems/ts3audiobot-wrapper",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
