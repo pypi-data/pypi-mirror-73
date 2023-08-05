7db0958bcea3a704892554ee5456d550cb968841
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:13:27 +0000

    Add package larq/0.10.0/larq-0.10.0.tar.gz

---
+++ packages/larq/larq-0.10.0.tar.gz/larq-0.10.0/setup.py
@@ -0,0 +1,60 @@
+from setuptools import find_packages, setup
+
+
+def readme():
+    with open("README.md", "r") as f:
+        return f.read()
+
+
+setup(
+    name="larq",
+    version="0.10.0",
+    python_requires=">=3.6",
+    author="Plumerai",
+    author_email="lukas@plumerai.co.uk",
+    description="An Open Source Machine Learning Library for Training Binarized Neural Networks",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://larq.dev/",
+    packages=find_packages(exclude=["larq.snapshots"]),
+    license="Apache 2.0",
+    install_requires=[
+        "numpy >= 1.15.4, < 2.0",
+        "terminaltables>=3.1.0",
+        "dataclasses ; python_version<'3.7'",
+    ],
+    extras_require={
+        "tensorflow": ["tensorflow>=1.14.0"],
+        "tensorflow_gpu": ["tensorflow-gpu>=1.14.0"],
+        "test": [
+            "black==19.10b0",
+            "flake8>=3.7.9,<3.9.0",
+            "isort==5.0.4",
+            "packaging>=19.2,<21.0",
+            "pytest>=5.2.4,<5.5.0",
+            "pytest-cov>=2.8.1,<2.11.0",
+            "pytest-xdist>=1.30,<1.33",
+            "pytest-mock>=2.0,<3.2",
+            "pytype==2020.6.26",
+            "snapshottest~=0.5.1",
+        ],
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
