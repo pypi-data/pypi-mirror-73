bd8bc1f94a6bfa08e7fe6a42ed2c34f6e39ad38b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:55 +0000

    Add package ApiBiuBiu/0.0.2/ApiBiuBiu-0.0.2.tar.gz

---
+++ packages/ApiBiuBiu/ApiBiuBiu-0.0.2.tar.gz/ApiBiuBiu-0.0.2/setup.py
@@ -0,0 +1,35 @@
+#!python
+# -*- coding:utf-8 -*-
+from __future__ import print_function
+from setuptools import setup, find_packages
+
+# with open("README.md", "r", encoding='utf-8') as fh:
+#     long_description = fh.read()
+
+setup(
+    name="ApiBiuBiu",
+    version="0.0.2",
+    author="满眼乱世妖娆",
+    author_email="597038837@qq.com",
+    description="unittestAndMultiprocessing",
+    # long_description=long_description,
+    # long_description_content_type="text",
+    license="MIT",
+    url="https://gitee.com/jpf999/unittest_report",
+    packages=find_packages(),
+    install_requires=[
+        ],
+    classifiers=[
+        "Topic :: Games/Entertainment ",
+        'Topic :: Games/Entertainment :: Puzzle Games',
+        'Topic :: Games/Entertainment :: Board Games',
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+)
