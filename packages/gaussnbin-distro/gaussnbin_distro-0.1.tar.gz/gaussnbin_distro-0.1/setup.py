b2bb09facced0c2ce30ffb851ac7614c631e4a91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:16 +0000

    Add package gaussnbin-distro/0.1/gaussnbin_distro-0.1.tar.gz

---
+++ packages/gaussnbin-distro/gaussnbin_distro-0.1.tar.gz/gaussnbin_distro-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'gaussnbin_distro',
+      version = '0.1',
+      description = 'Gaussian and Binomial distributions',
+      packages = ['gaussnbin_distro'],
+      author = 'Uchechukwu Fortune Njoku',
+      author_email = 'njokuuchechi@gmail.com',
+      zip_safe=False)
