c651cc0336061b085317392a6ae14d5226d15b38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:09 +0000

    Add package distributions-lp/0.2/distributions_lp-0.2.tar.gz

---
+++ packages/distributions-lp/distributions_lp-0.2.tar.gz/distributions_lp-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_lp',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['distributions_lp'],
+      author='itz_me_LP',
+      zip_safe=False)
