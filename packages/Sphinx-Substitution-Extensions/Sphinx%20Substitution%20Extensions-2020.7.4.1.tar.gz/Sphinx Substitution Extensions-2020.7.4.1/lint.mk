c596f80f5b0b804ed79a6511033a554123b4db38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:43 +0000

    Add package Sphinx-Substitution-Extensions/2020.7.4.1/Sphinx%20Substitution%20Extensions-2020.7.4.1.tar.gz

---
+++ packages/Sphinx-Substitution-Extensions/Sphinx%20Substitution%20Extensions-2020.7.4.1.tar.gz/Sphinx Substitution Extensions-2020.7.4.1/lint.mk
@@ -0,0 +1,88 @@
+# Make commands for linting
+
+SHELL := /bin/bash -euxo pipefail
+
+.PHONY: yapf
+yapf:
+	yapf \
+	    --diff \
+	    --recursive \
+	    --exclude .eggs \
+	    .
+
+.PHONY: fix-yapf
+fix-yapf:
+	yapf \
+	    --in-place \
+	    --recursive \
+	    --exclude .eggs \
+	    .
+
+.PHONY: mypy
+mypy:
+	mypy *.py src/ tests/ admin
+
+.PHONY: check-manifest
+check-manifest:
+	check-manifest .
+
+.PHONY: doc8
+doc8:
+	doc8 .
+
+.PHONY: flake8
+flake8:
+	flake8 .
+
+.PHONY: isort
+isort:
+	isort --recursive --check-only
+
+.PHONY: pip-extra-reqs
+pip-extra-reqs:
+	# We ignore the sphinx-prompt because we do not import it but we require it
+	# so that users can set it before our extension.
+	pip-extra-reqs --ignore-requirement sphinx-prompt src/
+
+.PHONY: pip-missing-reqs
+pip-missing-reqs:
+	pip-missing-reqs src/
+
+.PHONY: pylint
+pylint:
+	pylint *.py src/ tests/ admin/
+
+.PHONY: pyroma
+pyroma:
+	pyroma --min 10 .
+
+.PHONY: vulture
+vulture:
+	vulture --min-confidence 100 --exclude _vendor --exclude .eggs .
+
+.PHONY: linkcheck
+linkcheck:
+	$(MAKE) -C docs/ linkcheck SPHINXOPTS=$(SPHINXOPTS)
+
+.PHONY: spelling
+spelling:
+	$(MAKE) -C docs/ spelling SPHINXOPTS=$(SPHINXOPTS)
+
+.PHONY: shellcheck
+shellcheck:
+	shellcheck --exclude SC2164,SC1091 */*.sh
+
+.PHONY: autoflake
+autoflake:
+	autoflake \
+	    --in-place \
+	    --recursive \
+	    --remove-all-unused-imports \
+	    --remove-unused-variables \
+	    --expand-star-imports \
+	    --exclude _vendor,release \
+	    .
+
+.PHONY: pydocstyle
+pydocstyle:
+	pydocstyle
