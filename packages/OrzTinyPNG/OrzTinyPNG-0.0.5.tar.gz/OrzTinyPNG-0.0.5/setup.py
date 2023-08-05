4406f7600f0cde65af0c12553cac4f5568a19e02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:35:43 +0000

    Add package OrzTinyPNG/0.0.5/OrzTinyPNG-0.0.5.tar.gz

---
+++ packages/OrzTinyPNG/OrzTinyPNG-0.0.5.tar.gz/OrzTinyPNG-0.0.5/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="OrzTinyPNG",
+    version="0.0.5",
+    author="wangzhizhou",
+    author_email="824219521@qq.com",
+    description="A tool for compress image such as png and jpg using TinyPNG API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/OrzGeeker/OrzTinyPNG",
+    packages=setuptools.find_packages(),
+    # all classifiers: https://pypi.org/classifiers/
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3, <4',
+    install_requires=[
+        'twine',
+        'tinify'
+    ],
+    entry_points = {
+        'console_scripts': [
+            'tiny = src:main_task'
+        ]
+    }
+)
---
+++ packages/OrzTinyPNG/OrzTinyPNG-0.0.5.tar.gz/OrzTinyPNG-0.0.5/src/TinyPNG.py
@@ -0,0 +1,56 @@
+#-*- coding: utf-8 -*-
+import tinify
+import os
+
+class TinyPNG:
+
+    def __init__(self, app_key):
+        tinify.key = app_key
+
+    def compress(self,source, destination):
+        source = tinify.from_file(source)
+        source.to_file(destination)
+
+    def compressDir(self, dir):
+        for fpathe, _ , fs in os.walk(dir):
+            for f in fs:
+                filePath = os.path.join(fpathe,f)
+                self.compressFile(filePath)
+
+    def compressFile(self,filePath):
+        _, ext = os.path.splitext(filePath)
+        if ext in ['.png', '.jpg', '.JPEG', '.PNG', '.jpeg']:
+            self.compress(source = filePath, destination = filePath)
+            print('✅' + filePath)
+        else:
+            print('❎Not Image!!')
+
+    def process(self, path):
+        print(path)
+        try:
+            # Use the Tinify API client.
+            if os.path.exists(path):
+                if os.path.isdir(path):
+                    self.compressDir(path)
+                else:
+                    self.compressFile(path)
+                print('👍👍👍Completed!!!!')
+            else:
+                print('💥💥💥Not Exist!!!!')
+
+        except tinify.AccountError as e:
+            # Verify your API key and account limit.
+            print("The error message is: %s" % e.message)
+        except tinify.ClientError as e:
+            # Check your source image and request options.
+            print("The error message is: %s" % e.message)
+        except tinify.ServerError as e:
+            # Temporary issue with the Tinify API.
+            print("The error message is: %s" % e.message)
+            pass
+        except tinify.ConnectionError as e:
+            # A network connection error occurred.
+            print("The error message is: %s" % e.message)
+        except Exception as e:
+            # Something else went wrong, unrelated to the Tinify API.
+            print("The error message is: %s" % e.message)
