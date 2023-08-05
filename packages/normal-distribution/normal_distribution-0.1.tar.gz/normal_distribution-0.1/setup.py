174fd379e5568ac469ff25583d5b7340ca4032f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:02 +0000

    Add package normal-distribution/0.1/normal_distribution-0.1.tar.gz

---
+++ packages/normal-distribution/normal_distribution-0.1.tar.gz/normal_distribution-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='normal_distribution',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['normal_distribution'],
+      author = 'Kishore',
+      zip_safe=False)
