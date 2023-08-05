533482ee5a58839995caac7ae415c6bb8f9bf857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:56 +0000

    Add package dsnd-probability-oa/0.1/dsnd_probability_oa-0.1.tar.gz

---
+++ packages/dsnd-probability-oa/dsnd_probability_oa-0.1.tar.gz/dsnd_probability_oa-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_oa',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_probability_oa'],
+      autor = 'Olena Chudnovets',
+      autor_email = 'olenairy@gmail.com',
+      zip_safe=False)
