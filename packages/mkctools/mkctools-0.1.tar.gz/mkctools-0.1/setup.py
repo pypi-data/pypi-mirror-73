a79ae17264c1bb2cf7f3cb8bcf08bb884236f176
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:45 +0000

    Add package mkctools/0.1/mkctools-0.1.tar.gz

---
+++ packages/mkctools/mkctools-0.1.tar.gz/mkctools-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mkctools',
+      version='0.1',
+      description='Machine Learning Libraries',
+      url='http://github.com/mc6666/mkctools',
+      author='Michael Chen',
+      author_email='mc6666@gmail.com',
+      license='MIT',
+      packages=['mkctools'],
+      zip_safe=False)
