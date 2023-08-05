8866a37297d6e6a358e97d1eaf6d41fc31bf039d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:33 +0000

    Add package ricco/0.0.6/ricco-0.0.6.tar.gz

---
+++ packages/ricco/ricco-0.0.6.tar.gz/ricco-0.0.6/src/ricco/__init__.py
@@ -0,0 +1,10 @@
+__version__ = '0.0.6'
+
+from ricco.util import add
+
+from ricco.util import csv2shp
+from ricco.util import pinyin
+from ricco.util import rdf
+from ricco.util import shp2csv
+from ricco.gis_tools import circum_pio_num_geo_aoi
+from ricco.gis_tools import mark_tags_df
