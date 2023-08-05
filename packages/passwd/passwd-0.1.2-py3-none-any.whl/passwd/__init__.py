2056bfd24b5adf4201edc918c38fd1551f9e7fc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:10 +0000

    Add package passwd/0.1.2/passwd-0.1.2-py3-none-any.whl

---
+++ packages/passwd/passwd-0.1.2-py3-none-any.whl/passwd/__init__.py
@@ -0,0 +1,21 @@
+__version__ = "0.1.2"
+
+import hashlib
+
+import requests
+
+
+def check_password(password: str) -> int:
+    sha1 = hashlib.sha1(password.encode("utf-8")).hexdigest()
+
+    response = requests.get(f"https://api.pwnedpasswords.com/range/{sha1[:5]}")
+
+    hash_suffix_list = [x.split(":") for x in response.text.splitlines(False)]
+
+    try:
+        count = [count for suffix,
+                 count in hash_suffix_list if sha1.endswith(suffix.lower())][0]
+    except IndexError:
+        return 0
+
+    return int(count)
