402864e3261dcb796946505bdd1186433778e1e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:17 +0000

    Add package pyalgen/0.0.2/pyalgen-0.0.2.tar.gz

---
+++ packages/pyalgen/pyalgen-0.0.2.tar.gz/pyalgen-0.0.2/pyalgen/__init__.py
@@ -0,0 +1,6 @@
+from pyalgen.population import *
+from pyalgen.selection import *
+from pyalgen.genetic import *
+from pyalgen.crossover import *
+from pyalgen.mutation import *
+from pyalgen.test.test_functions import *
