a55f397a7a55048c4df4e9347153bbff4fcb9930
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:50 +0000

    Add package wurlitzer/2.0.1/wurlitzer-2.0.1.tar.gz

---
+++ packages/wurlitzer/wurlitzer-2.0.1.tar.gz/wurlitzer-2.0.1/setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+import sys
+
+from setuptools import setup
+from setuptools.command.bdist_egg import bdist_egg
+
+with open("README.md") as f:
+    long_description = f.read()
+
+version_ns = {}
+with open("wurlitzer.py") as f:
+    for line in f:
+        if line.startswith("__version__"):
+            exec(line, version_ns)
+
+
+class bdist_egg_disabled(bdist_egg):
+    """Disabled version of bdist_egg
+
+    Prevents setup.py install from performing setuptools' default easy_install,
+    which it should never ever do.
+    """
+
+    def run(self):
+        sys.exit(
+            "Aborting implicit building of eggs. Use `pip install .` to install from source."
+        )
+
+
+setup_args = dict(
+    name="wurlitzer",
+    version=version_ns["__version__"],
+    author="Min RK",
+    author_email="benjaminrk@gmail.com",
+    description="Capture C-level output in context managers",
+    install_requires=["selectors2; python_version<'3.4'"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/minrk/wurlitzer",
+    py_modules=["wurlitzer"],
+    python_requires=">=2.7",
+    license="MIT",
+    cmdclass={
+        "bdist_egg": bdist_egg if "bdist_egg" in sys.argv else bdist_egg_disabled
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+    ],
+)
+
+if __name__ == "__main__":
+    setup(**setup_args)
---
+++ packages/wurlitzer/wurlitzer-2.0.1.tar.gz/wurlitzer-2.0.1/test.py
@@ -0,0 +1,125 @@
+# coding: utf-8
+from __future__ import print_function
+
+import io
+import os
+from tempfile import TemporaryFile
+import time
+
+import mock
+
+from wurlitzer import (
+    libc, pipes, STDOUT, PIPE, c_stderr_p, c_stdout_p,
+    sys_pipes, sys_pipes_forever,
+    stop_sys_pipes,
+    Wurlitzer,
+)
+
+def printf(msg):
+    """Call C printf"""
+    libc.printf((msg + '\n').encode('utf8'))
+
+def printf_err(msg):
+    """Cal C fprintf on stderr"""
+    libc.fprintf(c_stderr_p, (msg + '\n').encode('utf8'))
+
+def test_pipes():
+    with pipes(stdout=PIPE, stderr=PIPE) as (stdout, stderr):
+        printf(u"Hellø")
+        printf_err(u"Hi, stdérr")
+
+    assert stdout.read() == u"Hellø\n"
+    assert stderr.read() == u"Hi, stdérr\n"
+
+def test_pipe_bytes():
+    with pipes(encoding=None) as (stdout, stderr):
+        printf(u"Hellø")
+        printf_err(u"Hi, stdérr")
+
+    assert stdout.read() == u"Hellø\n".encode('utf8')
+    assert stderr.read() == u"Hi, stdérr\n".encode('utf8')
+
+def test_forward():
+    stdout = io.StringIO()
+    stderr = io.StringIO()
+    with pipes(stdout=stdout, stderr=stderr) as (_stdout, _stderr):
+        printf(u"Hellø")
+        printf_err(u"Hi, stdérr")
+        assert _stdout is stdout
+        assert _stderr is stderr
+
+    assert stdout.getvalue() == u"Hellø\n"
+    assert stderr.getvalue() == u"Hi, stdérr\n"
+
+def test_pipes_stderr():
+    stdout = io.StringIO()
+    with pipes(stdout=stdout, stderr=STDOUT) as (_stdout, _stderr):
+        printf(u"Hellø")
+        libc.fflush(c_stdout_p)
+        time.sleep(0.1)
+        printf_err(u"Hi, stdérr")
+        assert _stdout is stdout
+        assert _stderr is None
+
+    assert stdout.getvalue() == u"Hellø\nHi, stdérr\n"
+
+def test_flush():
+    stdout = io.StringIO()
+    w = Wurlitzer(stdout=stdout, stderr=STDOUT)
+    with w:
+        printf_err(u"Hellø")
+        time.sleep(0.5)
+        assert stdout.getvalue().strip() == u"Hellø"
+
+def test_sys_pipes():
+    stdout = io.StringIO()
+    stderr = io.StringIO()
+    with mock.patch('sys.stdout', stdout), mock.patch('sys.stderr', stderr), sys_pipes():
+        printf(u"Hellø")
+        printf_err(u"Hi, stdérr")
+
+    assert stdout.getvalue() == u"Hellø\n"
+    assert stderr.getvalue() == u"Hi, stdérr\n"
+
+def test_redirect_everything():
+    stdout = io.StringIO()
+    stderr = io.StringIO()
+    with mock.patch('sys.stdout', stdout), mock.patch('sys.stderr', stderr):
+        sys_pipes_forever()
+        printf(u"Hellø")
+        printf_err(u"Hi, stdérr")
+        stop_sys_pipes()
+    assert stdout.getvalue() == u"Hellø\n"
+    assert stderr.getvalue() == u"Hi, stdérr\n"
+
+
+def count_fds():
+    """utility for counting file descriptors"""
+    proc_fds = '/proc/{}/fd'.format(os.getpid())
+    if os.path.isdir(proc_fds):
+        return len(proc_fds)
+    else:
+        # this is an approximate count,
+        # but it should at least be stable if we aren't leaking
+        with TemporaryFile() as tf:
+            return tf.fileno()
+
+
+def test_fd_leak():
+    base_count = count_fds()
+    with pipes():
+        print('ok')
+    assert count_fds() == base_count
+    for i in range(10):
+        with pipes():
+            print('ok')
+        assert count_fds() == base_count
+
+
+def test_buffer_full():
+    with pipes(stdout=None, stderr=io.StringIO()) as (stdout, stderr):
+        long_string = "x" * 1000000  # create a very long string
+        printf_err(long_string)
+
+    # Test never reaches here as the process hangs.
+    assert stderr.getvalue() == long_string + "\n"
