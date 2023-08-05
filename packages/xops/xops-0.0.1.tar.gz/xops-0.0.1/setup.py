1851793cb8b4db7728b7f646e6ea16ba63b36f9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:41 +0000

    Add package xops/0.0.1/xops-0.0.1.tar.gz

---
+++ packages/xops/xops-0.0.1.tar.gz/xops-0.0.1/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import setuptools
+
+setuptools.setup(
+  name = 'xops',
+  version = '0.0.1',
+  description = 'A simple x-ops toolkit',
+  author = 'acegik',
+  license = 'GPL-3.0',
+  url = 'https://github.com/acegik/xops',
+  download_url = 'https://github.com/acegik/xops/downloads',
+  keywords = ['sysops', 'devops', 'tools'],
+  classifiers = [],
+  packages = setuptools.find_packages(),
+)
