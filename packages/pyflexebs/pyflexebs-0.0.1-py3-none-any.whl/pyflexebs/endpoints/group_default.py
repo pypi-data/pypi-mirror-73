796e3e1e9ce37bd97fe1061b6fa1cfafd092194a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:13 +0000

    Add package pyflexebs/0.0.1/pyflexebs-0.0.1-py3-none-any.whl

---
+++ packages/pyflexebs/pyflexebs-0.0.1-py3-none-any.whl/pyflexebs/endpoints/group_default.py
@@ -0,0 +1,31 @@
+"""
+The default group of operations that pyflexebs has
+"""
+
+from pytconf.config import register_endpoint, register_function_group
+
+import pyflexebs
+import pyflexebs.version
+
+GROUP_NAME_DEFAULT = "default"
+GROUP_DESCRIPTION_DEFAULT = "all pyflexebs commands"
+
+
+def register_group_default():
+    """
+    register the name and description of this group
+    """
+    register_function_group(
+        function_group_name=GROUP_NAME_DEFAULT,
+        function_group_description=GROUP_DESCRIPTION_DEFAULT,
+    )
+
+
+@register_endpoint(
+    group=GROUP_NAME_DEFAULT,
+)
+def version() -> None:
+    """
+    Print version
+    """
+    print(pyflexebs.version.VERSION_STR)
