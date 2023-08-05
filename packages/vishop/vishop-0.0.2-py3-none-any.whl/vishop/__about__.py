07c823fcc65cfac85ebbb2163c6f6f3d523dab19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:48 +0000

    Add package vishop/0.0.2/vishop-0.0.2-py3-none-any.whl

---
+++ packages/vishop/vishop-0.0.2-py3-none-any.whl/vishop/__about__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020 Xvezda <xvezda@naver.com>
+#
+# Use of this source code is governed by an MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT.
+
+__title__ = 'vishop'
+__version__ = '0.0.2'
+__author__ = 'Xvezda'
+__email__ = 'xvezda@naver.com'
+
+
