1c1d968d7f9e8da747e68a0d729720c00bcf8e05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:31 +0000

    Add package tinyrecord/0.2.0/tinyrecord-0.2.0.tar.gz

---
+++ packages/tinyrecord/tinyrecord-0.2.0.tar.gz/tinyrecord-0.2.0/setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup
+
+classifiers = """\
+Development Status :: 5 - Production/Stable
+Intended Audience :: Developers
+License :: OSI Approved :: MIT License
+Programming Language :: Python
+Programming Language :: Python :: 3.5
+Programming Language :: Python :: 3.6
+Programming Language :: Python :: 3.7
+Programming Language :: Python :: 3.8
+Programming Language :: Python :: 3 :: Only
+Topic :: Software Development :: Libraries :: Python Modules
+Operating System :: Microsoft :: Windows
+Operating System :: Unix
+Operating System :: MacOS :: MacOS X
+"""
+
+curr_path = os.path.abspath(os.path.dirname(__file__))
+setup(
+    name='tinyrecord',
+    version='0.2.0',
+    packages=['tinyrecord'],
+    python_requires='>=3.5',
+    classifiers=filter(None, classifiers.split('\n')),
+    zip_safe=True,
+    author='Eugene Eeo',
+    author_email='141bytes@gmail.com',
+    long_description=open(os.path.join(curr_path, 'README.rst'), 'r').read(),
+    long_description_content_type='text/x-rst',
+    description='Atomic transactions for TinyDB',
+    license='MIT',
+    keywords='tinydb nosql database transaction',
+    url='https://github.com/eugene-eeo/tinyrecord',
+)
