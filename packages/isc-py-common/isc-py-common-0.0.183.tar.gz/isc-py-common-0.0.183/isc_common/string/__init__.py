0bea63dee8f90a45b6e5817c099861a337dba63f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:55 +0000

    Add package isc-py-common/0.0.183/isc-py-common-0.0.183.tar.gz

---
+++ packages/isc-py-common/isc-py-common-0.0.183.tar.gz/isc-py-common-0.0.183/isc_common/string/__init__.py
@@ -0,0 +1,14 @@
+def get_NoneStr(str):
+    if str == None:
+        return ''
+    else:
+        return str
+
+
+def get_NullStr(str):
+    if str == None:
+        return ''
+    elif str == 'null':
+        return ''
+    else:
+        return str
