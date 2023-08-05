0266440fe425f42fa8b4e320f3473a1422bd2255
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:28 +0000

    Add package netbox-client/1.0.3/netbox_client-1.0.3-py3-none-any.whl

---
+++ packages/netbox-client/netbox_client-1.0.3-py3-none-any.whl/netbox_client/api/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from netbox_client.api.circuits_api import CircuitsApi
+from netbox_client.api.dcim_api import DcimApi
+from netbox_client.api.extras_api import ExtrasApi
+from netbox_client.api.ipam_api import IpamApi
+from netbox_client.api.secrets_api import SecretsApi
+from netbox_client.api.tenancy_api import TenancyApi
+from netbox_client.api.virtualization_api import VirtualizationApi
