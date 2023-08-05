4f7bb60c76af8963d265da02a80cfb2275939921
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:13:46 +0000

    Add package pyargcbr/0.2.1/pyargcbr-0.2.1-py2.py3-none-any.whl

---
+++ packages/pyargcbr/pyargcbr-0.2.1-py2.py3-none-any.whl/pyargcbr/knowledge_resources/acceptability_status.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class AcceptabilityStatus(Enum):
+    """Different types of acceptability status considered
+
+    Author:
+        Stella Heras
+    """
+    ACCEPTABLE = 1
+    UNACCEPTABLE = 2
+    UNDECIDED = 3
