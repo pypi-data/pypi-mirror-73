a6f2941fe533dcde203ba537a65f01541465844a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:06 +0000

    Add package ps-minifier/0.2.0/ps-minifier-0.2.0.tar.gz

---
+++ packages/ps-minifier/ps-minifier-0.2.0.tar.gz/ps-minifier-0.2.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    longdesc = f.read()
+
+setuptools.setup(
+    name = "ps-minifier",
+    version = "0.2.0",
+    author = "Willumz",
+    description = "A minifier for PowerShell scripts.",
+    long_description = longdesc,
+    long_description_content_type="text/markdown",
+    url = "https://github.com/Willumz/ps-minifier",
+    packages = setuptools.find_packages(),
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ],
+    entry_points = {
+        'console_scripts': ['psminifier=ps_minifier.psminifier:main']
+    }
+)
