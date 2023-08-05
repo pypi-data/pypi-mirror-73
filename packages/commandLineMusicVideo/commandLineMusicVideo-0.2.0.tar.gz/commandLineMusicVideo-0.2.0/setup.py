0c2476d428012ddb27edebd788921633654fef3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:05 +0000

    Add package commandLineMusicVideo/0.2.0/commandLineMusicVideo-0.2.0.tar.gz

---
+++ packages/commandLineMusicVideo/commandLineMusicVideo-0.2.0.tar.gz/commandLineMusicVideo-0.2.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="commandLineMusicVideo",
+    version="0.2.0",
+    author="Martin Barker",
+    author_email="martinbarker99@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MartinBarker/commandLineMusicVideo",
+    packages=setuptools.find_packages(),
+    #install_requires=[
+    #    'ffmpeg-python',
+    #],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
