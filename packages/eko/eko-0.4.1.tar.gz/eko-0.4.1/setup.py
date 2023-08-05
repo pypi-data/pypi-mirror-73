62ea4558dfb01e38262c48fd70e1656b4e7fa914
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:38:12 +0000

    Add package eko/0.4.1/eko-0.4.1.tar.gz

---
+++ packages/eko/eko-0.4.1.tar.gz/eko-0.4.1/setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# Installation script for python
+from setuptools import setup, find_packages
+
+# paste Readme
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='eko',
+      version='0.4.1',
+      description='Evolution Kernel Operator',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author = 'S. Carrazza, J. Cruz-Martinez, F. Hekhorn',
+      author_email='stefano.carrazza@cern.ch',
+      url='https://github.com/N3PDF/eko',
+      package_dir={'': 'src'},
+      packages=find_packages('src'),
+      package_data = {
+          '' : ['doc/source/img/Logo.svg'],
+      },
+      classifiers=[
+          'Operating System :: Unix',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          'Topic :: Scientific/Engineering',
+          'Topic :: Scientific/Engineering :: Physics',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+      ],
+      install_requires=[
+          'numpy',
+          'scipy',
+          'numba',
+          'joblib',
+          'pyyaml',
+      ],
+      setup_requires=[
+          'wheel'
+      ],
+      python_requires='>=3.7'
+)
