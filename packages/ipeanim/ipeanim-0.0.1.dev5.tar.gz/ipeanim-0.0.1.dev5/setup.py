7a86cf9c7254946d6c191a84d9d3d00c8979ae6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:23 +0000

    Add package ipeanim/0.0.1.dev5/ipeanim-0.0.1.dev5.tar.gz

---
+++ packages/ipeanim/ipeanim-0.0.1.dev5.tar.gz/ipeanim-0.0.1.dev5/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+import os
+
+github_url = f'https://github.com/{os.environ.get("GITHUB_REPOSITORY")}'
+
+setup(
+    setup_requires=['pbr'],
+    pbr=True,
+    tests_require=['nose'],
+    test_suite='nose.collector',
+    project_urls={
+        'Source Code': github_url,
+        'Bug Tracker': f'{github_url}/issues',
+        'Documentation': f'{github_url}/blob/master/README.md',
+    },
+    scripts=[
+        'bin/ipeanim',
+    ]
+)
