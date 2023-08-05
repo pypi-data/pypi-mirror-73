7bfc7cf1187ff65abc42560c71beb03983875e2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:34 +0000

    Add package blogspotapi/0.3.4/blogspotapi-0.3.4-py3-none-any.whl

---
+++ packages/blogspotapi/blogspotapi-0.3.4-py3-none-any.whl/blogspotapi/blog_client.py
@@ -0,0 +1,115 @@
+
+from googleapiclient import sample_tools
+from bs4 import BeautifulSoup  # Or from BeautifulSoup import BeautifulSoup
+import datetime
+from collections import namedtuple
+import re
+from datetime import date, datetime
+BlogPost = namedtuple('BlogPost', 'postId url title videoId content labels amara_embed last_updated')
+
+
+
+
+class BlogClient:
+
+    def __init__(self, client_json_file):
+        self.client_json_file = client_json_file
+        self.service, self.flags = self.login()
+        self.posts = self.service.posts()
+
+    def stripHtmlTags(self, htmlTxt):
+        if htmlTxt is None:
+            return None
+        else:
+            all_lines = BeautifulSoup(htmlTxt).findAll(text=True)
+            not_empty_lines = [line for line in all_lines if len(line) > 0]
+            return '\n'.join(not_empty_lines)
+
+    def login(self):
+        service, flags = sample_tools.init(
+            ['--noauth_local_webserver'], 'blogger', 'v3', __doc__, self.client_json_file ,
+            scope='https://www.googleapis.com/auth/blogger')
+        return service, flags
+
+    def replace_object_in_blog_post(self, blogId, postId):
+
+        content, videoId, posts_doc = self.extract_content_and_video(blogId, postId)
+        obind1 = content.find('<object')
+        obind2 = content.find('object>')
+
+        if videoId and obind1 >= 0 and obind2 >=0 :
+            newcontent = content[0:obind1]+'<iframe src="https://www.youtube.com/embed/'+videoId+'" width="640" height="390" frameborder="0" allowfullscreen></iframe>'+content[obind2+7:len(content)]
+            posts_doc['content'] = newcontent
+            request = self.posts.update(blogId=blogId, postId=postId, body=posts_doc, last_updated=datetime.utcnow())
+            print('Replacing object' + postId)
+            request.execute()
+
+    def extract_video(self, blogId, postId):
+        return self.extract_content_and_video(blogId, postId)['videoId']
+
+    def extract_content_and_video(self, blogId, postId):
+        request = self.posts.get(blogId=blogId, postId=postId)
+        posts_doc = request.execute()
+        content = posts_doc['content']
+        #print('Processing post ' + postId + ':' + posts_doc['title'])
+        #search_youtube = re.search('youtube\.com\/v\/([\w\-]{11})', content)
+        search_youtube = re.search('\\\".*?youtube\.com\/embed\/([\w\-]{11})[\"\?]', content)
+        videoId = None
+        if search_youtube :
+            videoId = search_youtube.group(1)
+        result_dict = {'content':content, 'videoId': videoId, 'posts_doc': posts_doc}
+        return result_dict
+
+    def insert_amara_tags(self, blogId, postId, language_code):
+        result_dict = self.extract_content_and_video(blogId, postId)
+        content, videoId, posts_doc = result_dict['content'],result_dict['videoId'],result_dict['posts_doc']
+        if content.find('amara') > -1:
+            return
+        pos_iframe= content.find('<iframe')
+        snippet_amara = '<div class="amara-embed" data-height="390px" data-resizable="true" data-show-subtitles-default="true" data-url="http://www.youtube.com/watch?v='+videoId+'" data-width="640px" data-initial-language="'+language_code+'"></div></br>'
+        newContent = content[0:pos_iframe]+snippet_amara +content[pos_iframe:len(content)]
+        posts_doc['content'] = newContent
+        posts_doc['labels'].append('subtitled')
+        posts_doc['updated'] = posts_doc['published'] = str(datetime.datetime.now().isoformat(timespec='microseconds'))
+        request = self.posts.update(blogId=blogId,postId=postId,body=posts_doc, last_updated=datetime.utcnow())
+        request.execute()
+
+
+    def update_video_in_blog_post(self, blogId, postId, old_youtube_ref,new_youtube_ref, posts):
+        request = posts.get(blogId=blogId, postId=postId)
+        posts_doc = request.execute()
+        posts_doc['content'] = posts_doc['content'].replace(old_youtube_ref, new_youtube_ref)
+        request = posts.update(blogId=blogId, postId=postId, body=posts_doc, last_updated=datetime.utcnow())
+        request.execute()
+
+    def retrieve_lyrics(self, blogId, postId ):
+        request = self.posts.get(blogId=blogId, postId=postId)
+        posts_doc = request.execute()
+        content = posts_doc['content']
+        return self.stripHtmlTags(content )
+
+
+    def iterate_blog_items(self, blogId):
+
+        request = self.posts.list(blogId=blogId)
+        while request != None:
+            posts_doc = request.execute()
+            if 'items' in posts_doc and not (posts_doc['items'] is None):
+                for post in posts_doc['items']:
+                    yield post
+            request = self.posts.list_next(request, posts_doc)
+
+    def iterate_blog_posts(self, blogId):
+        for item in self.iterate_blog_items(blogId):
+            content = item['content']
+            m_you_tube = re.search('src=\\\".*?youtube\.com\/embed\/([\w\-]{11})[\"\?]', content)
+            m_amara_embed = re.search('amara-embed', content)
+            video_id = m_you_tube.group(1) if m_you_tube else None
+            yield BlogPost(postId=item['id'],
+                           url=item['url'],
+                           title=item['title'].strip(),
+                           videoId=video_id,
+                           content=self.stripHtmlTags(item['content']),
+                           labels=item.get('labels', None),
+                           amara_embed=1 if m_amara_embed else 0,
+                           last_updated=datetime.utcnow())
---
+++ packages/blogspotapi/blogspotapi-0.3.4-py3-none-any.whl/blogspotapi/blog_repository.py
@@ -0,0 +1,133 @@
+from pymongo import MongoClient
+from .blog_client import BlogPost
+from amaraapi import AmaraTools, AmaraVideo
+import traceback
+import sys
+from datetime import date, datetime, timedelta
+
+class BlogRepository:
+
+    def __init__(self, mongo_connection, blogId):
+        self.mongo_connect = mongo_connection
+        self.client = MongoClient(mongo_connection)
+        self.musicblogs_database = self.client.musicblogs
+        self.blogId = blogId
+        self.posts_collection = self.musicblogs_database['posts.' + blogId]
+        self.posts_in_blog = self.posts_collection.find()
+        self.posts_map = \
+            {p['postId']: BlogPost(
+                postId=p['postId'], title=p['title'], videoId=p['videoId'], content=p['content'],
+                labels=p.get('labels', 0),
+                url=p.get('url', ''),
+                amara_embed=p.get('amara_embed', ''), last_updated=datetime.utcnow()
+            ) for p in self.posts_in_blog
+            }
+
+        self.postids = set(self.posts_map.keys())
+        self.subtitles_collection = self.musicblogs_database['subtitles.' + blogId]
+
+
+    def get_subtitles_for(self, video_url):
+        sub_titles_rec = self.subtitles_collection.find_one({"video_url": video_url})
+        if sub_titles_rec:
+            return sub_titles_rec['subtitles']
+
+    def update_blog_post(self, blog_post):
+        if not blog_post:
+            return
+
+        if not hasattr(blog_post, "postId"):
+            return
+        if blog_post.postId in self.postids:
+            self.postids.remove(blog_post.postId)
+
+        if blog_post.postId in self.posts_map:
+
+            update_key, update_value = {'postId': blog_post.postId}, {k: v for k, v in blog_post._asdict().items() if k not in "postId"}
+
+
+            print("updating {}".format(update_key ))
+            self.posts_collection.update_one(update_key,   { '$set' : update_value } )
+            print("updated {} ".format(update_key))
+        else:
+            print("inserting {} ".format(blog_post.postId))
+            self.posts_collection.insert_one(blog_post._asdict())
+            print("inserted {} ".format(blog_post.postId))
+
+    def update_sub_titles(self, blog_post, languages, amara_headers):
+
+        amara_tools = AmaraTools(amara_headers)
+        if hasattr(blog_post, "labels"):
+            labels = blog_post.labels
+            video_url = blog_post.videoId
+
+            if labels and ('subtitled' in labels or 'SUBTITLED' in labels):
+                found = False
+                print("Trying to get video for {}".format(video_url))
+                try:
+                    amara_id = amara_tools.get_video_id(video_url='https://youtu.be/' + video_url)
+                    amara_video = AmaraVideo(amara_headers, amara_id)
+                    if amara_video:
+                        print("Found video in Amara")
+                        languages_video = amara_video.get_languages()
+                        common_languages = [l['code'] for l in languages_video if l['code'] in languages]
+                        if common_languages:
+                            print("Found languages : {}".format(common_languages))
+                            all_subtitles = [amara_video.get_subtitles(sel_language) for sel_language in common_languages]
+                            valid_subtitles = [subtitle for subtitle in all_subtitles if subtitle and len(subtitle['subtitles']) > 0 and '-->' in subtitle['subtitles']]
+                            if valid_subtitles:
+                                subtitles = valid_subtitles[0]
+                                version_number = subtitles.get('version_number', 1)
+                                lang = subtitles['language']["code"]
+                                print("Saving subtitles for {}, v{}, {}".format(video_url, version_number, lang))
+                                self.subtitles_collection.replace_one(
+                                    filter={"video_url": video_url, "version_number": version_number},
+                                    replacement={"video_url": video_url, "video_id": amara_id,
+                                                 "lang": subtitles['language'],
+                                                 "subtitles": subtitles['subtitles'],
+                                                 "version_number": version_number,
+                                                 "last_updated": datetime.utcnow()},
+                                    upsert=True,
+
+                                )
+                                found = True
+
+                except:
+                    traceback.print_exc(file=sys.stderr)
+                    print("Could not process {} from {}".format(video_url, blog_post.url))
+                if not found:
+                    print("Could not find subtitles for {}, {}".format(video_url, blog_post.url))
+        self.subtitles_collection.remove(
+                {"version_number": {"$exists": False}},
+        )
+        self.subtitles_collection.remove(
+            {"last_updated": {"$exists": False}},
+        )
+
+    def delete_invalid_posts(self):
+        for postid in self.postids:
+            self.posts_collection.delete_one({'postId': postid})
+            print("post {} deleted".format(postid))
+
+    def delete_old_posts(self):
+
+        older_date = datetime.utcnow() + timedelta(-30)
+        updated_month_ago = {"last_updated":{"$lte": older_date}}
+
+        self.posts_collection.remove(updated_month_ago )
+        self.subtitles_collection.remove(updated_month_ago )
+
+
+    def save_to_videos(self):
+        videos_collection = self.musicblogs_database['blog_videos.' + str(self.blogId)]
+
+        for postId, blog_post in self.posts_map.items():
+            videoId = blog_post.videoId
+            replacement = {"videoId": videoId, "title": blog_post.title, "blogId": self.blogId, "postId": postId,
+                           "last_updated": datetime.utcnow()}
+
+            videos_collection.replace_one(
+                filter={"blogId": self.blogId, "postId": postId},
+                replacement=replacement,
+                upsert=True
+            )
