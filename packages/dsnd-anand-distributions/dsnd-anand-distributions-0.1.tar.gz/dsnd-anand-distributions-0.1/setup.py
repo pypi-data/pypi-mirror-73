f2173bb0192bb43d0a9f0538199e7c2241ffd001
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:39 +0000

    Add package dsnd-anand-distributions/0.1/dsnd-anand-distributions-0.1.tar.gz

---
+++ packages/dsnd-anand-distributions/dsnd-anand-distributions-0.1.tar.gz/dsnd-anand-distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dsnd-anand-distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd-anand-distributions'],
+      author=['Anand Nakat'],
+      zip_safe=False)
