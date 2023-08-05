9ea79fbd565613e64a2ea2c916ff5eeb6ae02bac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:29 +0000

    Add package convertapi/1.3.0/convertapi-1.3.0-py2.py3-none-any.whl

---
+++ packages/convertapi/convertapi-1.3.0-py2.py3-none-any.whl/convertapi/__init__.py
@@ -0,0 +1,20 @@
+__version__ = '1.3.0'
+
+from .exceptions import *
+from .client import Client
+from .upload_io import UploadIO
+from .api import convert, user
+
+# configuration
+
+api_secret = None
+base_uri = 'https://v2.convertapi.com/'
+user_agent = 'ConvertAPI-Python/' + __version__
+timeout = 60
+conversion_timeout = 180
+conversion_timeout_delta = 10
+upload_timeout = 600
+download_timeout = 600
+max_parallel_uploads = 10
+
+client = Client()
