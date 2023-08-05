aa7b90a36137470bd08bc2294949d6e68e2e630e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:19 +0000

    Add package folpy/0.1.2/folpy-0.1.2-py3-none-any.whl

---
+++ packages/folpy/folpy-0.1.2-py3-none-any.whl/folpy/utils/minion/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+from .minion import (
+    MinionSol,
+    MorphMinionSol,
+    ParallelMorphMinionSol,
+    homomorphisms,
+    embeddings,
+    isomorphisms,
+    is_homomorphic_image,
+    is_substructure,
+    is_isomorphic,
+    is_isomorphic_to_any
+)
