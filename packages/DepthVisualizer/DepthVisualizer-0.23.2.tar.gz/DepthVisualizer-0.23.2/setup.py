84aeabb58347a064b09d0b1e2305f1f7b9f031ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:54 +0000

    Add package DepthVisualizer/0.23.2/DepthVisualizer-0.23.2.tar.gz

---
+++ packages/DepthVisualizer/DepthVisualizer-0.23.2.tar.gz/DepthVisualizer-0.23.2/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+setup(
+    name="DepthVisualizer",
+    packages=["DepthVisualizer"],
+    version='0.23.2',
+    license="MIT",
+    description="Point Cloud & Depth Map visualization library",
+    author="Eren Balatkan",
+    author_email="e.balatkan@gmail.com",
+    url="https://github.com/Navhkrin/DepthVisualizer",
+    download_url="https://github.com/Navhkrin/DepthVisualizer/archive/v0.2.tar.gz",
+    keywords=["Point Cloud", "Depth Map", "OpenGL", "Renderer"],
+    install_requires=[
+        'glfw',
+        'PyOpenGL',
+        'PyGLM',
+        'numpy'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
