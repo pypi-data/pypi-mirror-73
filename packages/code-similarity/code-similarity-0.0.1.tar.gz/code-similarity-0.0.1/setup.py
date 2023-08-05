c6a8f9f776f32a3ce518dbd53a849ed4bd0557b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:06 +0000

    Add package code-similarity/0.0.1/code-similarity-0.0.1.tar.gz

---
+++ packages/code-similarity/code-similarity-0.0.1.tar.gz/code-similarity-0.0.1/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*-coding:utf-8 -*-
+# Reference:**********************************************
+# @Time     : 2020/7/4 9:30
+# @Author   : Raymond Luo
+# @File     : setup.py.py
+# @User     : luoli
+# @Software: PyCharm
+# Reference:**********************************************
+import setuptools
+
+setuptools.setup(
+    name="code-similarity",  # Replace with your own username
+    version="0.0.1",
+    author="Raymond Luo",
+    author_email="luolinhao1998@gmail.com",
+    description="An out of box code and text similarity computation package",
+    url="https://github.com/RManLuo/code-similarity",
+    packages=['CodeSimilarity'],
+    include_package_data=True,
+)
