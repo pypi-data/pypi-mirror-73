eefb4e77530f331e3aa56da6c682a584de7683b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:38:36 +0000

    Add package hexson/0.0.1/hexson-0.0.1.tar.gz

---
+++ packages/hexson/hexson-0.0.1.tar.gz/hexson-0.0.1/hexson/__init__.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# Json library support hex escape.
+from .dumpper import Dumpper
+from .parser import Parser
+
+
+VERSION = (0, 0, 1)
+__version__ = VERSION
+__versionstr__ = "0.0.1"
+
+
+__all__ = [
+    "dump",
+    "load",
+    "dumps",
+    "loads"
+]
+
+
+def dump(input_obj, fd):
+    """ Convert an Python type to a JSON string and write to a file. """
+    fd.write(Dumpper(input_obj).dump_to_json())
+
+
+def load(fd, utf_8_string=False):
+    """ Read from a file and convert a JSON string to an Python type. """
+    return Parser(fd.read()).parse_from_json(utf_8_string)
+
+
+def dumps(input_obj):
+    """ Convert an Python type to a JSON string. """
+    return Dumpper(input_obj).dump_to_json()
+
+
+def loads(input_json, utf_8_string=False):
+    """ Convert a JSON string to an Python type. """
+    return Parser(input_json).parse_from_json(utf_8_string)
