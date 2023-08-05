5bb5364a33579025b3d0cdeca20736b1b4e9583c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:21 +0000

    Add package decorators/2.0.0/decorators-2.0.0.tar.gz

---
+++ packages/decorators/decorators-2.0.0.tar.gz/decorators-2.0.0/decorators/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, division, print_function, absolute_import
+import logging
+
+from .base import (
+    Decorator,
+    InstanceDecorator,
+    ClassDecorator,
+    FuncDecorator,
+)
+from .descriptor import (
+    property,
+    classproperty,
+)
+
+
+__version__ = "2.0.0"
+
+
+# get rid of "No handler found" warnings (cribbed from requests)
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
