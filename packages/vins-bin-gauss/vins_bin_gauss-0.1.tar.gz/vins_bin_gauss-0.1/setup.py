839ff1db4def9cc3c7358e4ed9b11e1a3c1de1f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:59 +0000

    Add package vins-bin-gauss/0.1/vins_bin_gauss-0.1.tar.gz

---
+++ packages/vins-bin-gauss/vins_bin_gauss-0.1.tar.gz/vins_bin_gauss-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='vins_bin_gauss',
+      version='0.1',
+      description='Binomial and Gaussian Distributions',
+      packages=['vins_bin_gauss'],
+      author = "M.Vineeth",
+      author_email = "vinscoder1627@gmail.com",
+      zip_safe=False)
