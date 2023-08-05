2cf4d048c445a1e4f5fd6fff8357145dddbaa809
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:52 +0000

    Add package xclim/0.18.0/xclim-0.18.0-py3-none-any.whl

---
+++ packages/xclim/xclim-0.18.0-py3-none-any.whl/xclim/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""Climate indices computation package based on xArray"""
+from xclim.core import units
+from xclim.core.options import set_options
+from xclim.indicators import anuclim
+from xclim.indicators import atmos
+from xclim.indicators import ICCLIM
+from xclim.indicators import land
+from xclim.indicators import seaIce
+
+__author__ = """Travis Logan"""
+__email__ = "logan.travis@ouranos.ca"
+__version__ = "0.18.0-beta"
