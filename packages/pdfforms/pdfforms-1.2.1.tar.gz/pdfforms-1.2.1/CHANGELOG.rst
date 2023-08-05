d1a1b688e16d82f73acea7160b7dd248827de63d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:04 +0000

    Add package pdfforms/1.2.1/pdfforms-1.2.1.tar.gz

---
+++ packages/pdfforms/pdfforms-1.2.1.tar.gz/pdfforms-1.2.1/CHANGELOG.rst
@@ -0,0 +1,28 @@
+Changelog
+-----------
+
+1.2.1
+"""""
+:date: 3 July, 2020
+
+* Don’t crash when subcommand not supplied.
+
+1.2.0
+"""""
+:date: 24 September, 2019
+
+* Added ``--no-flatten`` option to keep form fillable
+* ``inspect`` doesn’t crash if passed a pdf without fillable form
+
+1.1.0
+"""""
+:date: 4 July, 2018
+
+* Fixed handling of whitespace (Thanks @rohitkhirapate for the bug report)
+* Added python 3.4 compatibility (Thanks @oneyb for the PR)
+
+1.0.0
+"""""
+:date: 1 May, 2017
+
+* Initial release
---
+++ packages/pdfforms/pdfforms-1.2.1.tar.gz/pdfforms-1.2.1/setup.py
@@ -0,0 +1,34 @@
+from os.path import join, dirname
+from setuptools import setup
+
+package_name = "pdfforms"
+package_version = "1.2.1"
+base_dir = dirname(__file__)
+
+def read(filename):
+    f = open(join(base_dir, filename))
+    return f.read()
+
+setup(
+    name = package_name,
+    version = package_version,
+    description = "Populate fillable pdf forms from csv data file",
+    long_description = read("README.rst") + '\n\n' + read("CHANGELOG.rst"),
+    author = "Aryeh Leib Taurog",
+    author_email = "python@aryehleib.com",
+    license = 'MIT',
+    url = "https://github.com/altaurog/pdfforms",
+    packages = [package_name],
+    entry_points = {'console_scripts': ['pdfforms=pdfforms.pdfforms:main']},
+    classifiers = [
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: End Users/Desktop",
+        "Environment :: Console",
+        "Topic :: Office/Business",
+    ],
+)
