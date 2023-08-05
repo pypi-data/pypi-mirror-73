17cba109ecc33fae352ec88bff608fc42ac83d27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:49 +0000

    Add package rlprop/0.0.2/rlprop-0.0.2.tar.gz

---
+++ packages/rlprop/rlprop-0.0.2.tar.gz/rlprop-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rlprop",
+    version="0.0.2",
+    author="Aziz Alfoudari",
+    author_email="aziz.alfoudari@gmail.com",
+    description="Reinforcement Learning agents implemented in pytorch",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/abstractpaper/prop",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
