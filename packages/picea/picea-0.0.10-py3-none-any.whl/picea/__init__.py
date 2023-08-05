fa03384a15bc3de8e002d89982962ee7dd250a12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:07 +0000

    Add package picea/0.0.10/picea-0.0.10-py3-none-any.whl

---
+++ packages/picea/picea-0.0.10-py3-none-any.whl/picea/__init__.py
@@ -0,0 +1,18 @@
+__author__ = 'Rens Holmer'
+__version__ = '0.0.10'
+
+from .tree import Tree  # noqa
+from .sequence import (  # noqa
+    Alphabet,
+    DNA,
+    AMINO_ACID,
+    Sequence,
+    SequenceReader,
+    DNA,
+    AMINO_ACID,
+    SequenceCollection,
+    SequenceList,
+    MultipleSequenceAlignment,
+    SequenceAnnotation,
+    SequenceInterval,
+)
