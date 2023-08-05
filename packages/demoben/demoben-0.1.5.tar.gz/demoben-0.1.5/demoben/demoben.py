84bee1428ee81d82d4779680791cd2834a153091
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:18 +0000

    Add package demoben/0.1.5/demoben-0.1.5.tar.gz

---
+++ packages/demoben/demoben-0.1.5.tar.gz/demoben-0.1.5/demoben/demoben.py
@@ -0,0 +1,6 @@
+class Demo(object):
+    """docstring for Demo"""
+
+    def __init__(self, arg):
+        super(Demo, self).__init__()
+        self.arg = arg
---
+++ packages/demoben/demoben-0.1.5.tar.gz/demoben-0.1.5/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['demoben']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'demoben',
+    'version': '0.1.5',
+    'description': 'This is a test',
+    'long_description': None,
+    'author': 'Ben-Cheng',
+    'author_email': 'bcheng@linz.govt.nz',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6.1,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
