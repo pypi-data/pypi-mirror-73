c74a8ed83368e357b65822906c42256446509e84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:49 +0000

    Add package jokeapi/0.2.2/jokeapi-0.2.2.tar.gz

---
+++ packages/jokeapi/jokeapi-0.2.2.tar.gz/jokeapi-0.2.2/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jokeapi",
+    packages=["jokeapi"],
+    version="0.2.2",
+    license="GNU General Public License v3 (GPLv3)",
+    description="An API Wrapper for Sv443's JokeAPI",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="thenamesweretakenalready",
+    author_email="leet_haker@cyber-wizard.com",
+    url="""https://github.com/thenamesweretakenalready/Sv443s-JokeAPI-Python-Wrapper""",
+    download_url="https://github.com/user/Sv443s-JokeAPI-Python-Wrapper/archive/v0.2.2.tar.gz",
+    keywords=["api wrapper", "wrapper", "api", "jokes", "python", "joke api"],
+    install_requires=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
