35ceb3545c4d77d051fba4333ff5bd9e684f61c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:39 +0000

    Add package distributions-gungz/0.1/distributions_gungz-0.1.tar.gz

---
+++ packages/distributions-gungz/distributions_gungz-0.1.tar.gz/distributions_gungz-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_gungz',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_gungz'],
+      author='Gungz')
