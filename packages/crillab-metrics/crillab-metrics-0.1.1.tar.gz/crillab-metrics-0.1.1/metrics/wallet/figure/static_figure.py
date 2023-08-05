bb481e1e95a8d276f90bf78e8dc12fd8946764fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:26 +0000

    Add package crillab-metrics/0.1.1/crillab-metrics-0.1.1.tar.gz

---
+++ packages/crillab-metrics/crillab-metrics-0.1.1.tar.gz/crillab-metrics-0.1.1/metrics/wallet/figure/static_figure.py
@@ -0,0 +1,250 @@
+# ##############################################################################
+#  Wallet - A Metrics Module                                                   #
+#  Copyright (c) 2019-2020 - Univ Artois & CNRS, Exakis Nelite                 #
+#  --------------------------------------------------------------------------  #
+#  mETRICS - rEproducible sofTware peRformance analysIs in perfeCt Simplicity  #
+#  wALLET - Automated tooL for expLoiting Experimental resulTs                 #
+#                                                                              #
+#                                                                              #
+#  This program is free software: you can redistribute it and/or modify it     #
+#  under the terms of the GNU Lesser General Public License as published by    #
+#  the Free Software Foundation, either version 3 of the License, or (at your  #
+#  option) any later version.                                                  #
+#                                                                              #
+#  This program is distributed in the hope that it will be useful, but         #
+#  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY  #
+#  or FITNESS FOR A PARTICULAR PURPOSE.                                        #
+#  See the GNU General Public License for more details.                        #
+#                                                                              #
+#  You should have received a copy of the GNU Lesser General Public License    #
+#  along with this program.                                                    #
+#  If not, see <https://www.gnu.org/licenses/>.                                #
+# ##############################################################################
+from typing import List
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from metrics.wallet.dataframe.dataframe import CampaignDFFilter, CampaignDataFrame
+from metrics.wallet.figure.abstract_figure import CactusPlot, BoxPlot, ScatterPlot, Table
+
+LINE_STYLES = ['-', ':', '-.', '--']
+"""
+Corresponds to a sample of existing line styles for matplotlib.
+"""
+
+
+class StatTable(Table):
+    """
+    Creation of a stat table representing main statistics for a campaign.
+    """
+
+    def get_data_frame(self):
+        df_stat = pd.DataFrame(index=self._campaign_df.xp_ware_names)
+
+        only_solved_df = self._campaign_df.filter_by([
+            CampaignDFFilter.ONLY_SOLVED,
+        ]).data_frame
+
+        no_common_failed = self._campaign_df.filter_by([
+            CampaignDFFilter.DELETE_COMMON_TIMEOUT,
+        ]).data_frame
+
+        common_inputs = self._campaign_df.filter_by([
+            CampaignDFFilter.ONLY_COMMON_SOLVED,
+        ]).data_frame
+
+        no_common_solved_no_out = self._campaign_df.filter_by([
+            CampaignDFFilter.DELETE_COMMON_SOLVED,
+            CampaignDFFilter.ONLY_SOLVED,
+        ]).data_frame
+
+        df_stat['count'] = only_solved_df.groupby('experiment_ware')['cpu_time'].count()
+        df_stat['sum'] = no_common_failed.groupby('experiment_ware')['cpu_time'].sum()
+        df_stat['common_count'] = common_inputs.groupby('experiment_ware')['cpu_time'].count()
+        df_stat['common_sum'] = common_inputs.groupby('experiment_ware')['cpu_time'].sum()
+        df_stat['uncommon_count'] = no_common_solved_no_out.groupby('experiment_ware')['cpu_time'].count()
+        df_stat['total'] = len(self._campaign_df.data_frame['input'].unique())
+
+        return df_stat.sort_values(['count', 'sum'], ascending=[False, True]).fillna(0).astype(int)
+
+
+def contribution_agg(slice: pd.DataFrame, to: float):
+    slice = slice.sort_values(by='cpu_time')
+    first = slice.iloc[0]
+    second = slice.iloc[1]
+
+    if first['cpu_time'] < to:
+        return pd.Series([first['experiment_ware'], first['cpu_time'], second['cpu_time'] >= to],
+                         index=['first', 'cpu_time', 'unique'])
+
+    return pd.Series([None, None, False], index=['first', 'cpu_time', 'unique'])
+
+
+class ContributionTable(Table):
+    """
+    Creation of a table representing the different contributions of each solver.
+    """
+
+    def __init__(self, campaign_df: CampaignDataFrame, deltas: List[float], **kwargs):
+        super().__init__(campaign_df, **kwargs)
+        self._deltas = deltas
+
+    def get_data_frame(self):
+        contrib_raw = self._campaign_df.data_frame.groupby('input').apply(
+            lambda x: contribution_agg(x, self._campaign_df.campaign.timeout))
+        contrib = pd.DataFrame()
+
+        contrib['vbew_simple'] = contrib_raw.groupby('first').cpu_time.count()
+
+        for delta in self._deltas:
+            sub = contrib_raw[contrib_raw.cpu_time > delta]
+            contrib[f'vbew > {delta}'] = sub.groupby('first').cpu_time.count()
+
+        contrib['contribution'] = contrib_raw.groupby('first').unique.sum()
+
+        return contrib.fillna(0).astype(int).sort_values(['vbew_simple', 'contribution'], ascending=[False, False])
+
+
+class CactusMPL(CactusPlot):
+    """
+    Creation of a static cactus plot.
+    """
+
+    def get_figure(self):
+        """
+
+        @return: the figure.
+        """
+        df = self.get_data_frame()
+        self._set_font()
+
+        fig, ax = plt.subplots(figsize=self._figsize)
+        ax.set_title(self.get_title())
+        ax.set_xlabel(self.get_x_axis_name())
+        ax.set_ylabel(self.get_y_axis_name())
+        ax.set_xscale('log' if self._logx else 'linear')
+        ax.set_yscale('log' if self._logy else 'linear')
+
+        self._set_plot(df, ax)
+        self._set_legend(ax)
+
+        ax.set_xlim(self._get_x_lim(ax))
+        ax.set_ylim(self._get_y_lim(ax))
+
+        if self._output is not None:
+            fig.savefig(self._output, bbox_inches='tight', transparent=True)
+
+        return ax
+
+    def _set_plot(self, df, ax):
+        styles = [self.style_map.get(x) for x in df.columns] if self.style_map else None
+
+        kwargs = [
+            {
+                'color': self.color_map.get(x) if self.color_map else None,
+                'linewidth': 3 if x in self._campaign_df.vbew_names else 1,
+                'marker': 'o' if self.show_marker else None,
+            } for x in df.columns
+        ]
+
+        for i, col in enumerate(df.columns):
+            if styles is None:
+                ax.plot(df.index, df[col], label=self._get_final_xpware_name(col), **(kwargs[i]))
+            else:
+                ax.plot(df.index, df[col], styles[i], label=self._get_final_xpware_name(col), **(kwargs[i]))
+
+    def _set_legend(self, ax):
+        if self._xp_ware_name_map is None:
+            ax.legend(self.get_data_frame().columns, loc=self._legend_location, bbox_to_anchor=self._bbox_to_anchor,
+                      ncol=self._ncol_legend)
+        else:
+            ax.legend([self._xp_ware_name_map[x] for x in self.get_data_frame().columns], loc=self._legend_location,
+                      bbox_to_anchor=self._bbox_to_anchor, ncol=self._ncol_legend)
+
+
+class BoxMPL(BoxPlot):
+    """
+    Creation of a static box plot.
+    """
+
+    def get_figure(self):
+        """
+
+        @return: the figure.
+        """
+        df = self.get_data_frame()
+        self._set_font()
+
+        fig, ax = plt.subplots(figsize=self._figsize)
+        ax.set_title(self.get_title())
+        ax.set_yscale('log' if self._logy else 'linear')
+
+        if self._xp_ware_name_map is not None:
+            df = df.rename(columns=self._xp_ware_name_map)
+
+        df.boxplot(ax=ax, rot=15, meanline=True, showmeans=True)
+
+        if self._output is not None:
+            fig.savefig(self._output, bbox_inches='tight', transparent=True)
+
+        return ax
+
+
+
+
+class ScatterMPL(ScatterPlot):
+    """
+    Creation of a static scatter plot.
+    """
+
+    def get_figure(self):
+        """
+
+        @return: the figure.
+        """
+        df = self.get_data_frame()
+        self._set_font()
+        limits = [self.min, self._campaign_df.campaign.timeout]
+
+        fig, ax = plt.subplots(figsize=self._figsize)
+        ax.set_title(self.get_title())
+        ax.set_xscale('log' if self._logx else 'linear')
+        ax.set_yscale('log' if self._logy else 'linear')
+        ax.plot(limits, limits, ls="--", c=".3")
+
+        plt.xlim(limits)
+        plt.ylim(limits)
+
+        ax.set_xlim(self._get_x_lim(ax))
+        ax.set_ylim(self._get_y_lim(ax))
+
+        df.plot.scatter(x=self.xp_ware_i, y=self.xp_ware_j, ax=ax)
+        ax.set_xlabel(self.get_x_axis_name())
+        ax.set_ylabel(self.get_y_axis_name())
+
+        if self._output is not None:
+            fig.savefig(self._output, bbox_inches='tight', transparent=True)
+
+        return ax
+
+    def get_x_axis_name(self):
+        """
+
+        @return: the x axis name.
+        """
+        return self._get_final_xpware_name(self.xp_ware_i)
+
+    def get_y_axis_name(self):
+        """
+
+        @return: the y axis name.
+        """
+        return self._get_final_xpware_name(self.xp_ware_j)
+
+    def get_title(self):
+        """
+
+        @return: the title of the plot.
+        """
+        return f'Comparison of {self._get_final_xpware_name(self.xp_ware_i)} and {self._get_final_xpware_name(self.xp_ware_j)}'
---
+++ packages/crillab-metrics/crillab-metrics-0.1.1.tar.gz/crillab-metrics-0.1.1/tests/test_wallet/test_figure.py
@@ -0,0 +1,211 @@
+import os
+import unittest
+
+import matplotlib.pyplot as plt
+
+from tests.test_core.json_reader import JsonReader
+from metrics.wallet.dataframe.builder import CampaignDataFrameBuilder
+from metrics.wallet.figure.abstract_figure import DEFAULT_COLORS
+from metrics.wallet.figure.static_figure import StatTable, CactusMPL, BoxMPL, ScatterMPL, LINE_STYLES, ContributionTable
+
+
+class MyTestCase(unittest.TestCase):
+    STAT_TABLE_RESULT_NO_VBS = {
+        'CHS': {'common_count': 374,
+                'common_sum': 21656,
+                'count': 380,
+                'sum': 66237,
+                'total': 500,
+                'uncommon_count': 6},
+        'ExplorationLuby': {'common_count': 374,
+                            'common_sum': 22161,
+                            'count': 389,
+                            'sum': 38602,
+                            'total': 500,
+                            'uncommon_count': 15},
+        'WDegCAxCD': {'common_count': 374,
+                      'common_sum': 27034,
+                      'count': 381,
+                      'sum': 67638,
+                      'total': 500,
+                      'uncommon_count': 7}}
+
+    STAT_TABLE_RESULT_VBS = {
+        "CHS": {
+            "count": 380,
+            "sum": 66237,
+         'total': 500,
+            "common_count": 374,
+            "common_sum": 21656,
+            "uncommon_count": 6
+        },
+        "ExplorationLuby": {
+            "count": 389,
+            "sum": 38602,
+         'total': 500,
+            "common_count": 374,
+            "common_sum": 22161,
+            "uncommon_count": 15
+        },
+        "WDegCAxCD": {
+            "count": 381,
+            "sum": 67638,
+         'total': 500,
+            "common_count": 374,
+            "common_sum": 27034,
+            "uncommon_count": 7
+        },
+        "vbew": {
+            "count": 387,
+            "sum": 39302,
+         'total': 500,
+            "common_count": 374,
+            "common_sum": 15317,
+            "uncommon_count": 13
+        }
+    }
+
+    SUB_INPUT_SET = [
+        '/home/cril/wattez/XCSP17/MagicSquare/MagicSquare-m1-gp/MagicSquare-9-f10-20.xml.lzma',
+        '/home/cril/wattez/XCSP17/Wwtpp/Wwtpp-ord-s1/Wwtpp-ord-ex06460.xml.lzma',
+        '/home/cril/wattez/XCSP17/Hanoi/Hanoi-m1-s1/Hanoi-08.xml.lzma',
+        '/home/cril/wattez/XCSP17/QRandom/QRandom-bdd-18-21-2/bdd-18-21-2-133-78-06.xml.lzma',
+        '/home/cril/wattez/XCSP17/CryptoPuzzle/CryptoPuzzle-m1-s1/CryptoPuzzle-no-no-yes.xml.lzma',
+    ]
+
+    STAT_TABLE_RESULT_SUB_INPUT_SET = {
+        "CHS": {
+            "count": 5,
+            "sum": 130,
+         'total': 5,
+            "common_count": 5,
+            "common_sum": 130,
+            "uncommon_count": 0
+        },
+        "ExplorationLuby": {
+            "count": 5,
+            "sum": 23,
+         'total': 5,
+            "common_count": 5,
+            "common_sum": 23,
+            "uncommon_count": 0
+        },
+        "WDegCAxCD": {
+            "count": 5,
+            "sum": 47,
+         'total': 5,
+            "common_count": 5,
+            "common_sum": 47,
+            "uncommon_count": 0
+        }
+    }
+
+    SUB_XP_WARE_SET = [
+        'CHS',
+        'ExplorationLuby'
+    ]
+
+    STAT_TABLE_RESULT_SUB_XP_WARE_SET = {
+        "CHS": {
+            "count": 380,
+            "sum": 59037,
+         'total': 500,
+            "common_count": 380,
+            "common_sum": 26637,
+            "uncommon_count": 0
+        },
+        "ExplorationLuby": {
+            "count": 389,
+            "sum": 31402,
+         'total': 500,
+            "common_count": 380,
+            "common_sum": 25761,
+            "uncommon_count": 9
+        }
+    }
+
+    def setUp(self) -> None:
+        dirname = os.path.dirname(__file__)
+        filename = os.path.join(dirname, '../../data/xcsp17.json')
+        jr = JsonReader(filename)
+        self.campaign = jr.campaign
+
+    def test_stat_table_no_vbs(self):
+        campaign_df = CampaignDataFrameBuilder(self.campaign).build_from_campaign()
+        campaign_df.data_frame['success'] = campaign_df.data_frame.apply((lambda x: x['cpu_time'] < self.campaign.timeout), axis=1)
+        stat = StatTable(campaign_df)
+        self.assertEqual(self.STAT_TABLE_RESULT_NO_VBS, stat.get_figure().T.to_dict())
+
+    def test_stat_table_vbs(self):
+        cdfb = CampaignDataFrameBuilder(self.campaign).build_from_campaign().add_vbew(['CHS', 'WDegCAxCD'], 'cpu_time')
+        cdfb.data_frame['success'] = cdfb.data_frame.apply((lambda x: x['cpu_time'] < self.campaign.timeout), axis=1)
+        stat = StatTable(cdfb)
+        self.assertEqual(self.STAT_TABLE_RESULT_VBS, stat.get_figure().T.to_dict())
+
+    def test_stat_table_sub_set_inputs(self):
+        cdfb = CampaignDataFrameBuilder(self.campaign).build_from_campaign().sub_data_frame('input', self.SUB_INPUT_SET)
+        cdfb.data_frame['success'] = cdfb.data_frame.apply((lambda x: x['cpu_time'] < self.campaign.timeout), axis=1)
+        stat = StatTable(cdfb)
+        self.assertEqual(self.STAT_TABLE_RESULT_SUB_INPUT_SET,stat.get_figure().T.to_dict())
+
+    def test_stat_table_sub_set_xpware(self):
+        cdfb = CampaignDataFrameBuilder(self.campaign).build_from_campaign().sub_data_frame('experiment_ware', self.SUB_XP_WARE_SET)
+        cdfb.data_frame['success'] = cdfb.data_frame.apply((lambda x: x['cpu_time'] < self.campaign.timeout), axis=1)
+        stat = StatTable(cdfb)
+        self.assertEqual(stat.get_figure().T.to_dict(), self.STAT_TABLE_RESULT_SUB_XP_WARE_SET)
+
+    def test_contribution_table(self):
+        cdfb = CampaignDataFrameBuilder(self.campaign).build_from_campaign()
+        cdfb.data_frame['success'] = cdfb.data_frame.apply((lambda x: x['cpu_time'] < self.campaign.timeout), axis=1)
+        contrib = ContributionTable(cdfb, [0, 10, 100])
+        self.assertEqual('WDegCAxCD', contrib.get_figure().iloc[0].name)
+        self.assertEqual(168, contrib.get_figure().iloc[0].vbew_simple)
+
+    def test_static_cactus(self):
+        color_map = {
+            'CHS': DEFAULT_COLORS[0],
+            'WDegCAxCD': DEFAULT_COLORS[1],
+            'ExplorationLuby': DEFAULT_COLORS[2],
+            'vbew': DEFAULT_COLORS[3],
+        }
+
+        style_map = {
+            'CHS': LINE_STYLES[0],
+            'WDegCAxCD': LINE_STYLES[1],
+            'ExplorationLuby': LINE_STYLES[2],
+            'vbew': LINE_STYLES[3],
+        }
+
+        xp_ware_name_map = {
+            'CHS': 'ChS',
+            'WDegCAxCD': 'ca.cd',
+            'ExplorationLuby': 'expLuby',
+            'vbew': 'vbew',
+        }
+
+        cdfb = CampaignDataFrameBuilder(self.campaign).build_from_campaign()
+        cdfb.data_frame['success'] = cdfb.data_frame.apply((lambda x: x['cpu_time'] < self.campaign.timeout), axis=1)
+        cdfb = cdfb.add_vbew({'CHS', 'WDegCAxCD'}, opti_col='cpu_time')
+        cactus = CactusMPL(cdfb, x_min=300, cumulated=True, color_map=color_map, style_map=style_map, xp_ware_name_map=xp_ware_name_map)
+        cactus.get_figure()
+
+        #plt.savefig('cactus.pdf', transparent=True, bbox_inches='tight', figsize=(7, 2))
+        plt.show()
+
+    def test_box(self):
+        cdfb = CampaignDataFrameBuilder(self.campaign).build_from_campaign()
+        cdfb.data_frame['success'] = cdfb.data_frame.apply((lambda x: x['cpu_time'] < self.campaign.timeout), axis=1)
+        box = BoxMPL(cdfb)
+        box.get_figure()
+        plt.show()
+
+    def test_scatter(self):
+        cdfb = CampaignDataFrameBuilder(self.campaign).build_from_campaign()
+        cdfb.data_frame['success'] = cdfb.data_frame.apply((lambda x: x['cpu_time'] < self.campaign.timeout), axis=1)
+        scatter = ScatterMPL(cdfb, 'CHS', 'WDegCAxCD')
+        scatter.get_figure()
+        plt.show()
+
+
+if __name__ == '__main__':
+    unittest.main()
