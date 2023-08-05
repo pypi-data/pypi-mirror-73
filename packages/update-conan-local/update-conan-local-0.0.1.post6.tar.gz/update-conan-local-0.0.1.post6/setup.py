8b05f537f925fb8f30d5c67feeda1111ecf6d4b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:50 +0000

    Add package update-conan-local/0.0.1.post6/update-conan-local-0.0.1.post6.tar.gz

---
+++ packages/update-conan-local/update-conan-local-0.0.1.post6.tar.gz/update-conan-local-0.0.1.post6/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+  name = 'update-conan-local',    
+  packages = setuptools.find_packages(), 
+  version = '0.0.1-6',
+  license='MIT',        
+  description = 'Update Conan Local Cache',  
+  author = 'Wei Wan', 
+  author_email = 'emwanwei@gmail.com',
+  url = 'https://github.com/weiwan-gmail/update-conan-local',
+  download_url = 'https://github.com/user/weiwan-gmail/archive/v_01.tar.gz',
+  keywords = ['pypi', 'conan', 'cache'],
+  classifiers=[
+    'Development Status :: 1 - Planning',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+  ],
+  python_requires='>=3.6',
+)
