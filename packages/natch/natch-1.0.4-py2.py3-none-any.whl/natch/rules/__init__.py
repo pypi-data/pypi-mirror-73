c6301bfbeae5d68e5f53e805881033661e420e67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:10 +0000

    Add package natch/1.0.4/natch-1.0.4-py2.py3-none-any.whl

---
+++ packages/natch/natch-1.0.4-py2.py3-none-any.whl/natch/rules/__init__.py
@@ -0,0 +1,33 @@
+from natch.abstract import Rule
+from natch.rules.any import Any
+from natch.rules.any_of import AnyOf
+from natch.rules.all_of import AllOf
+from natch.rules.eq import Eq
+from natch.rules.neq import Neq
+from natch.rules.gt import Gt
+from natch.rules.gte import Gte
+from natch.rules.lt import Lt
+from natch.rules.lte import Lte
+from natch.rules.contains import Contains
+from natch.rules.not_contains import NotContains
+from natch.rules.condition import Condition
+from natch.rules.partial import Partial
+from natch.rules.pattern import Pattern
+
+
+__all__ = [
+    'Any',
+    'AnyOf',
+    'AllOf',
+    'Eq',
+    'Neq',
+    'Gt',
+    'Gte',
+    'Lt',
+    'Lte',
+    'Contains',
+    'NotContains',
+    'Condition',
+    'Partial',
+    'Pattern',
+]
---
+++ packages/natch/natch-1.0.4-py2.py3-none-any.whl/natch/rules/pattern.py
@@ -0,0 +1,34 @@
+from natch.abstract import Rule
+from natch.rules.eq import Eq
+
+
+class Pattern(Rule):
+
+    def __init__(self, *rules, **kwargs):
+        super(Pattern, self).__init__(*rules)
+
+    def set_args(self, args):
+        self.del_args()
+        for arg in args:
+            if not isinstance(arg, Rule):
+                arg = Eq(arg)
+            self.args.append(arg)
+
+    def does_match(self, *args, **kwargs):
+        len_args = len(args)
+        check_kwargs = bool(len(kwargs))
+        if check_kwargs:
+            len_args += 1
+        if len(self.args) != len_args:
+            return False
+        for rule_idx, arg in enumerate(args):
+            rule = self.args[rule_idx]
+            has_match = rule.does_match(arg)
+            if not has_match:
+                return False
+        if check_kwargs:
+            rule = self.args[len_args - 1]
+            has_match = rule.does_match(kwargs)
+            if not has_match:
+                return False
+        return True
