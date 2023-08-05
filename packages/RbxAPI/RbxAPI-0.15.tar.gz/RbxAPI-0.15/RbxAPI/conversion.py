3b3bb5fe116662f1f7bbea8f383d170c02be32ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:13:19 +0000

    Add package RbxAPI/0.15/RbxAPI-0.15.tar.gz

---
+++ packages/RbxAPI/RbxAPI-0.15.tar.gz/RbxAPI-0.15/RbxAPI/conversion.py
@@ -0,0 +1,17 @@
+from typing import NamedTuple
+
+
+class UserPresence(NamedTuple):
+    presence_type: str
+    last_location: str
+    place_id: int
+    root_place_id: int
+    game_id: str
+    universe_id: int
+    userid: int
+    last_online: str
+
+
+class GameVotes(NamedTuple):
+    upvotes: int
+    downvotes: int
