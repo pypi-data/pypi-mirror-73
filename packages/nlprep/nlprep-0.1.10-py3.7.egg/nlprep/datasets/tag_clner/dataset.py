37034bd2262b92651ca992b75089ee68363ec4d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:59 +0000

    Add package nlprep/0.1.10/nlprep-0.1.10-py3.7.egg

---
+++ packages/nlprep/nlprep-0.1.10-py3.7.egg/nlprep/datasets/tag_clner/dataset.py
@@ -0,0 +1,26 @@
+from nlprep.middleformat import MiddleFormat
+
+DATASET_FILE_MAP = {
+    "train": "https://raw.githubusercontent.com/lancopku/Chinese-Literature-NER-RE-Dataset/master/ner/train.txt",
+    "test": "https://raw.githubusercontent.com/lancopku/Chinese-Literature-NER-RE-Dataset/master/ner/test.txt",
+    "validation": "https://raw.githubusercontent.com/lancopku/Chinese-Literature-NER-RE-Dataset/master/ner/validation.txt",
+}
+TYPE = "tag"
+
+
+def toMiddleFormat(path):
+    dataset = MiddleFormat(TYPE)
+    with open(path, encoding='utf8') as f:
+        sent_input = []
+        sent_target = []
+        for i in list(f.readlines()):
+            i = i.strip()
+            if len(i) > 1:
+                sent, tar = i.split(' ')
+                sent_input.append(sent)
+                sent_target.append(tar)
+            else:
+                dataset.add_data(sent_input, sent_target)
+                sent_input = []
+                sent_target = []
+    return dataset
