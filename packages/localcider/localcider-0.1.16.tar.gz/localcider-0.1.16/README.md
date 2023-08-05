cdc5a6590218c517fa475aa0a756eaa5069568d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:59 +0000

    Add package localcider/0.1.16/localcider-0.1.16.tar.gz

---
+++ packages/localcider/localcider-0.1.16.tar.gz/localcider-0.1.16/README.md
@@ -0,0 +1,11 @@
+localCIDER
+==========
+
+localCIDER is the Python backend for CIDER
+
+For more information please 
+[see the documentation](http://pappulab.github.io/localCIDER/).
+
+localCIDER was written by Alex Holehouse and James Ahad in the [Pappu Lab](http://pappulab.wustl.edu/).
+
+Please address all questions to [Alex](http://pappulab.wustl.edu/people.html#grads). 
---
+++ packages/localcider/localcider-0.1.16.tar.gz/localcider-0.1.16/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+import sys
+
+extras = {}
+if sys.version_info >= (3, 0):
+    extras.update(
+            use_2to3=True,
+    )
+
+setup(
+    name='localcider',
+    version='0.1.16',
+    author='Alex Holehouse',
+    author_email='alex.holehouse@wustl.edu',
+    packages=['localcider', 'localcider.tests', 'localcider.backend', 'localcider.backend.data'],
+    scripts=[],
+    url='http://pappulab.github.io/localCIDER/',
+    license='LICENSE.txt',
+    description='Tools for calculating sequence properties of disordered proteins [from the Pappu Lab at Washington University in St. Louis]',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "numpy","matplotlib","scipy"],
+    **extras
+)
