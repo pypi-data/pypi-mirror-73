d1afa4908fedae51ff1736e7f80f3b6b81f9c0cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:43 +0000

    Add package fzj-ak-filenames/2.0/fzj-ak-filenames-2.0.tar.gz

---
+++ packages/fzj-ak-filenames/fzj-ak-filenames-2.0.tar.gz/fzj-ak-filenames-2.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+	name = 'fzj-ak-filenames',
+	version = '2.0',
+	author = 'Alexander Knieps',
+	author_email = 'a.knieps@fz-juelich.de',
+	description = 'Filename parser & writer for fusion device configuration-related files',
+	url = 'https://github.com/alexrobomind/ak_filenames',
+	py_modules = ['ak_filenames'],
+	install_requires = [
+		'textX>=2.1.0',
+	]
+);
+
