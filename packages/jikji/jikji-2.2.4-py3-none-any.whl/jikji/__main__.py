a8fe342b4015e5d1f10ceca8985844e36e5c29bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:05 +0000

    Add package jikji/2.2.4/jikji-2.2.4-py3-none-any.whl

---
+++ packages/jikji/jikji-2.2.4-py3-none-any.whl/jikji/__main__.py
@@ -0,0 +1,13 @@
+"""
+	Jikji/__main__
+	----------------
+	
+	:author: Prev(prevdev@gmail.com)
+	:license: MIT
+	
+"""
+
+if __name__ == '__main__' :
+	from . import cli
+	cli.main(as_module=True)
+
