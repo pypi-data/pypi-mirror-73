54e56577803cf20709d64818c9c740fa8ab141eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:40 +0000

    Add package ZahwJucie/1.1/ZahwJucie-1.1-py3-none-any.whl

---
+++ packages/ZahwJucie/ZahwJucie-1.1-py3-none-any.whl/juicezahw/drink_juice.py
@@ -0,0 +1,15 @@
+"""this is a documentaion for jucie."""
+
+
+def drink(juice):
+    """  
+    hack the jucie 
+
+    parameters : 
+        (juice) give me lemon
+
+    returns : 
+        str : baked jucie
+    """
+
+    print("Drinkes!!")
