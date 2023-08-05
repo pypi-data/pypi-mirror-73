c58c591cd05c6f74facae2fb7f5a449efa4fe578
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:31 +0000

    Add package shrtcodes/0.2.0/shrtcodes-0.2.0.tar.gz

---
+++ packages/shrtcodes/shrtcodes-0.2.0.tar.gz/shrtcodes-0.2.0/shrtcodes/shrtcodes.py
@@ -0,0 +1,55 @@
+import ast
+import re
+from typing import List, Dict, Callable
+
+
+def _find_shortcode_start(lines: List[str], handler_name: str):
+    regex = re.compile(r"{%\s*" + handler_name + r"(\s.*\s)?\s*%}")
+    for idx, line in enumerate(lines):
+        match = regex.match(line)
+        if match:
+            if match.group(1):
+                params = ast.literal_eval(f"[{match.group(1)}]")
+            else:
+                params = []
+            return idx, params
+    return -1, None
+
+
+def _find_shortcode_end(lines: List[str], handler_name: str):
+    regex = re.compile(r"{%\s*end" + handler_name + r"(\s.*\s)?\s*%}")
+    for idx, line in enumerate(lines):
+        match = regex.match(line)
+        if match:
+            return idx
+    return -1
+
+
+def make_process(
+    handlers: Dict[str, Callable[..., str]] = {},
+    block_handlers: Dict[str, Callable[..., str]] = {},
+):
+    def process(text: str):
+        lines = text.splitlines()
+
+        for handler_name, handler in handlers.items():
+            while True:
+                start_idx, params = _find_shortcode_start(lines, handler_name)
+                if start_idx < 0:
+                    break
+                lines = lines[:start_idx] + [handler(*params)] + lines[start_idx + 1 :]
+
+        for handler_name, handler in block_handlers.items():
+            while True:
+                start_idx, params = _find_shortcode_start(lines, handler_name)
+                if start_idx < 0:
+                    break
+                end_idx = _find_shortcode_end(lines, handler_name)
+                block = "\n".join(lines[start_idx + 1 : end_idx])
+                lines = (
+                    lines[:start_idx] + [handler(block, *params)] + lines[end_idx + 1 :]
+                )
+
+        return "\n".join(lines)
+
+    return process
