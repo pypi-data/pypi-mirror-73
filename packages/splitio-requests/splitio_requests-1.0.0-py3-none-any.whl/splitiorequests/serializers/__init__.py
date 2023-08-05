4a442f1e8d0ce6c574e744456b9d953861d87b3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:04 +0000

    Add package splitio-requests/1.0.0/splitio_requests-1.0.0-py3-none-any.whl

---
+++ packages/splitio-requests/splitio_requests-1.0.0-py3-none-any.whl/splitiorequests/serializers/__init__.py
@@ -0,0 +1,18 @@
+from .splits import (load_split, dump_split, load_splits, dump_splits, load_split_definition,
+                     dump_split_definition, load_split_definitions, dump_split_definitions)
+from .environments import load_environment, dump_environment, load_environments, dump_environments
+
+__all__ = [
+    'load_split',
+    'dump_split',
+    'load_splits',
+    'dump_splits',
+    'load_split_definition',
+    'dump_split_definition',
+    'load_split_definitions',
+    'dump_split_definitions',
+    'load_environment',
+    'dump_environment',
+    'load_environments',
+    'dump_environments',
+]
