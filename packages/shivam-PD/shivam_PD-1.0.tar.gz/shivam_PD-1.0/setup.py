86c56a79daca6656c630a46af895c6c14c8514ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:35 +0000

    Add package shivam-PD/1.0/shivam_PD-1.0.tar.gz

---
+++ packages/shivam-PD/shivam_PD-1.0.tar.gz/shivam_PD-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='shivam_PD',
+      version='1.0',
+      description='This python package is created to help user to perform complex mathematical operations in Probability Distribution.',
+      packages=['shivam_PD'],
+      author = 'Shivam Chaubey',
+      author_email = 'shivamchaubey83@gmail.com',
+      zip_safe=False)
