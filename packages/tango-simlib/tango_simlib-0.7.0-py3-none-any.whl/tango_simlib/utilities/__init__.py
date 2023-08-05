3689fa95798466dd33d0a7064836f9350048d0fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:29 +0000

    Add package tango-simlib/0.7.0/tango_simlib-0.7.0-py3-none-any.whl

---
+++ packages/tango-simlib/tango_simlib-0.7.0-py3-none-any.whl/tango_simlib/utilities/__init__.py
@@ -0,0 +1,9 @@
+
+# Automatically added by katversion
+__version__ = '0.6.0'
+
+# Automatically added by katversion
+__version__ = '0.6.1.dev866+master.ebac261'
+
+# Automatically added by katversion
+__version__ = '0.7.0'
