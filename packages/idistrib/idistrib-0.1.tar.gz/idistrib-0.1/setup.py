ed6b8f00c3ec6edfed1870606d5add9423fc13de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:56 +0000

    Add package idistrib/0.1/idistrib-0.1.tar.gz

---
+++ packages/idistrib/idistrib-0.1.tar.gz/idistrib-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='idistrib',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['idistrib'],
+      zip_safe=False)
