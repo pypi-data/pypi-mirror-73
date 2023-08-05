f9713076e557ddd26eee31cbae42a8fa56774533
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:55 +0000

    Add package leodistributions/0.2/leodistributions-0.2.tar.gz

---
+++ packages/leodistributions/leodistributions-0.2.tar.gz/leodistributions-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='leodistributions',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['leodistributions'],
+      zip_safe=False)
