ee9d85d8dcaa9af8a2c47f45cba5a58c1e862b24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:10 +0000

    Add package nrlmsise00/0.0.2rc1/nrlmsise00-0.0.2rc1-cp27-cp27m-manylinux2010_i686.whl

---
+++ packages/nrlmsise00/nrlmsise00-0.0.2rc1-cp27-cp27m-manylinux2010_i686.whl/nrlmsise00/__init__.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2019 Stefan Bender
+#
+# This module is part of pynrlmsise00.
+# pynrlmsise00 is free software: you can redistribute it or modify
+# it under the terms of the GNU General Public License as published
+# by the Free Software Foundation, version 2.
+# See accompanying LICENSE file or http://www.gnu.org/licenses/gpl-2.0.html.
+"""Python interface to the NRLMSISE-00 model
+
+"""
+__version__ = "0.0.2-rc1"
+
+from . import _nrlmsise00
+from .core import *
