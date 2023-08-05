29dc3824ab64f8ad2322a5bb0d68b5ef751f1587
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:19 +0000

    Add package custom-wordcloud-generator/0.4/custom_wordcloud_generator-0.4.tar.gz

---
+++ packages/custom-wordcloud-generator/custom_wordcloud_generator-0.4.tar.gz/custom_wordcloud_generator-0.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+from setuptools.extension import Extension
+
+setup(name='custom_wordcloud_generator',
+      version='0.4',
+      description='Slightly modified Muellers Wordcloud generator',
+      packages=['custom_wordcloud_generator'],
+      author_email='ekertdenis@gmail.com',
+      install_requires=['numpy>=1.6.1', 'pillow', 'matplotlib'],
+      ext_modules=[Extension("custom_wordcloud_generator.query_integral_image",
+                           ["custom_wordcloud_generator/query_integral_image.c"])],
+      zip_safe=False)
