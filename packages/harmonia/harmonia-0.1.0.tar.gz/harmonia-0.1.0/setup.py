62d7be5775d1fe0a91a4f0ea21281ce6a6feffbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:52 +0000

    Add package harmonia/0.1.0/harmonia-0.1.0.tar.gz

---
+++ packages/harmonia/harmonia-0.1.0.tar.gz/harmonia-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['harmonia']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'harmonia',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Ryan Eloff',
+    'author_email': 'ryan.peter.eloff@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=2.7,<3.0',
+}
+
+
+setup(**setup_kwargs)
