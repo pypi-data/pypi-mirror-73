234b20314f31eedc8aa08f472dc7493483f8a607
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:35:24 +0000

    Add package PyStarter/1.5.0/PyStarter-1.5.0-py2.py3-none-any.whl

---
+++ packages/PyStarter/PyStarter-1.5.0-py2.py3-none-any.whl/pystarter/pystarter.py
@@ -0,0 +1,23 @@
+def pystarterVersion():
+    return '1.5.0'
+
+
+def pystarterCommands():
+    return ('''
+Command list:
+
+pystarter create <option>
+
+option can be left blank
+
+Options you can use (do not use <> in command arguments):
+    <git> for git only projects
+    <python> for python only projects
+    <all> for both git and python projects (setup.py not included)
+    <setup> to include a setup.py template
+    or leave it blank for for projects with git and python
+
+pystarter clean
+
+Clean out pyc files and __pycache__ files
+           ''')
