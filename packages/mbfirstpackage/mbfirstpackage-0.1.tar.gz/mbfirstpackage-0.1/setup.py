cf0a4ceecb118fc669a471b0b16dd73bf04a3ed6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:28 +0000

    Add package mbfirstpackage/0.1/mbfirstpackage-0.1.tar.gz

---
+++ packages/mbfirstpackage/mbfirstpackage-0.1.tar.gz/mbfirstpackage-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='mbfirstpackage',
+      version='0.1',
+      description='My Gaussian and binomial distributions',
+      packages=['mbfirstpackage'],
+      zip_safe=False)
