53b9e8e823c21fdb17c9264f3cf8d6f0f8ecfa23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:47 +0000

    Add package gauss-and-binomial/1.0.0/gauss-and-binomial-1.0.0.tar.gz

---
+++ packages/gauss-and-binomial/gauss-and-binomial-1.0.0.tar.gz/gauss-and-binomial-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gauss-and-binomial',
+      version='1.0.0',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions'],
+      author = "Siddharth Notani",
+      author_email = "s.notani@outlook.com",
+    zip_safe=False)
