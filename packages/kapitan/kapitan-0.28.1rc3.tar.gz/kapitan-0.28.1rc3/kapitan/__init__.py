cd312bf4cb3987aaf16f39a42fff96906d508458
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:04 +0000

    Add package kapitan/0.28.1rc3/kapitan-0.28.1rc3.tar.gz

---
+++ packages/kapitan/kapitan-0.28.1rc3.tar.gz/kapitan-0.28.1rc3/kapitan/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Copyright 2019 The Kapitan Authors
+# SPDX-FileCopyrightText: 2020 The Kapitan Authors <kapitan-admins@googlegroups.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+import sys
+
+# Adding reclass to PYTHONPATH
+sys.path.insert(0, os.path.dirname(__file__) + "/reclass")
