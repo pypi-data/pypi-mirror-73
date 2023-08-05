9f1522ed27b701da7a843d9133cb7816696f16d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:18 +0000

    Add package validpar/1.0.2/validpar-1.0.2.tar.gz

---
+++ packages/validpar/validpar-1.0.2.tar.gz/validpar-1.0.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+import setuptools
+
+setup(
+    # 包名称
+    name = 'validpar',
+    # 版本
+    version = '1.0.2',
+    # 作者
+    author = '王哈哈',
+    # 作者邮箱
+    author_email = 'mail65656@163.com',
+    # 描述
+    description = '用于参数校验',
+    # 长描述
+    long_description = "用于做参数的校验",
+    # 让setuptools自动发现包
+    packages = setuptools.find_packages(),
+    # 平台
+    platforms = "any",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    # 依赖的包
+    install_requires = [
+        "validus"
+    ]
+)
