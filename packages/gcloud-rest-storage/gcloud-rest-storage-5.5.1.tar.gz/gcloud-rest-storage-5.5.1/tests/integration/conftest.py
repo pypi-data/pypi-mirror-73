f8207fc045eb5303d83c5ff1bfe07b96d33b6133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:25 +0000

    Add package gcloud-rest-storage/5.5.1/gcloud-rest-storage-5.5.1.tar.gz

---
+++ packages/gcloud-rest-storage/gcloud-rest-storage-5.5.1.tar.gz/gcloud-rest-storage-5.5.1/tests/integration/conftest.py
@@ -0,0 +1,20 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+from future import standard_library
+standard_library.install_aliases()
+import os
+
+import pytest
+
+
+@pytest.fixture(scope='module')  # type: ignore
+def bucket_name()       :
+    return 'dialpad-oss-public-test'
+
+
+@pytest.fixture(scope='module')  # type: ignore
+def creds()       :
+    # TODO: bundle public creds into this repo
+    return os.environ['GOOGLE_APPLICATION_CREDENTIALS']
