560bcc710066e891dfaa0d95a5ecc2c9a53738ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:14 +0000

    Add package hm-probability/1.0/hm_probability-1.0.tar.gz

---
+++ packages/hm-probability/hm_probability-1.0.tar.gz/hm_probability-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='hm_probability',
+      version='1.0',
+      description='Gaussian distributions',
+      author='Hardik Makwana during Udacity Course ',
+      packages=['hm_probability'],
+      zip_safe=False)
