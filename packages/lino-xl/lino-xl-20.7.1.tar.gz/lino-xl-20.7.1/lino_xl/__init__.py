f16696102a3d246d4f96e8cd17e85ac849f633c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:53 +0000

    Add package lino-xl/20.7.1/lino-xl-20.7.1.tar.gz

---
+++ packages/lino-xl/lino-xl-20.7.1.tar.gz/lino-xl-20.7.1/lino_xl/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2014-2019 Rumma & Ko Ltd
+#
+# License: BSD (see file COPYING for details)
+
+"""This package contains the code of the :ref:`xl`.
+
+See :ref:`specs.xl` for the list of plugins.
+
+.. autosummary::
+   :toctree:
+
+   lib
+
+"""
+
+import os
+
+fn = os.path.join(os.path.dirname(__file__), 'setup_info.py')
+exec(compile(open(fn, "rb").read(), fn, 'exec'))
+
+__version__ = SETUP_INFO['version']
+
+intersphinx_urls = dict(docs="http://xl.lino-framework.org")
+srcref_url = 'https://github.com/lino-framework/xl/blob/master/%s'
+doc_trees = ['docs']
