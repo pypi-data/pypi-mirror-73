14f6397ff6f4031fc2c12e5421554fcbcbfe3da1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:13:59 +0000

    Add package container-inspector/3.1.2/container_inspector-3.1.2-py2.py3-none-any.whl

---
+++ packages/container-inspector/container_inspector-3.1.2-py2.py3-none-any.whl/container_inspector/__init__.py
@@ -0,0 +1,41 @@
+# Copyright (c) nexB Inc. and others. All rights reserved.
+# http://nexb.com and https://github.com/nexB/container-inspector/
+#
+# This software is licensed under the Apache License version 2.0.#
+#
+# You may not use this software except in compliance with the License.
+# You may obtain a copy of the License at:
+#     http://apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import re
+
+__version__ = '3.1.2'
+
+REPOSITORIES_FILE = 'repositories'  # legacy v1
+
+MANIFEST_JSON_FILE = 'manifest.json'
+
+LAYER_VERSION_FILE = 'VERSION'
+LAYER_JSON_FILE = 'json'  # legacy v1
+LAYER_TAR_FILE = 'layer.tar'
+
+docker_version = re.compile('docker/([^\s]+)')
+
+EMPTY_SHA256 = 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
+EMPTY_DIGEST = 'sha256:' + EMPTY_SHA256
+
+
+def is_image_or_layer_id(s):
+    """
+    Return True if the string `s` looks like a layer ID e.g. a SHA256-like id.
+    """
+    return re(r'^[a-f0-9]{64}$', re.IGNORECASE).match(s)
+
