9e93a5f01f5cfab7fe412eeaf5b5fe07ba43df10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:05 +0000

    Add package nstap-probability/1.0/nstap-probability-1.0.tar.gz

---
+++ packages/nstap-probability/nstap-probability-1.0.tar.gz/nstap-probability-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='nstap-probability',
+      version='1.0',
+      description='Gaussian & Binomial distributions',
+      packages=['nstap-probability'],
+      author='Niall Stapleton',
+      author_email='niall.stapleton@gmail.com',
+      zip_safe=False)
