04181e97bd1d3a0e20c17d5f7e2c498e1641fb24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:08 +0000

    Add package yasirabd-probability/0.1/yasirabd_probability-0.1.tar.gz

---
+++ packages/yasirabd-probability/yasirabd_probability-0.1.tar.gz/yasirabd_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='yasirabd_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['yasirabd_probability'],
+      author='Yasir Abdur Rohman',
+      author_email='sir.yasirabd@gmail.com',
+      zip_safe=False)
