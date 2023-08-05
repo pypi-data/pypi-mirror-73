495c20e90be781e13fe80d92e40ca0732c599884
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:28 +0000

    Add package autopylogger/1.0/autopylogger-1.0.tar.gz

---
+++ packages/autopylogger/autopylogger-1.0.tar.gz/autopylogger-1.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="autopylogger",
+    packages = ['autopylogger'], 
+    version="1.0",
+    license='MIT',
+    description="Wrapper module for logging with out of box log rotation and critical errors mailing feature",
+    author="Rohit Lal @rohitlal125555",
+    author_email="rohitlal125555@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://rohitlal.in",
+    keywords = ['logging', 'autologging', 'pyautologging', 'python logger', 'log rotate', 'log rotation', 'log mailing'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.3',
+)
