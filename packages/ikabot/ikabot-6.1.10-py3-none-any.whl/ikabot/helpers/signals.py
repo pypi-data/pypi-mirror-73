ab7a347cf68d3dab13184d7b53b277efa6532d10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:45 +0000

    Add package ikabot/6.1.10/ikabot-6.1.10-py3-none-any.whl

---
+++ packages/ikabot/ikabot-6.1.10-py3-none-any.whl/ikabot/helpers/signals.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import signal
+import gettext
+from ikabot.config import *
+from ikabot.helpers.botComm import *
+
+t = gettext.translation('signals',
+                        localedir,
+                        languages=languages,
+                        fallback=True)
+_ = t.gettext
+
+def do_nothing(signal, frame):
+	pass
+
+def deactivate_sigint():
+	signal.signal(signal.SIGINT, do_nothing) #signal.SIGHUP replaced with signal.SIGINT for compatibility
+	signal.signal(signal.SIGINT, do_nothing)
+
+def create_handler(s):
+	def _handler(signum, frame):
+		raise Exception(_('Signal number {:d} received').format(signum))
+	return _handler
+
+def setSignalsHandlers(s):
+	signals = [signal.SIGINT, signal.SIGABRT] #signal.SIGQUIT replaced with signal.SIGINT for compatibility, SIGTERM removed at the end of list
+	for sgn in signals:
+		signal.signal(sgn, create_handler(s))
+
+def setInfoSignal(session, info): # send process info to bot
+	"""
+	Parameters
+	----------
+	session : ikabot.web.session.Session
+	info : str
+	"""
+	info = _('information of the process {}:\n{}').format(os.getpid(), info)
+	def _sendInfo(signum, frame):
+		sendToBot(session, info)
+	signal.signal(signal.SIGTERM, _sendInfo) # kill -SIGUSR1 pid, SIGUSR1 replaced with SIGTERM
