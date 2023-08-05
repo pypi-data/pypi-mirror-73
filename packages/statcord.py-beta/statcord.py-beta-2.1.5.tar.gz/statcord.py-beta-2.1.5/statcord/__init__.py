420eb9ea6856a956c1897b1093f3d9eecf5d9ab8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:11 +0000

    Add package statcord.py-beta/2.1.5/statcord.py-beta-2.1.5.tar.gz

---
+++ packages/statcord.py-beta/statcord.py-beta-2.1.5.tar.gz/statcord.py-beta-2.1.5/statcord/__init__.py
@@ -0,0 +1,15 @@
+__title__ = 'statcord.py-beta'
+__author__ = 'statcord.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2020, statcord.com'
+__version__ = '2.1.5'
+
+name = "statcord"
+
+from collections import namedtuple
+from .client import Client
+from .exceptions import *
+
+VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
+
+version_info = VersionInfo(major=2, minor=1, micro=5, releaselevel='final', serial=0)
