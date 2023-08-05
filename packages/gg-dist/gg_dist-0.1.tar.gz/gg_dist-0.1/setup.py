9857d80bae1d9d0687ba780e347adf83cddaa683
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:36 +0000

    Add package gg-dist/0.1/gg_dist-0.1.tar.gz

---
+++ packages/gg-dist/gg_dist-0.1.tar.gz/gg_dist-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gg_dist',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['gg_dist'],
+      author='Goutam Kumar Ghadai',
+      author_email='goutam.ghadai2199@gmail.com',
+      zip_safe=False)
