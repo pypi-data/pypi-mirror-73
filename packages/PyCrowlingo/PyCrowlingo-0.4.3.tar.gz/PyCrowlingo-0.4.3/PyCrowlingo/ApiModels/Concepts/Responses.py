d3ae61faadb3561a5cfb16416a5ef2dd25f46d44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:24 +0000

    Add package PyCrowlingo/0.4.3/PyCrowlingo-0.4.3.tar.gz

---
+++ packages/PyCrowlingo/PyCrowlingo-0.4.3.tar.gz/PyCrowlingo-0.4.3/PyCrowlingo/ApiModels/Concepts/Responses.py
@@ -0,0 +1,48 @@
+from typing import List
+
+from .Examples import Responses as Examples
+from ..Attributes import Concepts, ConceptId, LabelId, ConceptsId, LabelsId, LabelModel, ConceptModel
+
+
+class Extract(Examples.Extract, Concepts):
+    pass
+
+
+class CreateConcepts(Examples.CreateConcepts, ConceptsId):
+    pass
+
+
+class CreateLabels(Examples.CreateLabels, LabelsId):
+    pass
+
+
+class UpdateConcept(Examples.UpdateConcept, ConceptId):
+    pass
+
+
+class UpdateLabel(Examples.UpdateLabel, LabelId):
+    pass
+
+
+class DeleteLabel(Examples.DeleteLabel, LabelId):
+    pass
+
+
+class DeleteConcept(Examples.DeleteConcept, ConceptId):
+    pass
+
+
+class GetLabel(Examples.GetLabel, LabelModel):
+    pass
+
+
+class GetConcept(Examples.GetConcept, ConceptModel):
+    pass
+
+
+class ListLabels(Examples.ListLabels):
+    labels: List[LabelModel]
+
+
+class ListConcepts(Examples.ListConcepts):
+    concepts: List[ConceptModel]
