bb4949d94df78a4efb31c40f65f977a053501abe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:47 +0000

    Add package pymagicc/2.0.0rc4/pymagicc-2.0.0rc4.tar.gz

---
+++ packages/pymagicc/pymagicc-2.0.0rc4.tar.gz/pymagicc-2.0.0rc4/pymagicc/scenarios/__init__.py
@@ -0,0 +1,41 @@
+from copy import deepcopy
+from os.path import abspath, dirname, join
+
+from ..config import default_config
+from ..io import MAGICCData, read_scen_file
+
+# path to load files from included package
+_magicc6_included_distribution_path = dirname(default_config["EXECUTABLE_6"])
+
+
+rcp26 = read_scen_file(
+    join(_magicc6_included_distribution_path, "RCP26.SCEN"),
+    columns={"model": ["IMAGE"], "scenario": ["RCP26"]},
+)
+rcp45 = read_scen_file(
+    join(_magicc6_included_distribution_path, "RCP45.SCEN"),
+    columns={"model": ["MiniCAM"], "scenario": ["RCP45"]},
+)
+rcp60 = read_scen_file(
+    join(_magicc6_included_distribution_path, "RCP60.SCEN"),
+    columns={"model": ["AIM"], "scenario": ["RCP60"]},
+)
+rcp85 = read_scen_file(
+    join(_magicc6_included_distribution_path, "RCP85.SCEN"),
+    columns={"model": ["MESSAGE"], "scenario": ["RCP85"]},
+)
+
+rcps = deepcopy(rcp26)
+for rcp in [rcp45, rcp60, rcp85]:
+    rcps.append(rcp)
+
+zero_emissions = MAGICCData(
+    join(dirname(abspath(__file__)), "RCP3PD_EMISSIONS.DAT"),
+    columns={
+        "scenario": ["idealised"],
+        "model": ["unspecified"],
+        "climate_model": ["unspecified"],
+    },
+).filter(region="World")
+
+zero_emissions._data[:] = 0
