2d05b941ee493000b6dd8b41d8cfeaa6f6716c2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:27 +0000

    Add package fastapi-satella-metrics/1.0.1/fastapi_satella_metrics-1.0.1-py2.py3-none-any.whl

---
+++ packages/fastapi-satella-metrics/fastapi_satella_metrics-1.0.1-py2.py3-none-any.whl/fastapi_satella_metrics/prometheus_exporter.py
@@ -0,0 +1,25 @@
+import typing as tp
+
+import fastapi
+from satella.instrumentation.metrics import getMetric
+from satella.instrumentation.metrics.exporters import metric_data_collection_to_prometheus
+
+
+def PrometheusExporter(app: fastapi.FastAPI,
+                       extra_labels: tp.Optional[dict] = None,
+                       url: str = '/metrics') -> None:
+    labels = extra_labels or {}
+
+    @app.get(url)
+    def export_prometheus():
+        metric = getMetric()
+        metric_data = metric.to_metric_data()
+        for datum in metric_data.values:
+            if datum.internal:
+                metric_data.values.remove(datum)
+        if labels is not None:
+            metric_data.add_labels(labels)
+        return fastapi.Response(content=metric_data_collection_to_prometheus(metric_data),
+                                media_type='text/plain')
+
+
