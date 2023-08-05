459964132002e1e947311f1441c043fae13cc068
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:41 +0000

    Add package Grid2Op/1.1.0/Grid2Op-1.1.0.tar.gz

---
+++ packages/Grid2Op/Grid2Op-1.1.0.tar.gz/Grid2Op-1.1.0/grid2op/Converter/ToVect.py
@@ -0,0 +1,63 @@
+# Copyright (c) 2019-2020, RTE (https://www.rte-france.com)
+# See AUTHORS.txt
+# This Source Code Form is subject to the terms of the Mozilla Public License, version 2.0.
+# If a copy of the Mozilla Public License, version 2.0 was not distributed with this file,
+# you can obtain one at http://mozilla.org/MPL/2.0/.
+# SPDX-License-Identifier: MPL-2.0
+# This file is part of Grid2Op, Grid2Op a testbed platform to model sequential decision making in power systems.
+
+from grid2op.Converter.Converters import Converter
+
+
+class ToVect(Converter):
+    """
+    This converters allows to manipulate the vector representation of the actions and observations.
+
+    In this converter:
+
+    - `encoded_act` are numpy ndarray
+    - `transformed_obs` are numpy ndarray
+
+    """
+    def __init__(self, action_space):
+        Converter.__init__(self, action_space)
+        self.__class__ = ToVect.init_grid(action_space)
+        self.do_nothing_vect = action_space({}).to_vect()
+
+    def convert_obs(self, obs):
+        """
+        This converter will match the observation to a vector, using the
+        :func:`grid2op.Observation.BaseObservation.to_vect`
+        function.
+
+        Parameters
+        ----------
+        obs: :class:`grid2op.Observation.Observation`
+            The observation, that will be processed into a numpy ndarray vector.
+
+        Returns
+        -------
+        transformed_obs: ``numpy.ndarray``
+            The vector representation of the action.
+
+        """
+        return obs.to_vect()
+
+    def convert_act(self, encoded_act):
+        """
+        In this converter `encoded_act` is a numpy ndarray. This function transforms it back to a valid action.
+
+        Parameters
+        ----------
+        encoded_act: ``numpy.ndarray``
+            The action, representated as a vector
+
+        Returns
+        -------
+        regular_act: :class:`grid2op.Action.Action`
+            The corresponding action transformed with the :func:`grid2op.Action.BaseAction.from_vect`.
+
+        """
+        res = self.__call__({})
+        res.from_vect(encoded_act)
+        return res
---
+++ packages/Grid2Op/Grid2Op-1.1.0.tar.gz/Grid2Op-1.1.0/grid2op/Rules/PreventReconnection.py
@@ -0,0 +1,40 @@
+# Copyright (c) 2019-2020, RTE (https://www.rte-france.com)
+# See AUTHORS.txt
+# This Source Code Form is subject to the terms of the Mozilla Public License, version 2.0.
+# If a copy of the Mozilla Public License, version 2.0 was not distributed with this file,
+# you can obtain one at http://mozilla.org/MPL/2.0/.
+# SPDX-License-Identifier: MPL-2.0
+# This file is part of Grid2Op, Grid2Op a testbed platform to model sequential decision making in power systems.
+
+import numpy as np
+from grid2op.Rules.BaseRules import BaseRules
+
+
+class PreventReconnection(BaseRules):
+    """
+    A subclass is used to check that an action will not attempt to reconnect a powerlines disconnected because of
+    an overflow, or to check that 2 actions acting on the same powerline are distant from the right number of timesteps
+    (see :attr:`grid2op.Parameters.Parameters.NB_TIMESTEP_LINE_STATUS_REMODIF`) or if two topological modification
+    of the same substation are too close in time
+    (see :attr:`grid2op.Parameters.Parameters.NB_TIMESTEP_TOPOLOGY_REMODIF`)
+
+    """
+    def __call__(self, action, env):
+        """
+        This function check only that the action doesn't attempt to reconnect  a powerline that has been disconnected
+        due to an overflow.
+
+        See :func:`BaseRules.__call__` for a definition of the parameters of this function.
+
+        """
+        aff_lines, aff_subs = action.get_topological_impact()
+        if np.any(env.times_before_line_status_actionable[aff_lines] > 0):
+            # i tried to act on a powerline too shortly after a previous action
+            # or shut down due to an overflow or opponent or hazards or maintenance
+            return False
+
+        if np.any(env.times_before_topology_actionable[aff_subs] > 0):
+            # I tried to act on a topology too shortly after a previous action
+            return False
+
+        return True
