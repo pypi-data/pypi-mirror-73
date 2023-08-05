988c8969eda1299f46d3e8ff6d644d6c24c5cadf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:43 +0000

    Add package distributions-package-iamanshika/0.1/distributions_package_iamanshika-0.1.tar.gz

---
+++ packages/distributions-package-iamanshika/distributions_package_iamanshika-0.1.tar.gz/distributions_package_iamanshika-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_package_iamanshika',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_package_iamanshika'],
+      author = 'Anshika Gupta',
+      author_email = 'iamanshika19@gmail.com',
+      zip_safe=False)
