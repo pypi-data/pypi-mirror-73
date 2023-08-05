7927b85b861b6ae450ccbe0e462a403f58556e81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:50 +0000

    Add package pyps4-2ndscreen/1.1.1/pyps4_2ndscreen-1.1.1-py3-none-any.whl

---
+++ packages/pyps4-2ndscreen/pyps4_2ndscreen-1.1.1-py3-none-any.whl/pyps4_2ndscreen/ddp.py
@@ -0,0 +1,413 @@
+# -*- coding: utf-8 -*-
+"""Device Discovery Protocol for PS4."""
+from __future__ import print_function
+
+import re
+import socket
+import logging
+import select
+import asyncio
+import time
+from typing import Optional
+
+_LOGGER = logging.getLogger(__name__)
+
+BROADCAST_IP = '255.255.255.255'
+UDP_IP = '0.0.0.0'
+UDP_PORT = 0
+
+DDP_PORT = 987
+DDP_VERSION = '00020020'
+DDP_TYPE_SEARCH = 'SRCH'
+DDP_TYPE_LAUNCH = 'LAUNCH'
+DDP_TYPE_WAKEUP = 'WAKEUP'
+DDP_MSG_TYPES = (DDP_TYPE_SEARCH, DDP_TYPE_LAUNCH, DDP_TYPE_WAKEUP)
+
+DEFAULT_POLL_COUNT = 5
+
+DEFAULT_STANDBY_DELAY = 50
+
+STATUS_OK = 200
+STATUS_STANDBY = 620
+
+
+class DDPProtocol(asyncio.DatagramProtocol):
+    """Async UDP Client."""
+
+    def __init__(self, max_polls=DEFAULT_POLL_COUNT):
+        """Init Instance."""
+        super().__init__()
+        self.callbacks = {}
+        self.max_polls = max_polls
+        self._transport = None
+        self._remote_port = DDP_PORT
+        self._local_port = UDP_PORT
+        self._message = get_ddp_search_message()
+        self._standby_start = 0
+
+    def __repr__(self):
+        return (
+            "<{}.{} local_port={} max_polls={}>".format(
+                self.__module__,
+                self.__class__.__name__,
+                self.local_port,
+                self.max_polls,
+            )
+        )
+
+    def _set_write_port(self, port):
+        """Only used for tests."""
+        self._remote_port = port
+
+    def set_max_polls(self, poll_count: int):
+        """Set number of unreturned polls neeeded to assume no status."""
+        self.max_polls = poll_count
+
+    def connection_made(self, transport):
+        """On Connection."""
+        self._transport = transport
+        sock = self._transport.get_extra_info('socket')
+        self._local_port = sock.getsockname()[1]
+        _LOGGER.debug("PS4 Transport created with port: %s", self.local_port)
+
+    def send_msg(self, ps4, message=None):
+        """Send Message."""
+        # PS4 won't respond to polls right after standby
+        if self.polls_disabled:
+            elapsed = time.time() - self._standby_start
+            seconds = DEFAULT_STANDBY_DELAY - elapsed
+            _LOGGER.debug("Polls disabled for %s seconds", round(seconds, 2))
+            return
+        self._standby_start = 0
+        if message is None:
+            message = self._message
+        sock = self._transport.get_extra_info('socket')
+        _LOGGER.debug(
+            "SENT MSG @ DDP Proto SPORT=%s DEST=%s",
+            sock.getsockname()[1], (ps4.host, self._remote_port))
+        self._transport.sendto(
+            message.encode('utf-8'),
+            (ps4.host, self._remote_port))
+
+        # Track polls that were never returned.
+        ps4.poll_count += 1
+
+        # Assume PS4 is not available.
+        if ps4.poll_count > self.max_polls:
+            if not ps4.unreachable:
+                _LOGGER.info("PS4 @ %s is unreachable", ps4.host)
+                ps4.unreachable = True
+            ps4.status = None
+            if ps4.host in self.callbacks:
+                callback = self.callbacks[ps4.host].get(ps4)
+                if callback is not None:
+                    callback()
+
+    def datagram_received(self, data, addr):
+        """When data is received."""
+        if data is not None:
+            sock = self._transport.get_extra_info('socket')
+            _LOGGER.debug(
+                "RECV MSG @ DDP Proto DPORT=%s SRC=%s",
+                sock.getsockname()[1], addr)
+            self._handle(data, addr)
+
+    def _handle(self, data, addr):
+        data = parse_ddp_response(data.decode('utf-8'))
+        data[u'host-ip'] = addr[0]
+
+        address = addr[0]
+
+        if address in self.callbacks:
+            for ps4, callback in self.callbacks[address].items():
+                ps4.poll_count = 0
+                ps4.unreachable = False
+                old_status = ps4.status
+                ps4.status = data
+                if old_status != data:
+                    _LOGGER.debug("Status: %s", ps4.status)
+                    callback()
+                    # Status changed from OK to Standby/Turned Off
+                    if old_status is not None and \
+                            old_status.get('status_code') == STATUS_OK and \
+                            ps4.status.get('status_code') == STATUS_STANDBY:
+                        self._standby_start = time.time()
+                        _LOGGER.debug(
+                            "Status changed from OK to Standby."
+                            "Disabling polls for %s seconds",
+                            DEFAULT_STANDBY_DELAY)
+
+    def connection_lost(self, exc):
+        """On Connection Lost."""
+        if self._transport is not None:
+            _LOGGER.error("DDP Transport Closed")
+            self._transport.close()
+
+    def error_received(self, exc):
+        """Handle Exceptions."""
+        _LOGGER.warning("Error received at DDP Transport")
+
+    def close(self):
+        """Close Transport."""
+        self._transport.close()
+        self._transport = None
+        _LOGGER.debug(
+            "Closing DDP Transport: Port=%s",
+            self._local_port)
+
+    def add_callback(self, ps4, callback):
+        """Add callback to list. One per PS4 Object."""
+        if ps4.host not in self.callbacks:
+            self.callbacks[ps4.host] = {}
+        self.callbacks[ps4.host][ps4] = callback
+
+    def remove_callback(self, ps4, callback):
+        """Remove callback from list."""
+        if ps4.host in self.callbacks:
+            if self.callbacks[ps4.host][ps4] == callback:
+                self.callbacks[ps4.host].pop(ps4)
+
+                # If no callbacks remove host key also.
+                if not self.callbacks[ps4.host]:
+                    self.callbacks.pop(ps4.host)
+
+    @property
+    def local_port(self):
+        """Return local port."""
+        return self._local_port
+
+    @property
+    def remote_port(self):
+        """Return remote port."""
+        return self._remote_port
+
+    @property
+    def polls_disabled(self):
+        """Return true if polls disabled."""
+        elapsed = time.time() - self._standby_start
+        if elapsed < DEFAULT_STANDBY_DELAY:
+            return True
+        self._standby_start = 0
+        return False
+
+
+async def async_create_ddp_endpoint(sock=None):
+    """Create Async UDP endpoint."""
+    local_addr = (UDP_IP, UDP_PORT)
+    reuse_port = hasattr(socket, 'SO_REUSEPORT')
+    allow_broadcast = True
+    loop = asyncio.get_event_loop()
+    if sock is not None:
+        sock.settimeout(0)
+        local_addr = None
+        reuse_port = None
+        allow_broadcast = None
+    connect = loop.create_datagram_endpoint(
+        lambda: DDPProtocol(), local_addr=local_addr,  # noqa: pylint: disable=unnecessary-lambda
+        reuse_port=reuse_port, allow_broadcast=allow_broadcast, sock=sock)
+    transport, protocol = await loop.create_task(connect)
+    return transport, protocol
+
+
+def get_ddp_message(msg_type, data=None):
+    """Get DDP message."""
+    if msg_type not in DDP_MSG_TYPES:
+        raise TypeError(
+            "DDP MSG type: '{}' is not a valid type".format(msg_type))
+    msg = u'{} * HTTP/1.1\n'.format(msg_type)
+    if data is not None:
+        for key, value in data.items():
+            msg += '{}:{}\n'.format(key, value)
+    msg += 'device-discovery-protocol-version:{}\n'.format(DDP_VERSION)
+    return msg
+
+
+def parse_ddp_response(rsp):
+    """Parse the response."""
+    data = {}
+    app_name = None
+    for line in rsp.splitlines():
+        if 'running-app-name' in line:
+            app_name = line
+            app_name = app_name.replace('running-app-name:', '')
+        re_status = re.compile(r'HTTP/1.1 (?P<code>\d+) (?P<status>.*)')
+        line = line.strip()
+        # skip empty lines
+        if not line:
+            continue
+        if re_status.match(line):
+            data[u'status_code'] = int(re_status.match(line).group('code'))
+            data[u'status'] = re_status.match(line).group('status')
+        else:
+            values = line.split(':')
+            data[values[0]] = values[1]
+    if app_name is not None:
+        data['running-app-name'] = app_name
+    return data
+
+
+def get_ddp_search_message():
+    """Get DDP search message."""
+    return get_ddp_message('SRCH')
+
+
+def get_ddp_wake_message(credential):
+    """Get DDP wake message."""
+    data = {
+        'user-credential': credential,
+        'client-type': 'a',
+        'auth-type': 'C',
+    }
+    return get_ddp_message('WAKEUP', data)
+
+
+def get_ddp_launch_message(credential):
+    """Get DDP launch message."""
+    data = {
+        'user-credential': credential,
+        'client-type': 'a',
+        'auth-type': 'C',
+    }
+    return get_ddp_message('LAUNCH', data)
+
+
+def get_socket(timeout=3, port: Optional[int] = UDP_PORT):
+    """Return DDP socket object."""
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.settimeout(timeout)
+    try:
+        if port != UDP_PORT:
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        if hasattr(socket, "SO_REUSEPORT"):
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)  # noqa: pylint: disable=no-member
+        sock.bind((UDP_IP, port))
+    except socket.error as error:
+        _LOGGER.error(
+            "Error getting DDP socket with port: %s: %s", port, error)
+        sock = None
+    return sock
+
+
+def _send_recv_msg(host, broadcast, msg, receive=True, sock=None):
+    """Send a ddp message and receive the response."""
+    recv = None
+    if sock is None:
+        sock = get_socket()
+
+    if broadcast:
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        _host = host or BROADCAST_IP
+    else:
+        _host = host
+    _LOGGER.debug(
+        "SENT DDP MSG: SPORT=%s DEST=%s",
+        sock.getsockname()[1], (_host, DDP_PORT))
+    sock.sendto(msg.encode('utf-8'), (_host, DDP_PORT))
+
+    if receive:
+        available, _, _ = select.select([sock], [], [], 0.01)
+        if sock in available:
+            recv = sock.recvfrom(1024)
+            _LOGGER.debug(
+                "RECV DDP MSG: DPORT=%s SRC=%s",
+                sock.getsockname()[1], (_host, DDP_PORT))
+    sock.close()
+    return recv
+
+
+def _send_msg(host, broadcast, msg, sock=None):
+    """Send a ddp message."""
+    return _send_recv_msg(host, broadcast, msg, receive=False, sock=sock)
+
+
+def send_search_msg(host, sock=None):
+    """Send message only."""
+    msg = get_ddp_search_message()
+    return _send_msg(host, True, msg, sock=sock)
+
+
+def search(host=None, broadcast=True, sock=None):
+    """Discover PS4s."""
+    ps_list = None
+    msg = get_ddp_search_message()
+    data, addr = _send_recv_msg(host, broadcast, msg, sock=sock)
+    if data is not None:
+        ps_list = []
+        data = parse_ddp_response(data.decode('utf-8'))
+        data[u'host-ip'] = addr[0]
+        ps_list.append(data)
+    return ps_list
+
+
+def get_status(host, sock=None):
+    """Get status."""
+    try:
+        ps_list = search(host=host, sock=sock)
+    except TypeError:
+        return None
+    return ps_list[0]
+
+
+def wakeup(host, credential, broadcast=False, sock=None):
+    """Wakeup PS4s."""
+    msg = get_ddp_wake_message(credential)
+    _send_msg(host, broadcast, msg, sock)
+
+
+def launch(host, credential, broadcast=False, sock=None):
+    """Launch."""
+    msg = get_ddp_launch_message(credential)
+    _send_msg(host, broadcast, msg, sock)
+
+
+class Discovery:
+    """Device Discovery server."""
+
+    def __init__(self):
+        """Init."""
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.settimeout(6.0)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        self.msg = get_ddp_search_message()
+        self.host = '255.255.255.255'
+        self.ps_list = []
+
+    def search(self, host):
+        """Search for Devices."""
+        if host is None:
+            host = self.host
+        null_responses = 0
+        try:
+            self.send(host)
+        except (socket.error, socket.timeout):
+            self.sock.close()
+            return self.ps_list
+
+        while null_responses < 3:
+            try:
+                device = self.receive()
+                if device is not None:
+                    if device not in self.ps_list:
+                        self.ps_list.append(device)
+                        continue
+                null_responses += 1
+            except (socket.error, socket.timeout):
+                self.sock.close()
+                return self.ps_list
+
+        return self.ps_list
+
+    def send(self, host):
+        """Broadcast Message."""
+        self.sock.sendto(self.msg.encode('utf-8'), (host, DDP_PORT))
+
+    def receive(self):
+        """Receive Message."""
+        data = None
+        available, _, _ = select.select([self.sock], [], [], 0.01)
+        if self.sock in available:
+            data, addr = self.sock.recvfrom(1024)
+            if data is not None:
+                data = parse_ddp_response(data.decode('utf-8'))
+                data[u'host-ip'] = addr[0]
+        return data
---
+++ packages/pyps4-2ndscreen/pyps4_2ndscreen-1.1.1-py3-none-any.whl/pyps4_2ndscreen/media_art.py
@@ -0,0 +1,474 @@
+"""Media Art Functions."""
+import logging
+from ssl import SSLError
+import asyncio
+import html
+import re
+import urllib
+import aiohttp
+from aiohttp.client_exceptions import ContentTypeError
+
+from .errors import PSDataIncomplete
+
+_LOGGER = logging.getLogger(__name__)
+
+DEPRECATED_REGIONS = {'R1': 'en/us', 'R2': 'en/gb',
+                      'R3': 'en/hk', 'R4': 'en/au',
+                      'R5': 'en/in'}
+
+"""
+Excluded Countries:
+
+China: Store closed by Sony
+Phillipines: Store not available
+Serbia: Store not available
+Vietnam: Store not available
+"""
+
+COUNTRIES = {
+    "Argentina": "en/ar",
+    "Australia": "en/au",
+    "Austria": "de/at",
+    "Bahrain": "en/ae",
+    "Belgium": "fr/be",
+    "Brazil": "en/br",
+    "Bulgaria": "en/bg",
+    "Canada": "en/ca",
+    "Chile": "en/cl",
+    "Columbia": "en/co",
+    "Costa Rica": "es/cr",
+    "Croatia": "en/hr",
+    "Cyprus": "en/cy",
+    "Czech Republic": "en/cz",
+    "Denmark": "en/dk",
+    "Ecuador": "es/ec",
+    "El Salvador": "es/sv",
+    "Finland": "en/fi",
+    "France": "fr/fr",
+    "Germany": "de/de",
+    "Greece": "en/gr",
+    "Guatemala": "es/gt",
+    "Honduras": "es/hn",
+    "Hong Kong": "en/hk",
+    "Hungary": "en/hu",
+    "Iceland": "en/is",
+    "India": "en/in",
+    "Indonesia": "en/id",
+    "Ireland": "en/ie",
+    "Israel": "en/il",
+    "Italy": "it/it",
+    "Japan": "ja/jp",
+    "Korea": "ko/kr",
+    "Kuwait": "en/ae",
+    "Lebanon": "en/ae",
+    "Luxembourg": "de/lu",
+    "Maylasia": "en/my",
+    "Malta": "en/mt",
+    "Mexico": "en/mx",
+    "Middle East": "en/ae",
+    "Nederland": "nl/nl",
+    "New Zealand": "en/nz",
+    "Nicaragua": "es/ni",
+    "Norway": "en/no",
+    "Oman": "en/ae",
+    "Panama": "es/pa",
+    "Peru": "en/pe",
+    "Poland": "en/pl",
+    "Portugal": "pt/pt",
+    "Qatar": "en/ae",
+    "Romania": "en/ro",
+    "Russia": "ru/ru",
+    "Saudi Arabia": "en/sa",
+    "Singapore": "en/sg",
+    "Slovenia": "en/si",
+    "Slovakia": "en/sk",
+    "South Africa": "en/za",
+    "Spain": "es/es",
+    "Sweden": "en/se",
+    "Switzerland": "de/ch",
+    "Taiwan": "en/tw",
+    "Thailand": "en/th",
+    "Turkey": "en/tr",
+    "Ukraine": "ru/ua",
+    "United Arab Emirates": "en/ae",
+    "United States": "en/us",
+    "United Kingdom": "en/gb",
+}
+
+TYPE_LIST = {
+    'de': ['Vollversion', 'Spiel', 'PSN-Spiel', 'Paket', 'App'],
+    'en': ['Full Game', 'Game', 'PSN Game', 'Bundle', 'App'],
+    'es': ['Juego completo', 'Juego', 'Juego de PSN', 'Paquete', 'Aplicación'],
+    'fr': ['Jeu complet', 'Jeu', 'Jeu PSN', 'Offre groupée', 'App'],
+    'it': ['Gioco completo', 'Gioco', 'Gioco PSN', 'Bundle', 'App'],
+    'ja': ['ゲーム本編', 'ゲーム本編', 'DL専用ゲーム', 'ゲーム本編', 'アプリ'],
+    'ko': ['제품판', '게임', 'PSN 게임', '번들', '앱'],
+    'nl': ['Volledige game', 'game', 'PSN-game', 'Bundel', 'App'],
+    'pt': ['Jogo completo', 'jogo', 'Jogo da PSN', 'Pacote', 'Aplicação'],
+    'ru': ['Полная версия', 'Игра', 'Игра PSN', 'Комплект', 'Приложение'],
+}
+
+TYPE_APP = 'App'
+
+DEFAULT_HEADERS = {
+    'User-Agent':
+        'Mozilla/5.0 '
+        '(Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
+        '(KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36'
+}
+
+FORMATS = ['chars', 'chars+', 'orig', 'tumbler']
+
+BASE_IMAGE_URL = (
+    'https://store.playstation.com'
+    '/store/api/chihiro/00_09_000/container/US/en/999/'
+)
+
+LEGACY_URL = (
+    'https://store.playstation.com/'
+    'valkyrie-api/{0}/19/faceted-search/'
+    '{1}?query={1}&platform=ps4'
+)
+
+TUMBLER_URL = (
+    'https://store.playstation.com/valkyrie-api/{}/19/'
+    'tumbler-search/{}?suggested_size=9&mode=game'
+    '&platform=ps4'
+)
+
+PINNED_TITLES = {
+    'CUSA01780': {
+        'name': 'Spotify',
+        'sku_id': 'EP4950-CUSA01780_00-US00000000000000',
+        'url': '/1559667794000/image',
+        'type': 'App',
+    }
+}
+
+
+def _get_pinned_data(data: dict):
+    """Format pinned data as if retrieved from request."""
+    result_data = {
+        'name': data['name'],
+        'default-sku-id': data['sku_id'],
+        'game-content-type': data['type'],
+        'thumbnail-url-base': '{}{}{}'.format(
+            BASE_IMAGE_URL, data['sku_id'], data['url']),
+    }
+    return {'attributes': result_data}
+
+
+def get_pinned_item(data: dict):
+    """Return a pinned ResultItem using pinned data."""
+    result_data = _get_pinned_data(data)
+    item = ResultItem(result_data, TYPE_LIST['en'])
+    return item
+
+
+def get_region(region):
+    """Validate and format region."""
+    regions = COUNTRIES
+    d_regions = DEPRECATED_REGIONS
+
+    if region not in regions:
+        if region in d_regions:
+            _LOGGER.warning('Region: %s is deprecated', region)
+            return d_regions[region]
+        _LOGGER.error('Region: %s is not valid', region)
+        return None
+    return regions[region]
+
+
+def get_lang(region) -> str:
+    """Get language code from region."""
+    regions = COUNTRIES
+    lang = regions[region]
+    lang = lang.split('/')
+    lang = lang[0]
+    assert lang in TYPE_LIST.keys()
+    return lang
+
+
+def get_ps_store_url(title, region, reformat='chars', legacy=False):
+    """Get URL for title search in PS Store."""
+    if reformat == 'chars':  # No Special Chars.
+        title = re.sub(r'[^A-Za-z0-9\ ]+', '', title)
+    elif reformat == 'chars+':  # ignore ' and - and :
+        title = re.sub(r'[^A-Za-z0-9\-\'\: ]+', '', title)
+    elif reformat == 'tumbler':  # Sub special chars with space
+        title = re.sub(r'[^A-Za-z0-9\ ]+', ' ', title)
+        title = re.sub(r'[  ]+', ' ', title)  # Remove multiple spaces
+        title = re.sub(r' .*', '', title)
+    elif reformat == 'orig':
+        pass
+    title = html.escape(title.rstrip())
+    title = urllib.parse.quote(title.encode('utf-8'))
+    if legacy is True:
+        _url = LEGACY_URL.format(region, title)
+    else:
+        _url = TUMBLER_URL.format(region, title)
+
+    url = [_url, DEFAULT_HEADERS, region.split('/')[0]]
+    return url
+
+
+def _format_url(url):
+    """Format url for aiohttp."""
+    f_params = {}
+    url = url[0]
+    url = url.split('?')
+    params = url[1]
+    params = params.replace('?', '')
+    params = params.split('&')
+    for item in params:
+        item = item.split('=')
+        f_params[item[0]] = item[1]
+    url = url[0]
+    return url, f_params
+
+
+async def fetch(url, params, session):
+    """Get Request."""
+    try:
+        response = await session.get(url, params=params, timeout=3)
+        return await response.json()
+    except (asyncio.TimeoutError, ContentTypeError, SSLError):
+        return None
+
+
+async def async_get_ps_store_requests(title, region,
+                                      session: aiohttp.ClientSession) -> list:
+    """Return Title and Cover data with aiohttp."""
+    responses = []
+
+    for format_type in FORMATS:
+        _url = get_ps_store_url(
+            title, get_region(region), reformat=format_type, legacy=True)
+        url, params = _format_url(_url)
+
+        response = await fetch(url, params, session)
+        if response is not None:
+            responses.append(response)
+
+    for format_type in FORMATS:
+        _url = get_ps_store_url(
+            title, get_region(region), reformat=format_type, legacy=False)
+        url, params = _format_url(_url)
+
+        response = await fetch(url, params, session)
+        if response is not None:
+            responses.append(response)
+
+    return responses
+
+
+async def async_get_ps_store_requests_tumbler(
+        title, region, session: aiohttp.ClientSession) -> list:
+    """Perform tumbler search."""
+    responses = []
+    short_title = title.split(' ')
+    _title = short_title[0]
+    _url = get_ps_store_url(
+        _title, get_region(region), reformat='tumbler', legacy=False)
+    url, params = _format_url(_url)
+    _LOGGER.debug("Tumbler URL: %s", _url)
+
+    response = await fetch(url, params, session)
+    if response is not None:
+        responses.append(response)
+    return responses
+
+
+async def async_search_ps_store(
+        title: str, title_id: str, region: str, search_all: bool = False):
+    """Search PS Store for title data."""
+    # Check if title is a pinned title first and return.
+    pinned = None
+    pinned = PINNED_TITLES.get(title_id)
+    if pinned is not None:
+        return get_pinned_item(pinned)
+
+    # Conduct Search Requests.
+    searches = [
+        async_get_ps_store_requests,
+        async_get_ps_store_requests_tumbler,
+    ]
+    if search_all:
+        searches.append('all')
+    result_item = None
+    searching_all = False
+    _LOGGER.debug("Starting search request")
+
+    async with aiohttp.ClientSession() as session:
+        for search in searches:
+            if search == 'all':
+                searches.extend([_r for _r in COUNTRIES])
+                searching_all = True
+                _LOGGER.info(
+                    "Searching other regions; Result may be incorrect")
+                continue
+            if searching_all:
+                region = search
+                search = async_get_ps_store_requests_tumbler
+            await asyncio.sleep(0)
+            responses = await search(
+                title, region, session)
+
+            for response in responses:
+                try:
+                    result_item = parse_data(response, title_id, region)
+                except (TypeError, AttributeError):
+                    result_item = None
+                    raise PSDataIncomplete
+                if result_item is not None:
+                    break
+            if result_item is not None:
+                break
+        await session.close()
+    return result_item
+
+
+def parse_data(result, title_id, region):
+    """Filter through each item in search request."""
+    lang = get_lang(region)
+    item_list = []
+    type_list = TYPE_LIST[lang]
+    type_list.append(TYPE_APP)
+    parent_list = []
+
+    for item in result['included']:
+        item_list.append(ResultItem(item, type_list))
+
+    # Filter each item by prioritized type
+    for g_type in type_list:
+        for item in item_list:
+            if item.game_type == g_type:
+                if item.sku_id == title_id:
+                    _LOGGER.debug(
+                        "Item: %s, %s", item.name, item.sku_id)
+                    if not item.parent or item.parent.data is None:
+                        _LOGGER.info("Direct Match")
+                        return item
+                    parent_list.append(item.parent)
+
+    for item in parent_list:
+        if item.data is not None:
+            if item.sku_id == title_id:
+                _LOGGER.info("Parent Match")
+                return item
+    return None
+
+
+def parse_id(sku_id):
+    """Format SKU."""
+    try:
+        sku_id = sku_id.split("-")
+        sku_id = sku_id[1].split("_")
+        parsed_id = sku_id[0]
+        return parsed_id
+    except IndexError:
+        return None
+
+
+class ResultItem():
+    """Item object."""
+
+    def __init__(self, data, type_list):
+        """Init Class."""
+        self.type_list = type_list
+        self.data = data['attributes']
+
+    def __repr__(self):
+        return (
+            "<{}.{} name={} sku_id={} game_type={} parent={}>".format(
+                self.__module__,
+                self.__class__.__name__,
+                self.name,
+                self.sku_id,
+                self.game_type,
+                self.parent is not None,
+            )
+        )
+
+    @property
+    def name(self):
+        """Get Item Name."""
+        return self.data.get('name')
+
+    @property
+    def game_type(self):
+        """Get Game Type."""
+        game_type = self.data.get('game-content-type')
+        if game_type is not None:
+            if game_type == self.type_list[4]:
+                game_type = 'App'
+        return game_type
+
+    @property
+    def sku_id(self):
+        """Get SKU."""
+        sku_id = self.data.get('default-sku-id')
+        if sku_id is not None:
+            sku_id = parse_id(sku_id)
+        return sku_id
+
+    @property
+    def cover_art(self):
+        """Get Art URL."""
+        return self.data.get('thumbnail-url-base')
+
+    @property
+    def parent(self):
+        """Get Parents."""
+        parent = self.data.get('parent')
+        if self.game_type is not None and parent is not None \
+                and parent != 'null':
+            return ParentItem(parent, self.game_type)
+        return None
+
+
+class ParentItem():
+    """Item object."""
+
+    def __init__(self, data, game_type=None):
+        """Init Class."""
+        self.data = data
+        self._game_type = game_type
+
+    def __repr__(self):
+        return (
+            "<{}.{} name={} sku_id={} game_type={}>"
+            .format(
+                self.__module__,
+                self.__class__.__name__,
+                self.name,
+                self.sku_id,
+                self.game_type,
+            )
+        )
+
+    @property
+    def name(self):
+        """Parent Name."""
+        return self.data.get('name')
+
+    @property
+    def sku_id(self):
+        """Parent SKU."""
+        sku_id = self.data.get('id')
+        if sku_id is not None:
+            sku_id = parse_id(sku_id)
+        return sku_id
+
+    @property
+    def cover_art(self):
+        """Parent Art."""
+        url = self.data.get('url')
+        if url is not None:
+            url = "{}{}".format(url, "/image")
+        return url
+
+    @property
+    def game_type(self):
+        """Parent Game type."""
+        return self._game_type
