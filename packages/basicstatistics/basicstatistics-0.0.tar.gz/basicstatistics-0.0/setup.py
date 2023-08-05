e5390e80370cfe9ec3dfe4540b07c553f56694e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:20 +0000

    Add package basicstatistics/0.0/basicstatistics-0.0.tar.gz

---
+++ packages/basicstatistics/basicstatistics-0.0.tar.gz/basicstatistics-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='basicstatistics',
+      version='0.0',
+      description='Gaussian, Binomial and Normal distributions',
+      packages=['basicstatistics'],
+      author_email='sjahnaviprasad2000@gmail.com',
+      zip_safe=False)
