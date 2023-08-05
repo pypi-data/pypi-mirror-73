1a39ff26c704988aeb84f33af59a033aad5fd609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:23 +0000

    Add package equities/1.2.15/equities-1.2.15-py3-none-any.whl

---
+++ packages/equities/equities-1.2.15-py3-none-any.whl/equities/data/symbols/promise.py
@@ -0,0 +1,7 @@
+# This is a blank promise.py file. 
+# Please promise me that this dir and all of its contents will be included.
+# Thanks :)
+
+class promise(object):
+    def __init__():
+        self.promise=";)"
