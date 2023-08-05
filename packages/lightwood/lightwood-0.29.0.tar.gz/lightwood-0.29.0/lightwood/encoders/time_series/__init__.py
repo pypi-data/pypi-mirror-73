dfa9b2425120c5a73b9ebfec0779265e8f71b35b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:35 +0000

    Add package lightwood/0.29.0/lightwood-0.29.0.tar.gz

---
+++ packages/lightwood/lightwood-0.29.0.tar.gz/lightwood-0.29.0/lightwood/encoders/time_series/__init__.py
@@ -0,0 +1,12 @@
+from lightwood.encoders.time_series.ts_fresh_ts import TsFreshTsEncoder
+from lightwood.encoders.time_series.rnn import RnnEncoder
+
+#default = TsFreshTsEncoder
+default = RnnEncoder
+
+# Optional encoders
+try:
+    from lightwood.encoders.time_series.cesium_ts import CesiumTsEncoder
+    CesiumTsEncoder = CesiumTsEncoder
+except:
+    pass
