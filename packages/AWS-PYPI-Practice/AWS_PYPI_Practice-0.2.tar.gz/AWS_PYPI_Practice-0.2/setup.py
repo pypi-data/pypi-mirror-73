038e4476803262c7f06a529cf3bf8d975c5ac20c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:31 +0000

    Add package AWS-PYPI-Practice/0.2/AWS_PYPI_Practice-0.2.tar.gz

---
+++ packages/AWS-PYPI-Practice/AWS_PYPI_Practice-0.2.tar.gz/AWS_PYPI_Practice-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='AWS_PYPI_Practice',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author = "Ao Wang",
+      author_email = "aw3338@drexel.edu",
+      zip_safe=False)
