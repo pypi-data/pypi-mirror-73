8ddc9c4bf66e69963d6c20ce4012d4a5e776c3af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:27 +0000

    Add package okt-distributions/0.1/okt_distributions-0.1.tar.gz

---
+++ packages/okt-distributions/okt_distributions-0.1.tar.gz/okt_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='okt_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['okt_distributions'],
+      author='Oktay Ergun',
+      author_email='oergun45@gmail.com',
+      zip_safe=False)
