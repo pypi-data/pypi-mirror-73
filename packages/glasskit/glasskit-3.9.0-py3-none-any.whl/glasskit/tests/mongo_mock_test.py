0c74ad29d15be3a7d70009b79d174201905d01e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:54 +0000

    Add package glasskit/3.9.0/glasskit-3.9.0-py3-none-any.whl

---
+++ packages/glasskit/glasskit-3.9.0-py3-none-any.whl/glasskit/tests/mongo_mock_test.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+from ..uorm.db import db
+
+
+class MongoMockTest(TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        super().setUpClass()
+        db.configure({
+            "meta": {"uri": "mongodb://fake_server:27017/unittest_meta"},
+            "shards": {
+                "s1": {"uri": "mongodb://fake_server:27017/unittest_s1"},
+                "s2": {"uri": "mongodb://fake_server:27017/unittest_s2"},
+                "s3": {"uri": "mongodb://fake_server:27017/unittest_s3"},
+                "s4": {"uri": "mongodb://fake_server:27017/unittest_s4"},
+            }
+        }, mock=True)
---
+++ packages/glasskit/glasskit-3.9.0-py3-none-any.whl/glasskit/tests/test_validators.py
@@ -0,0 +1,129 @@
+from unittest import TestCase
+from ..uorm.models.fields import StringField, IntField, FloatField, ListField, DictField, BoolField
+from ..uorm.models.base_model import BaseModel
+from ..uorm.errors import ValidationError
+
+
+class TestValidators(TestCase):
+
+    def test_required(self):
+
+        class Model(BaseModel):
+            field: StringField(required=True)
+
+        model = Model()
+        self.assertRaises(ValidationError, model.validate)
+        model.field = "string"
+        model.validate()
+
+    def test_string_field(self):
+
+        class Model(BaseModel):
+            field: StringField()
+
+        model = Model({"field": 1})
+        self.assertRaises(ValidationError, model.validate)
+        model.field = "string"
+        model.validate()
+
+        model.field = "   auto-trimmed   "
+        self.assertEqual(model.field, "auto-trimmed")
+
+        class Model(BaseModel):
+            field: StringField(auto_trim=False)
+
+        model = Model({"field": "    no-auto-trim    "})
+        self.assertEqual(model.field, "    no-auto-trim    ")
+
+        class Model(BaseModel):
+            field: StringField(min_length=3, max_length=5, re_match="^\d+$")
+
+        model = Model({"field": "1"})
+        self.assertRaises(ValidationError, model.validate)
+        model.field = "123456"
+        self.assertRaises(ValidationError, model.validate)
+        model.field = "1234a"
+        self.assertRaises(ValidationError, model.validate)
+        model.field = "1234"
+        model.validate()
+
+    def test_list_field(self):
+        class Model(BaseModel):
+            field: ListField()
+
+        model = Model({"field": 1})
+        self.assertRaises(ValidationError, model.validate)
+        model.field = [1, 2, 3]
+        model.validate()
+
+        class Model(BaseModel):
+            field: ListField(min_length=3, max_length=5)
+
+        model = Model({"field": [1]})
+        self.assertRaises(ValidationError, model.validate)
+        model.field = [1, 2, 3, 4, 5, 6]
+        self.assertRaises(ValidationError, model.validate)
+        model.field = [1, 2, 3, 4]
+        model.validate()
+
+    def test_bool_field(self):
+
+        class Model(BaseModel):
+            field: BoolField()
+
+        model = Model({"field": 1})
+        self.assertRaises(ValidationError, model.validate)
+        model.field = False
+        model.validate()
+
+    def test_int_field(self):
+
+        class Model(BaseModel):
+            field: IntField()
+
+        model = Model({"field": "string"})
+        self.assertRaises(ValidationError, model.validate)
+        model.field = -34
+        model.validate()
+
+        class Model(BaseModel):
+            field: IntField(min_value=0, max_value=10)
+
+        model = Model({"field": -34})
+        self.assertRaises(ValidationError, model.validate)
+        model.field = 34
+        self.assertRaises(ValidationError, model.validate)
+        model.field = 5
+        model.validate()
+
+    def test_float_field(self):
+
+        class Model(BaseModel):
+            field: FloatField()
+
+        model = Model({"field": "string"})
+        self.assertRaises(ValidationError, model.validate)
+        model.field = -34
+        self.assertRaises(ValidationError, model.validate)
+        model.field = 28.0
+        model.validate()
+
+        class Model(BaseModel):
+            field: FloatField(min_value=0, max_value=10)
+
+        model = Model({"field": -15.5})
+        self.assertRaises(ValidationError, model.validate)
+        model.field = 15.5
+        self.assertRaises(ValidationError, model.validate)
+        model.field = 5.3
+        model.validate()
+
+    def test_dict_field(self):
+
+        class Model(BaseModel):
+            field: DictField()
+
+        model = Model({"field": 1})
+        self.assertRaises(ValidationError, model.validate)
+        model.field = {}
+        model.validate()
