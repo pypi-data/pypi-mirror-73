d89232c1e9783a532cbe40b00de61f99b7875cc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:05 +0000

    Add package PWrapper/0.0.3/PWrapper-0.0.3-py3-none-any.whl

---
+++ packages/PWrapper/PWrapper-0.0.3-py3-none-any.whl/PWrapper/__init__.py
@@ -0,0 +1,33 @@
+import requests
+
+__version__ = '0.0.1'
+
+session = None
+
+# User settable options
+API_KEY = None
+API_PATH = 'http://politicsandwar.com/api'
+def load(**kwargs):
+    if 'key' in kwargs:
+        global API_KEY
+        API_KEY = kwargs['key']
+
+    if 'test_server' in kwargs:
+        global API_PATH
+        if kwargs['test_server'] == True:
+            API_PATH = 'http://test.politicsandwar.com/api'
+        else:
+            API_PATH = 'http://politicsandwar.com/api'
+    else:
+        API_PATH = 'http://politicsandwar.com/api'
+
+    # Create a requests session and set API key if applicable
+    global session
+    session = requests.Session()
+
+def init(**kwargs):
+    load(**kwargs)
+
+# Imports
+from .nation import Nation
+from .alliance import Alliance
