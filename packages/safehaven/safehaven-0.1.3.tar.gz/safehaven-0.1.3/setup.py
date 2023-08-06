4fc1b289cf8bf15e55f8fe26de3b47d939a855c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:08 +0000

    Add package safehaven/0.1.3/safehaven-0.1.3.tar.gz

---
+++ packages/safehaven/safehaven-0.1.3.tar.gz/safehaven-0.1.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='UTF-8') as f:
+	readme = f.read()
+
+setup(
+	name='safehaven',
+	version='0.1.3',
+	author="Tzankov",
+    author_email="tzankotz@hotmail.com",
+	description='Library to list, upload and download objects from AWS S3, Azure Blob Storage, and GCP Storage.',
+	keywords="aws gcp azure s3 azureblob gcpblob blob cloud storage cloudstorage",
+	long_description=readme,
+	url="https://github.com/tzankov/safehaven",
+	install_requires=[
+		'boto3==1.12.31',
+		'azure-storage-blob==12.3.0',
+		'google-cloud-storage==1.25.0'
+		],
+	packages=['safehaven'],
+	python_requires=">=3.6"
+	)
