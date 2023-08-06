aed16004096d9a6ad6eaee7e7afeac2f1b6782d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:36 +0000

    Add package lahiru-distributions/0.1/lahiru-distributions-0.1.tar.gz

---
+++ packages/lahiru-distributions/lahiru-distributions-0.1.tar.gz/lahiru-distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='lahiru-distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      author = 'Lahiru Senevirathne',
+      author_email = 'lahiru.16@cse.mrt.ac.lk',
+      packages=['lahiru-distributions'],
+      zip_safe=False)
