9b8b817e2130dfc6607d3699cad75068bffc9064
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:32 +0000

    Add package gitutor/0.1.53/gitutor-0.1.53-py3-none-any.whl

---
+++ packages/gitutor/gitutor-0.1.53-py3-none-any.whl/gitutor/cli.py
@@ -0,0 +1,29 @@
+import click
+import git
+from init.commands import init
+
+
+@click.group()
+@click.pass_context
+def cli(ctx):
+    """ Git the easy way """
+    #Check ctx was initialized
+    ctx.ensure_object(dict)
+    
+    if ctx.invoked_subcommand != 'init':
+        try:
+            repo = git.Repo(".", search_parent_directories=True)
+            ctx.obj['REPO'] = repo
+            print( f"Location {repo.working_tree_dir}" )
+            print(f"Remote from init: {repo.remote('origin').url} ")
+        except:
+            print("not git repo")
+            exit()
+
+cli.add_command(init)
+
+def main():
+    cli(obj={})
+
+if __name__ == '__main__':
+    cli(obj={})
