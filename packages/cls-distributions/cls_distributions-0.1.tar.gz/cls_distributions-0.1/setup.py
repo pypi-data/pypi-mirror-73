e8753894a01d533b96d81b234fe3df3e572e8473
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:48 +0000

    Add package cls-distributions/0.1/cls_distributions-0.1.tar.gz

---
+++ packages/cls-distributions/cls_distributions-0.1.tar.gz/cls_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='cls_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['cls_distributions'],
+      author = "Mo Shabahang",
+      author_email = "dymax1010@gmail.com", 
+      zip_safe=False)
