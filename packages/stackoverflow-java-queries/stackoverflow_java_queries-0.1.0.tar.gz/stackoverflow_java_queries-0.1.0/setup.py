64e4aa7e13317da0c2f30310bd1b54fad837b1ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:20 +0000

    Add package stackoverflow-java-queries/0.1.0/stackoverflow_java_queries-0.1.0.tar.gz

---
+++ packages/stackoverflow-java-queries/stackoverflow_java_queries-0.1.0.tar.gz/stackoverflow_java_queries-0.1.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="stackoverflow_java_queries",
+    version="0.1.0",
+    author="Ariel",
+    author_email="arielblobstein@gmail.com",
+    description="Stack Overflow java queries",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ArielBlG/stackoverflow_java_queries",
+    packages=setuptools.find_packages(),
+    install_requires=['pandas','javalang','google-cloud-bigquery',],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
