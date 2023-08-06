2dbfad2327f1da28394f6ebf2a02a743c0a858ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:47 +0000

    Add package pygan/1.0.9/pygan-1.0.9-py3-none-any.whl

---
+++ packages/pygan/pygan-1.0.9-py3-none-any.whl/pygan/noisesampler/sine_wave_sampler.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+from pygan.noise_sampler import NoiseSampler
+
+
+class SineWaveSampler(NoiseSampler):
+    '''
+    Generate samples based on the noise prior by sine wave distribution.
+    '''
+
+    def __init__(
+        self,
+        batch_size,
+        seq_len,
+        amp=0.5,
+        sampling_freq=8000,
+        freq=440,
+        sec=5,
+        mu=0.0,
+        sigma=1.0,
+        norm_mode="z_score"
+    ):
+        '''
+        Init.
+
+        Args:
+            batch_size:                     Batch size.
+            seq_len:                        The length of sequences.
+            amp:                            Amp.
+            sample_freq:                    Sample frequency.
+            freq:                           Frequency.
+            sec:                            Second.
+            mu:                             Mean of Gauss noise.
+            sigma:                          STD of Gauss noise.
+            norm_mode:                      How to normalize pixel values of images.
+                                            - `z_score`: Z-Score normalization.
+                                            - `min_max`: Min-max normalization.
+                                            - `tanh`: Normalization by tanh function.
+
+        '''
+        self.__batch_size = batch_size
+        self.__seq_len = seq_len
+        self.__amp = amp
+        self.__sampling_freq = sampling_freq
+        self.__freq = freq
+        self.__sec = sec
+        self.__mu = mu
+        self.__sigma = sigma
+
+    def generate(self):
+        '''
+        Generate noise samples.
+        
+        Returns:
+            `np.ndarray` of samples.
+        '''
+        observed_arr = None
+        for row in range(self.__batch_size):
+            arr = self.__generate_sin(
+                amp=self.__amp,
+                sampling_freq=self.__sampling_freq,
+                freq=self.__freq,
+                sec=self.__sec,
+                seq_len=self.__seq_len
+            )
+            arr = np.expand_dims(arr, axis=0)
+
+            if observed_arr is None:
+                observed_arr = arr
+            else:
+                observed_arr = np.r_[observed_arr, arr]
+
+        observed_arr = np.expand_dims(observed_arr, axis=-1)
+        gauss_noise = np.random.normal(loc=self.__mu, scale=self.__sigma, size=observed_arr.shape)
+        return observed_arr + gauss_noise
+
+    def __generate_sin(self, amp=0.5, sampling_freq=8000, freq=440, sec=5, seq_len=100):
+        sin_list = []
+        for n in np.arange(sampling_freq * sec):
+            sin = amp * np.sin(2.0 * np.pi * freq * n / sampling_freq)
+            sin_list.append(sin)
+            if len(sin_list) > seq_len:
+                break
+        return np.array(sin_list[:seq_len])
---
+++ packages/pygan/pygan-1.0.9-py3-none-any.whl/pygan/truesampler/image_sampler.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+from pygan.true_sampler import TrueSampler
+from pydbm.cnn.featuregenerator.image_generator import ImageGenerator
+
+
+class ImageSampler(TrueSampler):
+    '''
+    Sampler which draws samples from the `true` distribution of images.
+    '''
+
+    def __init__(
+        self,
+        batch_size,
+        image_dir,
+        seq_len=None,
+        gray_scale_flag=True,
+        wh_size_tuple=(100, 100),
+        norm_mode="z_score"
+    ):
+        '''
+        Init.
+
+        Args:
+            batch_size:                     Batch size.
+            image_dir:                      Dir path which stores image files.
+            seq_len:                        The length of one sequence.
+            gray_scale_flag:                Gray scale or not(RGB).
+            wh_size_tuple:                  Tuple(`width`, `height`).
+            norm_mode:                      How to normalize pixel values of images.
+                                            - `z_score`: Z-Score normalization.
+                                            - `min_max`: Min-max normalization.
+                                            - `tanh`: Normalization by tanh function.
+
+        '''
+        self.__feature_generator = ImageGenerator(
+            epochs=1,
+            batch_size=batch_size,
+            training_image_dir=image_dir,
+            test_image_dir=image_dir,
+            seq_len=seq_len,
+            gray_scale_flag=gray_scale_flag,
+            wh_size_tuple=wh_size_tuple,
+            norm_mode=norm_mode
+        )
+
+    def draw(self):
+        '''
+        Draws samples from the `true` distribution.
+        
+        Returns:
+            `np.ndarray` of samples.
+        '''
+        observed_arr = None
+        for result_tuple in self.__feature_generator.generate():
+            observed_arr = result_tuple[0]
+            break
+        return observed_arr
