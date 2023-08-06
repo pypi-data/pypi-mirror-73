4484b29d68c312df13c0f9ce6ccd6ce7eef53946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:20 +0000

    Add package natdabprob/0.1/natdabprob-0.1.tar.gz

---
+++ packages/natdabprob/natdabprob-0.1.tar.gz/natdabprob-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='natdabprob',
+      version='0.1',
+      description='Binomial and Gaussian distributions',
+      packages=['natdabprob'],
+      zip_safe=False)
