adbdcad8f42c7bdea84a0d569479a84a14b187e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:04 +0000

    Add package macross-serial/1.0.8/macross-serial-1.0.8.tar.gz

---
+++ packages/macross-serial/macross-serial-1.0.8.tar.gz/macross-serial-1.0.8/packages/macross_serial/multiprocessing.py
@@ -0,0 +1,26 @@
+import multiprocessing
+import queue
+from typing import Union
+
+import ipcq
+
+from .util import Singleton
+
+
+class DummyQueueManagerClient(ipcq.QueueManagerClient):
+    def get_queue(self, *args, **kwargs) -> 'DummyQueue':
+        return DummyQueue()
+
+
+class DummyQueue(queue.Queue):
+    def put(self, *args, **kwargs):
+        pass
+
+
+class ProgressNotifier(metaclass=Singleton):
+    def __init__(self, path: str):
+        self.qmc: Union[DummyQueueManagerClient, ipcq.QueueManagerClient] = \
+            ipcq.QueueManagerClient(path, authkey=ipcq.AuthKey.DEFAULT) if path else DummyQueueManagerClient(path)
+
+    def notify(self, progress: str):
+        self.qmc.get_queue().put(progress)
