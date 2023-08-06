fc51ed4be2f9d84716b09376b329507678d00bdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:31 +0000

    Add package laptools/0.2.4/laptools-0.2.4.tar.gz

---
+++ packages/laptools/laptools-0.2.4.tar.gz/laptools-0.2.4/src/laptools/__init__.py
@@ -0,0 +1,17 @@
+"""Top-level package for laptools."""
+
+# Do not edit this string manually, always use bumpversion
+# Details in CONTRIBUTING.md
+__version__ = "0.2.4"
+
+__author__ = "Jacob Moorman"
+__email__ = "jacob@moorman.me"
+
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2020, Jacob Moorman"
+
+from _augment import _solve
+
+from . import clap, lap
+
+__all__ = ["clap", "lap", "_solve"]
