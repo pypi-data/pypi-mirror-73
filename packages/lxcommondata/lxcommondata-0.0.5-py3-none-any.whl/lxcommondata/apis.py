8de8737c3fd23893a2d07cfc89c8ef16669f3e4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:53 +0000

    Add package lxcommondata/0.0.5/lxcommondata-0.0.5-py3-none-any.whl

---
+++ packages/lxcommondata/lxcommondata-0.0.5-py3-none-any.whl/lxcommondata/apis.py
@@ -0,0 +1,23 @@
+import json
+
+from . import commondata_pb2, commondata_pb2_grpc
+from .client import Client
+
+con = Client.get_conn()
+
+# 调用 rpc 服务
+db_interface_stub = commondata_pb2_grpc.DBInterfaceStub(con)
+
+def do_query(query_obj):
+    """
+    :param query_json:
+    :return: status, data
+    """
+    query_json = json.dumps(query_obj)
+    ret = db_interface_stub.query_db(commondata_pb2.query_params(params=query_json))
+    ret_data = json.loads(ret.data)
+
+    status = ret_data['status']
+    data = json.loads(ret_data['data_json'])
+   
+    return status, data
