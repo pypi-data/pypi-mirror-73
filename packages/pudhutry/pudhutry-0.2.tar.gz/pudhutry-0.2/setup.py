c987a6b7d0b3c762229f9b116395162286a44486
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:13 +0000

    Add package pudhutry/0.2/pudhutry-0.2.tar.gz

---
+++ packages/pudhutry/pudhutry-0.2.tar.gz/pudhutry-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='pudhutry',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['pudhutry'],
+      zip_safe=False)
