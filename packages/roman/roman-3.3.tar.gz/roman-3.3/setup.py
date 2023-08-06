a2c697b8e6168141b01f77bef705e7c4cf28f413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:53 +0000

    Add package roman/3.3/roman-3.3.tar.gz

---
+++ packages/roman/roman-3.3.tar.gz/roman-3.3/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup
+
+
+desc = ('%s\n\n%s' % (open('README.rst').read(), open('CHANGES.txt').read()))
+
+setup(
+    name='roman',
+    version='3.3',
+    author="Mark Pilgrim",
+    author_email="f8dy@diveintopython.org",
+    description="Integer to Roman numerals converter",
+    long_description=desc,
+    long_description_content_type='text/x-rst',
+    license="Python 2.1.1",
+    keywords="roman",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'License :: OSI Approved :: Python Software Foundation License',
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Operating System :: OS Independent'],
+    url='https://github.com/zopefoundation/roman',
+    package_dir={"": "src"},
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
+    py_modules=["roman"],
+    include_package_data=True,
+    test_suite='tests',
+    zip_safe=True,
+    entry_points={
+        'console_scripts': [
+            'roman=roman:main',
+        ]
+    }
+)
---
+++ packages/roman/roman-3.3.tar.gz/roman-3.3/src/tests.py
@@ -0,0 +1,83 @@
+import os
+import sys
+import unittest
+
+
+if sys.version_info[0] > 2:
+    from io import StringIO
+else:
+    from StringIO import StringIO
+
+import roman
+
+
+TEST_MAP = ((0, 'N'), (1, 'I'), (3, 'III'), (4, 'IV'), (9, 'IX'), (14, 'XIV'),
+            (19, 'XIX'), (24, 'XXIV'), (40, 'XL'), (49, 'XLIX'), (90, 'XC'),
+            (99, 'XCIX'), (400, 'CD'), (490, 'CDXC'), (499, 'CDXCIX'),
+            (900, 'CM'), (990, 'CMXC'), (998, 'CMXCVIII'), (999, 'CMXCIX'),
+            (2013, 'MMXIII'))
+
+if sys.version_info[0] > 2:
+    _str = str
+else:
+    _str = unicode  # NOQA: F821
+
+
+class TestRoman(unittest.TestCase):
+
+    def test_toRoman(self):
+        for num_arabic, num_roman in TEST_MAP:
+            self.assertEqual(roman.toRoman(num_arabic), num_roman,
+                             '%s should be %s' % (num_arabic, num_roman))
+
+    def test_toRoman_errors(self):
+        self.assertRaises(roman.OutOfRangeError, roman.toRoman, 100000)
+        self.assertRaises(roman.NotIntegerError, roman.toRoman, '1')
+
+    def test_fromRoman(self):
+        for num_arabic, num_roman in TEST_MAP:
+            self.assertEqual(roman.fromRoman(num_roman), num_arabic,
+                             '%s should be %s' % (num_roman, num_arabic))
+
+    def test_fromRoman_errors(self):
+        self.assertRaises(
+            roman.InvalidRomanNumeralError, roman.fromRoman, '')
+        self.assertRaises(
+            roman.InvalidRomanNumeralError, roman.fromRoman, 'Q12')
+
+    def test_parse_args(self):
+        sys.argv = ['roman', '10']
+        args = roman.parse_args()
+        self.assertFalse(args.reverse)
+        self.assertEqual(args.number, '10')
+
+    def test_main_toRoman(self):
+        for num_arabic, num_roman in TEST_MAP:
+            sys.argv = ['roman', _str(num_arabic)]
+            sys.stdout = StringIO()
+            ex_st = roman.main()
+            output = sys.stdout.getvalue().strip()
+            self.assertEqual(output, num_roman)
+            self.assertEqual(ex_st, os.EX_OK)
+
+    def test_main_fromRoman(self):
+        for num_arabic, num_roman in TEST_MAP:
+            sys.argv = ['roman', '--reverse', num_roman]
+            sys.stdout = StringIO()
+            ex_st = roman.main()
+            output = sys.stdout.getvalue().strip()
+            self.assertEqual(output, _str(num_arabic))
+            self.assertEqual(ex_st, os.EX_OK)
+
+    def test_main_fromRoman_caseInsensitive(self):
+        for num_arabic, num_roman in TEST_MAP:
+            sys.argv = ['roman', '--reverse', num_roman.lower()]
+            sys.stdout = StringIO()
+            ex_st = roman.main()
+            output = sys.stdout.getvalue().strip()
+            self.assertEqual(output, _str(num_arabic))
+            self.assertEqual(ex_st, os.EX_OK)
+
+
+def test_suite():
+    return unittest.makeSuite(TestRoman)
