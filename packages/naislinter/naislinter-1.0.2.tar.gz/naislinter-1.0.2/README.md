f68552e104a3b7e5e9977f173483530a96ae6e06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:15 +0000

    Add package naislinter/1.0.2/naislinter-1.0.2.tar.gz

---
+++ packages/naislinter/naislinter-1.0.2.tar.gz/naislinter-1.0.2/README.md
@@ -0,0 +1,42 @@
+# naislinter
+
+A simple Python script to validate that your NAIS yaml file adheres to the NAIS specifications.
+
+## Local installation
+
+```bash
+$ pip install naislinter
+```
+
+## Local usage
+
+```bash
+$ naislinter nais.yaml
+```
+
+Will print any path in the config tree that falls outside of the spec and return a non-zero code if any were encountered.
+
+Returns 0 if the file is valid according to the NAIS specifications.
+
+
+## Limitations
+
+This does not validate value types, this is not in scope as `nais/deploy` verifies types and rejects
+a deployment if the NAIS yaml file is malformed in this fashion.
+
+Additionally, this tool requires a fully formed NAIS yaml file, after any template injections.
+A file containing `{{ image }}` will therefore fail, unless the variable has been injected.
+
+Finally, in its current state, the tool is unable to verify objects within lists. For instance:
+
+```yaml
+spec:
+    env:
+        - name: KEY
+          value: value
+        - name: ANOTHER_KEY
+          value: another_value
+          key_outside_of_spec: hello!
+```
+
+`key_outside_of_spec` will not be caught as an error since it's in an object within a list.
---
+++ packages/naislinter/naislinter-1.0.2.tar.gz/naislinter-1.0.2/naislinter/__init__.py
@@ -0,0 +1,55 @@
+import yaml
+
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+    from yaml import Loader, Dumper
+
+import argparse
+import requests
+import sys
+from naislinter import linter
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        "naislinter", description="Validates naiserator yaml files"
+    )
+    parser.add_argument("input", type=str, help="The input naiserator yaml file")
+
+    args = parser.parse_args()
+    return args.input
+
+
+def fetch_reference():
+    return yaml.load(
+        requests.get(
+            "https://raw.githubusercontent.com/nais/naiserator/master/config/nais.io_applications.yaml"
+        ).text,
+        Loader=Loader,
+    )["spec"]["validation"]["openAPIV3Schema"]
+
+
+def get_target(infile):
+    with open(infile, "r") as f:
+        contents = f.read()
+        return yaml.load(contents, Loader=Loader)
+
+
+def main():
+    infile = parse_args()
+    target = get_target(infile)
+    reference = fetch_reference()
+    faults = linter.check_keys(target, reference, [], [])
+    if len(faults) > 0:
+        print("Found keys outside of the NAIS spec", file=sys.stderr)
+        for fault in faults:
+            print(f"\t{fault}", file=sys.stderr)
+        print(
+            "You may want to refer to https://doc.nais.io/nais-application/nais.yaml/reference"
+        )
+        exit(1)
+
+
+if __name__ == "__main__":
+    main()
