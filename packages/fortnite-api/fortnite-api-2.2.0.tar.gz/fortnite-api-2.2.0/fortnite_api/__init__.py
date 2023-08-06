ff3169fa8c42a4b768fba0d067e6fa6499d86c38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:38 +0000

    Add package fortnite-api/2.2.0/fortnite-api-2.2.0.tar.gz

---
+++ packages/fortnite-api/fortnite-api-2.2.0.tar.gz/fortnite-api-2.2.0/fortnite_api/__init__.py
@@ -0,0 +1,13 @@
+__version__ = '2.2.0'
+
+from .account import *
+from .aes import *
+from .api import FortniteAPI
+from .cosmetics import *
+from .creator_code import *
+from .endpoints import *
+from .enums import *
+from .errors import *
+from .news import *
+from .shop import *
+from .stats import *
