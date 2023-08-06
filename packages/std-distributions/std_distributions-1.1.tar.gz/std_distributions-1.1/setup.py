5fb4945c672591c40cda9d16fe1e244dbc03dd7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:56 +0000

    Add package std-distributions/1.1/std_distributions-1.1.tar.gz

---
+++ packages/std-distributions/std_distributions-1.1.tar.gz/std_distributions-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='std_distributions',
+      version='1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['std_distributions'],
+      author = 'Mutahar Majid',
+      author_email = 'mutaharmajid243@gmail.com',
+      zip_safe=False)
