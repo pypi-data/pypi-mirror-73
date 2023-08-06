44b6b480e4ec571fcd1d0091ec484ac30a78d832
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:06 +0000

    Add package chibi/0.10.0/chibi-0.10.0.tar.gz

---
+++ packages/chibi/chibi-0.10.0.tar.gz/chibi-0.10.0/chibi/file/other/chibi_python.py
@@ -0,0 +1,25 @@
+import sys
+import importlib.util
+import copy
+import json
+import importlib
+from chibi.file import Chibi_file
+from chibi.atlas import _wrap
+from chibi.module import import_
+
+
+__all__ = [ 'Chibi_python' ]
+
+
+class Chibi_python( Chibi_file ):
+    def read( self ):
+        value = super().read()
+        return value
+
+    def import_( self ):
+        name = self.file_name[:-3]
+        spec = importlib.util.spec_from_file_location(
+            name, str( self.path.inflate ) )
+        module = importlib.util.module_from_spec( spec )
+        spec.loader.exec_module( module )
+        return module
