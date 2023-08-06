02abf3a721f3d53d05e53cccdc0563f21aff8091
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:09 +0000

    Add package gilp/0.0.1rc2/gilp-0.0.1rc2.tar.gz

---
+++ packages/gilp/gilp-0.0.1rc2.tar.gz/gilp-0.0.1rc2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gilp",
+    version="0.0.1-rc-2",
+    author="Henry Robbins",
+    author_email="hwr26@cornell.edu",
+    description="A Python package for visualizing the geometry of linear programs.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/henryrobbins/gilp.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'numpy', 'typing', 'scipy', 'plotly'
+    ],
+    python_requires='>=3.6',
+)
