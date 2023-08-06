fda25c31866f01720c066430b578d7b5104e152f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:07 +0000

    Add package wbia-utool/3.0.2/wbia-utool-3.0.2.tar.gz

---
+++ packages/wbia-utool/wbia-utool-3.0.2.tar.gz/wbia-utool-3.0.2/run_doctests.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+xdoctest -m utool --style=google all
---
+++ packages/wbia-utool/wbia-utool-3.0.2.tar.gz/wbia-utool-3.0.2/run_tests.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+if __name__ == '__main__':
+    import pytest
+    import sys
+
+    package_name = 'utool'
+    pytest_args = [
+        '--cov-config',
+        '.coveragerc',
+        '--cov-report',
+        'html',
+        '--cov-report',
+        'term',
+        '--xdoctest',
+        '--cov=' + package_name,
+        package_name,
+    ]
+    pytest_args = pytest_args + sys.argv[1:]
+    pytest.main(pytest_args)
+
+
+# OLD UTOOL TESTER (still works)
+# if __name__ == '__main__':
+#     import utool.tests.run_tests
+#     import multiprocessing
+#     multiprocessing.freeze_support()
+#     ut.change_term_title('RUN UTOOL TESTS')
+#     retcode = utool.tests.run_tests.run_tests()
+#     sys.exit(retcode)
