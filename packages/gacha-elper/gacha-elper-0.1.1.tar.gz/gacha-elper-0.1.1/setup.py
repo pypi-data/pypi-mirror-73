a19b53d6b8b7982489309d04cbb8a58839442a4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:44 +0000

    Add package gacha-elper/0.1.1/gacha-elper-0.1.1.tar.gz

---
+++ packages/gacha-elper/gacha-elper-0.1.1.tar.gz/gacha-elper-0.1.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open('README.md') as fh:
+    long_description = fh.read()
+long_description_content_type="text/markdown"
+
+setuptools.setup(
+    name='gacha-elper',
+    version='0.1.1',
+    author='cytopz',
+    author_email='cytopz@protonmail.com',
+    description='A small utility to help automate mobile (Android) video games',
+    long_description=long_description,
+    url='https://github.com/cytopz/gacha-elper',
+    packages=setuptools.find_packages(),
+    install_requires=['opencv-python', 'scipy', 'numpy'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3 :: Only",
+        "Operating System :: OS Independent",
+        "Topic :: Games/Entertainment",
+        "License :: OSI Approved :: MIT License"
+    ],
+    python_requires='>=3.6'
+)
