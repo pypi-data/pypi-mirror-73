ca771c5296183fb433a0455512491fc1ff97d57a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:46 +0000

    Add package relocate/1.2.0/relocate-1.2.0.tar.gz

---
+++ packages/relocate/relocate-1.2.0.tar.gz/relocate-1.2.0/relocate/relocate.py
@@ -0,0 +1,34 @@
+import os
+import time
+
+
+def relocateNow():
+    try:
+        source = os.getcwd()
+        for path,dir1,files in os.walk(source):
+            if files and path==source:
+                for file in files:
+                    print(file)
+                    try:
+                        parts = file.split(".")
+                        file_type = parts[-1].upper()
+                        source_folder = os.path.join(path,file)
+                        ctime = time.ctime(os.path.getctime(source_folder)).split(" ")
+                        file_time = ""+ctime[1].upper()+" "+ctime[-1]
+                        target_folder = os.path.join(source,"RELOCATED")
+                        if not os.path.exists(target_folder):
+                            os.mkdir(target_folder)
+                        target_folder = os.path.join(target_folder, file_time)
+                        if not os.path.exists(target_folder):
+                            os.mkdir(target_folder)
+                        target_folder = os.path.join(target_folder, file_type)
+                        if not os.path.exists(target_folder):
+                            os.mkdir(target_folder)
+                        target_folder = os.path.join(target_folder, file)
+                        if not os.path.isfile(target_folder):
+                            os.rename(source_folder , target_folder)
+                    except:
+                        print("Failed to relocate "+file)
+        print("ALL FILES RELOCATED")
+    except Exception as e:
+        print(e)
---
+++ packages/relocate/relocate-1.2.0.tar.gz/relocate-1.2.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='relocate',
+      version='1.2.0',
+      license='MIT',
+      author='Sandip Palit',
+      author_email='sandippalit009@gmail.com',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      description='Relocate will organise your files in separate folders, according to month of creation and then according to extensions.',
+      packages=['relocate'],
+      python_requires='>=3.6',
+      zip_safe=False)
