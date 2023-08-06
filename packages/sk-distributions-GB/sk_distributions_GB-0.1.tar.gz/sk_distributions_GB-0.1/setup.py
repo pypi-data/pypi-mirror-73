e759744149a5ab87f2465a332460e3ab024bb619
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:19 +0000

    Add package sk-distributions-GB/0.1/sk_distributions_GB-0.1.tar.gz

---
+++ packages/sk-distributions-GB/sk_distributions_GB-0.1.tar.gz/sk_distributions_GB-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='sk_distributions_GB',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['sk_distributions_GB'],
+      zip_safe=False)
