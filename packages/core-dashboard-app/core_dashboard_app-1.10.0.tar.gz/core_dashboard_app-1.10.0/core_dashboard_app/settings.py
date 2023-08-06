bdd004e5ba523799da4e419a189aa487637e07a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:47 +0000

    Add package core-dashboard-app/1.10.0/core_dashboard_app-1.10.0.tar.gz

---
+++ packages/core-dashboard-app/core_dashboard_app-1.10.0.tar.gz/core_dashboard_app-1.10.0/core_dashboard_app/settings.py
@@ -0,0 +1,12 @@
+"""
+    Django settings for core_dashboard_app app
+"""
+
+from django.conf import settings
+
+if not settings.configured:
+    settings.configure()
+
+SERVER_URI = getattr(settings, "SERVER_URI", "http://localhost")
+
+INSTALLED_APPS = getattr(settings, "INSTALLED_APPS", [])
