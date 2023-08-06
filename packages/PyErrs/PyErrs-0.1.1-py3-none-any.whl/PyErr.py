8620ba09fecc3ec9f2e01b72e16796afbf39eb56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:38 +0000

    Add package PyErrs/0.1.1/PyErrs-0.1.1-py3-none-any.whl

---
+++ packages/PyErrs/PyErrs-0.1.1-py3-none-any.whl/PyErr.py
@@ -0,0 +1,23 @@
+class NumberError(TypeError):
+    pass
+
+class LessThanZeroError(NumberError):
+    pass
+
+class MoreThanZeroError(NumberError):
+    pass
+
+class EqualToZeroError(NumberError):
+    pass
+
+class NotLessThanZeroError(NumberError):
+    pass
+
+class NotMoreThanZeroError(NumberError):
+    pass
+
+class NotEqualToZeroError(NumberError):
+    pass
+
+class ArgError(TypeError):
+    pass
