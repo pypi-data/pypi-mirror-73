d6a9f4c8c464a5a545f9956ae515e46385076fde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:11 +0000

    Add package proton-client/0.0.3/proton-client-0.0.3.tar.gz

---
+++ packages/proton-client/proton-client-0.0.3.tar.gz/proton-client-0.0.3/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from distutils.core      import setup
+from distutils.extension import Extension
+
+
+long_description = '''
+
+This package, originally forked from python-srp module implements a simple
+wrapper to the Proton Technologies API, abstracting from the SRP authentication.
+'''
+
+setup(name             = 'proton-client',
+      version          = '0.0.3',
+      description      = 'Proton Technologies API wrapper',
+      author           = 'Proton Technologies',
+      author_email     = 'contact@protonmail.com',
+      url              = 'https://github.com/ProtonMail/proton-python-client',
+      long_description = long_description,
+      install_requires = ['six', 'bcrypt', 'gnupg'],
+      packages         = ['proton'],
+      package_data     = {'proton' : ['doc/*.rst', 'doc/*.py']},
+      license          = "MIT",
+      platforms        = "OS Independent",
+      classifiers      = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python',
+        'Topic :: Security',
+        ],)
