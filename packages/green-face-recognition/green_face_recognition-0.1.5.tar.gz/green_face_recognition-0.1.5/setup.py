950e45d8b9179f3bd9efd9a73d4f7de05b20e92f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:54 +0000

    Add package green-face-recognition/0.1.5/green_face_recognition-0.1.5.tar.gz

---
+++ packages/green-face-recognition/green_face_recognition-0.1.5.tar.gz/green_face_recognition-0.1.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+import setuptools
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+name='green_face_recognition',
+version='0.1.5',
+description='HungLV package',
+url='https://github.com/leviethung2103/greenlab_ai_utils',
+author='Hung LV',
+long_description= long_description,
+long_description_content_type="text/markdown",
+author_email='leviethung1280@gmail.com',
+license='MIT',
+packages=setuptools.find_packages(),
+package_data={'cutils':['Aller_Bd.ttf']},
+# packages=['personDect','headPose','personPose','videos','custom_configs','unitest','mtcnn','cutils'],
+classifiers=[
+     "Programming Language :: Python :: 3",
+     "License :: OSI Approved :: MIT License",
+     "Operating System :: OS Independent",
+ ],
+ python_requires='>=3.6',
+zip_safe=False)
