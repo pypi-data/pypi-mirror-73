259729a86647dfe825816cd06d4656ade76350c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:14 +0000

    Add package videomass/2.9.0/videomass-2.9.0.tar.gz

---
+++ packages/videomass/videomass-2.9.0.tar.gz/videomass-2.9.0/videomass3/vdms_sys/app_const.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-#
+#
+# Author:  Werner F. Bruhin
+# Purpose: Application constants
+# Created: 06/04/2012
+
+import wx
+
+# language domain
+langDomain = "videomass"
+# languages you want to support
+supLang = {"en": wx.LANGUAGE_ENGLISH,
+           "it": wx.LANGUAGE_ITALIAN,
+           "fr": wx.LANGUAGE_FRENCH,
+           "de": wx.LANGUAGE_GERMAN,
+           "pl": wx.LANGUAGE_POLISH,
+           "es": wx.LANGUAGE_SPANISH,
+           }
