16f37d0894828d84372adf37140d17b065c87943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:38 +0000

    Add package moda/0.3.0/moda-0.3.0.tar.gz

---
+++ packages/moda/moda-0.3.0.tar.gz/moda-0.3.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+# read the contents of the README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+    #print(long_description)
+
+setup(
+    name='moda',
+    long_description='Tools for analyzing trending topics',
+    long_description_content_type='text/markdown',
+    version='0.3.0',
+    packages=['moda.dataprep',
+              'moda.evaluators',
+              'moda.models'
+              ],
+    url='https://www.github.com/omri374/moda',
+    license='MIT',
+    author='Omri Mendels',
+    author_email='omri.mendels@microsoft.com',
+    description='Tools for analyzing trending topics',
+    install_requires=['numpy', 'pandas', 'stldecompose', 'statsmodels==0.10.2', 'comet_ml', 'requests', 'matplotlib', 'pytest',
+                      'scikit_learn==0.23.0', 'pytest-cov']
+
+)
