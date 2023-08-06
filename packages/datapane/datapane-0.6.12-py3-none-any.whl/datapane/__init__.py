3a9504e56a5464d91e5ace4129240f4ada82d339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:28 +0000

    Add package datapane/0.6.12/datapane-0.6.12-py3-none-any.whl

---
+++ packages/datapane/datapane-0.6.12-py3-none-any.whl/datapane/__init__.py
@@ -0,0 +1,27 @@
+# flake8: noqa F401
+try:
+    from ._version import __rev__
+except ImportError:
+    # NOTE - could use subprocess to get from git?
+    __rev__ = "local"
+
+__version__ = "0.6.12"
+
+# Public API re-exports
+from .client.api import (
+    Asset,
+    Blob,
+    Markdown,
+    Params,
+    PivotTable,
+    Plot,
+    Report,
+    Result,
+    Run,
+    Script,
+    Table,
+    Variable,
+    by_datapane,
+    init,
+    on_datapane,
+)
