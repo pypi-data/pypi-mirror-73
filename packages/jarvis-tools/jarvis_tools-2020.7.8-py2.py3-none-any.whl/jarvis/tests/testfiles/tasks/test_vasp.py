b187b023fab4c2e410ac37b9533c7fdcacd54e95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:01 +0000

    Add package jarvis-tools/2020.7.8/jarvis_tools-2020.7.8-py2.py3-none-any.whl

---
+++ packages/jarvis-tools/jarvis_tools-2020.7.8-py2.py3-none-any.whl/jarvis/tests/testfiles/tasks/test_vasp.py
@@ -0,0 +1,16 @@
+import os
+
+vasp_path = os.path.join(
+    os.path.dirname(__file__), "..", "..", "..", "examples", "vasp", "SiOptb88"
+)
+
+cwd = str(os.getcwd())
+
+
+def test_vasp():
+    os.chdir(vasp_path)
+    cmd = str("python master.py")
+    os.system(cmd)
+
+
+os.chdir(cwd)
