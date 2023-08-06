6348d17bb9693ce69400b310ca31fc7ba1e68813
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:02 +0000

    Add package distributions-nanya/0.1/distributions_nanya-0.1.tar.gz

---
+++ packages/distributions-nanya/distributions_nanya-0.1.tar.gz/distributions_nanya-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_nanya',
+      version='0.1',
+      description='Gaussian distribution and Binomial distribution',
+      packages=['distributions_nanya'],
+      author='Adeniran Rachel',
+      author_email='ihunanyayekini@gmail.com')
