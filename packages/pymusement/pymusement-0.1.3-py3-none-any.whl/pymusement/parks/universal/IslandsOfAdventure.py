9601ecff2375801b15cfa8da1f1095d5538bb49c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:56 +0000

    Add package pymusement/0.1.3/pymusement-0.1.3-py3-none-any.whl

---
+++ packages/pymusement/pymusement-0.1.3-py3-none-any.whl/pymusement/parks/universal/IslandsOfAdventure.py
@@ -0,0 +1,11 @@
+from pymusement.parks.universal.UniversalPark import UniversalPark
+
+class IslandsOfAdventure(UniversalPark):
+    def __init__(self):
+        super(IslandsOfAdventure, self).__init__()
+
+    def getId(self):
+        return 10000
+
+    def getName(self):
+        return 'Islands of Adventure'
