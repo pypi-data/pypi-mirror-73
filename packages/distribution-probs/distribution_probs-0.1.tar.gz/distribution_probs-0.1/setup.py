cab1c0b0e1bb37a85b347b4b1e282590b7b23b7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:27 +0000

    Add package distribution-probs/0.1/distribution_probs-0.1.tar.gz

---
+++ packages/distribution-probs/distribution_probs-0.1.tar.gz/distribution_probs-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distribution_probs',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distribution_probs'],
+      author= 'David Ukwen',
+      author_email = 'ukwendavid@gmail.com',
+      zip_safe=False)
