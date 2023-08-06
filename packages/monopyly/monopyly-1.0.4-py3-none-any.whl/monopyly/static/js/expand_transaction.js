6e21fbde0ee9036010dcf0837d9eca5bd331be2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:44 +0000

    Add package monopyly/1.0.4/monopyly-1.0.4-py3-none-any.whl

---
+++ packages/monopyly/monopyly-1.0.4-py3-none-any.whl/monopyly/static/js/expand_transaction.js
@@ -0,0 +1,35 @@
+/*
+ * Expand a transaction when the plus icon is clicked.
+ *
+ * Expands a row in the transaction table when the plus icon offering
+ * more information is clicked. The expanded row shows transactions in
+ * more detail (and a larger font) than the rest of the transaction
+ * table. A user can exit the expanded view by clicking the 'x' icon
+ * button that is shown in the expanded view.
+ */
+import {
+	replaceDisplayContentsAjaxRequest
+} from './modules/update_display_ajax.js';
+
+
+(function() {
+
+	// Identify the plus/minus icons
+	const $iconsMoreInfo = $('.transaction .more.button');
+	const $iconsLessInfo = $('.transaction .less.button');
+
+	$iconsMoreInfo.on('click', function() {
+		const $transaction = $(this).closest('.transaction');
+		const endpoint = TRANSACTION_TAGS_ENDPOINT;
+		const rawData = $transaction[0].id;
+		const $tagsContainer = $transaction.find('.tags-container');
+		replaceDisplayContentsAjaxRequest(endpoint, rawData, $tagsContainer);
+		$transaction.addClass('expanded');
+	});
+
+	$iconsLessInfo.on('click', function() {
+		const $transaction = $(this).closest('.transaction');
+		$transaction.removeClass('expanded');
+	});
+
+})();
