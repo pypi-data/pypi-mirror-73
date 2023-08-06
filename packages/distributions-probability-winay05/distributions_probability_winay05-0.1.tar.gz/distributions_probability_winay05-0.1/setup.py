664de5286a7f1b5390a0089945d7523190b8c603
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:54 +0000

    Add package distributions-probability-winay05/0.1/distributions_probability_winay05-0.1.tar.gz

---
+++ packages/distributions-probability-winay05/distributions_probability_winay05-0.1.tar.gz/distributions_probability_winay05-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_probability_winay05',
+      version='0.1',
+      description='Gaussian & Binomial distributions',
+      packages=['distributions_probability_winay05'],
+      author = 'Vinay Kumar Dahiya',
+      author_email='vinaykumardahiya@gmail.com', 
+      zip_safe=False)
