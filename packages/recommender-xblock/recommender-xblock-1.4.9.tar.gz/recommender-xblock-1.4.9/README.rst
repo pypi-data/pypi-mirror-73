e4ebe7ef670cccc5154c8588daf94ec94e3802a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:01 +0000

    Add package recommender-xblock/1.4.9/recommender-xblock-1.4.9.tar.gz

---
+++ packages/recommender-xblock/recommender-xblock-1.4.9.tar.gz/recommender-xblock-1.4.9/README.rst
@@ -0,0 +1,33 @@
+RecommenderXBlock
+=================
+
+This XBlock shows students a list of recommended resources for a given
+problem. The resources are recommended, edited, and voted by students.
+For each resource, we show its title, link, short summary, preview
+screenshot, and votes:
+
+.. image:: recommender_xblock.png
+   :alt: Recommender screenshot
+
+This is an module where students can share useful resources/hints and rate
+them. This crowdsourcing mechanism allows a scalable solution to fulfill
+students with varying learning needs.
+
+* Staff Interface: manage problematic resourse easier, add comments, endorse,
+  de-endorse resource
+* Discussion around each resource
+* Better interface for adding varying types of resource (e.g., specific timestamps
+  in the video or specific elements in a learning sequence)
+* Better user help/documentation
+* Tag/categorize resources around specific misconceptions
+
+In a randomized control trial in a computer science course, this XBlock led to 
+similar learning outcomes in about 10% less time than without it (so efficiency of 
+learning was about 10% better than without the XBlock -- students learned the same
+in less time). Qualitative analysis as well as quantitative analysis of usage data 
+showed it was helpful in contexts where there were complex, multiconcept problems. 
+It was not helpful or used in contexts where there were simple, single-step problems. 
+
+In an analysis comparing to other remediation systems within edX, it was more 
+effective for deeper, more complex misconceptions, and less effective for simple 
+errors. 
---
+++ packages/recommender-xblock/recommender-xblock-1.4.9.tar.gz/recommender-xblock-1.4.9/setup.py
@@ -0,0 +1,79 @@
+"""Setup for recommender XBlock."""
+
+from __future__ import absolute_import
+import os
+import subprocess
+from setuptools.command.install import install as _install
+from setuptools import setup
+
+base_path = os.path.dirname(__file__)
+README = open(os.path.join(base_path, "README.rst")).read()
+
+
+class XBlockInstall(_install):
+    """Custom XBlock install command."""
+
+    def run(self):
+        _install.run(self)
+        self.compile_translations()
+
+    def compile_translations(self):
+        """
+        Compiles textual translations files(.po) to binary(.mo) files.
+        """
+        self.announce('Compiling translations')
+        try:
+            for dirname, _, files in os.walk(os.path.join('recommender', 'translations')):
+                for fname in files:
+                    if os.path.splitext(fname)[1] == '.po':
+                        po_path = os.path.join(dirname, fname)
+                        mo_path = os.path.splitext(po_path)[0] + '.mo'
+                        self.announce('Compiling translation at %s' % po_path)
+                        subprocess.check_call(['msgfmt', po_path, '-o', mo_path], cwd=self.install_lib)
+        except Exception as ex:
+            self.announce('Translations compilation failed: %s' % ex.message)
+
+
+def package_data(pkg, root_list):
+    """Generic function to find package_data for `pkg` under `root`."""
+    data = []
+    for root in root_list:
+        for dirname, _, files in os.walk(os.path.join(pkg, root)):
+            for fname in files:
+                data.append(os.path.relpath(os.path.join(dirname, fname), pkg))
+
+    return {pkg: data}
+
+
+setup(
+    name='recommender-xblock',
+    version='1.4.9',
+    description='recommender XBlock',   # TODO: write a better description.
+    long_description=README,
+    author='edX',
+    author_email='oscm@edx.org',
+    url='https://github.com/edx/RecommenderXBlock',
+    packages=[
+        'recommender',
+    ],
+    license='AGPL 3.0',
+    entry_points={
+        'xblock.v1': [
+            'recommender = recommender:RecommenderXBlock',
+        ]
+    },
+    package_data=package_data("recommender", ["static", "translations"]),
+    cmdclass={
+        'install': XBlockInstall,
+    },
+    keywords="edx recommender",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
