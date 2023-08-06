04ff4d6ade9023c303a709802b1d79bcbf705aef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:48 +0000

    Add package aiowebthing/0.1.12/aiowebthing-0.1.12.tar.gz

---
+++ packages/aiowebthing/aiowebthing-0.1.12.tar.gz/aiowebthing-0.1.12/webthing/mixins.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import asyncio
+
+
+class AsyncMixin:
+    def _run_async(self, coro):
+        future = asyncio.run_coroutine_threadsafe(coro, self._loop)
+        return future.result()
