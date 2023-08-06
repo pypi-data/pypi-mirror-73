cf47cb84596e83f97276ba0ffaaaf207662329cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:42 +0000

    Add package gabd-probability/0.1.2.2/gabd_probability-0.1.2.2.tar.gz

---
+++ packages/gabd-probability/gabd_probability-0.1.2.2.tar.gz/gabd_probability-0.1.2.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open("/home/workspace/5_exercise_upload_to_pypi/gabd_probability/README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(name='gabd_probability',
+      version='0.1.2.2',
+      description='Gaussian and Binomial distributions',
+      packages=['gabd_probability'],
+      author='Biswajeet Mahato',
+      author_email='biswajeetm156@gmail.com',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/biswa-star",
+#       packages=setuptools.find_packages(),
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+      ],
+      python_requires='>=3.6',
+      zip_safe=False)
