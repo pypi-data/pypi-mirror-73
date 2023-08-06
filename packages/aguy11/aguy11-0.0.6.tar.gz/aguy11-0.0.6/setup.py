b5835051c427212738c95155b933f4423b9ca794
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:32 +0000

    Add package aguy11/0.0.6/aguy11-0.0.6.tar.gz

---
+++ packages/aguy11/aguy11-0.0.6.tar.gz/aguy11-0.0.6/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name = "aguy11",
+  version = "0.0.6",
+  description = "Module for finding cool and useful stuff quickly",
+  long_description = long_description,
+  long_description_content_type = "text/markdown",
+  author = "aguy11",
+  author_email = "aguy11@guerrillamail.org",
+  license = "MIT License",
+  packages=setuptools.find_packages(),
+  classifiers = [
+  "Programming Language :: Python :: 3",
+  "License :: OSI Approved :: MIT License",
+  "Operating System :: OS Independent",
+],
+  zip_safe=True,
+  python_requires = ">=3.0",
+)
