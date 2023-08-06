4faed89aa74a67b86a606a418743ba1274349ff1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:38 +0000

    Add package greenviz/1.0.2/greenviz-1.0.2.tar.gz

---
+++ packages/greenviz/greenviz-1.0.2.tar.gz/greenviz-1.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+from distutils.core import setup
+import os
+cwd=os.getcwd()
+setup(
+      name="greenviz",
+      version="1.0.2",
+      description="welcome",
+      author="R.raja subramanian",
+      url="https://github.com/RRajaSubramanian/Greenviz",
+      author_email="rajasubramanian.r1@gmail.com",
+      py_modules=["greenviz"],
+      package_dir={"":"src"},
+      data_files=[("",["shiridi pic rezied.jpeg","bg1.jpg","raja12.jpeg","resized achuth pic.jpeg"])],
+      include_package_data=True,
+      install_requires=["pillow","matplotlib","sklearn","pandas","pymongo","numpy","IPython","graphviz"]
+      )
+
