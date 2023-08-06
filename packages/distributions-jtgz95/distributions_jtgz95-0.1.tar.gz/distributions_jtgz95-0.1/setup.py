483eeacb1e836921b628a0e291244ae122b787d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:21 +0000

    Add package distributions-jtgz95/0.1/distributions_jtgz95-0.1.tar.gz

---
+++ packages/distributions-jtgz95/distributions_jtgz95-0.1.tar.gz/distributions_jtgz95-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_jtgz95',
+      version='0.1',
+      description='Gaussian and bionomial distributions',
+      packages=['distributions_jtgz95'],
+      author_email = "myemail@email.com",
+      zip_safe=False)
