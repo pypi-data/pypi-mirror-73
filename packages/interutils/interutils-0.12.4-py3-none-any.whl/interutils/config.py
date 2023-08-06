4aa638c218fe725ab157d029eba80e59c0723f1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:03 +0000

    Add package interutils/0.12.4/interutils-0.12.4-py3-none-any.whl

---
+++ packages/interutils/interutils-0.12.4-py3-none-any.whl/interutils/config.py
@@ -0,0 +1,32 @@
+import json
+import typing
+from pathlib import PosixPath
+from .interactive import pr
+
+
+class DictConfig(dict):
+    def __init__(self, conf_path: PosixPath,
+                 default_config: dict, quiet=False):
+        super(DictConfig, self).__init__()
+        self.conf_path = conf_path
+
+        if data := self.load():
+            self.update(data)
+        else:
+            if not quiet:
+                pr('Recreated config!', '!')
+            self.update(default_config)
+            self.save()
+
+    def load(self) -> (typing.Dict[str, str], bool):
+        try:
+            with self.conf_path.open() as f:
+                return json.load(f)
+        except FileNotFoundError:
+            return False
+        except json.JSONDecodeError:
+            return False
+
+    def save(self) -> None:
+        with self.conf_path.open('w') as f:
+            json.dump(self, f)
