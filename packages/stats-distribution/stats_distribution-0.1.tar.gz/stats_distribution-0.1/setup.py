8ebf751af8f85b61b4b02908d5b12ecc694eb5d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:10 +0000

    Add package stats-distribution/0.1/stats_distribution-0.1.tar.gz

---
+++ packages/stats-distribution/stats_distribution-0.1.tar.gz/stats_distribution-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='stats_distribution',
+      version='0.1',
+      description='Gaussian distributions',
+      author='Ajinkya Joshi',
+      author_email='ajinkya.dj92@gmail.com',     
+      packages=['stats_distribution'],
+      zip_safe=False)
