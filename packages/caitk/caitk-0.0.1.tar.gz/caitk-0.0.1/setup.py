2acbfd5f7c658decdfc7f435b43173227751b63e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:30 +0000

    Add package caitk/0.0.1/caitk-0.0.1.tar.gz

---
+++ packages/caitk/caitk-0.0.1.tar.gz/caitk-0.0.1/setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2020 Caitum Technologies
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='caitk',
+    version='0.0.1',
+    description='Caitum Artificial Intelligence Toolkit',
+    license='GPL-3.0-only',
+
+    packages=find_packages(),
+    install_requires=[],
+
+    include_package_data=True,
+    zip_safe=True
+)
