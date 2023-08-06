5ab0d0afa12982b73d779618a6545bc1f5f2cf31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:28 +0000

    Add package mrkdwn2html/0.0.3/mrkdwn2html-0.0.3.tar.gz

---
+++ packages/mrkdwn2html/mrkdwn2html-0.0.3.tar.gz/mrkdwn2html-0.0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mrkdwn2html",
+    version="0.0.3",
+    author="John Montgomery",
+    author_email="john@johnmontgomery.tech",
+    description="Convert markdown to html.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/john-montgomery2003/mrkdwn2html",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
