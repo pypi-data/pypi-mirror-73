606a25a6af590c5bfb65ef137b28353c3a79bc82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:39 +0000

    Add package pyobjc-framework-GameController/6.2.2/pyobjc-framework-GameController-6.2.2.tar.gz

---
+++ packages/pyobjc-framework-GameController/pyobjc-framework-GameController-6.2.2.tar.gz/pyobjc-framework-GameController-6.2.2/setup.py
@@ -0,0 +1,21 @@
+"""
+Wrappers for the "GameController" framework on macOS introduced in macOS 10.9.
+
+These wrappers don't include documentation, please check Apple's documention
+for information on how to use this framework and PyObjC's documentation
+for general tips and tricks regarding the translation between Python
+and (Objective-)C frameworks
+"""
+
+from pyobjc_setup import setup
+
+VERSION = "6.2.2"
+
+setup(
+    name="pyobjc-framework-GameController",
+    description="Wrappers for the framework GameController on macOS",
+    packages=["GameController"],
+    version=VERSION,
+    install_requires=["pyobjc-core>=" + VERSION, "pyobjc-framework-Cocoa>=" + VERSION],
+    long_description=__doc__,
+)
