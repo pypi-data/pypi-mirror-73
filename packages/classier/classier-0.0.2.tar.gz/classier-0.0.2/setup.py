451fc58aa665906928e96ecb719c8beadbc0c7d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:30 +0000

    Add package classier/0.0.2/classier-0.0.2.tar.gz

---
+++ packages/classier/classier-0.0.2.tar.gz/classier-0.0.2/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+setup(
+  name = 'classier',
+  packages = ['classier'],
+  version = '0.0.2',
+  license='MIT',
+  description = 'Make your classes classier.',
+  author = 'Ozgen Eren',
+  author_email = 'ozgeneral@gmail.com',
+  url = 'https://github.com/ozgeneral/classier',
+  download_url = 'https://github.com/ozgeneral/classier/archive/0.0.2.tar.gz',
+  keywords = ['classier', 'utils', 'convenience'],
+  install_requires=[
+  ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
