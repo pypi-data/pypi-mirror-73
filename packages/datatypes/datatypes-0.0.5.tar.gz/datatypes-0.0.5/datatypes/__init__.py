05cf4c13827e33bbff5ebad048354041e6896c5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:38 +0000

    Add package datatypes/0.0.5/datatypes-0.0.5.tar.gz

---
+++ packages/datatypes/datatypes-0.0.5.tar.gz/datatypes-0.0.5/datatypes/__init__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, division, print_function, absolute_import
+import logging
+
+from .string import (
+    String,
+    Unicode, # alias for String
+    ByteString,
+    Base64,
+    HTMLCleaner,
+)
+from .integer import (
+    Shorten,
+    Integer,
+)
+from .collections import (
+    Pool,
+    PriorityQueue,
+    NormalizeDict,
+    idict,
+    AppendList,
+)
+from .csv import (
+    CSV,
+)
+from .enum import (
+    Enum,
+)
+
+
+__version__ = "0.0.5"
+
+
+# get rid of "No handler found" warnings (cribbed from requests)
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
