dfeb668785f177358dcad519c25a53e80e221421
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:48 +0000

    Add package nav-distributions/0.1/nav_distributions-0.1.tar.gz

---
+++ packages/nav-distributions/nav_distributions-0.1.tar.gz/nav_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='nav_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['nav_distributions'],
+      zip_safe=False)
