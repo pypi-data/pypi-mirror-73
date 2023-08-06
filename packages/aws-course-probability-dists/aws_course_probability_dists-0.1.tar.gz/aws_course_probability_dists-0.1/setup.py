7f98c8c12b8e3cce84401046c5166ea52dbe5abb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:31 +0000

    Add package aws-course-probability-dists/0.1/aws_course_probability_dists-0.1.tar.gz

---
+++ packages/aws-course-probability-dists/aws_course_probability_dists-0.1.tar.gz/aws_course_probability_dists-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='aws_course_probability_dists',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['aws_course_probability_dists'],
+      author='Noel Mathew',
+      author_email='email@something.com',
+      zip_safe=False)
