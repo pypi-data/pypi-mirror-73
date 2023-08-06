e89021f4f26db0b595dbd0de1196790325e84efb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:25 +0000

    Add package pyobjc-framework-VideoSubscriberAccount/6.2.2/pyobjc-framework-VideoSubscriberAccount-6.2.2.tar.gz

---
+++ packages/pyobjc-framework-VideoSubscriberAccount/pyobjc-framework-VideoSubscriberAccount-6.2.2.tar.gz/pyobjc-framework-VideoSubscriberAccount-6.2.2/PyObjCTest/test_VSSubscription.py
@@ -0,0 +1,17 @@
+import sys
+
+
+if sys.maxsize > 2 ** 32:
+    from PyObjCTools.TestSupport import TestCase
+    import VideoSubscriberAccount
+
+    class TestVSSubscription(TestCase):
+        def testConstants(self):
+            self.assertEqual(VideoSubscriberAccount.VSSubscriptionAccessLevelUnknown, 0)
+            self.assertEqual(
+                VideoSubscriberAccount.VSSubscriptionAccessLevelFreeWithAccount, 1
+            )
+            self.assertEqual(VideoSubscriberAccount.VSSubscriptionAccessLevelPaid, 2)
+
+        def testClasses(self):
+            VideoSubscriberAccount.VSSubscription
