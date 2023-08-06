25d31a567a3b13167014688cce8ebb45bbe3e94f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:33 +0000

    Add package ps2-analysis/0.15.1/ps2_analysis-0.15.1-py3-none-any.whl

---
+++ packages/ps2-analysis/ps2_analysis-0.15.1-py3-none-any.whl/ps2_analysis/fire_groups/ammo.py
@@ -0,0 +1,46 @@
+import math
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class Ammo:
+    # Basic information
+    clip_size: int
+    total_capacity: int
+    ammo_per_shot: int
+    block_auto: Optional[bool]
+    continuous: Optional[bool]
+
+    # Reload timing
+    short_reload_time: int
+    reload_chamber_time: int
+    loop_start_time: Optional[int]
+    loop_end_time: Optional[int]
+
+    @property
+    def shots_per_clip(self) -> int:
+
+        if self.ammo_per_shot > 0 and self.clip_size > 0:
+
+            return int(math.floor(self.clip_size / self.ammo_per_shot))
+
+        else:
+
+            return -1
+
+    @property
+    def long_reload_time(self) -> int:
+
+        if self.loop_start_time and self.loop_end_time:
+
+            return (
+                self.loop_start_time
+                + self.clip_size * self.short_reload_time
+                + self.loop_end_time
+                + self.reload_chamber_time
+            )
+
+        else:
+
+            return self.short_reload_time + self.reload_chamber_time
