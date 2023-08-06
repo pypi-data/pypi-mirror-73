d112c3d219d445eba1cb9ccea7681d48acdbddcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:48 +0000

    Add package napalm-ros/0.8.0/napalm-ros-0.8.0.tar.gz

---
+++ packages/napalm-ros/napalm-ros-0.8.0.tar.gz/napalm-ros-0.8.0/setup.py
@@ -0,0 +1,44 @@
+# -*- coding: UTF-8 -*-
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+def parse_reqs(file_path):
+    with open(file_path, 'rt') as fobj:
+        lines = map(str.strip, fobj)
+        lines = filter(None, lines)
+        lines = filter(lambda x: x.startswith("#"), lines)
+        return tuple(lines)
+
+
+setup(
+    name="napalm-ros",
+    version="0.8.0",
+    packages=find_packages(),
+    author="Łukasz Kostka",
+    author_email="lukasz.kostka@netng.pl",
+    description="Network Automation and Programmability Abstraction Layer driver for Mikrotik ROS",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    classifiers=[
+        'Topic :: Utilities',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS',
+    ],
+    url="https://github.com/napalm-automation-community/napalm-ros",
+    include_package_data=True,
+    install_requires=(
+        'napalm==2.*',
+        'librouteros==2.*',
+    ),
+)
