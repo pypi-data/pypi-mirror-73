7fecc5cbc6590ff8df883fbdb75238609c6743b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:11 +0000

    Add package num-distributions/0.1/num_distributions-0.1.tar.gz

---
+++ packages/num-distributions/num_distributions-0.1.tar.gz/num_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='num_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['num_distributions'],
+      author= 'Raj Vaghashiya',
+      author_email = 'rajvaghasiya*****@gmail.com',
+      zip_safe=False)
