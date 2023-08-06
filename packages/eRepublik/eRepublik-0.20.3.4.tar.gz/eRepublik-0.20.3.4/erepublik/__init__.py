72dc37865c1071402323be0dc849dc3026bd3501
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:58 +0000

    Add package eRepublik/0.20.3.4/eRepublik-0.20.3.4.tar.gz

---
+++ packages/eRepublik/eRepublik-0.20.3.4.tar.gz/eRepublik-0.20.3.4/erepublik/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for eRepublik script."""
+
+__author__ = """Eriks Karls"""
+__email__ = 'eriks@72.lv'
+__version__ = '0.20.3.4'
+__commit_id__ = "8ce56a3"
+
+from erepublik import classes, utils
+from erepublik.citizen import Citizen
+
+__all__ = ["classes", "utils", "Citizen"]
