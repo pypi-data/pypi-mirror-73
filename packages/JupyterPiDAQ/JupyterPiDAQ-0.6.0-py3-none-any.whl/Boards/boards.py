fc2fefe594e7ca1d6823f3dbccffd9f8a9d3a86a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:54 +0000

    Add package JupyterPiDAQ/0.6.0/JupyterPiDAQ-0.6.0-py3-none-any.whl

---
+++ packages/JupyterPiDAQ/JupyterPiDAQ-0.6.0-py3-none-any.whl/Boards/boards.py
@@ -0,0 +1,204 @@
+"""
+This file handles loading adc board control software and sensor information.
+It uses the list of known boards. It will skip boards that produce an error
+either because the pypi package is not installed or an error occurs when
+trying to communicate with the board.
+
+The ADC simulator will be installed if no boards are available.
+"""
+from importlib import import_module
+import logging
+
+logger = logging.getLogger(__name__)
+
+# TODO: Update this list of available board options as they are created.
+# Name format is `.package.boardname` where `boardname` is the name of the
+# python file defining the required board operations.
+
+knownboardpkgs = ('.PiGPIO.ADS1115', '.PiGPIO.DAQC2')
+knownsimulators = ('.Simulated.ADCsim', '.Simulated.ADCsim_line')
+
+
+def load_boards():
+    """
+    Uses the list of known board packages to search for available boards.
+    The file <boardname>.py should at minimum
+    implement a `find_boards(): routine that overrides the function below and
+    define a class for the particular board that extends the `Board` class
+    defined below.
+
+    :return: list of adc board objects.
+    """
+    # Load available board driver packages
+    boardpkgs = []
+    for pkg in knownboardpkgs:
+        tmpmod = None
+        try:
+            tmpmod = import_module('Boards'+pkg)
+        except ImportError as e:
+            logger.debug(e)
+            tmpmod = None
+        if (tmpmod):
+            boardpkgs.append(tmpmod)
+
+    # Check for available boards
+    boards = []
+    if len(boardpkgs) >= 1:
+        # All board pkgs must implement `find_boards().
+        for drv in boardpkgs:
+            avail = drv.find_boards()
+            for hdw in avail:
+                boards.append(hdw)
+    if len(boards) == 0:
+        # We found no boards
+        print('\nNo ADC boards found. Using simulated boards...',end='')
+        boards = _load_simulators()
+    return boards
+
+def find_boards():
+    """
+    A function overriding this must be implemented by all board packages.
+    See examples in working packages. This is highly board dependent.
+
+    :return: list of board objects
+    """
+    raise NotImplementedError
+
+def _load_simulators():
+    """
+    Private function to add simulated ADC boards
+    :return:
+    boards  list of adc board objects.
+    """
+    simpkgs = []
+    boards = []
+    tmpmod = None
+    for sim in knownsimulators:
+        try:
+            tmpmod = import_module('Boards'+sim)
+        except ImportError as e:
+            logger.debug(e)
+            tmpmod = None
+        if (tmpmod):
+            simpkgs.append(tmpmod)
+        logging.log(logging.DEBUG,str(simpkgs))
+    if len(simpkgs) >= 1:
+        # All board pkgs must implement `find_boards().
+        for drv in simpkgs:
+            avail = drv.find_boards()
+            logging.log(logging.DEBUG,str(avail))
+            if avail:
+                boards.append(avail)
+    return boards
+
+
+class Board:
+    """
+    Base class for all boards. Each board should be an extension of this class.
+    """
+    def __init__(self):
+        """
+        Should be overridden by each board and define at minimum:
+        self.name = 'board name/adc name/type' Short an useful to end user
+        self.vendor = 'Vendor/Manufacturer name`
+        self.channels = tuple of available channel IDs
+        self.gains = list of gains
+        self.Vdd = voltage provided by board to sensors
+        """
+        self.name = None
+        self.vendor = None
+        self.channels = None
+        self.gains = [1]
+        self.Vdd = None
+
+    def getname(self):
+        """
+        :return: string value of the board name, a short label of board type.
+        """
+        return self.name
+
+    def getchannels(self):
+        """
+        :return: tuple of ids for available channels
+        """
+        return self.channels
+
+    def getgains(self):
+        """
+        If not defined for a specific board the gain is fixed at 1.
+        :return: tuple of gains availabe for onboard preamp
+        """
+        return self.gains
+    
+    def getvendor(self):
+        """
+        :return: string value of the vendor name
+        """
+        return self.vendor
+
+    def getVdd(self):
+        """
+        :return: numerical value of the Vdd, voltage provided to sensors
+        """
+        return self.Vdd
+
+    def getsensors(self):
+        """
+        This returns a list of objects that allow the software to translate
+        the measured voltage into a sensor reading in appropriate units.
+        Must be provided by the specific board implementation. See examples
+        in working board packages.
+        :return: A list of valid sensor objects to use with this board. This
+        should be a subset of all the sensors returned by the listSensors
+        function in sensors.py.
+        """
+        raise NotImplementedError
+
+    def V_oversampchan(self, chan, gain, avg_sec, **kwargs):
+        """
+        This function should return a tuple with average, minimum and maximum
+        for a channel averaged over the period of time avg_sec. How the
+        averaging is performed will depend on the board.
+        :param chan: id of the channel to be measured
+        :param gain: gain of the channel if adjustable
+        :param avg_sec: float period of time over which to average
+        :return: a tuple consisting of V_avg, V_min, V_max, time_stamp, avg_Vdd
+            The time_stamp is the time the data was collected, usually the
+            middle of the averaging period. avg_Vdd should be the measured
+            average Vdd taken simultaneously, immediately before,
+            or immediately after the voltage being measured. If the board or
+            power supply is very stable self.Vdd can be returned instead.
+        """
+        raise NotImplementedError
+
+    def V_oversampchan_stats(self, chan, gain, avg_sec, **kwargs):
+        """
+        This function should return a tuple of statistical information for a
+        channel averaged over the period of time avg_sec.
+        :param chan: id of the channel to be measured
+        :param gain: gain of the channel if adjustable
+        :param avg_sec: float period of time over which to average
+        :return: tuple consisting of V_avg, stdev, stdev_avg, time_stamp,
+            avg_Vdd where stdev_avg is the estimated standard deviation
+            of the average not the standard deviation of the values
+            sampled (stdev). avg_Vdd should be the measured
+            average Vdd taken simultaneously, immediately before,
+            or immediately after the voltage being measured. If the board or
+            power supply is very stable self.Vdd can be returned instead.
+        """
+        raise NotImplementedError
+
+    def V_sampchan(self, chan, gain, **kwargs):
+        """
+        This function returns a single measurement and the time it was
+        collected.
+        :param chan: id of the channel to be measured
+        :param gain: gain of the channel if adjustable
+        :return: a tuple consisting of V, time_stamp, ref_Vdd, where V = the
+            single voltage measurement and time_stamp the time it was
+            collected. ref_Vdd should be the measured
+            Vdd taken simultaneously, immediately before,
+            or immediately after the voltage being measured. If the board or
+            power supply is very stable self.Vdd can be returned instead.
+        """
+        raise NotImplementedError
---
+++ packages/JupyterPiDAQ/JupyterPiDAQ-0.6.0-py3-none-any.whl/jupyterpidaq/Boards/Simulated/ADCsim_line.py
@@ -0,0 +1,212 @@
+# Noisy line substitute for actual analog to digital converter.
+# Used to allow a 'demo' mode for JupyterPiDAQ.
+# J. Gutow <jgutow@new.rr.com> June 11, 2020
+# license GPL V3 or greater
+
+import time
+
+from numpy import around
+from numpy import float64
+from numpy import floor
+from numpy import log10
+from numpy import random
+from numpy import sqrt
+from numpy import std
+
+from jupyterpidaq.Boards import Board
+
+# mimicking an installed ADS1115 ADC PiHAT.
+RATE = 475
+# 475 Hz with oversampling best S/N on Pi 3B+ per unit time interval.
+
+
+# other rates 8, 16, 32, 64, 128, 250, 475, 860 in Hz.
+def find_boards():
+    return Board_ADCsim_line('placeholder')
+
+
+class Board_ADCsim_line(Board):
+    """
+    This class simulates an Analog-to-Digital board that returns a linearly
+    increasing signal with a small amount of noise on the signal. The
+    intercept and slope depend upon which hour of the day the simulation is
+    run.
+    """
+    def __init__(self, adc):
+        super().__init__()
+        self.name = 'ADCsym Line'
+        self.vendor = 'JupyterPiDAQ'
+        self.channels = (0, 1, 2, 3)
+        self.gains = [1]
+        self.Vdd = 3.3
+        self.adc = adc
+
+    def getsensors(self):
+        """
+        Return a list of valid sensor object names for this board.
+        :return: list of classnames
+        """
+        sensorlist = ['RawAtoD',
+                      'VernierSSTemp',
+                      'VernierGasP',
+                      'VernierpH',
+                      'VernierFlatpH'
+                      ]
+        # TODO: extend this list as appropriate. You can get a full list
+        #  using the `Sensors.sensors.listSensors()` call.
+        # The main program will use this list to access the actual sensor
+        # objects when converting the raw board voltage and for producing
+        # a menu of valid options for this particular board.
+        return sensorlist
+
+    def V_oversampchan(self, chan, gain, avg_sec, data_rate=RATE):
+        """
+        This routine returns the average voltage for the channel
+        averaged at (0.0012 + 1/data_rate)^-1 Hz for avg_sec
+        number of seconds. The 0.0012 is the required loop time
+        on a RPI 3B+ in python3. The voltage is rounded to the number
+        of decimals indicated by the standard deviation. The standard
+        deviation and the estimated deviation of the mean are also
+        returned.
+        Parameters
+            chan    the channel number 0, 1, 2, 3
+            gain    2/3 (+/-6.144V), 1(+/-4.096V), 2(+/-2.048V), 4(+/-1.024V),
+                    8 (+/-0.512V), 16 (+/-0.256V)
+            data_rate the ADC sample rate in Hz (8, 16, 32, 64, 128, 250, 475
+             or 860 Hz)
+            avg_sec seconds to average for, actual averaging interval will be
+             as close as possible for an integer number of samples.
+        Returns a tuple (V_avg, V_min, V_max, time_stamp)
+            V_avg   the averaged voltage
+            stdev   estimated standard deviation of the measurements
+            stdev_avg   estimated standard deviation of the mean
+            time_stamp the time at halfway through the averaging interval in
+             seconds since the beginning of the epoch (OS dependent begin
+             time).
+
+        """
+        time_tuple = time.localtime()
+        nearesthr = time.mktime((time_tuple.tm_year, time_tuple.tm_mon,
+                                 time_tuple.tm_mday, time_tuple.tm_hour, 0, 0,
+                                 time_tuple.tm_wday, time_tuple.tm_yday,
+                                 time_tuple.tm_isdst))
+        n_samp = int(round(avg_sec / (0.0017 + 1 / data_rate)))
+        if n_samp < 1:
+            n_samp = 1
+        value = []
+        start = time.time()
+        intercept = (time.time() - nearesthr) / 1800
+        slope = (time.time() - nearesthr) / 692000
+        for k in range(n_samp):
+            tempval = intercept + slope * (time.time() - nearesthr) + (
+                    random.random() - 0.5) * slope
+            value.append(tempval)
+        end = time.time()
+        time_stamp = (start + end) / 2.0
+        V_avg = sum(value) / len(value) / gain
+        V_max = max(value)
+        V_min = min(value)
+        return V_avg, V_min, V_max, time_stamp, self.Vdd
+
+    def V_oversampchan_stats(self, chan, gain, avg_sec, data_rate=RATE):
+        """
+        This routine returns the average voltage for the channel
+        averaged at (0.0012 + 1/data_rate)^-1 Hz for avg_sec
+        number of seconds. The 0.0012 is the required loop time
+        on a RPI 3B+ in python3. The voltage is rounded to the number
+        of decimals indicated by the standard deviation. The standard
+        deviation and the estimated deviation of the mean are also
+        returned.
+        Parameters
+            chan    the channel number 0, 1, 2, 3
+            gain    2/3 (+/-6.144V), 1(+/-4.096V), 2(+/-2.048V), 4(+/-1.024V),
+                    8 (+/-0.512V), 16 (+/-0.256V)
+            data_rate the ADC sample rate in Hz (8, 16, 32, 64, 128, 250, 475
+             or 860 Hz)
+            avg_sec seconds to average for, actual averaging interval will be
+             as close as possible for an integer number of samples.
+        Returns a tuple (V_avg, V_min, V_max, time_stamp)
+            V_avg   the averaged voltage
+            stdev   estimated standard deviation of the measurements
+            stdev_avg   estimated standard deviation of the mean
+            time_stamp the time at halfway through the averaging interval in
+              seconds since the beginning of the epoch (OS dependent begin
+              time).
+
+        """
+        time_tuple = time.localtime()
+        currhr = time.mktime((time_tuple.tm_year, time_tuple.tm_mon,
+                                 time_tuple.tm_mday, time_tuple.tm_hour, 0, 0,
+                                 time_tuple.tm_wday, time_tuple.tm_yday,
+                                 time_tuple.tm_isdst))
+        currdy = time.mktime((time_tuple.tm_year, time_tuple.tm_mon,
+                                 time_tuple.tm_mday, 0, 0, 0,
+                                 time_tuple.tm_wday, time_tuple.tm_yday,
+                                 time_tuple.tm_isdst))
+        n_samp = int(round(avg_sec / (0.0017 + 1 / data_rate)))
+        if n_samp < 1:
+            n_samp = 1
+        value = []
+        start = time.time()
+        intercept = (currhr - currdy) / 24 / 3600 - 0.5
+        slope = (currhr - currdy) / 24 / 3600 / 300
+        for k in range(n_samp):
+            tempval = intercept + slope * (time.time() - currhr) + (
+                    random.random() - 0.5) * slope
+            value.append(tempval)
+        end = time.time()
+        time_stamp = (start + end) / 2
+        V_avg = sum(value) / len(value) / gain
+        stdev = std(value, ddof=1, dtype=float64) / gain
+        stdev_avg = stdev / sqrt(float(len(value)))
+        decimals = 0
+        if stdev_avg == 0:
+            decimals = 6
+        else:
+            if (stdev_avg != float('inf')) and (stdev_avg > 0):
+                decimals = -int(floor(log10(stdev_avg)))
+        # print('decimals = '+str(decimals))
+        V_avg = around(V_avg, decimals=decimals)
+        stdev = around(stdev, decimals=decimals)
+        stdev_avg = around(stdev_avg, decimals=decimals)
+        return V_avg, stdev, stdev_avg, time_stamp, self.Vdd
+
+    def V_sampchan(self, chan, gain, data_rate=RATE):
+        """
+        This routine returns the average voltage for the channel
+        averaged at (0.0012 + 1/data_rate)^-1 Hz for avg_sec
+        number of seconds. The 0.0012 is the required loop time
+        on a RPI 3B+ in python3. The voltage is rounded to the number
+        of decimals indicated by the standard deviation. The standard
+        deviation and the estimated deviation of the mean are also
+        returned.
+        Parameters
+            chan    the channel number 0, 1, 2, 3
+            gain    2/3 (+/-6.144V), 1(+/-4.096V), 2(+/-2.048V), 4(+/-1.024V),
+                    8 (+/-0.512V), 16 (+/-0.256V)
+            data_rate the ADC sample rate in Hz (8, 16, 32, 64, 128, 250, 475
+             or 860 Hz) avg_sec seconds to average for, actual averaging
+             interval will be as close as possible for an integer number of
+             samples.
+        Returns a tuple (V_avg, V_min, V_max, time_stamp)
+            V_avg   the averaged voltage
+            stdev   estimated standard deviation of the measurements
+            stdev_avg   estimated standard deviation of the mean
+            time_stamp the time at halfway through the averaging interval in
+             seconds since the beginning of the epoch (OS dependent begin
+             time).
+
+        """
+        time_tuple = time.localtime()
+        nearesthr = time.mktime((time_tuple.tm_year, time_tuple.tm_mon,
+                                 time_tuple.tm_mday, time_tuple.tm_hour, 0, 0,
+                                 time_tuple.tm_wday, time_tuple.tm_yday,
+                                 time_tuple.tm_isdst))
+        start = time.time()
+        intercept = (time.time() - nearesthr) / 1800
+        slope = (time.time() - nearesthr) / 692000
+        V = intercept + slope * (time.time() - nearesthr) + (random.random()
+                                                             - 0.5) * slope
+        end = time.time()
+        time_stamp = (start + end) / 2.0
+        return V, time_stamp, self.Vdd
