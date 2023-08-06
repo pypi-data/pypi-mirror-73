863c98152e137232d3e36d9a831f8b8c123de35c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:40 +0000

    Add package c2sun-distributions/0.1/c2sun_distributions-0.1.tar.gz

---
+++ packages/c2sun-distributions/c2sun_distributions-0.1.tar.gz/c2sun_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='c2sun_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['c2sun_distributions'],
+      author = 'Chenggui Sun',
+      author_email = 'cgsun0521@hotmail.com',
+      zip_safe=False)
