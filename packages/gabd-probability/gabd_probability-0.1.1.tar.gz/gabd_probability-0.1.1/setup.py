15657523e722258894332a61153578fdae29d859
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:10 +0000

    Add package gabd-probability/0.1.1/gabd_probability-0.1.1.tar.gz

---
+++ packages/gabd-probability/gabd_probability-0.1.1.tar.gz/gabd_probability-0.1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gabd_probability',
+      version='0.1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['gabd_probability'],
+      author='Biswajeet Mahato',
+      author_email='biswajeetm156@gmail.com',
+      zip_safe=False)
