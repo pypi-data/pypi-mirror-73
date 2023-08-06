e42650b055ad7d50e59178936cc845fcb4e8dd5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:19 +0000

    Add package ajax-views/0.4.0/ajax_views-0.4.0-py2.py3-none-any.whl

---
+++ packages/ajax-views/ajax_views-0.4.0-py2.py3-none-any.whl/tests/urls.py
@@ -0,0 +1,9 @@
+try:
+    from django.urls import include, re_path
+except:
+    from django.conf.urls import include, url as re_path
+
+
+urlpatterns = [
+    re_path(r'^ajax/', include('ajax_views.urls', namespace='ajax_views')),
+]
