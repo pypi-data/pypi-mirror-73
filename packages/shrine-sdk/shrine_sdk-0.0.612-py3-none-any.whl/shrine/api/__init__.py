3453bd6b609c897d12ae6388062e43d23f6de735
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:21 +0000

    Add package shrine-sdk/0.0.612/shrine_sdk-0.0.612-py3-none-any.whl

---
+++ packages/shrine-sdk/shrine_sdk-0.0.612-py3-none-any.whl/shrine/api/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from shrine.api.application_metadata_api import ApplicationMetadataApi
+from shrine.api.licences_api import LicencesApi
+from shrine.api.policies_api import PoliciesApi
+from shrine.api.roles_api import RolesApi
