2929f090ef11c25bb141dad44eb42111bb3fd217
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:42 +0000

    Add package magnelPy/0.1.0/magnelPy-0.1.0.tar.gz

---
+++ packages/magnelPy/magnelPy-0.1.0.tar.gz/magnelPy-0.1.0/magnelPy/SFE/__init__.py
@@ -0,0 +1,23 @@
+"""
+magnelPy.SFE
+============
+Provides
+  1. Fire definition
+  2. Thermal calculation tools
+  3. Structural calculation tools
+
+=============== ==========================================================
+Fire definition ~ FireCurve
+==========================================================================
+ISO834          ISO834 standard fire definition
+=============== ==========================================================
+
+"""
+
+### get submodules ###
+from magnelPy.SFE import FireCurve
+from magnelPy.SFE import ThermalTools
+
+
+### get command line functionality ###
+from magnelPy.SFE.ThermalTools import EC_concreteSlab_ISO834
