e55d035792bf13ec3c5b6b16cacb5ad4c2833888
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:08 +0000

    Add package distributions-nick/0.1/distributions_nick-0.1.tar.gz

---
+++ packages/distributions-nick/distributions_nick-0.1.tar.gz/distributions_nick-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_nick',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_nick'],
+      zip_safe=False)
