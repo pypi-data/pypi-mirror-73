eacd11c07711f0286d644e09cda42f863121e676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:22 +0000

    Add package gcloud-rest-pubsub/1.2.2/gcloud_rest_pubsub-1.2.2-py2.py3-none-any.whl

---
+++ packages/gcloud-rest-pubsub/gcloud_rest_pubsub-1.2.2-py2.py3-none-any.whl/tests/unit/subscription_test.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+import warnings
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    from future import standard_library
+standard_library.install_aliases()
+def test_importable():
+    assert True
