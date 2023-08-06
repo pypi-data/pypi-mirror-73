a2b6b1b9ebad3a7481bbe18af1127ee817ae4492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:57 +0000

    Add package distributions234/0.1/distributions234-0.1.tar.gz

---
+++ packages/distributions234/distributions234-0.1.tar.gz/distributions234-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions234',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions234'],
+      zip_safe=False)
