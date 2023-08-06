cc3ddeb89a6002c04c2142711d9d4c0fd7ce734b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:27 +0000

    Add package rst2json/0.2.0.post1/rst2json-0.2.0.post1-py3-none-any.whl

---
+++ packages/rst2json/rst2json-0.2.0.post1-py3-none-any.whl/rst2json/writers/html5.py
@@ -0,0 +1,30 @@
+from   docutils.writers import html5_polyglot
+from   ._json_base      import JSONTranslatorBase, JSONWriterBase, joinstrs
+
+class Writer(JSONWriterBase, html5_polyglot.Writer):
+    format_name = 'html5'
+
+    json_fields = dict(JSONWriterBase.json_fields, html={
+        "math_requires": ("math_header", joinstrs),
+        "meta_tags": ("meta_tags", joinstrs),
+    })
+
+    def __init__(self):
+        super().__init__()
+        self.translator_class = HTMLJSONTranslator
+
+
+class HTMLJSONTranslator(JSONTranslatorBase, html5_polyglot.HTMLTranslator):
+    out_varname = 'body'
+
+    def visit_field_body(self, node):
+        super().visit_field_body(node)
+        if self.in_docinfo and not node.children:
+            self.body.append('<p></p>')
+
+    def visit_meta(self, node):
+        if node.hasattr('lang'):
+            node['xml:lang'] = node['lang']
+            # del(node['lang'])
+        meta = self.emptytag(node, 'meta', **node.non_default_attributes())
+        self.meta_tags.append(meta)
