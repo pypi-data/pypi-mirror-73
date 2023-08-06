fdb6602d1930c251af95eee6d9928c24b21e8bbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:04 +0000

    Add package electrumsv-sdk/0.0.4/electrumsv_sdk-0.0.4-py3-none-win32.whl

---
+++ packages/electrumsv-sdk/electrumsv_sdk-0.0.4-py3-none-win32.whl/electrumsv_sdk/runners.py
@@ -0,0 +1,29 @@
+import subprocess
+import sys
+
+from .config import Config
+
+
+def run_electrumsv_node():
+    from electrumsv_node import electrumsv_node
+    electrumsv_node.start()
+
+
+def run_electrumx_server():
+    if sys.platform == "win32":
+        electrumx_server_script = Config.run_scripts_dir.joinpath("electrumx.bat")
+    else:
+        electrumx_server_script = Config.run_scripts_dir.joinpath("electrumx.sh")
+
+    process = subprocess.Popen(f"{electrumx_server_script}", creationflags=subprocess.CREATE_NEW_CONSOLE)
+    return process
+
+
+def run_electrumsv_daemon():
+    esv_script = Config.depends_dir_electrumsv.joinpath("electrum-sv").__str__()
+    process = subprocess.Popen(
+        f"{sys.executable} {esv_script} --regtest daemon --v=debug "
+        f"--file-logging "
+        f"--restapi --server=127.0.0.1:51001:t", creationflags=subprocess.CREATE_NEW_CONSOLE
+    )
+    return process
