fa5c03c976e0ce0fe9c7dfc1a49b9d6bf02dbf25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:45 +0000

    Add package tshirt/0.1.dev2/tshirt-0.1.dev2.tar.gz

---
+++ packages/tshirt/tshirt-0.1.dev2.tar.gz/tshirt-0.1.dev2/docs/modules.rst
@@ -0,0 +1,24 @@
+Modules
+===========
+
+Photometric Module
+~~~~~~~~~~~~~~~~~~~
+.. autoclass:: tshirt.pipeline.phot_pipeline.phot
+   :members:
+
+.. automodule:: tshirt.pipeline.phot_pipeline
+   :members:
+
+Spectroscopic Module
+~~~~~~~~~~~~~~~~~~~~~
+.. autoclass:: tshirt.pipeline.spec_pipeline.spec
+   :members:
+
+.. automodule:: tshirt.pipeline.spec_pipeline
+   :members:
+
+Image Preparation Module
+~~~~~~~~~~~~~~~~~~~~~~~~~~~
+.. automodule:: tshirt.pipeline.prep_images
+   :members:
+   
---
+++ packages/tshirt/tshirt-0.1.dev2.tar.gz/tshirt-0.1.dev2/setup.py
@@ -0,0 +1,17 @@
+#from distutils.core import setup
+from setuptools import setup#, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='tshirt',
+    version='0.1dev2',
+    author='Everett Schlawin',
+    packages=['tshirt','tshirt.pipeline',
+              'tshirt.pipeline.instrument_specific'],
+    url="https://github.com/eas342/tshirt",
+    description="A package to analyze time series data, especially for exoplanets",
+    include_package_data=True,
+    long_description_content_type='text/markdown'
+)
