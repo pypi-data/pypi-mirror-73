9fb614a2b8674fcd8661ef17bf2796f124e6bea5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:08 +0000

    Add package evergreen.py/1.4.1/evergreen.py-1.4.1-py3-none-any.whl

---
+++ packages/evergreen.py/evergreen.py-1.4.1-py3-none-any.whl/evergreen/config.py
@@ -0,0 +1,51 @@
+# -*- encoding: utf-8 -*-
+"""Get configuration about connecting to evergreen."""
+from __future__ import absolute_import
+
+from collections import namedtuple
+import os
+from typing import Dict, Optional
+
+import yaml
+
+EvgAuth = namedtuple("EvgAuth", ["username", "api_key"])
+
+DEFAULT_NETWORK_TIMEOUT_SEC = 5 * 60
+DEFAULT_API_SERVER = "https://evergreen.mongodb.com"
+CONFIG_FILE_LOCATIONS = [
+    os.path.expanduser(os.path.join("~", "cli_bin", ".evergreen.yml")),
+    os.path.expanduser(os.path.join("~", ".evergreen.yml")),
+]
+
+
+def read_evergreen_from_file(filename: str) -> Dict:
+    """
+    Read evergreen config from given filename.
+
+    :param filename: Filename to read config.
+    :return: Config read from file.
+    """
+    with open(filename, "r") as fstream:
+        return yaml.safe_load(fstream)
+
+
+def read_evergreen_config() -> Optional[Dict]:
+    """
+    Search known location for the evergreen config file.
+
+    :return: First found evergreen configuration.
+    """
+    for filename in [filename for filename in CONFIG_FILE_LOCATIONS if os.path.isfile(filename)]:
+        return read_evergreen_from_file(filename)
+
+    return None
+
+
+def get_auth_from_config(config: Dict) -> EvgAuth:
+    """
+    Get the evergreen authentication from the specified config file.
+
+    :param config: Evergreen configuration.
+    :return: Authentication information for evergreen.
+    """
+    return EvgAuth(config["user"], config["api_key"])
