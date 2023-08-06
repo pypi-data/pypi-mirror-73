6655067a9e2e2f848c3b51b30715d5ae8b335cda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:25 +0000

    Add package nspywrapper/0.1.5/nspywrapper-0.1.5.tar.gz

---
+++ packages/nspywrapper/nspywrapper-0.1.5.tar.gz/nspywrapper-0.1.5/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name = "nspywrapper",
+    packages = ["nspywrapper"],
+    version = "0.1.5",
+    license = "Apache",
+    description = "A python wrapper for the NationStates API",
+    long_description = "See here for more information: https://github.com/abrow425/nspy_wrapper",
+    author = "Andrew Brown (aka SherpDaWerp)",
+    author_email = "abrow425@gmail.com",
+    url = "https://github.com/abrow425/nspy_wrapper",
+    download_url = "https://github.com/abrow425/nspy_wrapper/archive/v0.1.5.tar.gz",
+    keywords = ["NATIONSTATES", "NS", "API", "WRAPPER"],
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
