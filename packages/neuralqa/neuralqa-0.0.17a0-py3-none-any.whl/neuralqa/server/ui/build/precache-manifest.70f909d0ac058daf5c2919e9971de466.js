9624f85e2c50a377b93c850abf18132b567e48ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:10 +0000

    Add package neuralqa/0.0.17a0/neuralqa-0.0.17a0-py3-none-any.whl

---
+++ packages/neuralqa/neuralqa-0.0.17a0-py3-none-any.whl/neuralqa/server/ui/build/precache-manifest.70f909d0ac058daf5c2919e9971de466.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6df6627ec0daa53d6cd47c6f8bdbcbe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2483ace0c8c8f3432a2",
+    "url": "/static/css/main.fcaade4a.chunk.css"
+  },
+  {
+    "revision": "7ec5634352642cd9b632",
+    "url": "/static/js/2.ecd8a758.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ecd8a758.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2483ace0c8c8f3432a2",
+    "url": "/static/js/main.0da85579.chunk.js"
+  },
+  {
+    "revision": "b7d5e4d288fb48c3a4d5",
+    "url": "/static/js/runtime-main.985d1449.js"
+  }
+]);
