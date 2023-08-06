9ab0e0c8f0148998cf8477be2c58920400d761cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:06 +0000

    Add package maha-my-first-package/0.1/maha-my-first-package-0.1.tar.gz

---
+++ packages/maha-my-first-package/maha-my-first-package-0.1.tar.gz/maha-my-first-package-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='maha-my-first-package',
+      version='0.1',
+      description='Gaussian distributions and Binomial distributions',
+      packages=['maha-my-first-package'],
+      zip_safe=False)
