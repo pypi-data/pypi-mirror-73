21adc0ce62d40fe2daaa2eba057d935010eb22d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:40 +0000

    Add package anawesomepackage/0.1/anawesomepackage-0.1.tar.gz

---
+++ packages/anawesomepackage/anawesomepackage-0.1.tar.gz/anawesomepackage-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='anawesomepackage',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['anawesomepackage'],
+      zip_safe=False)
