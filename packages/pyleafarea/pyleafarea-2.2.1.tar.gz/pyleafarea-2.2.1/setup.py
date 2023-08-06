bcf44ab164d4b203d2187add628db1fbd3a74a44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:52 +0000

    Add package pyleafarea/2.2.1/pyleafarea-2.2.1.tar.gz

---
+++ packages/pyleafarea/pyleafarea-2.2.1.tar.gz/pyleafarea-2.2.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name='pyleafarea',
+    version='2.2.1',
+    packages=['pyleaf'],
+    url='',
+    license='MIT License',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'Keras>=2.2.4',
+        'numpy>=1.15.2',
+        'pandas>=0.23.0',
+        'Pillow>=5.3.0',
+        'tensorflow==1.12.0',
+        'tensorboard==1.12.0',
+        'Keras-Applications>=1.0.6',
+        'Keras-Preprocessing>=1.0.5',
+        'protobuf>=3.6.1',
+        'pyzbar>=0.1.8',
+        'opencv-python>=3.4.3.18'
+        ],
+    tests_require=["pytest"],
+    include_package_data=True,
+    authors =[
+            "Vishal Sonawane <vishalsonawane1515@gmail.com>", 
+            "Balasaheb Sonawane <balasahebsonawane@gmail.com>"],
+    description='Automated Leaf Area Calculator Using Tkinter and Deep Learning Image Classification Model.'
+)
