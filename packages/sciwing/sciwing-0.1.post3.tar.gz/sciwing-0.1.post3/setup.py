520ccb9aa7bb4d76f299efc7d9ff4b1cfaac978d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:51 +0000

    Add package sciwing/0.1.post3/sciwing-0.1.post3.tar.gz

---
+++ packages/sciwing/sciwing-0.1.post3.tar.gz/sciwing-0.1.post3/setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup, find_packages
+import pathlib
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="sciwing",
+    version="0.1.post3",
+    packages=find_packages(exclude=("tests",)),
+    url="https://github.com/abhinavkashyap/sciwing",
+    license="",
+    author="abhinav",
+    author_email="abhinav@comp.nus.edu.sg",
+    description="Modern Scientific Document Processing Framework ",
+    entry_points={"console_scripts": ["sciwing=sciwing.commands.sciwing_group:main"]},
+    long_description=README,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "networkx",
+        "wandb",
+        "logzero",
+        "typing",
+        "torch",
+        "wasabi",
+        "boto3",
+        "tqdm",
+        "wrapt",
+        "stopwords",
+        "allennlp",
+        "botocore",
+        "gensim",
+        "pytorch-pretrained-bert",
+        "spacy",
+        "questionary",
+        "pandas",
+        "pytorch-crf",
+        "colorful",
+        "fastapi",
+        "numpy",
+        "click",
+        "toml",
+        "requests",
+        "scikit-learn",
+        "tensorboardX",
+        "Deprecated",
+        "urllib3",
+    ],
+)
