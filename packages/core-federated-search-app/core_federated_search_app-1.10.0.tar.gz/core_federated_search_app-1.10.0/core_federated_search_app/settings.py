95c0480c9566565c82652913f0cfffd52355cfa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:53 +0000

    Add package core-federated-search-app/1.10.0/core_federated_search_app-1.10.0.tar.gz

---
+++ packages/core-federated-search-app/core_federated_search_app-1.10.0.tar.gz/core_federated_search_app-1.10.0/core_federated_search_app/settings.py
@@ -0,0 +1,10 @@
+""" Settings
+"""
+from django.conf import settings
+
+if not settings.configured:
+    settings.configure()
+
+CUSTOM_NAME = getattr(settings, "CUSTOM_NAME", "Local")
+""" :py:class:`str`: Name of the local instance
+"""
