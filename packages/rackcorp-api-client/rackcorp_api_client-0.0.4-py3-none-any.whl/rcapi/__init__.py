d67776848bd0cef1afa6d0f822337e1d72b60827
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:53 +0000

    Add package rackcorp-api-client/0.0.4/rackcorp_api_client-0.0.4-py3-none-any.whl

---
+++ packages/rackcorp-api-client/rackcorp_api_client-0.0.4-py3-none-any.whl/rcapi/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Command-line application access to RackCorp's API
+
+.. currentmodule:: rcapi
+.. moduleauthor:: dboekelman <dboekelman@rackcorp.com>
+"""
+
+from .version import __version__, __release__  # noqa
---
+++ packages/rackcorp-api-client/rackcorp_api_client-0.0.4-py3-none-any.whl/rcapi/version.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+This module contains project version information.
+
+.. currentmodule:: rcapi.version
+.. moduleauthor:: dboekelman <dboekelman@rackcorp.com>
+"""
+
+__version__ = "0.0.4"  #: the working version
+__release__ = "0.0.4"  #: the release version
