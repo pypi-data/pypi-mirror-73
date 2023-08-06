3703e987afe8804015a8f1fcc719c3a7bebc1023
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:33 +0000

    Add package lagom/0.15.0/lagom-0.15.0.tar.gz

---
+++ packages/lagom/lagom-0.15.0.tar.gz/lagom-0.15.0/scripts/simple_timing.py
@@ -0,0 +1,35 @@
+from lagom import Container, Singleton, bind_to_container
+import time
+
+
+class SomeService:
+    def do_it(self):
+        pass
+
+
+class AThingIMightNeed:
+    service: SomeService
+
+    def __init__(self, service: SomeService):
+        self.service = service
+
+    def do_it(self):
+        self.service.do_it()
+
+
+container = Container()
+
+
+@bind_to_container(container, shared=[SomeService])
+def do_work(thing: AThingIMightNeed):
+    thing.do_it()
+
+
+if __name__ == "__main__":
+    start = time.perf_counter()
+    for i in range(0, 800_00):
+        do_work()
+    end = time.perf_counter()
+    print(f"Time taken {end - start}")
+
+# 2.0
