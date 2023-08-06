65fc488370916a991c48bd6c8277e098092858d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:29 +0000

    Add package jsonbourne/0.4.3/jsonbourne-0.4.3.tar.gz

---
+++ packages/jsonbourne/jsonbourne-0.4.3.tar.gz/jsonbourne-0.4.3/jsonbourne/jsonlib/_json_stdlib.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+import json
+from typing import Any
+from typing import Callable
+from typing import Optional
+
+from jsonbourne.jsonlib.base import JsonLib
+from jsonbourne.jsonlib.base import _json_encode_default
+
+
+class JSON_STDLIB(JsonLib):
+    @staticmethod
+    def dumps(
+        data: Any,
+        pretty: bool = False,
+        sort_keys: bool = False,
+        default: Optional[Callable[[Any], Any]] = None,
+        **kwargs: Any
+    ) -> str:
+        separators = (",", ": ") if pretty else (",", ":")
+        return json.dumps(
+            data,
+            indent=2 if pretty else None,
+            sort_keys=sort_keys,
+            separators=separators,
+            default=default or _json_encode_default,
+            **kwargs,
+        )
+
+    @staticmethod
+    def dumpb(
+        data: Any,
+        pretty: bool = False,
+        sort_keys: bool = False,
+        default: Optional[Callable[[Any], Any]] = None,
+        **kwargs: Any
+    ) -> bytes:
+        return JSON_STDLIB.dumps(
+            data, pretty=pretty, sort_keys=sort_keys, default=default, **kwargs
+        ).encode()
+
+    @staticmethod
+    def loads(string: str, **kwargs: Any) -> Any:
+        return json.loads(string, **kwargs)
+
+
+JSONEncoder = json.JSONEncoder
+JSONDecoder = json.JSONDecoder
+JSONDecodeError = json.JSONDecodeError
+
+if __name__ == "__main__":
+    pass
