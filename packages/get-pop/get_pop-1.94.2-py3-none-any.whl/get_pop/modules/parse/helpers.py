dcfb34f67c4539cff0b306b66d20aefe2f09bdea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:02 +0000

    Add package get-pop/1.94.2/get_pop-1.94.2-py3-none-any.whl

---
+++ packages/get-pop/get_pop-1.94.2-py3-none-any.whl/get_pop/modules/parse/helpers.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+
+def clean_county(df: pd.DataFrame, field: str):
+    df[field] = df[field].str.replace(" County", "")
+    return df
+
+
+def merge_nyc_boroughs(df: pd.DataFrame):
+    """
+    Combines the five buroughs to create NYC row
+    """
+    df_buroughs = df.loc[
+        df["name"].isin(["Bronx", "Queens", "New York", "Kings", "Richmond"])
+    ]
+    nyc_pop = df_buroughs["population"].sum()
+    return df.append(
+        {"name": "New York City", "population": nyc_pop}, ignore_index=True
+    )
