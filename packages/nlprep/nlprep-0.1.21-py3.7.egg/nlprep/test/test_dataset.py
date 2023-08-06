4e5c8e782e3fa8d190311b980f43902ed9e098b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:00 +0000

    Add package nlprep/0.1.21/nlprep-0.1.21-py3.7.egg

---
+++ packages/nlprep/nlprep-0.1.21-py3.7.egg/nlprep/test/test_dataset.py
@@ -0,0 +1,21 @@
+import importlib
+import os
+import unittest
+
+import pytest
+
+
+class TestDataset(unittest.TestCase):
+
+    def testType(self):
+        ROOT_DIR = os.path.dirname(os.path.abspath(__file__ + "/../"))
+        DATASET_DIR = os.path.join(ROOT_DIR, './datasets')
+        datasets = list(filter(
+            lambda x: os.path.isdir(os.path.join(DATASET_DIR, x)) and '__pycache__' not in x and x != "clas_csv",
+            os.listdir(DATASET_DIR)))
+        for dataset in datasets:
+            print(dataset)
+            ds = importlib.import_module('.' + dataset, 'nlprep.datasets')
+            self.assertTrue("DATASETINFO" in dir(ds))
+            self.assertTrue("load" in dir(ds))
+            self.assertTrue(ds.DATASETINFO['TASK'] in ['clas', 'tag', 'qa', 'gen'])
