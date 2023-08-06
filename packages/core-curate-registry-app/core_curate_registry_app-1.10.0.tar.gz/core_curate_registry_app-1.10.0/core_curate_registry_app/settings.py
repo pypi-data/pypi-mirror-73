90cabed0733a66fc18c96bb3419e8492b1653418
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:47 +0000

    Add package core-curate-registry-app/1.10.0/core_curate_registry_app-1.10.0.tar.gz

---
+++ packages/core-curate-registry-app/core_curate_registry_app-1.10.0.tar.gz/core_curate_registry_app-1.10.0/core_curate_registry_app/settings.py
@@ -0,0 +1,21 @@
+"""Core Curate App Settings
+"""
+import os
+
+from django.conf import settings
+
+if not settings.configured:
+    settings.configure()
+
+REGISTRY_XSD_FILENAME = getattr(settings, "REGISTRY_XSD_FILENAME", "")
+""" str: Registry xsd filename used for the initialisation.
+"""
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+LOCALE_PATHS = (os.path.join(BASE_DIR, "core_curate_registry_app/locale"),)
+
+
+XPATH_TITLE = "/rsm:Resource[1]/rsm:identity[1]/rsm:title"
+""" str : Xpath of the resource name/title 
+"""
