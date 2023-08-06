c9cb05638020888c6934403abbc6da3f3af8f1c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:14 +0000

    Add package django-salesforce/1.1/django-salesforce-1.1.tar.gz

---
+++ packages/django-salesforce/django-salesforce-1.1.tar.gz/django-salesforce-1.1/salesforce/__init__.py
@@ -0,0 +1,26 @@
+# django-salesforce
+#
+# by Phil Christensen
+# (c) 2012-2013 Freelancers Union (http://www.freelancersunion.org)
+# See LICENSE.md for details
+#
+
+"""
+A database backend for the Django ORM.
+
+Allows access to all Salesforce objects accessible via the SOQL API.
+"""
+import logging
+
+from salesforce.dbapi.exceptions import (  # NOQA pylint:disable=unused-import,useless-import-alias
+    IntegrityError as IntegrityError, DatabaseError as DatabaseError, SalesforceError as SalesforceError,
+)
+
+__version__ = "1.1"
+
+log = logging.getLogger(__name__)
+
+# Default version of Force.com API.
+# It can be customized by settings.DATABASES['salesforce']['API_VERSION']
+# API_VERSION = '49.0'  # Summer '20
+API_VERSION = '48.0'  # Spring '20
