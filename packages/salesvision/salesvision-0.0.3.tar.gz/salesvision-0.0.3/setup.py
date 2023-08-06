65a78dfdf94f5d32a54d4ae8f341d0a4fc20f89a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:30 +0000

    Add package salesvision/0.0.3/salesvision-0.0.3.tar.gz

---
+++ packages/salesvision/salesvision-0.0.3.tar.gz/salesvision-0.0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="salesvision", # Replace with your own username
+    version="0.0.3",
+    author="broutonlab",
+    author_email="m.yurushkin@broutonlab.com",
+    description="Salesvision provides accurate, reliable and scalable fashion image analysis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
