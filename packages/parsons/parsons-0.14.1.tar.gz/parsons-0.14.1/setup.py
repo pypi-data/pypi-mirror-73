7276f617f000b4a761f47b5bcb4774fb620b65b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:46 +0000

    Add package parsons/0.14.1/parsons-0.14.1.tar.gz

---
+++ packages/parsons/parsons-0.14.1.tar.gz/parsons-0.14.1/setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import find_packages
+from distutils.core import setup
+
+THIS_DIR = os.path.abspath(os.path.dirname(__file__))
+
+
+def main():
+    with open(os.path.join(THIS_DIR, 'requirements.txt')) as reqs:
+        requirements = reqs.read().strip().split('\n')
+
+    setup(
+        name="parsons",
+        version='0.14.1',
+        author="The Movement Cooperative",
+        author_email="info@movementcooperative.org",
+        url='https://github.com/movementcoop/parsons',
+        keywords=['PROGRESSIVE', 'API', 'ETL'],
+        packages=find_packages(),
+        install_requires=requirements,
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7'
+        ]
+    )
+
+
+if __name__ == "__main__":
+    main()
