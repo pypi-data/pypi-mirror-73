ae01293a44c41466a2f4cccb1dd337fedb2f0d23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:31 +0000

    Add package py-redact/0.0.5/py_redact-0.0.5.tar.gz

---
+++ packages/py-redact/py_redact-0.0.5.tar.gz/py_redact-0.0.5/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="py_redact",
+    version="0.0.5",
+    author="Fahad Siddiqui",
+    author_email="fsdqui@gmail.com",
+    description="A python package to redact documents by regexes.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/datumbrain/py-redact",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'python-docx==0.8.10',
+        'python-pptx==0.6.18'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
