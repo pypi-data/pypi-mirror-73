03cf135acbb866f2ca9c8bb96dd09982ec7d3937
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:02 +0000

    Add package distributions-cyf/2.0/distributions_cyf-2.0.tar.gz

---
+++ packages/distributions-cyf/distributions_cyf-2.0.tar.gz/distributions_cyf-2.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_cyf',
+      version='2.0',
+      description='Gaussian distributions',
+      packages=['distributions_cyf'],
+      zip_safe=False)
