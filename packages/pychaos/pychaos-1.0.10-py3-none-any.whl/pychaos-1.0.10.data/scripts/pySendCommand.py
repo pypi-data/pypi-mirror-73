47fe5cc71fee249b3d560ed3eebef827218cc322
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:12 +0000

    Add package pychaos/1.0.10/pychaos-1.0.10-py3-none-any.whl

---
+++ packages/pychaos/pychaos-1.0.10-py3-none-any.whl/pychaos-1.0.10.data/scripts/pySendCommand.py
@@ -0,0 +1,43 @@
+#!python
+
+# import pychaos
+import sys
+import time
+import os
+import bson
+from kafka import KafkaProducer
+
+
+if len(sys.argv)<4:
+    print("You must specify a <broker:port [9092]> <NodeUID> <cmdname> [JSON VALUE]");
+    sys.exit(1);
+
+topic=sys.argv[2].replace('/','.')
+topic=topic+"_cmd"
+print("sending to:"+sys.argv[1]+" topic:"+topic)
+cmd={}
+
+cmd['bc_alias']=""
+producer = KafkaProducer(bootstrap_servers=sys.argv[1],acks='all',batch_size=0)
+
+if producer.bootstrap_connected():
+    print("Connected")
+else:
+    print("cannot connect to:"+sys.argv[1])
+    sys.exit(1)
+
+if len(sys.argv)==5:
+    cmd=sys.argv[4]
+
+cmd['bc_alias']=sys.argv[3]
+encoded=bson.dumps(cmd)
+
+future = producer.send(topic,encoded)
+result=future.get(timeout=60)
+
+if result:
+    print("sent ok")
+else:
+    print("## error sending "+cmd )
+
+sys.exit(0)
