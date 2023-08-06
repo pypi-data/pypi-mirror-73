e61e543ab9a896ae75f339ae08c80759fcde4841
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:13 +0000

    Add package prob-distrok/0.1/prob_distrok-0.1.tar.gz

---
+++ packages/prob-distrok/prob_distrok-0.1.tar.gz/prob_distrok-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='prob_distrok',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author='Godfrey Tutu',
+      author_email= 'godfrey_tutu@yahoo.com',
+      zip_safe=False)
