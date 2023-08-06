03438fa2422252ba74744d52311ed927f3dc8c0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:13 +0000

    Add package sui/0.1.7/sui-0.1.7-py3-none-any.whl

---
+++ packages/sui/sui-0.1.7-py3-none-any.whl/sui/rl/__init__.py
@@ -0,0 +1,19 @@
+"""sui.rl
+Reinforcement learning algorithm implementations
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+__api_info_dict = {}
+
+
+def api_info(api=None):
+    if api is not None:
+        if api in __api_info_dict:
+            return 'API: {}\nInfo: {}\n'.format(api, __api_info_dict[api])
+        else:
+            return '{} is not an appreciable API under sui.rl'.format(api)
+    else:
+        for api, info in __api_info_dict.items():
+            return 'API: {}\nInfo: {}\n'.format(api, info)
