19eb57bd4cde2d7265cc762a5f43169cfe8b4709
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:02 +0000

    Add package host-checker/1.0.2/host-checker-1.0.2.tar.gz

---
+++ packages/host-checker/host-checker-1.0.2.tar.gz/host-checker-1.0.2/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="host-checker",
+    version="1.0.2",
+    description="Simple CLI tool to check who is hosting a website",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SimoneCorazza/HostChecker",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
