c3aac3334f51486d90716f334693ba58e1b7c4af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:14 +0000

    Add package dexterous-gym/0.1.1/dexterous_gym-0.1.1.tar.gz

---
+++ packages/dexterous-gym/dexterous_gym-0.1.1.tar.gz/dexterous_gym-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+import setuptools
+
+setup(name='dexterous_gym',
+      version='0.1.1',
+      description='Challenging extensions to openAI Gyms hand manipulation environments',
+      url='http://github.com/henrycharlesworth/dexterous_gym',
+      author='Henry Charlesworth',
+      author_email='H.Charlesworth@warwick.ac.uk',
+      packages=setuptools.find_packages(),
+      zip_safe=False)
