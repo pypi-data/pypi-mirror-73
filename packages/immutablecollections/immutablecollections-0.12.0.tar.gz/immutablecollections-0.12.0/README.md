201e642d6df2742b3454b0130b3c538b14a8f064
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:39 +0000

    Add package immutablecollections/0.12.0/immutablecollections-0.12.0.tar.gz

---
+++ packages/immutablecollections/immutablecollections-0.12.0.tar.gz/immutablecollections-0.12.0/README.md
@@ -0,0 +1,12 @@
+# immutablecollections
+A library for immutable collections, in the spirit of Guava's Immutable Collections.
+
+[![Build status](https://ci.appveyor.com/api/projects/status/2cg5pi8pc46gp11l/branch/master?svg=true)](https://ci.appveyor.com/project/rgabbard/immutablecollections/branch/master)
+[![codecov](https://codecov.io/gh/isi-vista/immutablecollections/branch/master/graph/badge.svg)](https://codecov.io/gh/isi-vista/immutablecollections)
+
+
+# Contributing
+
+Run `make precommit` before commiting. Eventually this will be automated.
+
+You can run `make benchmark` to run (depressing) benchmarks.
---
+++ packages/immutablecollections/immutablecollections-0.12.0.tar.gz/immutablecollections-0.12.0/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from os.path import abspath, dirname, join
+
+from setuptools import find_packages
+
+_name = 'immutablecollections'
+
+with open(join(dirname(abspath(__file__)), _name, 'version.py')) as version_file:
+    exec(compile(version_file.read(), "version.py", 'exec'))
+
+setup(name=_name,
+      version=version,
+      author='Ryan Gabbard <gabbard@isi.edu> and Constantine Lignos <lignos@isi.edu>',
+      author_email='gabbard@isi.edu',
+      description='Immutable Collections (inspired by Google Guava)',
+      url='https://github.com/isi-vista/immutablecollections',
+      packages=['immutablecollections'],
+      package_data={'immutablecollections': ['py.typed']},
+      # 3.6 and up, but not Python 4
+      python_requires='~=3.6',
+      install_requires=[
+          'typing_extensions',
+      ],
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ]
+)
