b3bac4fee4c0eefd336c331bdf60aa3e52e3c2a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:45 +0000

    Add package django-rest-xauth/1.0.9/django-rest-xauth-1.0.9.tar.gz

---
+++ packages/django-rest-xauth/django-rest-xauth-1.0.9.tar.gz/django-rest-xauth-1.0.9/xauth/serializers/__init__.py
@@ -0,0 +1,23 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+from xauth.serializers.profile import request_serializer_class
+
+
+class AuthTokenOnlySerializer(serializers.HyperlinkedModelSerializer):
+    normal = serializers.CharField(source='token.tokens.normal', read_only=True, )
+    encrypted = serializers.CharField(source='token.tokens.encrypted', read_only=True, )
+
+    class Meta:
+        model = get_user_model()
+        fields = 'normal', 'encrypted',
+
+
+class AuthSerializer(request_serializer_class):
+    token = serializers.DictField(source='token.tokens', read_only=True, )
+
+    class Meta(request_serializer_class.Meta):
+        fields = tuple(request_serializer_class.Meta.fields) + ('token',)
+
+    def validate(self, attrs):
+        return super().validate(attrs)
