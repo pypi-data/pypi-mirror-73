450da38faa8ba79d0d246eff2ddb86825c648b71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:38 +0000

    Add package ratsnlp/0.0.1/ratsnlp-0.0.1.tar.gz

---
+++ packages/ratsnlp/ratsnlp-0.0.1.tar.gz/ratsnlp-0.0.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name="ratsnlp",
+    version="0.0.1",
+    license='MIT',
+    author="ratsgo",
+    author_email="ratsgo@naver.com",
+    description="tools for Natural Language Processing",
+    long_description=open('README.md').read(),
+    url="https://github.com/ratsgo/ratsnlp",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "tqdm",
+        "requests",
+        "logging",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+)
