c419515ad61ee0e10ec2c364f8b9ad69605e41e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:53 +0000

    Add package robotframework-testfors/2.4.1/robotframework-testfors-2.4.1.tar.gz

---
+++ packages/robotframework-testfors/robotframework-testfors-2.4.1.tar.gz/robotframework-testfors-2.4.1/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import version
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open("docs/README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(name='robotframework-testfors',
+      version=version.VERSION,
+      description='Robot Framework mini-framework.',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/Accruent/zoomba',
+      maintainer='zwang',
+      maintainer_email='836425419@qq.com',
+      license='GPL-3.0',
+      keywords='Robot Framework robot-framework selenium requests appium soap winappdriver appium robotframework'
+               'desktop windows zoomba python robotframework-library appium-windows appiumlibrary api-rest api '
+               'soap-api appium-mobile mobile',
+      platforms='any',
+      install_requires=requirements,
+      extras_require={
+        'testing': [
+          'Appium-Python-Client'
+        ]
+      },
+      classifiers="""
+        Development Status :: 5 - Production/Stable
+        Operating System :: OS Independent
+        Programming Language :: Python :: 3
+        Topic :: Software Development :: Testing
+        Framework :: Robot Framework :: Library
+        """.strip().splitlines(),
+      package_dir={'': 'src'},
+      packages=['testfors']
+      )
