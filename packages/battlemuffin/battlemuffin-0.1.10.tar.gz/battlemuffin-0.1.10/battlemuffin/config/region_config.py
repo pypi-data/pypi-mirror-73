4ccf391a37692a0a378ac18371de31b36eb636a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:39 +0000

    Add package battlemuffin/0.1.10/battlemuffin-0.1.10.tar.gz

---
+++ packages/battlemuffin/battlemuffin-0.1.10.tar.gz/battlemuffin-0.1.10/battlemuffin/config/region_config.py
@@ -0,0 +1,25 @@
+from enum import Enum
+
+
+class Region(Enum):
+    us = 1
+    eu = 3
+    kr = 2
+    tw = 4
+    cn = 5
+
+
+class Locale(Enum):
+    en_US = 1
+    es_MX = 2
+    pt_BR = 3
+    en_GB = 4
+    es_ES = 5
+    fr_FR = 6
+    ru_RU = 7
+    de_DE = 8
+    pt_PT = 9
+    it_IT = 10
+    ko_KR = 11
+    zh_TW = 12
+    zh_CN = 13
