8feb5aee730993dd2df09c42fa9f6e7d780ce5e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:33 +0000

    Add package dstbn-pack/0.1/dstbn_pack-0.1.tar.gz

---
+++ packages/dstbn-pack/dstbn_pack-0.1.tar.gz/dstbn_pack-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dstbn_pack',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dstbn_pack'],
+      Author = 'Chy.Md.Shahrear',
+    zip_safe = False)
