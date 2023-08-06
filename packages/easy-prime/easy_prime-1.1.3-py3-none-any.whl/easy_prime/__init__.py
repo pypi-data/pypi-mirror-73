9ed1a098fd91c5e3e3e78f9408df5ff1d5751d25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:08 +0000

    Add package easy-prime/1.1.3/easy_prime-1.1.3-py3-none-any.whl

---
+++ packages/easy-prime/easy_prime-1.1.3-py3-none-any.whl/easy_prime/__init__.py
@@ -0,0 +1,10 @@
+"""
+Using __init__.py to organize the structure.
+"""
+try:
+	from ._target_mutation import target_mutation
+except ImportError as e:
+	print (e)
+
+__all__ = ["target_mutation"]
+__version__ = "1.1.3"
