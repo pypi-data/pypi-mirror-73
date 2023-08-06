bec37b8ce2331edf6e476a4980869f9f5add70e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:38 +0000

    Add package easyweb3/0.3.1/easyweb3-0.3.1-py3-none-any.whl

---
+++ packages/easyweb3/easyweb3-0.3.1-py3-none-any.whl/easyweb3/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from .easyweb3 import EasyWeb3
+import logging
+
+__version__ = '0.3.1'
+
+logging.getLogger().setLevel(logging.INFO)
+logging.basicConfig(format='%(asctime)-15s [%(levelname)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+logging.info(f'EasyWeb3 v{__version__}')
