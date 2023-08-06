410c30d7aba581c193344f37b98a6dd63372081f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:42 +0000

    Add package fret/0.2.10/fret-0.2.10.tar.gz

---
+++ packages/fret/fret-0.2.10.tar.gz/fret-0.2.10/fret/__init__.py
@@ -0,0 +1,43 @@
+import logging as _logging
+
+from .util import nonbreak, stateful, set_saver, set_loader
+from .util import colored as _colored
+from .app import workspace, configurable, command, argspec, clean, summarize
+
+_LOGGER = _logging.getLogger()
+_LOGGER.setLevel(_logging.INFO)
+
+
+class _ColoredFormatter(_logging.Formatter):
+    """Formatter for colored log."""
+    _LOG_COLORS = {
+        'WARNING': 'y',
+        'INFO': 'g',
+        'DEBUG': 'b',
+        'CRITICAL': 'y',
+        'ERROR': 'r'
+    }
+
+    def format(self, record):
+        levelname = record.levelname
+        if levelname in self._LOG_COLORS:
+            record.levelname = _colored(
+                record.levelname[0],
+                self._LOG_COLORS[record.levelname],
+                style='b'
+            )
+        return _logging.Formatter.format(self, record)
+
+
+_LOG_FORMATTER = _ColoredFormatter(
+    '%(levelname)s %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+_CONSOLE_HANDLER = _logging.StreamHandler()
+_CONSOLE_HANDLER.setFormatter(_LOG_FORMATTER)
+_LOGGER.addHandler(_CONSOLE_HANDLER)
+
+__all__ = ['nonbreak', 'stateful', 'workspace', 'configurable', 'command',
+           'argspec', 'clean', 'summarize', 'set_saver', 'set_loader']
+
+__version__ = '0.2.10'
---
+++ packages/fret/fret-0.2.10.tar.gz/fret-0.2.10/fret/__main__.py
@@ -0,0 +1,4 @@
+from .cli import main  # pragma: no cover
+
+if __name__ == '__main__':
+    main()
