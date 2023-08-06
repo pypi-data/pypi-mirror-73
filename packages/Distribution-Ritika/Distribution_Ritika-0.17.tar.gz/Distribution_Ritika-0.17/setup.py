1f02f954eb4a017ac6a285f24fde748183770d14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:53 +0000

    Add package Distribution-Ritika/0.17/Distribution_Ritika-0.17.tar.gz

---
+++ packages/Distribution-Ritika/Distribution_Ritika-0.17.tar.gz/Distribution_Ritika-0.17/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='Distribution_Ritika',
+      version='0.17',
+      description='Gaussian and Binomial distributions',
+      packages=['Distribution_Ritika'],
+      author='Ritika_Gupta',
+      zip_safe=False)
