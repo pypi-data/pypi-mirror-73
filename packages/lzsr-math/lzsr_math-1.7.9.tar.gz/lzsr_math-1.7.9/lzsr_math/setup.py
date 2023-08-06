00517037a8c2eb347cbaf5e0b8a0bb3bb2fb9d16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:05 +0000

    Add package lzsr-math/1.7.9/lzsr_math-1.7.9.tar.gz

---
+++ packages/lzsr-math/lzsr_math-1.7.9.tar.gz/lzsr_math-1.7.9/lzsr_math/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lzsr_math",
+    version='1.7.9',
+    author="lzsr",
+    author_email="y15305083929@outlook.com",
+    description="The Python package can help you simplify some code in programming.",
+    long_description='The Python package can undertake some mathematical calculations, and some string encryption and decryption,Can help you to simplify the programming code.',
+    long_description_content_type="text/markdown",
+    url="https://github.com/lzsr/lzsr",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
