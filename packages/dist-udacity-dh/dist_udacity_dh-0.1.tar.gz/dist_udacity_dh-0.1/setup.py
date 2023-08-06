d9c0cff197e6ee565911f462a84db47b97c9328c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:22 +0000

    Add package dist-udacity-dh/0.1/dist_udacity_dh-0.1.tar.gz

---
+++ packages/dist-udacity-dh/dist_udacity_dh-0.1.tar.gz/dist_udacity_dh-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dist_udacity_dh',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dist_udacity_dh'],
+      author='Daria HERVIEUX',
+      zip_safe=False)
