64893c6eee092f4b70a428083d7363f52279f41e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:46 +0000

    Add package beginnerpy/0.1.0/BeginnerPy-0.1.0.tar.gz

---
+++ packages/beginnerpy/BeginnerPy-0.1.0.tar.gz/BeginnerPy-0.1.0/beginnerpy/challenges/adventure_game/tests.py
@@ -0,0 +1,79 @@
+from beginnerpy.challenges.adventure_game.solver import find_the_holy_grail
+from beginnerpy.challenges.adventure_game.game_builder import generate_game
+import unittest
+
+
+def run_tests():
+    unittest.main(module=__name__)
+
+
+class TestFindTheHolyGrail(unittest.TestCase):
+    def test_1(self):
+        player, grail = generate_game(1)
+        self.assertIs(
+            grail,
+            find_the_holy_grail(player)
+        )
+
+    def test_2(self):
+        player, grail = generate_game(2)
+        self.assertIs(
+            grail,
+            find_the_holy_grail(player)
+        )
+
+    def test_3(self):
+        player, grail = generate_game(3)
+        self.assertIs(
+            grail,
+            find_the_holy_grail(player)
+        )
+
+    def test_4(self):
+        player, grail = generate_game(4)
+        self.assertIs(
+            grail,
+            find_the_holy_grail(player)
+        )
+
+    def test_5(self):
+        player, grail = generate_game(5)
+        self.assertIs(
+            grail,
+            find_the_holy_grail(player)
+        )
+
+    def test_6(self):
+        player, grail = generate_game(6)
+        self.assertIs(
+            grail,
+            find_the_holy_grail(player)
+        )
+
+    def test_7(self):
+        player, grail = generate_game(7)
+        self.assertIs(
+            grail,
+            find_the_holy_grail(player)
+        )
+
+    def test_8(self):
+        player, grail = generate_game(8)
+        self.assertIs(
+            grail,
+            find_the_holy_grail(player)
+        )
+
+    def test_9(self):
+        player, grail = generate_game(9)
+        self.assertIs(
+            grail,
+            find_the_holy_grail(player)
+        )
+
+    def test_10(self):
+        player, grail = generate_game(10)
+        self.assertIs(
+            grail,
+            find_the_holy_grail(player)
+        )
