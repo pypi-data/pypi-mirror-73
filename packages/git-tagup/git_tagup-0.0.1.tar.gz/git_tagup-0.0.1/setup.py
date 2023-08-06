dc2e83edc6794661d4535a5c3b5c2c265b93b718
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:39 +0000

    Add package git-tagup/0.0.1/git_tagup-0.0.1.tar.gz

---
+++ packages/git-tagup/git_tagup-0.0.1.tar.gz/git_tagup-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="git_tagup",
+    version="0.0.1",
+    author="Jacob Stopak",
+    author_email="jacob@initialcommit.io",
+    description="A command-line tool for finding and tagging relevant Git commits based on existing commit messages.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://initialcommit.com/projects/git-tagup",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
