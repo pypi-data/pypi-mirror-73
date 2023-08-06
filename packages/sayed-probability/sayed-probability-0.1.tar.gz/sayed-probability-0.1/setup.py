9cd2397a8532f641a17d325013fc7f274a926765
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:42 +0000

    Add package sayed-probability/0.1/sayed-probability-0.1.tar.gz

---
+++ packages/sayed-probability/sayed-probability-0.1.tar.gz/sayed-probability-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='sayed-probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['sayed-probability'],
+      author='Sayedur Rahman',
+      zip_safe=False)
