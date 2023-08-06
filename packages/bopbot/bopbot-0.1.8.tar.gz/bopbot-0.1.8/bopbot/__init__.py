4539a72d3a70c8c63ca24e2aaffedb033d6233a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:26 +0000

    Add package bopbot/0.1.8/bopbot-0.1.8.tar.gz

---
+++ packages/bopbot/bopbot-0.1.8.tar.gz/bopbot-0.1.8/bopbot/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.1.8"
+
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
---
+++ packages/bopbot/bopbot-0.1.8.tar.gz/bopbot-0.1.8/bopbot/browser/exceptions.py
@@ -0,0 +1,8 @@
+class BrowserSetupError(Exception):
+    pass
+
+
+class PageError(Exception):
+    """Raised when loading page times out"""
+
+    pass
