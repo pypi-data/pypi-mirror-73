5fdb51b4249ddbd4d6e250bee12ab27976e06906
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:14 +0000

    Add package ip-inspector/0.0.11/ip_inspector-0.0.11-py3-none-any.whl

---
+++ packages/ip-inspector/ip_inspector-0.0.11-py3-none-any.whl/ip_inspector/__init__.py
@@ -0,0 +1,213 @@
+import os
+import shutil
+import logging
+
+from ip_inspector.config import CONFIG, WORK_DIR
+from ip_inspector import maxmind, tor
+
+
+class Inspected_IP(maxmind.MaxMind_IP):
+    """A MaxMind_IP result with whitelist/blacklist metadata added."""
+
+    def __init__(self, asn_result, city_result, country_result, blacklist_type=None, whitelist_type=None, tor_exit_node=False):
+        super().__init__(asn_result, city_result, country_result)
+        self._blacklist_str = '{} (!BLACKLISTED!)'
+        self._whitelist_str = '{} (whitelisted)'
+        self.blacklist_reason = blacklist_type
+        self.whitelist_reason = whitelist_type
+        self._blacklisted = False
+        self._whitelisted = False
+        if blacklist_type and whitelist_type:
+            logging.error("IP is both whitelisted and blacklisted!")
+        if blacklist_type:
+            self._blacklisted = True
+        if whitelist_type:
+            self._whitelisted = True
+        self.is_tor = tor_exit_node
+        # expand the map
+        if self.is_tor:
+            self.map['TOR'] = self.is_tor
+
+    def set_blacklist(self, blacklist_type):
+        self.blacklist_reason = blacklist_type
+        self._blacklisted = True
+
+    def set_whitelist(self, whitelist_type):
+        self.whitelist_reason = whitelist_type
+        self._whitelisted = True
+
+    @property
+    def is_whitelisted(self):
+        return self._whitelisted
+
+    @property
+    def is_blacklisted(self):
+        return self._blacklisted
+
+    @property
+    def reason(self):
+        if self.is_blacklisted:
+            return self.map[self.blacklist_reason]
+        if self.is_whitelisted:
+            return self.map[self.whitelist_reason]
+
+    def __str__(self):
+        if self.is_blacklisted:
+            self.map[self.blacklist_reason] = self._blacklist_str.format(self.get(self.blacklist_reason))
+        if self.is_whitelisted:
+            self.map[self.whitelist_reason] = self._whitelist_str.format(self.get(self.whitelist_reason))
+        txt = "\t--------------------\n"
+        for field in self.map:
+            if field == 'IP' and self.is_tor:
+                txt += "\t{}: {}\n".format(field, "{} (TOR EXIT)".format(self.get(field)))
+            elif self.get(field):
+                txt += "\t{}: {}\n".format(field, self.get(field))
+            else:
+                txt += "\t{}: {}\n".format(field, '')
+        return(txt)
+
+
+class Inspector():
+    """An a computer network inspector for the primary purpose of
+       Intel & Detection. Wrapper around maxmind.Client.
+
+    :mmc: MaxMind Clietn API object to query for resources.
+    :blacklists: Detection files.
+    :whitelists: good guys.
+    """
+
+    def __init__(self,
+                 mmc: maxmind.Client = maxmind.Client() or None,
+                 blacklists=CONFIG['default']['blacklists'],
+                 whitelists=CONFIG['default']['whitelists'],
+                 tor_exits: tor.ExitNodes = tor.ExitNodes() or None
+                 ):
+       
+        self.mmc = mmc
+        self.blacklists = {}
+        self.whitelists = {}
+        self.tor_exits = tor_exits
+        for bl_type, bl_path in blacklists.items():
+            full_path = bl_path
+            if not os.path.exists(full_path):
+                full_path = os.path.join(WORK_DIR, full_path)
+                if not os.path.exists(full_path):
+                    logging.debug("No {} blacklist found at {} or {}".format(bl_type, bl_path, full_path))
+                    continue
+            with open(full_path, 'r') as fp:
+                self.blacklists[bl_type] = [line.strip() for line in fp.readlines()]
+        for wl_type, wl_path in whitelists.items():
+            full_path = wl_path
+            if not os.path.exists(full_path):
+                full_path = os.path.join(WORK_DIR, full_path)
+                if not os.path.exists(full_path):
+                    logging.debug("No {} whitelist found at {} or {}".format(wl_type, wl_path, full_path))
+                    continue
+            with open(full_path, 'r') as fp:
+                self.whitelists[wl_type] = [line.strip() for line in fp.readlines()]
+
+
+    def inspect(self, ip):
+        """Check IP metadata against blacklist detections.
+
+        :param ip: IPv4 or IPv6
+        :return: 
+        """
+        try:
+            IIP = Inspected_IP(self.mmc.asn(ip), self.mmc.city(ip), self.mmc.country(ip), tor_exit_node=self.tor_exits.is_exit_node(ip))
+            for blacklist_type in self.blacklists.keys():
+                if IIP.get(blacklist_type) in self.blacklists[blacklist_type]:
+                    logging.debug("Blacklisted {} for {} : {}".format(blacklist_type, ip, IIP.get(blacklist_type)))
+                    IIP.set_blacklist(blacklist_type)
+            for whitelist_type in self.whitelists.keys():
+                if IIP.get(whitelist_type) in self.whitelists[whitelist_type]:
+                    logging.debug("Whitelisted {} for {} : {}".format(whitelist_type, ip, IIP.get(whitelist_type)))
+                    IIP.set_whitelist(whitelist_type)
+            return IIP
+        except Exception as e:
+            logging.warning("Problem inspecting ip={} : {}".format(ip, e))
+            return None
+
+    def get(self, ip):
+        """For convienice switching between Inspector and MaxMind Client"""
+        return self.inspect(ip)
+
+
+def append_to_(list_type, iip: Inspected_IP, field='ORG', list_path=None, force=False):
+    """Append to a whitelist OR blacklist.
+    """
+
+    if field not in CONFIG['default'][list_type+'s'].keys():
+        logging.warning("{} type '{}' not defined in default config".format(list_type, field))
+
+    full_path = os.path.join(WORK_DIR, CONFIG['default'][list_type+'s'][field])
+    if list_path and os.path.exists(list_path):
+       full_path = list_path
+    if not os.path.exists(full_path):
+        logging.warning("Creating {} at {}.".format(list_type, full_path))
+
+    logging.debug("Using {} at '{}'".format(list_type, full_path))
+    try:
+        origional_map = iip.build_map()
+        value = origional_map[field]
+        if iip.is_blacklisted and list_type == 'blacklist':
+            logging.warning("'{}' already defined in blacklist.".format(value))
+            return None
+        elif iip.is_whitelisted and list_type == 'whitelist':
+            logging.warning("'{}' already defined in whitelist.".format(value))
+            return None
+        # By default, do not blacklist things that are whitelisted and vice versa
+        if not force:
+            if iip.is_whitelisted and list_type == 'blacklist':
+                logging.warning("'{}' is whitelisted. Not adding to blacklist.".format(value))
+                return False
+            if iip.is_blacklisted and list_type == 'whitelist':
+                logging.warning("'{}' is blacklisted. Not adding to whitelist.".format(value))
+                return False
+        logging.info("Appending '{}'  to {} {}".format(value, field, list_type))
+        with open(full_path, 'a+') as l:
+            l.write(value+'\n')
+    except Exception as e:
+        logging.error("problem appending to {}: {}".format(list_type, e))
+        return False
+
+    return True
+
+def remove_from_(list_type, iip: Inspected_IP, field='ORG', list_path=None):
+    """remove from a whitelist OR blacklist.
+    """
+
+    if field not in CONFIG['default'][list_type+'s'].keys():
+        logging.warning("{} type '{}' not defined in default config".format(list_type, field))
+
+    full_path = os.path.join(WORK_DIR, CONFIG['default'][list_type+'s'][field])
+    if list_path and os.path.exists(list_path):
+       full_path = list_path
+    if not os.path.exists(full_path):
+        logging.error("No {} file exists at {}.".format(list_type, list_path))
+        return False
+
+    logging.debug("Using {} at '{}'".format(list_type, full_path))
+    try:
+        with open(full_path, 'r') as l:
+            current_list = l.readlines()
+    except Exception as e:
+        logging.error("Couldn't load current {} at {}".format(list_type, full_path))
+        return False
+    
+    try:
+        origional_map = iip.build_map()
+        value = origional_map[field]
+        if value+'\n' not in current_list:
+            logging.warning("{} already not in {} {}.".format(value, field, list_type))
+            return None
+        logging.info("Removing {} from {} {}".format(value, field, list_type))
+        current_list.remove(value+'\n')
+        with open(full_path, 'w') as fp:
+            for item in current_list:
+                fp.write(item)
+    except Exception as e:
+        logging.error("problem deleteing from {}: {}".format(list_type, e))
+        return False
+
+    return True
---
+++ packages/ip-inspector/ip_inspector-0.0.11-py3-none-any.whl/ip_inspector/cli.py
@@ -0,0 +1,207 @@
+#!/usr/bin/env python3
+# PYTHON_ARGCOMPLETE_OK
+
+import os
+import sys
+import json
+import argparse
+import argcomplete
+import logging
+import coloredlogs
+
+from pprint import pprint
+from ip_inspector.config import CONFIG, WORK_DIR, save, load
+from ip_inspector import maxmind, tor
+from ip_inspector import Inspector, append_to_, remove_from_
+
+def main():
+
+    parser = argparse.ArgumentParser(description="Inspect IP address metadata for IDR purposes")
+    parser.add_argument('-d', '--debug', default=False, action='store_true', help="Turn on debug logging.")
+    parser.add_argument('-u', '--update-databases', default=False, action='store_true', help="Update the MaxMind GeoLite2 Databases")
+    parser.add_argument('-r', '--raw-results', action='store_true', help="return results in their raw json format")
+    parser.add_argument('-pp', '--pretty-print', action='store_true', help="Pretty print the raw json results")
+    parser.add_argument('-i', '--ip', action='store', help="A single IP address to inspect.")
+    parser.add_argument('--print-tor-exits', action='store_true', help="Get tor exist nodes")
+    parser.add_argument('-f', '--field',  action='append', dest='fields', default=[], choices=maxmind.FIELDS, help='specific fields to return')
+    parser.add_argument('-csv', action='store_true', help="print fields as comma seperated with --from-stdin and fields")
+    parser.add_argument('--from-stdin', action='store_true', help="Inspect each IP in a list of IP addresses passed to STDIN")
+    parser.add_argument('-lk', '--license-key', action='store', help='MaxMind Liscense Key (saves to config for future use)')
+    # Alerting not implemented
+    #parser.add_argument('-a', '--alert-toggle', action='store_true', default=CONFIG['default']['alert'],
+    #                    help="If True, Alert when a detection is made. Default configured state: {}".format(CONFIG['default']['alert']))
+    parser.add_argument('-c', '--config-path', action='store',
+        help='A JSON config to override the default configuration. The path is saved for future use.')
+    help_note = ("Write a copy of the existing configuration to the local config path for easily making configuration overrides, "
+                 "changes, or updates. Edit the local config to meet your needs.")
+    parser.add_argument('--customize', action='store_true', help=help_note)
+
+    subparsers = parser.add_subparsers(dest='command')
+
+    wl_parser = subparsers.add_parser('whitelist', help="For interacting with the IP Network Organization whitelist")
+    wl_parser.add_argument('-sp', '--show-path', action='store_true', help="Show the existing whitelist location.")
+    wl_parser.add_argument('-p', '--print', action='store_true', help="Print the existing whitelist.")
+    wl_subparser = wl_parser.add_subparsers(dest='wl_command')
+    wl_add_parser = wl_subparser.add_parser('add', help="Append to a whitelist.")
+    wl_add_parser.add_argument('ip', help="The IP address to work with.")
+    wl_add_parser.add_argument('-t', '--whitelist-type', action='append', default=['ORG'], choices=CONFIG['default']['whitelists'].keys(),
+                                help="The type of metadata from this IP result that should be whitelisted. Can specify multiple times.")
+    wl_remove_parser = wl_subparser.add_parser('remove', help="Remove a whitelist entry.")
+    wl_remove_parser.add_argument('ip', help="The IP address to work with.")
+    wl_remove_parser.add_argument('-t', '--whitelist-type', action='append', default=['ORG'], choices=CONFIG['default']['whitelists'].keys(),
+                                help="The type of metadata from this IP result that should be removed from the respective whitelist. Can specify multiple times.")
+   
+
+    bl_parser = subparsers.add_parser('blacklist', help="For interacting with the IP Network Organization blacklist.")
+    bl_parser.add_argument('-sp', '--show-path', action='store_true', help="Show the existing blacklist location.")
+    bl_parser.add_argument('-p', '--print', action='store_true', help="Print the existing blacklist.")
+    bl_subparser = bl_parser.add_subparsers(dest='bl_command')
+    bl_add_parser = bl_subparser.add_parser('add', help="Append to a blacklist.")
+    bl_add_parser.add_argument('ip', help="The IP address to work with.")
+    bl_add_parser.add_argument('-t', '--blacklist-type', action='append', default=['ORG'], choices=CONFIG['default']['blacklists'].keys(),
+                                help="The type of metadata from this IP result that should be blacklisted. Can specify multiple times.")
+    bl_remove_parser = bl_subparser.add_parser('remove', help="Remove a blacklist entry.")
+    bl_remove_parser.add_argument('ip', help="The IP address to work with.")
+    bl_remove_parser.add_argument('-t', '--blacklist-type', action='append', default=['ORG'], choices=CONFIG['default']['blacklists'].keys(),
+                                help="The type of metadata from this IP result that should be removed from the respective blacklist. Can specify multiple times.")
+
+    argcomplete.autocomplete(parser)
+    args = parser.parse_args()
+
+    # configure logging
+    logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - [%(levelname)s] %(message)s')
+
+    logger = logging.getLogger()
+    coloredlogs.install(level='INFO', logger=logger)
+
+    if args.debug:
+        coloredlogs.install(level='DEBUG', logger=logger)
+
+    if args.print_tor_exits:
+        for EN in tor.ExitNodes().exit_nodes:
+            print(EN)
+
+    if args.config_path:
+        if not os.path.exists(args.config_path):
+            print("That file doesn't exist.")
+            return
+        # TODO weird bug.. if there is a saved config the logging configuration is wiped.
+        logging.warning("KNOWN BUG: if there is a saved config the logging configuration is wiped.")
+        save({'default': { 'saved_config_path': args.config_path}})
+        config = load()
+    else:
+        config = CONFIG
+
+    if args.customize:
+        # save will write the existing loaded configuration to the SAVED_CONFIG_PATH
+        if save(config, config_path='ip_inspector.config.json'):
+            print("Wrote the existing configuration to: ip_inspector.config.json")
+            print("Make any changes to that configuration file and then supply it to `ip-inspector --config-path`")
+        return
+
+    if args.license_key:
+        data = {}
+        data['maxmind'] = {'license_key': args.license_key}
+        #data['default'] = {'work_dir': config['default']['work_dir']}
+        save(data)
+        # put it in the right place so this session can continue
+        config['maxmind']['license_key'] = args.license_key
+    
+    if args.update_databases:
+        if not maxmind.update_databases(license_key=config['maxmind']['license_key']):
+            return 1
+
+    mmi = Inspector(maxmind.Client(license_key=config['maxmind']['license_key']))
+
+
+    if args.command == 'blacklist' or args.command == 'whitelist':
+        if args.show_path or args.print:
+            loaded_lists = config['default'][args.command+'s']
+            for _l, _path in loaded_lists.items():
+                if os.path.exists(_path):
+                    full_path = _path
+                else:
+                    full_path = os.path.join(WORK_DIR, _path)
+                if args.show_path:
+                    print("{}: {}".format(_l, full_path))
+                if args.print:
+                    if os.path.exists(full_path):
+                        with open(full_path, 'r') as fp:
+                            print(fp.read())
+            return
+        if args.ip:
+            ip = args.ip
+            if '/' in ip:
+                ip = ip[:ip.rfind('/')]
+            iip = mmi.inspect(ip)
+        if args.command == 'blacklist':
+            if args.bl_command == 'add':
+                for bl_type in list(set(args.blacklist_type)):
+                    blacklist_path = config['default']['blacklists'][bl_type]
+                    append_to_('blacklist', iip, field=bl_type, list_path=blacklist_path)
+            elif args.bl_command == 'remove':
+                for bl_type in list(set(args.blacklist_type)):
+                    blacklist_path = config['default']['blacklists'][bl_type]
+                    remove_from_('blacklist', iip, field=bl_type, list_path=blacklist_path)
+        if args.command == 'whitelist':
+            if args.wl_command == 'add':
+                for wl_type in list(set(args.whitelist_type)):
+                    whitelist_path = config['default']['whitelists'][wl_type]
+                    append_to_('whitelist', iip, field=wl_type, list_path=whitelist_path)
+            elif args.wl_command == 'remove':
+                for wl_type in list(set(args.whitelist_type)):
+                    whitelist_path = config['default']['whitelists'][wl_type]
+                    remove_from_('whitelist', iip, field=wl_type, list_path=whitelist_path)
+        return
+
+    # TODO Validate IP addresses?
+    if args.from_stdin:
+        if args.fields and args.csv:
+            header = "IP/Network," + ','.join(args.fields)
+            print(header)
+        iplist = [line.strip() for line in sys.stdin]
+        for ip in iplist:
+            network = None
+            if '/' in ip:
+                network = ip
+                ip = ip[:ip.rfind('/')]
+            try:
+                iip = mmi.inspect(ip)
+            except ValueError:
+                logging.warning("{} is not a valid ipv4 or ipv6".format(ip))
+            if iip:
+                if args.raw_results:
+                    print(json.dumps(iip.raw))
+                elif args.pretty_print:
+                    pprint(iip.raw)
+                elif args.fields:
+                    result_string = "--> {}".format(network if network else iip.ip)
+                    if args.csv:
+                        result_string = "{},".format(network if network else iip.ip)
+                    for field in args.fields:
+                        if args.csv:
+                            result_string += '"{}",'.format(iip.get(field))
+                        else:
+                            result_string += " | {}: {}".format(field, iip.get(field))
+                    if result_string.endswith(','):
+                        print(result_string[:-1])
+                    else:
+                        print(result_string)
+                else:
+                    print(iip)
+        return
+
+    if args.ip:
+        iip = mmi.inspect(args.ip)
+        if iip:
+            if args.raw_results:
+                print(json.dumps(iip.raw))
+            elif args.pretty_print:
+                pprint(iip.raw)
+            elif args.fields:
+                for field in args.fields:
+                    print(iip.get(field))
+            else:
+                print(iip)
+        return
