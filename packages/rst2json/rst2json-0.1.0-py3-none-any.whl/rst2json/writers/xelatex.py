fc22f44d92fbbd3b8681afc81af18b91db067340
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:09 +0000

    Add package rst2json/0.1.0/rst2json-0.1.0-py3-none-any.whl

---
+++ packages/rst2json/rst2json-0.1.0-py3-none-any.whl/rst2json/writers/xelatex.py
@@ -0,0 +1,44 @@
+from   docutils         import nodes
+from   docutils.writers import xetex
+from   ._json_base      import JSONTranslatorBase, JSONWriterBase, joinnl
+
+class Writer(JSONWriterBase, xetex.Writer):
+    format_name = 'xelatex'
+
+    json_fields = dict(JSONWriterBase.json_fields, latex={
+        "language": ("language_name", None),
+        "requirements": ("requirements", joinnl),
+        "fallbacks": ("fallbacks", joinnl),
+        "pdfsetup": ("pdfsetup", joinnl),
+    })
+
+    def __init__(self):
+        super().__init__()
+        self.translator_class = XeLaTeXJSONTranslator
+
+
+class XeLaTeXJSONTranslator(JSONTranslatorBase, xetex.XeLaTeXTranslator):
+    out_varname = 'out'
+
+    def __init__(self, document, **kwargs):
+        super().__init__(document, **kwargs)
+        self.language = self.language_module
+        self.language_name = self.babel.language
+
+    def visit_address(self, node):
+        super().visit_address(node)
+        self.insert_newline = True
+
+    def depart_address(self, node):
+        super().depart_address(node)
+        self.insert_newline = False
+
+    def visit_author(self, node):
+        super().visit_author(node)
+        self.pdfauthor.append(self.attval(node.astext()))
+
+    def visit_title(self, node):
+        super().visit_title(node)
+        if isinstance(node.parent, nodes.document):
+            self.context.append('')
+            self.pdfinfo.append('  pdftitle={%s},' % self.encode(node.astext()))
