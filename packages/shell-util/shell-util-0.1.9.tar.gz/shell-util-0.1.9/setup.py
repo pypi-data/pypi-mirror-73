3319e2064881ce05f39fe0bd2b1a35be21e46f66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:52 +0000

    Add package shell-util/0.1.9/shell-util-0.1.9.tar.gz

---
+++ packages/shell-util/shell-util-0.1.9.tar.gz/shell-util-0.1.9/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['shell_util']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=7.1.2,<8.0.0']
+
+extras_require = \
+{':python_version >= "3.6" and python_version < "3.7"': ['dataclasses>=0.7,<0.8']}
+
+entry_points = \
+{'console_scripts': ['shell = shell_util.cli:cli']}
+
+setup_kwargs = {
+    'name': 'shell-util',
+    'version': '0.1.9',
+    'description': '',
+    'long_description': '# shell-util\n',
+    'author': 'Eyal Levin',
+    'author_email': 'eyalev@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/eyalev/shell-util',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/shell-util/shell-util-0.1.9.tar.gz/shell-util-0.1.9/shell_util/__init__.py
@@ -0,0 +1,7 @@
+
+from .shell import run_command, run_command_and_return_result_object
+
+__all__ = [
+    'run_command',
+    'run_command_and_return_result_object'
+]
