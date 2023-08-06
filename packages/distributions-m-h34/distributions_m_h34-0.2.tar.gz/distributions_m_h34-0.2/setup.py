a532c07d1c17501390762371e3cbe7536b67636e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:25 +0000

    Add package distributions-m-h34/0.2/distributions_m_h34-0.2.tar.gz

---
+++ packages/distributions-m-h34/distributions_m_h34-0.2.tar.gz/distributions_m_h34-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_m_h34',
+      version='0.2',
+      description='Gaussian and binomial distributions',
+      packages=['distributions_m_h34'],
+      author = 'Mahmoud Hashem',
+      author_email = 'mahmoud.hashem34@outlook.com',
+      zip_safe=False)
