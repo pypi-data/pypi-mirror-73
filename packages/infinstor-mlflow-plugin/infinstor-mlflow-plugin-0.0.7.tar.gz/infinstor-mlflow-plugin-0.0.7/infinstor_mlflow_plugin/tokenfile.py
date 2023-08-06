48268953655ad44f35c704bf333f5dc81faf82ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:06 +0000

    Add package infinstor-mlflow-plugin/0.0.7/infinstor-mlflow-plugin-0.0.7.tar.gz

---
+++ packages/infinstor-mlflow-plugin/infinstor-mlflow-plugin-0.0.7.tar.gz/infinstor-mlflow-plugin-0.0.7/infinstor_mlflow_plugin/tokenfile.py
@@ -0,0 +1,29 @@
+
+def read_token_file(tokfile):
+    fclient_id = None
+    ftoken = None
+    frefresh_token = None
+    ftoken_time = None
+    fservice = None
+    with (open(tokfile)) as fp:
+        for count, line in enumerate(fp):
+            if (line.startswith('ClientId=')):
+                fclient_id = line[len('ClientId='):].rstrip()
+            if (line.startswith('Token=')):
+                ftoken = line[len('Token='):].rstrip()
+            if (line.startswith('RefreshToken=')):
+                frefresh_token = line[len('RefreshToken='):].rstrip()
+            if (line.startswith('TokenTimeEpochSeconds=')):
+                ftoken_time = int(line[len('TokenTimeEpochSeconds='):].rstrip())
+            if (line.startswith('Service=')):
+                fservice = line[len('Service='):].rstrip()
+    return ftoken, frefresh_token, ftoken_time, fclient_id, fservice
+
+def write_token_file(tokfile, token_time, token, refresh_token, client_id, service):
+    with open(tokfile, 'w') as wfile:
+        wfile.write("Token=" + token + "\n")
+        wfile.write("RefreshToken=" + refresh_token + "\n")
+        wfile.write("ClientId=" + client_id + "\n")
+        wfile.write("TokenTimeEpochSeconds=" + str(token_time) + "\n")
+        wfile.write("Service=" + service + "\n")
+        wfile.close()
