a861bd7c21bc0ea3365e6304b012b1827e7a73b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:52 +0000

    Add package djangoadmin/2.2.4/djangoadmin-2.2.4-py3-none-any.whl

---
+++ packages/djangoadmin/djangoadmin-2.2.4-py3-none-any.whl/djangoadmin/forms/__init__.py
@@ -0,0 +1,4 @@
+from djangoadmin.forms.UserForm import UserForm
+from djangoadmin.forms.SigninForm import SigninForm
+from djangoadmin.forms.RegistrationForm import RegistrationForm
+from djangoadmin.forms.EditProfileForm import EditProfileForm
