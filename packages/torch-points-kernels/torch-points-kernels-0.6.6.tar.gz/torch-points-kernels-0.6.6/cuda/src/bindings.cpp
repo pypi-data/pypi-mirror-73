af0fd4c686229f9ee822514b260b0a6202092a5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:26 +0000

    Add package torch-points-kernels/0.6.6/torch-points-kernels-0.6.6.tar.gz

---
+++ packages/torch-points-kernels/torch-points-kernels-0.6.6.tar.gz/torch-points-kernels-0.6.6/cuda/src/bindings.cpp
@@ -0,0 +1,18 @@
+#include "ball_query.h"
+#include "interpolate.h"
+#include "metrics.h"
+#include "sampling.h"
+
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
+{
+    m.def("furthest_point_sampling", &furthest_point_sampling);
+
+    m.def("three_nn", &three_nn);
+    m.def("three_interpolate", &three_interpolate);
+    m.def("three_interpolate_grad", &three_interpolate_grad);
+
+    m.def("ball_query_dense", &ball_query_dense);
+    m.def("ball_query_partial_dense", &ball_query_partial_dense);
+
+    m.def("instance_iou_cuda", &instance_iou_cuda);
+}
