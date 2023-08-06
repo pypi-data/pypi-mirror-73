511a5fe2c4210231d220a958271924ce1783975e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:14 +0000

    Add package axel-distributions/0.1/axel_distributions-0.1.tar.gz

---
+++ packages/axel-distributions/axel_distributions-0.1.tar.gz/axel_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='axel_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['axel_distributions'],
+      author= 'Axel González',
+      author_email='axeldasaev@gmail.com',
+      zip_safe=False)
