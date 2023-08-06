e4cbe06c43e2aae33462bf4dc6a5eca4715e2c82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:23 +0000

    Add package tap-slack/1.0.0/tap-slack-1.0.0.tar.gz

---
+++ packages/tap-slack/tap-slack-1.0.0.tar.gz/tap-slack-1.0.0/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='tap-slack',
+      version='1.0.0',
+      description='Singer.io tap for extracting data from the Slack Web API',
+      author='dwallace@envoy.com',
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      py_modules=['tap_slack'],
+      install_requires=[
+          'singer-python==5.9.0',
+          'slackclient==2.6.0',
+      ],
+      extras_require={
+          'dev': [
+              'pylint',
+              'ipdb',
+              'nose'
+          ]
+      },
+      python_requires='>=3.6',
+      entry_points='''
+          [console_scripts]
+          tap-slack=tap_slack:main
+      ''',
+      packages=find_packages(),
+      package_data={
+          'tap_slack': [
+              'schemas/*.json'
+          ]
+      })
