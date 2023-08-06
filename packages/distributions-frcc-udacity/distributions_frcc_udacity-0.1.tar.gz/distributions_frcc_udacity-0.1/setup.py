428d5d74bc3a98d2e764ed15fb0a443d9d9390cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:48 +0000

    Add package distributions-frcc-udacity/0.1/distributions_frcc_udacity-0.1.tar.gz

---
+++ packages/distributions-frcc-udacity/distributions_frcc_udacity-0.1.tar.gz/distributions_frcc_udacity-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_frcc_udacity',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_frcc_udacity'],
+      author='Fernando Raul Cortez Chavez',
+      author_email='fernandoraulcc@gmail.com',
+      zip_safe=False)
