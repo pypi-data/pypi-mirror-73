a62aa8678dd7728bd4f928ac4f919ad1649e14e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:47 +0000

    Add package beginnerpy/0.1.1/BeginnerPy-0.1.1.tar.gz

---
+++ packages/beginnerpy/BeginnerPy-0.1.1.tar.gz/BeginnerPy-0.1.1/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['beginnerpy',
+ 'beginnerpy.challenges.adventure_game',
+ 'beginnerpy.challenges.daily']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'beginnerpy',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Zech Zimmerman',
+    'author_email': 'hi@beginnerpy.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7.4,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
