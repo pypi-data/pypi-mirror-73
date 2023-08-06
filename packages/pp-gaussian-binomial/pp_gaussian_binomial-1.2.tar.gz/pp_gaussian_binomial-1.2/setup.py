f02e50fec5ace77ffa81388beb38b976d238e068
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:47 +0000

    Add package pp-gaussian-binomial/1.2/pp_gaussian_binomial-1.2.tar.gz

---
+++ packages/pp-gaussian-binomial/pp_gaussian_binomial-1.2.tar.gz/pp_gaussian_binomial-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pp_gaussian_binomial',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      packages=['pp_gaussian_binomial'],
+      author= 'Prajwala D',
+      autor_email = 'dprajwala11@gmail.com',
+     zip_safe=False)
