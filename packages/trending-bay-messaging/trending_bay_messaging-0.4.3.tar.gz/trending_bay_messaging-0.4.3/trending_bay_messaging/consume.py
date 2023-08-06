336843d80c68d7b7064422988323427650825eed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:55 +0000

    Add package trending-bay-messaging/0.4.3/trending_bay_messaging-0.4.3.tar.gz

---
+++ packages/trending-bay-messaging/trending_bay_messaging-0.4.3.tar.gz/trending_bay_messaging-0.4.3/trending_bay_messaging/consume.py
@@ -0,0 +1,11 @@
+def consume(channel, callback, exchange = None ,**kwargs):
+    if exchange is None:
+        exchange = "DEFAULT_EXCHANGE"
+    while True:
+        for method_frame, properties, body in channel.consume(exchange):
+            print(method_frame, properties, body)
+            channel.basic_ack(method_frame.delivery_tag)
+
+            if method_frame.delivery_tag == 10:
+                raise Exception("too many delivery tags".format(method_frame))
+            callback(body)
