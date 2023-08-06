3c8a7ab07d0b18be5a5c27c169da56d2c6d7c500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:25 +0000

    Add package procgraph-z6/6.0.1/procgraph-z6-6.0.1.tar.gz

---
+++ packages/procgraph-z6/procgraph-z6-6.0.1.tar.gz/procgraph-z6-6.0.1/src/procgraph/block_utils/executables.py
@@ -0,0 +1,6 @@
+
+
+def executable_exists(executable):
+    ''' Checks that the given executable exists. '''
+    # TODO: implement
+
---
+++ packages/procgraph-z6/procgraph-z6-6.0.1.tar.gz/procgraph-z6-6.0.1/src/procgraph_numpy_ops/__init__.py
@@ -0,0 +1,12 @@
+''' Various operations wrapping numpy functions. '''
+
+
+
+procgraph_info = {
+    # List of python packages
+    'requires': ['numpy'],
+}
+
+from . import filters
+from . import gradient1d
+from . import smooth1d
