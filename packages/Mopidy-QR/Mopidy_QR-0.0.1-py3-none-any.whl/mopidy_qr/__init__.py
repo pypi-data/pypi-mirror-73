e5583833ba3aba1129eb5940c7951861e924ccb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:46 +0000

    Add package Mopidy-QR/0.0.1/Mopidy_QR-0.0.1-py3-none-any.whl

---
+++ packages/Mopidy-QR/Mopidy_QR-0.0.1-py3-none-any.whl/mopidy_qr/__init__.py
@@ -0,0 +1,31 @@
+import logging
+import pathlib
+
+import pkg_resources
+
+from mopidy import config, ext
+
+__version__ = pkg_resources.get_distribution("Mopidy-QR").version
+
+# TODO: If you need to log, use loggers named after the current Python module
+logger = logging.getLogger(__name__)
+
+
+class Extension(ext.Extension):
+
+    dist_name = "Mopidy-QR"
+    ext_name = "qr"
+    version = __version__
+
+    def get_default_config(self):
+        return config.read(pathlib.Path(__file__).parent / "ext.conf")
+
+    def get_config_schema(self):
+        schema = super().get_config_schema()
+        schema["queue"] = config.Boolean()
+        return schema
+
+    def setup(self, registry):
+        from .frontend import QRFrontend
+
+        registry.add("frontend", QRFrontend)
