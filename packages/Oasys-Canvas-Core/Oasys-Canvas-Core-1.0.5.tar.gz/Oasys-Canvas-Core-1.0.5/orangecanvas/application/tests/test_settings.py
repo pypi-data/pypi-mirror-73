d9cbe8d10a3d8feddfbb5a21456ea56e50d4bead
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:04 +0000

    Add package Oasys-Canvas-Core/1.0.5/Oasys-Canvas-Core-1.0.5.tar.gz

---
+++ packages/Oasys-Canvas-Core/Oasys-Canvas-Core-1.0.5.tar.gz/Oasys-Canvas-Core-1.0.5/orangecanvas/application/tests/test_settings.py
@@ -0,0 +1,48 @@
+import logging
+
+import six
+
+from PyQt5.QtGui import QTreeView
+
+from ...gui import test
+
+from ..settings import UserSettingsDialog, UserSettingsModel
+from ...utils.settings import Settings, config_slot
+from ...utils.qtcompat import QSettings
+from ... import registry
+from ...registry import tests as registry_tests
+
+
+class TestUserSettings(test.QAppTestCase):
+    def setUp(self):
+        logging.basicConfig()
+        test.QAppTestCase.setUp(self)
+
+#     def test(self):
+#         registry.set_global_registry(registry_tests.small_testing_registry())
+#         settings = UserSettingsDialog()
+#         settings.show()
+#
+#         self.app.exec_()
+#         registry.set_global_registry(None)
+
+    def test_settings_model(self):
+        store = QSettings(QSettings.IniFormat, QSettings.UserScope,
+                          "biolab.si", "Orange Canvas UnitTests")
+
+        defaults = [config_slot("S1", bool, True, "Something"),
+                    config_slot("S2", six.text_type, "I an not a String",
+                                "Disregard the string.")]
+
+        settings = Settings(defaults=defaults, store=store)
+        model = UserSettingsModel(settings=settings)
+
+        self.assertEqual(model.rowCount(), len(settings))
+
+        view = QTreeView()
+        view.setHeaderHidden(False)
+
+        view.setModel(model)
+
+        view.show()
+        self.app.exec_()
---
+++ packages/Oasys-Canvas-Core/Oasys-Canvas-Core-1.0.5.tar.gz/Oasys-Canvas-Core-1.0.5/orangecanvas/gui/tests/test_tooltree.py
@@ -0,0 +1,103 @@
+"""
+Test for tooltree
+
+"""
+from __future__ import print_function
+
+from PyQt5.QtGui import QStandardItemModel, QStandardItem, QAction
+from PyQt5.QtCore import Qt
+
+from ..tooltree import ToolTree, FlattenedTreeItemModel
+
+from ...registry.qt import QtWidgetRegistry
+from ...registry.tests import small_testing_registry
+
+from ..test import QAppTestCase
+from ...utils.qtcompat import qunwrap
+
+
+class TestToolTree(QAppTestCase):
+    def test_tooltree(self):
+        tree = ToolTree()
+        role = tree.actionRole()
+        model = QStandardItemModel()
+        tree.setModel(model)
+        item = QStandardItem("One")
+        item.setData(QAction("One", tree), role)
+        model.appendRow([item])
+
+        cat = QStandardItem("A Category")
+        item = QStandardItem("Two")
+        item.setData(QAction("Two", tree), role)
+        cat.appendRow([item])
+        item = QStandardItem("Three")
+        item.setData(QAction("Three", tree), role)
+        cat.appendRow([item])
+
+        model.appendRow([cat])
+
+        def p(action):
+            print("triggered", action.text())
+
+        tree.triggered.connect(p)
+
+        tree.show()
+
+        self.app.exec_()
+
+    def test_tooltree_registry(self):
+        reg = QtWidgetRegistry(small_testing_registry())
+
+        tree = ToolTree()
+        tree.setModel(reg.model())
+        tree.setActionRole(reg.WIDGET_ACTION_ROLE)
+        tree.show()
+
+        def p(action):
+            print("triggered", action.text())
+
+        tree.triggered.connect(p)
+
+        self.app.exec_()
+
+    def test_flattened(self):
+        reg = QtWidgetRegistry(small_testing_registry())
+        source = reg.model()
+
+        model = FlattenedTreeItemModel()
+        model.setSourceModel(source)
+
+        tree = ToolTree()
+        tree.setActionRole(reg.WIDGET_ACTION_ROLE)
+        tree.setModel(model)
+        tree.show()
+
+        changed = []
+        model.dataChanged.connect(
+            lambda start, end: changed.append((start, end))
+        )
+
+        item = source.item(0).child(0)
+
+        item.setText("New text")
+
+        self.assertTrue(len(changed) == 1)
+        self.assertEqual(str(qunwrap(changed[-1][0].data(Qt.DisplayRole))),
+                        "New text")
+
+        self.assertEqual(str(qunwrap(model.data(model.index(1)))), "New text")
+
+        model.setFlatteningMode(FlattenedTreeItemModel.InternalNodesDisabled)
+
+        self.assertFalse(model.index(0, 0).flags() & Qt.ItemIsEnabled)
+
+        model.setFlatteningMode(FlattenedTreeItemModel.LeavesOnly)
+
+        self.assertEqual(model.rowCount(), len(reg.widgets()))
+
+        def p(action):
+            print("triggered", action.text())
+
+        tree.triggered.connect(p)
+
+        self.app.exec_()
