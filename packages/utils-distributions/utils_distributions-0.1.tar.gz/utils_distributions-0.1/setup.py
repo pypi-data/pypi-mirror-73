650387cf3f7e2355bdaf8246ecf70a7c6f6cd5b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:54 +0000

    Add package utils-distributions/0.1/utils_distributions-0.1.tar.gz

---
+++ packages/utils-distributions/utils_distributions-0.1.tar.gz/utils_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='utils_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['utils_distributions'],
+      author='Irune Lansorena',
+      authoremail='irunelansorena@gmail.com',
+      zip_safe=False)
