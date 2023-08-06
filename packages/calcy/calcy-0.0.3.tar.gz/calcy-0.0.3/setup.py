7bb065a02f791ab2d7e4fa1c3e6965bce72cc4bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:47 +0000

    Add package calcy/0.0.3/calcy-0.0.3.tar.gz

---
+++ packages/calcy/calcy-0.0.3.tar.gz/calcy-0.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='calcy',
+    version='0.0.3',
+    description='A basic calculator',
+    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    long_description_content_type="text/markdown",
+    url='',
+    author='Deepak Jha',
+    author_email='deepakjha18598@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='calculator',
+    packages=find_packages(),
+    install_requires=['']
+)
