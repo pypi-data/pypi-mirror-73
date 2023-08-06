8995d9f07ad899639797e12fb645821af760b762
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:19 +0000

    Add package davis-distributions/1.0.0/davis_distributions-1.0.0.tar.gz

---
+++ packages/davis-distributions/davis_distributions-1.0.0.tar.gz/davis_distributions-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='davis_distributions',
+    version='1.0.0',
+    author='Davis Peixoto',
+    url='https://github.com/davispeixoto/udacity-aws-ml',
+    author_email='davis.peixoto@gmail.com',
+    description='Udacity AWS Machine Learning Fundamentals Course',
+    packages=['davis_distributions'],
+    zip_safe=False
+)
