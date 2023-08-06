0dfe46c11691fa92950732421d4e3163eef33d5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:51 +0000

    Add package Number-Square/1.0.2/Number-Square-1.0.2.tar.gz

---
+++ packages/Number-Square/Number-Square-1.0.2.tar.gz/Number-Square-1.0.2/README.md
@@ -0,0 +1,4 @@
+Number-Square
+A Python package to get square of any number.
+
+Usage
---
+++ packages/Number-Square/Number-Square-1.0.2.tar.gz/Number-Square-1.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="Number-Square",
+    version="1.0.2",
+    description="A Python package to square a number.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Jhansi-27/Number-Square",
+    author="Jhansi Rani",
+    author_email="jhansi.rc26@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["Number_Square"],
+    include_package_data=True,
+    install_requires=["requests"],
+)
