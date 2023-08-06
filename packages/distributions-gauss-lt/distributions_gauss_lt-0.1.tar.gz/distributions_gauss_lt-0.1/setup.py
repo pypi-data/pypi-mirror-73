0b875f0e3bd8cf642f744fcf7bdfca65d43b45fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:23 +0000

    Add package distributions-gauss-lt/0.1/distributions_gauss_lt-0.1.tar.gz

---
+++ packages/distributions-gauss-lt/distributions_gauss_lt-0.1.tar.gz/distributions_gauss_lt-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_gauss_lt',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_gauss_lt'],
+      author= 'Li Tang',
+      author_email= 'tangli666@gmail.com',
+      zip_safe=False)
