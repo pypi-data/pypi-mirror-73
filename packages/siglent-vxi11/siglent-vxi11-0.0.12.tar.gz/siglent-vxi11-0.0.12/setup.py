f346e73438ec6fc88e2284c80afb550affefb2c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:01 +0000

    Add package siglent-vxi11/0.0.12/siglent-vxi11-0.0.12.tar.gz

---
+++ packages/siglent-vxi11/siglent-vxi11-0.0.12.tar.gz/siglent-vxi11-0.0.12/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+setuptools.setup(
+    name='siglent-vxi11',
+    version='0.0.12',
+    author='Richard Waschhauser',
+    keywords='Siglent, VXI, SDL1020X-E, SPD3303X, SPD3303X-E',
+    description='Siglent vxi11 library',
+    long_description_content_type='text/markdown',
+    long_description=open('README.md').read(),
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'python-vxi11',
+    ],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: System :: Hardware :: Hardware Drivers',
+        'Topic :: System :: Networking',
+    ],
+    python_requires='>=3.5',
+)
