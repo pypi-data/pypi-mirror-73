9b20243c027f9143d43670cdfe2ecb5fe485131a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:41 +0000

    Add package badminton/0.0.5/badminton-0.0.5.tar.gz

---
+++ packages/badminton/badminton-0.0.5.tar.gz/badminton-0.0.5/setup.py
@@ -0,0 +1,33 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="badminton",
+    version="0.0.5",
+    description="Badminton data",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/microprediction/badminton",
+    author="microprediction",
+    author_email="info@microprediction.org",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["badminton"],
+    test_suite='pytest',
+    tests_require=['pytest'],
+    include_package_data=True,
+    install_requires=["getjson"],
+    entry_points={
+        "console_scripts": [
+            "badminton=badminton.__main__:main",
+        ]
+     },
+     )
