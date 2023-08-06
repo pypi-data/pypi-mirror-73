0ac3a052427705906fb6a6434d9cb7df1540b13b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:26 +0000

    Add package spy-probability/0.1/spy_probability-0.1.tar.gz

---
+++ packages/spy-probability/spy_probability-0.1.tar.gz/spy_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='spy_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions'],
+      author = 'Aguwa Obinna',
+      author_email = 'macmartins081@gmail.com',
+      zip_safe=False)
