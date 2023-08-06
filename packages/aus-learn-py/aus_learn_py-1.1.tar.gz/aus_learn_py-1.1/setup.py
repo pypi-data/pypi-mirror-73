b4f940697184e0de093ac23e174d448af3d99c53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:47 +0000

    Add package aus-learn-py/1.1/aus_learn_py-1.1.tar.gz

---
+++ packages/aus-learn-py/aus_learn_py-1.1.tar.gz/aus_learn_py-1.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='aus_learn_py',
+      version='1.1',
+      description='Gaussian distributions',
+      packages=['aus_learn_py'],
+      zip_safe=False)
