3e32bf322172f725020d02976a98b533a4a70665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:51 +0000

    Add package core-explore-keyword-app/1.10.0/core_explore_keyword_app-1.10.0.tar.gz

---
+++ packages/core-explore-keyword-app/core_explore_keyword_app-1.10.0.tar.gz/core_explore_keyword_app-1.10.0/core_explore_keyword_app/settings.py
@@ -0,0 +1,9 @@
+from django.conf import settings
+
+if not settings.configured:
+    settings.configure()
+
+INSTALLED_APPS = getattr(settings, "INSTALLED_APPS", [])
+
+# MENU
+EXPLORE_MENU_NAME = getattr(settings, "EXPLORE_MENU_NAME", "Query by Keyword")
