0a3b97faf930b9893da79392bf8ca7257e35bfde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:56 +0000

    Add package core-parser-app/1.10.0/core_parser_app-1.10.0.tar.gz

---
+++ packages/core-parser-app/core_parser_app-1.10.0.tar.gz/core_parser_app-1.10.0/core_parser_app/settings.py
@@ -0,0 +1,12 @@
+from os.path import join, dirname, realpath
+
+from django.conf import settings
+
+if not settings.configured:
+    settings.configure()
+
+MODULES_ROOT = join(dirname(realpath(__file__)).replace("\\", "/"), "tools", "modules")
+
+MODULE_TAG_NAME = getattr(settings, "MODULE_TAG_NAME", "module")
+
+AUTO_ESCAPE_XML_ENTITIES = getattr(settings, "AUTO_ESCAPE_XML_ENTITIES", True)
