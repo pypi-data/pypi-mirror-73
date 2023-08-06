d45f2c79b9bae0ed287dffa313a4b6be284a4a4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:23 +0000

    Add package hardik-distributions/0.1/hardik_distributions-0.1.tar.gz

---
+++ packages/hardik-distributions/hardik_distributions-0.1.tar.gz/hardik_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'hardik_distributions',
+      version = '0.1',
+      description = 'Gaussian and Binomial distributions',
+      packages = ['hardik_distributions'],
+      author = 'Hardik Jain',
+      author_email = 'hardikjain249@gmail.com',
+      zip_safe = False)
