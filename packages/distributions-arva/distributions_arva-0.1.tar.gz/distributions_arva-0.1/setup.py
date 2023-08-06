39e5d8e3c3fad4d0072d2fc28b98e0497e8cbbec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:52 +0000

    Add package distributions-arva/0.1/distributions_arva-0.1.tar.gz

---
+++ packages/distributions-arva/distributions_arva-0.1.tar.gz/distributions_arva-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_arva',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_arva'],
+      author = 'Aravindh Vangal',
+      author_email = 'aravindh1987@gmail.com',
+      zip_safe=False)
