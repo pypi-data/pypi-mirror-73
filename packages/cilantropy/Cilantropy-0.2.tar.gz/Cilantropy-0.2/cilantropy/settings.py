9f6ed1d59c0fcf10f8500639d4ac199779c04c2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:11 +0000

    Add package cilantropy/0.2/Cilantropy-0.2.tar.gz

---
+++ packages/cilantropy/Cilantropy-0.2.tar.gz/Cilantropy-0.2/cilantropy/settings.py
@@ -0,0 +1,6 @@
+from werkzeug.security import generate_password_hash
+
+__all__ = ['main', 'metadata', 'console']
+__author__ = 'Tkachenko Igor'
+__author_url__ = 'https://github.com/foozzi'
+__version__ = '0.2'
