be8de9d27c5dfda2de2ff8fa3d599b2c9b3cfb03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:01 +0000

    Add package upload-to-pip-demo-v-4/0.0.4/upload_to_pip_demo_v_4-0.0.4.tar.gz

---
+++ packages/upload-to-pip-demo-v-4/upload_to_pip_demo_v_4-0.0.4.tar.gz/upload_to_pip_demo_v_4-0.0.4/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="upload_to_pip_demo_v_4", # Replace with your own username
+    version="0.0.4",
+    author="Gegham Vardanyan",
+    author_email="vardanyan.gegham95@gmail.com",
+    description="demo package to test uploading to PyPI",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/geghamcode/upload_to_pip_demo.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    
+    python_requires='>=3.6',
+    entry_points={
+        "console_scripts": [
+            "hub-login = mock_library.click_cmd:hello",
+        ],
+    },
+)
