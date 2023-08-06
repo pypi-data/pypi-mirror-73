d176ad547db6a587da1804e4e8d67383e8562519
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:34 +0000

    Add package cran-diff/0.1.9/cran-diff-0.1.9.tar.gz

---
+++ packages/cran-diff/cran-diff-0.1.9.tar.gz/cran-diff-0.1.9/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['cran_diff']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['bs4',
+ 'deb_pkg_tools',
+ 'psycopg2-binary',
+ 'python-dateutil',
+ 'requests',
+ 'sqlalchemy']
+
+setup_kwargs = {
+    'name': 'cran-diff',
+    'version': '0.1.9',
+    'description': '',
+    'long_description': None,
+    'author': 'Aoife Curran',
+    'author_email': 'aocurran@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
