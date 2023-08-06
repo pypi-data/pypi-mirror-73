2e7d67d72b73f460922c17c272969b128016bc4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:24 +0000

    Add package onedash/0.0.9/onedash-0.0.9-py3-none-any.whl

---
+++ packages/onedash/onedash-0.0.9-py3-none-any.whl/onedash/config.py
@@ -0,0 +1,4 @@
+api_url = 'https://app.onedash.cc/metrics/api/v1'
+insert_record_url = f'{api_url}/insert/record'
+insert_ref_url = f'{api_url}/insert/ref'
+check_token_url = f'{api_url}/token/check'
