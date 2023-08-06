efe5936d0d519da9fa030212823256f6960d9356
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:51 +0000

    Add package MicroTokenizer/0.19.2/MicroTokenizer-0.19.2-py2.py3-none-any.whl

---
+++ packages/MicroTokenizer/MicroTokenizer-0.19.2-py2.py3-none-any.whl/MicroTokenizer/cli.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+"""Console script for MicroTokenizer."""
+import plac
+import sys
+from .cli_command import (
+    train,
+    download,
+    link
+)
+
+
+def main(args=None):
+    commands = {
+        'train': train,
+        'download': download,
+        'link': link
+    }
+    if len(sys.argv) == 1:
+        print("{title}: {content}, exits: {exits}".format(
+            content=', '.join(commands),
+            title="Available commands",
+            exits=1)
+        )
+
+    command = sys.argv.pop(1)
+    sys.argv[0] = 'MicroTokenizer %s' % command
+
+    if command in commands:
+        plac.call(commands[command], sys.argv[1:])
+    else:
+        print("{title}: {content}, exits: {exits}".format(
+            content="Available: %s" % ', '.join(commands),
+            title="Unknown command: %s" % command,
+            exits=1)
+        )
+
+
+if __name__ == "__main__":
+    main()
