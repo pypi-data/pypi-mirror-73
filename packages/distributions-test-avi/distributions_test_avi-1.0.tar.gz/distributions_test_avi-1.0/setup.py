b5a309970b4bad51c14748d3409ea0113e57a6f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:54 +0000

    Add package distributions-test-avi/1.0/distributions_test_avi-1.0.tar.gz

---
+++ packages/distributions-test-avi/distributions_test_avi-1.0.tar.gz/distributions_test_avi-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_test_avi',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['distributions_test'],
+      author='avitomar12',
+      author_email='avitomar12@gmail.com',
+      zip_safe=False)
