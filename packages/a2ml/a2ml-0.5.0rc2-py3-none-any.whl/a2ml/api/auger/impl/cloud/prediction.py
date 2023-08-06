e6c717c89ec47787158b728a354ae68b9a9425a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:56 +0000

    Add package a2ml/0.5.0rc2/a2ml-0.5.0rc2-py3-none-any.whl

---
+++ packages/a2ml/a2ml-0.5.0rc2-py3-none-any.whl/a2ml/api/auger/impl/cloud/prediction.py
@@ -0,0 +1,21 @@
+from .base import AugerBaseApi
+from ..exceptions import AugerException
+
+
+class AugerPredictionApi(AugerBaseApi):
+    """Auger Trial API."""
+
+    def __init__(self, ctx, pipeline_api,
+        prediction_name=None, prediction_id=None):
+        super(AugerPredictionApi, self).__init__(
+            ctx, pipeline_api, prediction_name, prediction_id)
+        assert pipeline_api is not None, 'Pipeline must be set for Prediction'
+
+    def create(self, records, features, threshold=None, predicted_at=None):
+        return self._call_create({
+            'pipeline_id': self.parent_api.object_id,
+            'records': records, 
+            'features': features,
+            'threshold': threshold,
+            'predicted_at': str(predicted_at)
+            }, ['requested', 'running'])
