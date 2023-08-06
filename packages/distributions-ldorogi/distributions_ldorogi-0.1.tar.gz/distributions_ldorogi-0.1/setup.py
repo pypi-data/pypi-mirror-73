ada3eaadfbf44861cd7ebf5cbcd69c024337b919
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:01 +0000

    Add package distributions-ldorogi/0.1/distributions_ldorogi-0.1.tar.gz

---
+++ packages/distributions-ldorogi/distributions_ldorogi-0.1.tar.gz/distributions_ldorogi-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_ldorogi',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_ldorogi'],
+      zip_safe=False)
