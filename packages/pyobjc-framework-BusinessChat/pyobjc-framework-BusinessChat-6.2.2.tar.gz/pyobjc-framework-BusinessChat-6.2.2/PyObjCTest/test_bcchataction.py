2b1e6f66fb7fbef1193b642d2fa9ee7a25dfa7a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:34 +0000

    Add package pyobjc-framework-BusinessChat/6.2.2/pyobjc-framework-BusinessChat-6.2.2.tar.gz

---
+++ packages/pyobjc-framework-BusinessChat/pyobjc-framework-BusinessChat-6.2.2.tar.gz/pyobjc-framework-BusinessChat-6.2.2/PyObjCTest/test_bcchataction.py
@@ -0,0 +1,15 @@
+import sys
+
+
+if sys.maxsize > 2 ** 32:
+    import BusinessChat
+    from PyObjCTools.TestSupport import TestCase
+
+    class TestBCChatAction(TestCase):
+        def test_constants(self):
+            self.assertIsInstance(BusinessChat.BCParameterNameIntent, str)
+            self.assertIsInstance(BusinessChat.BCParameterNameGroup, str)
+            self.assertIsInstance(BusinessChat.BCParameterNameBody, str)
+
+        def test_classes(self):
+            BusinessChat.BCChatAction
---
+++ packages/pyobjc-framework-BusinessChat/pyobjc-framework-BusinessChat-6.2.2.tar.gz/pyobjc-framework-BusinessChat-6.2.2/PyObjCTest/test_bcchatbutton.py
@@ -0,0 +1,14 @@
+import sys
+
+
+if sys.maxsize > 2 ** 32:
+    import BusinessChat
+    from PyObjCTools.TestSupport import TestCase
+
+    class TestBCChatButton(TestCase):
+        def test_constants(self):
+            self.assertEqual(BusinessChat.BCChatButtonStyleLight, 0)
+            self.assertEqual(BusinessChat.BCChatButtonStyleDark, 1)
+
+        def test_classes(self):
+            BusinessChat.BCChatButton
