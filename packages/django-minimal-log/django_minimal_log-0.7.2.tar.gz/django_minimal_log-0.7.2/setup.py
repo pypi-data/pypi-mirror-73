f9695b321d06e2dcbf782dbb6a2ebbbc444afa11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:09 +0000

    Add package django-minimal-log/0.7.2/django_minimal_log-0.7.2.tar.gz

---
+++ packages/django-minimal-log/django_minimal_log-0.7.2.tar.gz/django_minimal_log-0.7.2/setup.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+"""
+	Adapted from https://hynek.me/articles/sharing-your-labor-of-love-pypi-quick-and-dirty/
+"""
+
+from setuptools import setup
+
+
+setup(
+	name='django_minimal_log',
+	description='Simple logging functionality for Django',
+	long_description='',
+	url='https://github.com/mverleg/django_minimal_log',
+	author='Mark V',
+	maintainer='(the author)',
+	author_email='mdilligaf@gmail.com',
+	license='LICENSE.txt',
+	keywords=['django', 'logging',],
+	version='0.7.2',
+	packages=[
+		'minimal_log_host',
+		'minimal_logger',
+		'minimal_log_host.migrations',
+	],
+	include_package_data=True,
+	zip_safe=False,
+	install_requires=[
+		'django',
+		'requests',
+		'django_ipware==2.*',
+	],
+	classifiers=[
+		'Development Status :: 5 - Production/Stable',
+		'Intended Audience :: Developers',
+		'Natural Language :: English',
+		'License :: OSI Approved :: BSD License',
+		'Operating System :: OS Independent',
+		'Topic :: System :: Logging',
+		'Topic :: System :: Monitoring',
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 2',
+		'Programming Language :: Python :: 2.6',
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.3',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: 3.5',
+		'Programming Language :: Python :: Implementation :: PyPy',
+		'Topic :: Software Development :: Libraries :: Python Modules',
+	],
+)
+
+
