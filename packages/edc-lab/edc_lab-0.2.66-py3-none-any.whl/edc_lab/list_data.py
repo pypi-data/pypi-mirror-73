c7de468e97bb107c4d80bc15c87a502da07598b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:34 +0000

    Add package edc-lab/0.2.66/edc_lab-0.2.66-py3-none-any.whl

---
+++ packages/edc-lab/edc_lab-0.2.66-py3-none-any.whl/edc_lab/list_data.py
@@ -0,0 +1,24 @@
+from edc_list_data import PreloadData
+
+from .constants import FILL_ACROSS
+
+
+list_data = {}
+
+model_data = {
+    "edc_lab.boxtype": [
+        {
+            "name": "9 x 9",
+            "across": 9,
+            "down": 9,
+            "total": 81,
+            "fill_order": FILL_ACROSS,
+        }
+    ]
+}
+
+unique_field_data = {}
+
+preload_data = PreloadData(
+    list_data=list_data, model_data=model_data, unique_field_data=unique_field_data
+)
