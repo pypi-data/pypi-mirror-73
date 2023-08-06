d6022a2ef317de4f0a1b7eecd5eb1f97377388ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:15 +0000

    Add package jicaiauto/0.0.1.2/jicaiauto-0.0.1.2.tar.gz

---
+++ packages/jicaiauto/jicaiauto-0.0.1.2.tar.gz/jicaiauto-0.0.1.2/jicaiauto/jicaicheck.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+__author__ = 'Tser'
+__email__ = '807447312@qq.com'
+__project__ = 'jicaiautoproject'
+__script__ = 'jicaicheck.py'
+__create_time__ = '2020/7/10 1:03'
+
+from jicaicheck import main as jicai_main
+
+def main():
+    jicai_main()
