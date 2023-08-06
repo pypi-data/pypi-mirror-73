2d1ed1ba1ff3a9f614045d20d7c440b2767ac163
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:36 +0000

    Add package unicode-babel/0.1.3/unicode_babel-0.1.3.tar.gz

---
+++ packages/unicode-babel/unicode_babel-0.1.3.tar.gz/unicode_babel-0.1.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="unicode_babel",
+    version="0.1.3",
+    author="Peter Houghton",
+    author_email="pete@investigatingsoftware.co.uk",
+    description="A tool for generating random characters/code-points",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/phoughton/unicode_babel",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
