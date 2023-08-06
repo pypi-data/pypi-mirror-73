7a4c0c8d74927f467495c182f0a34e0a6a89fc62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:34 +0000

    Add package django-nameko-standalone/2.0.1/django_nameko_standalone-2.0.1-py3-none-any.whl

---
+++ packages/django-nameko-standalone/django_nameko_standalone-2.0.1-py3-none-any.whl/django_nameko_standalone/__init__.py
@@ -0,0 +1,37 @@
+import os
+from nameko.extensions import DependencyProvider
+
+
+class DjangoModels(DependencyProvider):
+    def setup(self):
+        """Initialize the dependency"""
+        import django
+        if os.environ.get('DJANGO_NAMEKO_STANDALONE_SETTINGS_MODULE'):
+            os.environ.setdefault("DJANGO_SETTINGS_MODULE", os.environ.get('DJANGO_NAMEKO_STANDALONE_SETTINGS_MODULE'))
+        elif not os.environ.get('DJANGO_SETTINGS_MODULE'):
+            os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+        django.setup()
+
+    def get_dependency(self, worker_ctx):
+        """Get the dependency for the concrete service"""
+        from django.apps import apps
+        from django.conf import settings
+
+        apps_config = map(apps.get_app_config, settings.DJANGO_NAMEKO_STANDALONE_APPS)
+        models = type('NonExistingClass_', (), {})
+
+        for config in apps_config:
+            for model in config.get_models():
+                setattr(models, model.__name__, model)
+        return models
+
+    def worker_teardown(self, worker_ctx):
+        """Close all the connections on teardown
+
+        TODO: Autocommit??
+        """
+        from django.db import connections
+        connections.close_all()
+
+
+__all__ = ["DjangoModels"]
