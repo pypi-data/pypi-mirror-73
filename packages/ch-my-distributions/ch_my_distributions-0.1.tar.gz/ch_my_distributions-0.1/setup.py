b38d94a4e2a7fe4ce279461d9c5f96bb06d4f8ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:34 +0000

    Add package ch-my-distributions/0.1/ch_my_distributions-0.1.tar.gz

---
+++ packages/ch-my-distributions/ch_my_distributions-0.1.tar.gz/ch_my_distributions-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='ch_my_distributions',
+      version='0.1',
+      description='Udacity test fot Gaussian distributions',
+      packages=['ch_my_distributions'],
+      author_email='xathanas@gmail.com',
+      author = 'Christos Athanasiadis',
+      
+      zip_safe=False)
