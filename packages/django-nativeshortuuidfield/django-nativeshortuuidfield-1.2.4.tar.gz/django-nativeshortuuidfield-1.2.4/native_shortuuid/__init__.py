367d8101cc92175595fe96a6e6a5521303ece9f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:10 +0000

    Add package django-nativeshortuuidfield/1.2.4/django-nativeshortuuidfield-1.2.4.tar.gz

---
+++ packages/django-nativeshortuuidfield/django-nativeshortuuidfield-1.2.4.tar.gz/django-nativeshortuuidfield-1.2.4/native_shortuuid/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+from .fields import NativeShortUUIDField
+from .fields import NativeShortUUIDFormField
+from .admin import NativeUUIDSearchMixin
+from . import validation
+
+try:
+    VERSION = __import__('pkg_resources') \
+        .get_distribution('django-nativeshortuuidfield').version
+except Exception:
+    VERSION = 'unknown'
