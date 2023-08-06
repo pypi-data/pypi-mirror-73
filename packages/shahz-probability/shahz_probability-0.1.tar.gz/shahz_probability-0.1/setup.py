7d7d78a243e153a3c660180f8c8a6fd2fd1b5e03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:30 +0000

    Add package shahz-probability/0.1/shahz_probability-0.1.tar.gz

---
+++ packages/shahz-probability/shahz_probability-0.1.tar.gz/shahz_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='shahz_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['shahz_probability'],
+      author='shah bhatti',
+      author_email='cleashahbhatti@gmail.com',
+      zip_safe=False)
