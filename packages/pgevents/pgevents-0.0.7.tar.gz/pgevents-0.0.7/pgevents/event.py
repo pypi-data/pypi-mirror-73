44875998a0a1aca290e2e23a3f0f46658e82950d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:01 +0000

    Add package pgevents/0.0.7/pgevents-0.0.7.tar.gz

---
+++ packages/pgevents/pgevents-0.0.7.tar.gz/pgevents-0.0.7/pgevents/event.py
@@ -0,0 +1,38 @@
+import logging
+from dataclasses import dataclass
+from datetime import datetime
+from typing import Optional, Any
+
+from pgevents import data_access
+
+LOGGER = logging.getLogger(__name__)
+
+PENDING = "PENDING"
+PROCESSED = "PROCESSED"
+
+
+@dataclass
+class Event:
+    topic: str
+    status: str = PENDING
+    id: Optional[int] = None
+    payload: Optional[Any] = None
+    created_at: Optional[datetime] = None
+    process_after: Optional[datetime] = None
+    processed_at: Optional[datetime] = None
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(
+            topic=data["topic"],
+            status=data["status"],
+            id=data["id"],
+            payload=data["payload"],
+            created_at=data["created_at"],
+            process_after=data["process_after"],
+            processed_at=data["processed_at"],
+        )
+
+    def mark_processed(self, cursor):
+        assert self.id is not None, "Cannot mark processed when id is not set"
+        data_access.mark_event_processed(cursor, self.id)
