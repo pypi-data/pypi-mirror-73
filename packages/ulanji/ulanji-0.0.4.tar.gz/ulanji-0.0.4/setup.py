e74b1a0d133053f72a179755e827d316c3a389fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:00 +0000

    Add package ulanji/0.0.4/ulanji-0.0.4.tar.gz

---
+++ packages/ulanji/ulanji-0.0.4.tar.gz/ulanji-0.0.4/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ulanji",
+    version="0.0.4",
+    author="Jerven Clark Chua",
+    author_email="jervenclark@gmail.com",
+    description="Distraction free markdown editor",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jervenclark/ulanji",
+    packages=setuptools.find_packages(),
+    package_data = {
+        'ulanji.gui': ['*.ui'],
+        'ulanji.assets': ['*.html']
+    },
+    include_package_data=True,
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
