dd4e5dbc293f3efe0471f3729dea02d2569626fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:20 +0000

    Add package torchio/0.17.9/torchio-0.17.9.tar.gz

---
+++ packages/torchio/torchio-0.17.9.tar.gz/torchio-0.17.9/torchio/__init__.py
@@ -0,0 +1,17 @@
+"""Top-level package for torchio."""
+
+__author__ = """Fernando Perez-Garcia"""
+__email__ = 'fernando.perezgarcia.17@ucl.ac.uk'
+__version__ = '0.17.9'
+
+from . import utils
+from .torchio import *
+from .transforms import *
+from .data import io, sampler, inference, ImagesDataset, Image, Queue, Subject
+from . import datasets
+from . import reference
+
+print('If you use TorchIO for your research, please cite the following paper:')
+print('Pérez-García et al., TorchIO: a Python library for efficient loading,')
+print('preprocessing, augmentation and patch-based sampling of medical images')
+print('in deep learning. Link: https://arxiv.org/abs/2003.04696\n')
