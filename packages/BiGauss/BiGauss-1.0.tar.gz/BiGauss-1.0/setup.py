a2386348f56afecc86f4e932c50e603581641229
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:53 +0000

    Add package BiGauss/1.0/BiGauss-1.0.tar.gz

---
+++ packages/BiGauss/BiGauss-1.0.tar.gz/BiGauss-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='BiGauss',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['BiGauss'],
+      zip_safe=False)
