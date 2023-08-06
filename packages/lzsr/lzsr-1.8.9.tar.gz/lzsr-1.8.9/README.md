67c8c7eb36cc433ca3f74c5a2576ad041d7703ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:48 +0000

    Add package lzsr/1.8.9/lzsr-1.8.9.tar.gz

---
+++ packages/lzsr/lzsr-1.8.9.tar.gz/lzsr-1.8.9/README.md
@@ -0,0 +1,3 @@
+# lzsr
+They are some Python packages.
+There Python packages can help you simplify some code in programming.
---
+++ packages/lzsr/lzsr-1.8.9.tar.gz/lzsr-1.8.9/lzsr/__init__.py
@@ -0,0 +1,20 @@
+import lzsr.lzsr.lzsr_series
+import lzsr.lzsr.lzsr_lock
+import lzsr.lzsr.lzsr_key
+
+__all__ =[
+    'Isochromatic_Firstitem','Isochromatic_Lastitem','Isochromatic_tolerance','Isochromatic_Numberofitems',
+    'Proportional_Firstitem','Proportional_Lastitem','Proportional_Commonrato','Proportional_Numberofitems',
+    'Fibonacci_Specifyitem','Fidonacci_ordinalnumbersofitem',
+    'Catalan_Specifyitem','Catalan_ordinalnumbersofitem',
+    'Pascaltriangle_Specifyrow','Pascaltriangle_ordinalnumbersofrow',
+    'LookAndSay_Specifycontent','LookAndSay_Specifyitem','LookAndSay_ordinalnumbersofitem',
+    'Sine_Specifyitem','Cosine_Specifyitem','Tangent_Specifyitem','Cotangent_Specifyitem','Secant_Specifyitem','Cosecant_Specifyitem',
+    'Arcsine_Specifyitem','Arccosine_Specifyitem','Arctangent_Specifyitem','Arccotangent_Specifyitem','Arcsecant_Specifyitem','Arccsecant_Specifyitem',
+    'Hyperbolic_sine_Specifyitem','Hyperbolic_cosine_Specifyitem','Hyperbolic_tangent_Specifyitem','Hyperbolic_cotangent_Specifyitem','Hyperbolic_secant_Specifyitem','Hyperbolic_cosencant_Specifyitem',
+    'Area_Hyperbolic_sine_Specifyitem','Area_Hyperbolic_cosine_Specifyitem','Area_Hyperbolic_tangent_Specifyitem','Area_Hyperbolic_cotangent_Specifyitem', 'Area_Hyperbolic_secant_Specifyitem','Area_Hyperbolic_cosecant_Specifyitem',
+    'lock','locks',
+    'key','keys'
+]
+
+__version__ = '1.8.9'
