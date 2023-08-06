9db9cc4b8de8efe8499737c186c26fd2fcfea118
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:30 +0000

    Add package ai-probability/0.1/ai_probability-0.1.tar.gz

---
+++ packages/ai-probability/ai_probability-0.1.tar.gz/ai_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ai_probability',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['ai_probability'],
+      author = 'Zeeshan Keerio',
+      email = 'imzeeshan.ai@gmail.com',
+      zip_safe=False)
