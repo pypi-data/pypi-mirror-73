736813159afbbbcd8c8bd91b7f17096df00a6469
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:03 +0000

    Add package easydata-distributions/1.2/easydata_distributions-1.2.tar.gz

---
+++ packages/easydata-distributions/easydata_distributions-1.2.tar.gz/easydata_distributions-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='easydata_distributions',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      packages=['easydata_distributions'],
+      author='Sumit Paul',
+      author_email='sumit.18.paul@gmail.com',
+      zip_safe=False)
