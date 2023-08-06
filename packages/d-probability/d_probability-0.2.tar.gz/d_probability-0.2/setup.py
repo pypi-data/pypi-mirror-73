cc53f05a81b9ca66f9d9ae691b4e9f75187c2196
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:43 +0000

    Add package d-probability/0.2/d_probability-0.2.tar.gz

---
+++ packages/d-probability/d_probability-0.2.tar.gz/d_probability-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='d_probability',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['d_probability'],
+      zip_safe=False)
