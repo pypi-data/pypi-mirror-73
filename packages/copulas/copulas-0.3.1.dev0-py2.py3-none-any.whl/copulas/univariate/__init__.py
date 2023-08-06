ab642ef6e8c239c0bf435ee84ee6f49e31ebb16c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:31 +0000

    Add package copulas/0.3.1.dev0/copulas-0.3.1.dev0-py2.py3-none-any.whl

---
+++ packages/copulas/copulas-0.3.1.dev0-py2.py3-none-any.whl/copulas/univariate/__init__.py
@@ -0,0 +1,19 @@
+from copulas.univariate.base import BoundedType, ParametricType, Univariate
+from copulas.univariate.beta import BetaUnivariate
+from copulas.univariate.gamma import GammaUnivariate
+from copulas.univariate.gaussian import GaussianUnivariate
+from copulas.univariate.gaussian_kde import GaussianKDE
+from copulas.univariate.student_t import StudentTUnivariate
+from copulas.univariate.truncated_gaussian import TruncatedGaussian
+
+__all__ = (
+    'BetaUnivariate',
+    'GammaUnivariate',
+    'GaussianKDE',
+    'GaussianUnivariate',
+    'TruncatedGaussian',
+    'StudentTUnivariate',
+    'Univariate',
+    'ParametricType',
+    'BoundedType'
+)
