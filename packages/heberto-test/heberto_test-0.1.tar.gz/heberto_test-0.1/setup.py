3b18608faceedc0463077042755d53d53964c631
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:03 +0000

    Add package heberto-test/0.1/heberto_test-0.1.tar.gz

---
+++ packages/heberto-test/heberto_test-0.1.tar.gz/heberto_test-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='heberto_test',
+      version='0.1',
+      description='Test for Udacity with distributions',
+      packages=['heberto_test'],
+      autor=['Heberto'],
+      zip_safe=False)
