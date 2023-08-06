d53aef6dba1b6dd1abefaf9ed67333bcc8d52df0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:32 +0000

    Add package pbdist/1.0/pbdist-1.0.tar.gz

---
+++ packages/pbdist/pbdist-1.0.tar.gz/pbdist-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pbdist',
+      version='1.0',
+      description='Gaussian distribution',
+      packages=['pbdist'],
+      author='Charitha',
+      author_email='rsreecharitha@gmail.com',
+      zip_safe=False)
