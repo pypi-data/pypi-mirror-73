74c9035edf82b09efd0ec968b1d277d17f6a2246
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:50 +0000

    Add package pipfilemerge/0.0.6/pipfilemerge-0.0.6.tar.gz

---
+++ packages/pipfilemerge/pipfilemerge-0.0.6.tar.gz/pipfilemerge-0.0.6/README.md
@@ -0,0 +1,6 @@
+Merge Pipfile
+==============
+
+Merges the Pipfiles in the current directory with any other Pipfiles in sub-directories.
+
+Used as a tool to allow for unpublished custom libraries to be included in your virtual environment made by Pipfile
---
+++ packages/pipfilemerge/pipfilemerge-0.0.6.tar.gz/pipfilemerge-0.0.6/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as readmeFile:
+    readmeStr = readmeFile.read()
+
+setup(
+    name="pipfilemerge",
+    version="0.0.6",
+    metadata_version="1.0",
+    description="Merge the Pipfiles into a single global Pipfile",
+    author="Laurkan Rodriguez",
+    author_email="laurkan@engineer.com",
+    long_description=readmeStr,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lorkaan/merge-pipfile.git",
+    download_url="https://github.com/lorkaan/pipfilemerge/archive/v0.0.6.tar.gz",
+    install_requires =[
+        'toml'
+    ],
+    packages=['pipfilemerge'],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent"
+    ]
+)
