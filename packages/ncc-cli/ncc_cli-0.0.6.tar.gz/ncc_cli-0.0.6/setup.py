e1baaa6dd4649b7d6ec0d78545a28e10bd243c48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:54 +0000

    Add package ncc-cli/0.0.6/ncc_cli-0.0.6.tar.gz

---
+++ packages/ncc-cli/ncc_cli-0.0.6.tar.gz/ncc_cli-0.0.6/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ncc_cli",
+    version="0.0.6",
+    author="James Hirst",
+    author_email="jdhirst@hirstgroup.net",
+    description="A small command-line client for NextCloud/ownCloud",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jdhirst1/ncc_cli",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'pyocclient',
+        'humanize',
+        'tqdm',
+        'yaspin',
+        'texttable',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': [
+            'ncc = ncc_cli.ncc_cli:main',
+        ]
+    }
+)
