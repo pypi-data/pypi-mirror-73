72db11948e85b132756cc8d41edfc86738bf9919
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:29 +0000

    Add package printdescribe/0.2.2/printdescribe-0.2.2.tar.gz

---
+++ packages/printdescribe/printdescribe-0.2.2.tar.gz/printdescribe-0.2.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name = 'printdescribe',
+      version = '0.2.2',
+      description = 'Print and describe',
+      author='Okechukwu Okigbo',
+      author_email='okigbookey@gmail.com',
+      packages=['printdescribe'], 
+      install_requires=['matplotlib',
+                          'numpy', 'tabulate',
+                          'pandas'], 
+      zip_safe = False)
