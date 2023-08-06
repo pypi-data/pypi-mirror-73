568f11f97eead779246574146b8376e163973aae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:46 +0000

    Add package distv1/0.1/distv1-0.1.tar.gz

---
+++ packages/distv1/distv1-0.1.tar.gz/distv1-0.1/README.md
@@ -0,0 +1,9 @@
+# dnsd-distributation package
+
+Summery of the package
+
+# File
+
+Explanation o ftfiles in the package
+
+#Installation 
---
+++ packages/distv1/distv1-0.1.tar.gz/distv1-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distv1',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
