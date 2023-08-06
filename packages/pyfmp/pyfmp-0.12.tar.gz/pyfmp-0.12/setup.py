cb56f5e071cf1fed8da64f50c5f152d7d4d820de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:58 +0000

    Add package pyfmp/0.12/pyfmp-0.12.tar.gz

---
+++ packages/pyfmp/pyfmp-0.12.tar.gz/pyfmp-0.12/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+setup(
+  name = 'pyfmp',         
+  packages = ['pyfmp'],   
+  version = '0.12',      
+  license='bsd-3-clause',
+  description = 'A wrapper around Financial Modeling Prep\'s api',
+  author = 'Dylan E. Holland', 
+  author_email = 'salinson1138@gmail.com',
+  url = 'https://github.com/DylanEHolland/pyfmp',
+  download_url = 'https://github.com/DylanEHolland/pyfmp/archive/0.12.tar.gz', 
+  keywords = ['FINANCIAL', 'MODELING', 'PREP', 'API'],
+  install_requires=[
+          'requests',
+          'pandas'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python :: 3',    
+  ],
+)   
