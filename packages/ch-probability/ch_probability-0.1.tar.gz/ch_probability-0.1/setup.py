63f296842a436f876ccb90d5d1e65b3b0a793f97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:58 +0000

    Add package ch-probability/0.1/ch_probability-0.1.tar.gz

---
+++ packages/ch-probability/ch_probability-0.1.tar.gz/ch_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ch_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['ch_probability'],
+      author = 'Carlos Hernandez-Vaquero',
+      author_email = 'carloshvp@gmail.com',
+      zip_safe=False)
