e3a62d31c0193f854adfe336b9c799a2ed9478fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:56 +0000

    Add package suhani-distribution/1.0/suhani_distribution-1.0.tar.gz

---
+++ packages/suhani-distribution/suhani_distribution-1.0.tar.gz/suhani_distribution-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='suhani_distribution',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['suhani_distribution'],
+      author = 'Suhani',
+      author_mail = 'arorasuhani1511@gmail.com',
+    zip_safe=False)
