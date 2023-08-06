8d91ebb8535f0fe8fdedc14290385895a1fb81f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:03 +0000

    Add package vk-mda/11.9.2/vk_mda-11.9.2-py3-none-any.whl

---
+++ packages/vk-mda/vk_mda-11.9.2-py3-none-any.whl/vk_mda/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+:authors: python273
+:license: Apache License, Version 2.0, see LICENSE file
+
+:copyright: (c) 2019 python273
+"""
+from .enums import *
+from .exceptions import *
+from .requests_pool import VkRequestsPool, vk_request_one_param_pool
+from .tools import VkTools
+from .upload import VkUpload
+from .vk_api import VkApi
+
+
+__author__ = 'lionead'
+__version__ = '11.9.2'
+__email__ = 'lionia058@gmail.com'
