3e28ba0f7ed85d79c7b0170e46af41a420113718
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:46 +0000

    Add package novelsave/0.2.2/novelsave-0.2.2-py3-none-any.whl

---
+++ packages/novelsave/novelsave-0.2.2-py3-none-any.whl/novelsave/__main__.py
@@ -0,0 +1,40 @@
+import argparse
+from novelsave import NovelSave
+
+parser = argparse.ArgumentParser(description='tool to convert webnovel to epub', epilog='Own your novels')
+parser.add_argument('novel', type=str, help='either id or url of novel')
+parser.add_argument('-t', '--timeout', type=int, help='webdriver timeout', default=60)
+
+actions = parser.add_argument_group(title='actions')
+actions.add_argument('-u', '--update', action='store_true', help='update webnovel details')
+actions.add_argument('-p', '--pending', action='store_true', help='download pending')
+actions.add_argument('-c', '--create', action='store_true', help='create epub')
+
+credentials = parser.add_argument_group(title='credentials')
+credentials.add_argument('--email', type=str,  help='webnovel email')
+credentials.add_argument('--pass', type=str, help='webnovel password', dest='password')
+
+args = parser.parse_args()
+
+# parse novel id
+if 'https://' in args.novel:
+    novel_id = int(args.novel.split('/')[4])
+else:
+    novel_id = int(args.novel)
+
+novelsave = NovelSave(novel_id)
+novelsave.email = args.email
+novelsave.password = args.password
+novelsave.timeout = args.timeout
+
+if not any([args.update, args.pending, args.create]):
+    print('✗ No actions selected')
+
+if args.update:
+    novelsave.update_data()
+
+if args.pending:
+    novelsave.download_pending()
+
+if args.create:
+    novelsave.create_epub()
