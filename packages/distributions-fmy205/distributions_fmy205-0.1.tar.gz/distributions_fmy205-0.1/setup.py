51a3f999e31de585a2e8856fa787cebd5c7279f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:43 +0000

    Add package distributions-fmy205/0.1/distributions_fmy205-0.1.tar.gz

---
+++ packages/distributions-fmy205/distributions_fmy205-0.1.tar.gz/distributions_fmy205-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_fmy205',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_fmy205'],
+      zip_safe=False)
