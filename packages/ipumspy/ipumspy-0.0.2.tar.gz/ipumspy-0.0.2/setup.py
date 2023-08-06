78b6033f616e9ebeefc2d4239742e556bb6db5f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:01 +0000

    Add package ipumspy/0.0.2/ipumspy-0.0.2.tar.gz

---
+++ packages/ipumspy/ipumspy-0.0.2.tar.gz/ipumspy-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ipumspy",
+    version="0.0.2",
+    author="IPUMS",
+    author_email="ipums@umn.edu",
+    description="A placeholder for ipumspy",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mnpopcenter/ipumspy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
