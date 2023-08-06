8636b30561db89e0e49a17c8df0beb4d8c528f86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:14 +0000

    Add package odoo-build/3.0rc1/odoo-build-3.0rc1.tar.gz

---
+++ packages/odoo-build/odoo-build-3.0rc1.tar.gz/odoo-build-3.0rc1/odoo_build/command.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Part of odoo-build.
+# See LICENSE file for full copyright and licensing details.
+
+import subprocess
+from .console import out
+
+
+class Command():
+    def __init__(self, *args):
+        self.args = args or []
+
+    def call(self, parse_log=False):
+        out("Command: {}".format(' '.join(self.args)), 'important')
+        try:
+            process = subprocess.Popen(self.args)
+            process.communicate()
+        except KeyboardInterrupt:
+            try:
+                process.terminate()
+                process.wait()
+            except OSError:
+                pass
+            out("Odoo stopped by keyboard interrupt!", 'warning')
