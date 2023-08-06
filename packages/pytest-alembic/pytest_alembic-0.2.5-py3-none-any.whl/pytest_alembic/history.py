8eec75266184288b1dfc649abd0e7c5c0d170e07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:13 +0000

    Add package pytest-alembic/0.2.5/pytest_alembic-0.2.5-py3-none-any.whl

---
+++ packages/pytest-alembic/pytest_alembic-0.2.5-py3-none-any.whl/pytest_alembic/history.py
@@ -0,0 +1,66 @@
+import collections
+import functools
+import itertools
+import re
+from dataclasses import dataclass
+from typing import Dict, List, Optional, Tuple
+
+_RE_REVISION_PATTERN = r"-> ([a-zA-Z0-9]*)[ ,]"
+
+
+@dataclass
+class AlembicHistory:
+    revisions: List[str]
+    revision_indices: Dict[str, int]
+    revisions_by_index: Dict[int, str]
+
+    @classmethod
+    @functools.lru_cache()
+    def parse(cls, raw_history: Tuple[str]) -> "AlembicHistory":
+        """Extract the set of migration revision hashes from the result of an `alembic history` command.
+        """
+        revision_hashes = ["head"]
+        for line in raw_history:
+            match = re.search(_RE_REVISION_PATTERN, line)
+            if not match:
+                continue
+
+            revision_hashes.append(match.group(1))
+        revision_hashes.append("base")
+
+        revisions = list(reversed(revision_hashes))
+        revision_indices = {revision: i for i, revision in enumerate(revisions)}
+        revisions_by_index = {v: k for k, v in revision_indices.items()}
+        return cls(revisions, revision_indices, revisions_by_index)
+
+    def validate_revision(self, revision):
+        if revision not in self.revision_indices:
+            raise ValueError(f"Revision {revision} is not a valid revision in alembic's history")
+
+    def previous_revision(self, revision: str) -> Optional[str]:
+        self.validate_revision(revision)
+        revision_index = self.revision_indices[revision]
+        return self.revisions_by_index.get(revision_index - 1)
+
+    def next_revision(self, revision: str) -> Optional[str]:
+        self.validate_revision(revision)
+        revision_index = self.revision_indices[revision]
+        return self.revisions_by_index.get(revision_index + 1)
+
+    def revision_range(self, current_revision: str, dest_revision: str) -> List[str]:
+        self.validate_revision(current_revision)
+        self.validate_revision(dest_revision)
+        start_index = self.revision_indices[current_revision]
+        end_index = self.revision_indices[dest_revision]
+        return [self.revisions[index] for index in range(start_index, end_index + 1)]
+
+    def revision_window(self, current_revision: str, dest_revision: str) -> List[Tuple[str, str]]:
+        revision_range = self.revision_range(current_revision, dest_revision)
+        return list(
+            zip(  # type: ignore
+                *(
+                    collections.deque(itertools.islice(it, i), 0) or it
+                    for i, it in enumerate(itertools.tee(revision_range, 2))
+                )
+            )
+        )
---
+++ packages/pytest-alembic/pytest_alembic-0.2.5-py3-none-any.whl/pytest_alembic/runner.py
@@ -0,0 +1,202 @@
+import contextlib
+import functools
+from dataclasses import dataclass
+from typing import Dict, List, Optional
+
+from pytest_alembic.executor import CommandExecutor, ConnectionExecutor
+from pytest_alembic.history import AlembicHistory
+from pytest_alembic.revision_data import RevisionData
+
+
+@contextlib.contextmanager
+def runner(config, engine=None):
+    """Manage the alembic execution context, in a given context.
+
+    Yields:
+        `MigrationContext` to the caller.
+    """
+    command_executor = CommandExecutor.from_config(config)
+    migration_context = MigrationContext.from_config(
+        config, command_executor, ConnectionExecutor(engine)
+    )
+
+    command_executor.configure(connection=engine)
+    yield migration_context
+
+
+def _sequence_directives(*directives):
+    def directive_wrapper(*args, **kwargs):
+        for directive in directives:
+            if not directive:
+                continue
+            directive(*args, **kwargs)
+
+    return directive_wrapper
+
+
+@dataclass
+class MigrationContext:
+    """Within a given environment/execution context, executes alembic commands.
+    """
+
+    command_executor: CommandExecutor
+    revision_data: RevisionData
+    connection_executor: ConnectionExecutor
+
+    @classmethod
+    def from_config(
+        cls,
+        config: Dict,
+        command_executor: CommandExecutor,
+        connection_executor: ConnectionExecutor,
+    ):
+        return cls(
+            command_executor=command_executor,
+            revision_data=RevisionData.from_config(config),
+            connection_executor=connection_executor,
+        )
+
+    @property
+    def history(self) -> AlembicHistory:
+        """Get the revision history.
+        """
+        raw_history = self.command_executor.run_command("history")
+        return AlembicHistory.parse(tuple(raw_history))
+
+    @property
+    def heads(self) -> List[str]:
+        """Get the list of revision heads.
+        """
+        return self.command_executor.run_command("heads")
+
+    @property
+    def current(self) -> Optional[str]:
+        """Get the list of revision heads.
+        """
+        current = self.command_executor.run_command("current")
+        if current:
+            return current[0].strip().split(" ")[0]
+        return "base"
+
+    def generate_revision(self, process_revision_directives=None, **kwargs):
+        """Generate a test revision.
+
+        The final act of this process raises a `RevisionSuccess`, which is used as a sentinal
+        to indicate the revision was generated successfully, while not actually finishing the
+        generation of the revision file.
+        """
+        alembic_config = self.command_executor.alembic_config
+        config_directive = alembic_config.attributes["process_revision_directives"]
+        fn = RevisionSuccess.process_revision_directives(
+            _sequence_directives(config_directive, process_revision_directives)
+        )
+        try:
+            return self.command_executor.run_command(
+                "revision", process_revision_directives=fn, **kwargs
+            )
+        except RevisionSuccess:
+            pass
+
+    def raw_command(self, *args, **kwargs):
+        """Execute a raw alembic command.
+        """
+        return self.command_executor.run_command(*args, **kwargs)
+
+    def managed_upgrade(self, dest_revision):
+        """Perform an upgrade, one migration at a time, inserting static data at the given points.
+        """
+        current = self.current
+        for current_revision, next_revision in self.history.revision_window(current, dest_revision):
+            before_upgrade_data = list(self.revision_data.get_before(next_revision))
+            self.connection_executor.table_insert(current_revision, before_upgrade_data)
+
+            self.raw_command("upgrade", next_revision)
+
+            at_upgrade_data = list(self.revision_data.get_at(next_revision))
+            self.connection_executor.table_insert(next_revision, at_upgrade_data)
+
+    def migrate_up_before(self, revision):
+        """Upgrade up to, but not including the given `revision`.
+        """
+        preceeding_revision = self.history.previous_revision(revision)
+        self.managed_upgrade(preceeding_revision)
+
+    def migrate_up_to(self, revision):
+        """Upgrade up to, and including the given `revision`.
+        """
+        self.managed_upgrade(revision)
+
+    def migrate_up_one(self):
+        """Upgrade up by exactly one revision.
+        """
+        revision = self.history.next_revision(self.current)
+        self.managed_upgrade(revision)
+
+    def migrate_down_before(self, revision):
+        """Upgrade down to, but not including the given `revision`.
+        """
+        next_revision = self.history.next_revision(revision)
+        self.migrate_down_to(next_revision)
+
+    def migrate_down_to(self, revision):
+        """Upgrade down to, and including the given `revision`.
+        """
+        self.history.validate_revision(revision)
+        self.raw_command("downgrade", revision)
+
+    def migrate_down_one(self):
+        """Upgrade down by exactly one revision.
+        """
+        self.raw_command("downgrade", "-1")
+
+    def roundtrip_next_revision(self):
+        """Upgrade, downgrade then upgrade.
+
+        This is meant to ensure that the given revision is idempotent.
+        """
+        self.migrate_up_one()
+        self.migrate_down_one()
+        self.migrate_up_one()
+
+    def insert_into(self, table, data):
+        """Insert data into a given table.
+
+        Args:
+            table: The name of the table to insert data into
+            data: The data to insert. This is eventually passed through to SQLAlchemy's
+                Table class `values` method, and so should accept either a list of
+                `dict`s representing a list of rows, or a `dict` representing one row.
+        """
+        return self.connection_executor.table_insert(
+            revision=self.current, tablename=table, data=data
+        )
+
+    def table_at_revision(self, name, *, revision=None, schema=None):
+        """Insert data into a given table.
+
+        Args:
+            name: The name of the table to insert data into
+            revision: The revision of the table to return.
+        """
+        revision = revision or self.current
+        return self.connection_executor.table(revision=revision, name=name, schema=schema)
+
+
+class RevisionSuccess(Exception):
+    """Raise when a revision is successfully generated.
+
+    In order to prevent the generation of an actual revision file on disk when running tests,
+    this exception should be raised.
+    """
+
+    @classmethod
+    def process_revision_directives(cls, fn):
+        """Wrap a real `process_revision_directives` function, while preventing it from completing.
+        """
+
+        @functools.wraps(fn)
+        def _process_revision_directives(context, revision, directives):
+            fn(context, revision, directives)
+            raise cls()
+
+        return _process_revision_directives
