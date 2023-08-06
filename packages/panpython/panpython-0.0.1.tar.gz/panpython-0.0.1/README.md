850a90030f799070f80cc14f14e5c5040ae087c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:07 +0000

    Add package panpython/0.0.1/panpython-0.0.1.tar.gz

---
+++ packages/panpython/panpython-0.0.1.tar.gz/panpython-0.0.1/README.md
@@ -0,0 +1,4 @@
+# PanPython
+
+* This is a demo SDK to visualize Pandat HTC results.
+* Please visit https://github.com/andrewlyu/panpython to get the example notebook which uses this SDK.
---
+++ packages/panpython/panpython-0.0.1.tar.gz/panpython-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="panpython", # Replace with your own username
+    version="0.0.1",
+    author="Duchao Lv",
+    author_email="duchaolv@gmail.com",
+    description="a SDK for Pandat applications",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/andrewlyu/panpython",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
