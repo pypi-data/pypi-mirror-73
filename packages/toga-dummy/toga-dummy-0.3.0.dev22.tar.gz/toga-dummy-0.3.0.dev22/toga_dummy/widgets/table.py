3612361824ecc8846ea2a74ff097c9c2832284cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:36 +0000

    Add package toga-dummy/0.3.0.dev22/toga-dummy-0.3.0.dev22.tar.gz

---
+++ packages/toga-dummy/toga-dummy-0.3.0.dev22.tar.gz/toga-dummy-0.3.0.dev22/toga_dummy/widgets/table.py
@@ -0,0 +1,33 @@
+from .base import Widget
+
+
+class Table(Widget):
+    def create(self):
+        self._action('create Table')
+
+    def change_source(self, source):
+        self._action('change source', source=source)
+
+    def insert(self, index, item):
+        self._action('insert row', index=index, item=item)
+
+    def change(self, item):
+        self._action('change row', item=item)
+
+    def remove(self, item):
+        self._action('remove row', item=item)
+
+    def clear(self):
+        self._action('clear')
+
+    def set_on_select(self, handler):
+        self._set_value('on_select', handler)
+
+    def scroll_to_row(self, row):
+        self._set_value('scroll to', row)
+
+    def add_column(self, heading, accessor):
+        self._action('add column', heading=heading, accessor=accessor)
+
+    def remove_column(self, accessor):
+        self._action('remove column', accessor=accessor)
