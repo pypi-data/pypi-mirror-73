38541eb1b2dea9c47709221688d2dd9b6e75ac90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:32 +0000

    Add package ANIBBLE-X/0.7.6/ANIBBLE_X-0.7.6.tar.gz

---
+++ packages/ANIBBLE-X/ANIBBLE_X-0.7.6.tar.gz/ANIBBLE_X-0.7.6/ANIBBLE_X/__init__.py
@@ -0,0 +1,5 @@
+from .graph_visualisation_toolskit import draw_from_list, draw_from_networkx_graphs_list, draw_from_networkx, adjusting_someone_graph
+
+__version__ = '0.7.6'
+__author__ = 'Mélanie Sawaryn'
+
