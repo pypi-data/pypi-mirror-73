7e4b3d8608cb13af126e003c05c9bcdd85083549
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:23 +0000

    Add package pyIndego/2.0.24/pyIndego-2.0.24.tar.gz

---
+++ packages/pyIndego/pyIndego-2.0.24.tar.gz/pyIndego-2.0.24/setup.py
@@ -0,0 +1,24 @@
+"""Setup for pyIndego."""
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pyIndego",
+    version="2.0.24",
+    author="jm-73",
+    author_email="jens@myretyr.se",
+    description="API for Bosch Indego mower",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jm-73/pyIndego",
+    packages=find_packages("."),
+    install_requires=["requests", "aiohttp", "pytz"],
+    extras_require={"testing": ["pytest", "pytest-aiohttp", "pytest-cov", "mock"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
