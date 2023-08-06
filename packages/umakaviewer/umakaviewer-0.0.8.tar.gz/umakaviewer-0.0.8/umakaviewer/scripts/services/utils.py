b3f9ab0ac9f7b8fd6919fd67d67b30d66e8b7471
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:34 +0000

    Add package umakaviewer/0.0.8/umakaviewer-0.0.8.tar.gz

---
+++ packages/umakaviewer/umakaviewer-0.0.8.tar.gz/umakaviewer-0.0.8/umakaviewer/scripts/services/utils.py
@@ -0,0 +1,22 @@
+# coding:utf-8
+
+from rdflib.plugins.parsers.ntriples import r_literal
+from rdflib import Literal
+from mimetypes import guess_type
+import os
+
+
+def parse_literal(literal):
+    return Literal(*[l if l else None for l in r_literal.match(literal).groups()])
+
+
+def get_type(file_path):
+    mimetype, _ = guess_type(file_path)
+    if mimetype:
+        return mimetype.split('/')[1]
+
+    _, ext = os.path.splitext(file_path)
+    if ext == '.ttl':
+        return 'turtle'
+    elif ext in ('.n3', '.nt'):
+        return 'n3'
