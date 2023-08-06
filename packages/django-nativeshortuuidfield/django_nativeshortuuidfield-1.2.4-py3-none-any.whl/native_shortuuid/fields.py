3c90461427c139ccbc885bf36af7f8de5203d038
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:10 +0000

    Add package django-nativeshortuuidfield/1.2.4/django_nativeshortuuidfield-1.2.4-py3-none-any.whl

---
+++ packages/django-nativeshortuuidfield/django_nativeshortuuidfield-1.2.4-py3-none-any.whl/native_shortuuid/fields.py
@@ -0,0 +1,52 @@
+import django.db.models
+from django.core.exceptions import ValidationError
+from django.forms import CharField
+from django.utils.translation import gettext_lazy as _
+
+import uuid
+import shortuuid
+
+
+def short_uuid4():
+    return shortuuid.encode(uuid.uuid4())
+
+
+class NativeShortUUIDFormField(CharField):
+    default_error_messages = {
+        'invalid': _('Enter a valid ShortUUID.'),
+    }
+
+    def to_python(self, value):
+        value = super().to_python(value)
+        if value in self.empty_values:
+            return None
+        try:
+            shortuuid.decode(value)
+        except ValueError:
+            raise ValidationError(self.error_messages['invalid'], code='invalid')
+        return value
+
+
+class NativeShortUUIDField(django.db.models.UUIDField):
+    def __init__(self, verbose_name=None, **kwargs):
+        default = kwargs.get('default', None)
+        if default is uuid.uuid4:
+            kwargs['default'] = short_uuid4
+        super().__init__(verbose_name, **kwargs)
+
+    def from_db_value(self, value, expression, connection):
+        if value is None:
+            return value
+        return shortuuid.encode(value)
+
+    def to_python(self, value):
+        if value is None:
+            return value
+
+        return super().to_python(shortuuid.decode(value))
+
+    def formfield(self, **kwargs):
+        return super().formfield(**{
+            'form_class': NativeShortUUIDFormField,
+            **kwargs,
+        })
