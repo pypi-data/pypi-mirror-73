eb038eeb0fb1c2c86a91bcd7e37c250f70f47c44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:04 +0000

    Add package customerrors/0.0.5/customerrors-0.0.5.tar.gz

---
+++ packages/customerrors/customerrors-0.0.5.tar.gz/customerrors-0.0.5/setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup, find_packages
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "customerrors",
+    version = "0.0.5",
+    author = "Splatboy Dev",
+    author_email = "splatboy20081@gmail.com",
+    description = ("A pypi test."),
+    license = "BSD",
+    keywords = "example documentation tutorial",
+    url = "",
+    packages= find_packages(),
+    long_description=read('README.txt'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
