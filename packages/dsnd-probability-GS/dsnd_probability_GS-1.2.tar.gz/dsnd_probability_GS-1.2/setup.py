97419db9a969b11e3aef672b960ccb020ca7579a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:10 +0000

    Add package dsnd-probability-GS/1.2/dsnd_probability_GS-1.2.tar.gz

---
+++ packages/dsnd-probability-GS/dsnd_probability_GS-1.2.tar.gz/dsnd_probability_GS-1.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_GS',
+      version='1.2',
+      description='Gaussian and Binomail distributions',
+      packages=['dsnd_probability_GS'],
+      author='Gurnek',
+      zip_safe=False)
