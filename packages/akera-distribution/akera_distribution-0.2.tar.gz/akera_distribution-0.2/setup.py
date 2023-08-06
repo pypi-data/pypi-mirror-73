7f0e24e3442c51576424320b1756391841269cf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:07 +0000

    Add package akera-distribution/0.2/akera_distribution-0.2.tar.gz

---
+++ packages/akera-distribution/akera_distribution-0.2.tar.gz/akera_distribution-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='akera_distribution',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['akera_distribution'],
+      author = "Salaudeen Hafeez",
+      author_email = "amplifiedsalaudeenak21@gmail.com",
+      zip_safe=False)
