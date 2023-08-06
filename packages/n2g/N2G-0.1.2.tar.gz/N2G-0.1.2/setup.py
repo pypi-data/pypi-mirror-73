54979f8cc2598944eae55fc396141e108df61168
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:39 +0000

    Add package n2g/0.1.2/N2G-0.1.2.tar.gz

---
+++ packages/n2g/N2G-0.1.2.tar.gz/N2G-0.1.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+__author__ = "Denis Mulyalin <d.mulyalin@gmail.com>"
+
+setup(
+    name="N2G",
+    version="0.1.2",
+    author="Denis Mulyalin",
+    author_email="d.mulyalin@gmail.com",
+    description="Need To Graph",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dmulyalin/N2G",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Topic :: Utilities",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
