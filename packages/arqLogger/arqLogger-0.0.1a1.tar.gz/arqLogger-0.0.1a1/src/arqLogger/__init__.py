c2a7cdd89d56cc392c6ad0e853d7587e0226ef46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:24 +0000

    Add package arqLogger/0.0.1a1/arqLogger-0.0.1a1.tar.gz

---
+++ packages/arqLogger/arqLogger-0.0.1a1.tar.gz/arqLogger-0.0.1a1/src/arqLogger/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+    arqLogger: Python websocker logger for Arquants strategies.
+
+    This modules expose functions and enumerations of the library.
+"""
+from src.arqLogger.arquant_logging import ArquantLogger
+
+__version__ = "0.0.1a1"
