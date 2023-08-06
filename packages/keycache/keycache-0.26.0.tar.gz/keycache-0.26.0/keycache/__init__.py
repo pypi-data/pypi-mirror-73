178145dc133212ff20e56bc52e55e51bfe00d781
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:18 +0000

    Add package keycache/0.26.0/keycache-0.26.0.tar.gz

---
+++ packages/keycache/keycache-0.26.0.tar.gz/keycache-0.26.0/keycache/__init__.py
@@ -0,0 +1,12 @@
+
+
+# github.com/psytron/keycache
+# USAGE :
+# keycache.set_system_encrypt_key( sysfinger.generate() )
+# keycache.get('healthity' , alias='healthity')
+# future usage
+# levels.set( 'alias L 0' ,'domain L 1' )
+# def set( self , 'level_0' , 'level_1'):
+#   print(' next level ')
+
+from .keycache import Keycache
