90b3833698b786d9592b914b3b07c0e4f3756d96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:36 +0000

    Add package napalm-mos/3.0.0/napalm-mos-3.0.0.tar.gz

---
+++ packages/napalm-mos/napalm-mos-3.0.0.tar.gz/napalm-mos-3.0.0/setup.py
@@ -0,0 +1,26 @@
+"""setup.py file."""
+from setuptools import setup, find_packages
+
+__author__ = "Benny Holmgren <benny@holmgren.id.au>"
+
+with open("requirements.txt", "r") as fs:
+    reqs = [r for r in fs.read().splitlines() if (len(r) > 0 and not r.startswith("#"))]
+
+setup(
+    name="napalm-mos",
+    version="3.0.0",
+    packages=find_packages(),
+    author="Benny Holmgren, Brandon Ewing",
+    author_email="benny@holmgren.id.au, brandon.ewing@warningg.com",
+    description="Network Automation and Programmability Abstraction Layer with Multivendor support",
+    classifiers=[
+        "Topic :: Utilities",
+        "Programming Language :: Python",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS",
+    ],
+    url="https://github.com/napalm-automation-community/napalm-mos",
+    include_package_data=True,
+    install_requires=reqs,
+    python_requires=">=3.6",
+)
