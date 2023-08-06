8138d6d2c4ae1cef0025b8f86b85c6ed224985e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:20 +0000

    Add package neattext/0.0.3/neattext-0.0.3.tar.gz

---
+++ packages/neattext/neattext-0.0.3.tar.gz/neattext-0.0.3/neattext/__init__.py
@@ -0,0 +1,11 @@
+__version__ = '0.0.3'
+from .neattext import TextCleaner
+from .neattext import TextExtractor
+from .neattext import TextMetrics
+# import neattext.functions ##worked
+from neattext.functions import *
+from neattext.pattern_data import *
+from neattext.explainer import *
+
+
+
