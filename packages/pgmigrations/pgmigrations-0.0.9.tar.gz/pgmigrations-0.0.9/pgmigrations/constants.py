b401ff424c3fdd84d83967451c4418029bf95311
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:43 +0000

    Add package pgmigrations/0.0.9/pgmigrations-0.0.9.tar.gz

---
+++ packages/pgmigrations/pgmigrations-0.0.9.tar.gz/pgmigrations-0.0.9/pgmigrations/constants.py
@@ -0,0 +1,5 @@
+BOOTSTRAP_MIGRATIONS_DIRECTORY = "bootstrap_migrations"
+MIGRATIONS_DIRECTORY = "migrations"
+MIGRATIONS_TABLE_NAME = "pgmigrations"
+APPLY_FILENAME = "apply.sql"
+ROLLBACK_FILENAME = "rollback.sql"
