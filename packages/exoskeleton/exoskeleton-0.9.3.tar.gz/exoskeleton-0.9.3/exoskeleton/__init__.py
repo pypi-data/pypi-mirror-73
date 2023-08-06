36b72b2b6e0d162c68bd5dd891540ed30333e9a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:13 +0000

    Add package exoskeleton/0.9.3/exoskeleton-0.9.3.tar.gz

---
+++ packages/exoskeleton/exoskeleton-0.9.3.tar.gz/exoskeleton-0.9.3/exoskeleton/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from exoskeleton.__main__ import Exoskeleton
+from exoskeleton.TimeManager import TimeManager
+from exoskeleton import database_check
+
+name = "exoskeleton"
+__version__ = "0.9.3"
+__author__ = "Rüdiger Voigt"
