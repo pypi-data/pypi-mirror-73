1d40f5f7ec1151d7b521db27a2dc25f52c8be539
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:50 +0000

    Add package ipfs-api-mount/0.3.2/ipfs_api_mount-0.3.2.tar.gz

---
+++ packages/ipfs-api-mount/ipfs_api_mount-0.3.2.tar.gz/ipfs_api_mount-0.3.2/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+from setuptools import setup
+from distutils.command.build import build
+from setuptools.command.develop import develop
+from subprocess import check_call
+
+
+def compile_protobuf():
+    check_call(['protoc', '--python_out=.', 'ipfs_api_mount/unixfs.proto'])
+
+
+class custom_build(build):
+    def run(self):
+        if not self.dry_run:
+            compile_protobuf()
+        build.run(self)
+
+
+class custom_develop(develop):
+    def run(self):
+        if not self.dry_run:
+            compile_protobuf()
+        develop.run(self)
+
+
+exec(open('ipfs_api_mount/version.py').read())
+
+
+setup(
+    name='ipfs_api_mount',
+    version=__version__,  # noqa: F821
+    description='Mount IPFS directory as local FS.',
+    license='MIT',
+    url='https://github.com/SupraSummus/ipfs-api-mount',
+    classifiers=[
+        'Topic :: System :: Filesystems',
+    ],
+    keywords='ipfs fuse mount fs',
+    install_requires=[
+        'fusepy==3.0.*',
+        'ipfshttpclient==0.6.*',
+        'lru-dict==1.*',
+        'protobuf>=3.12,<4',
+        'py-multibase==1.*',
+    ],
+    packages=['ipfs_api_mount'],
+    scripts=['bin/ipfs-api-mount'],
+    package_data={
+        'ipfs_api_mount': ['ipfs_api_mount/unixfs.proto'],
+    },
+    cmdclass={
+        'build': custom_build,
+        'develop': custom_develop,
+    },
+)
