0323a02e3dea1cf90e333aba01262f2fba3bc2d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:12 +0000

    Add package cumulus-message-adapter-python/1.2.1/cumulus_message_adapter_python-1.2.1.tar.gz

---
+++ packages/cumulus-message-adapter-python/cumulus_message_adapter_python-1.2.1.tar.gz/cumulus_message_adapter_python-1.2.1/run_cumulus_task.py
@@ -0,0 +1,88 @@
+"""
+Interprets incoming messages, passes them to an inner handler, gets the
+response and transforms it into an outgoing message, returned by Lambda.
+"""
+import os
+import sys
+
+from cumulus_logger import CumulusLogger
+
+
+def set_sys_path():
+    # If the lambda has CUMULUS_MESSAGE_ADAPTER_DIR set, use the CMA lib
+    # present at that location
+    if os.environ.get('CUMULUS_MESSAGE_ADAPTER_DIR'):
+        sys.path.insert(0, os.environ.get('CUMULUS_MESSAGE_ADAPTER_DIR'))
+
+    # if the message adapter zip file has been included, put it in the path
+    # it'll be used instead of the version from the requirements file
+    if os.path.isfile('cumulus-message-adapter.zip'):
+        sys.path.insert(0, 'cumulus-message-adapter.zip')
+
+
+def run_cumulus_task(
+        task_function,
+        cumulus_message,
+        context=None,
+        schemas=None,
+        **taskargs):
+    """
+    Interprets incoming messages, passes them to an inner handler, gets the
+    response and transforms it into an outgoing message, returned by Lambda.
+
+    Arguments:
+        task_function -- Required. The function containing the business logic
+            of the cumulus task
+        cumulus_message -- Required. Either a full Cumulus Message or a Cumulus
+            Remote Message
+        context -- AWS Lambda context object
+        schemas -- Optional. A dict with filepaths of `input`, `config`, and
+            `output` schemas that are relative to the task root directory. All
+            three properties of this dict are optional. If omitted, the message
+            adapter will look in `/<task_root>/schemas/<schema_type>.json`, and
+            if not found there, will be ignored.
+        taskargs -- Optional. Additional keyword arguments for the
+            task_function
+    """
+
+    set_sys_path()
+    from message_adapter.message_adapter import MessageAdapter
+
+    context_dict = vars(context) if context else {}
+    logger = CumulusLogger()
+    logger.setMetadata(cumulus_message, context)
+    message_adapter_disabled = str(
+        os.environ.get('CUMULUS_MESSAGE_ADAPTER_DISABLED')
+    ).lower()
+
+    if message_adapter_disabled == 'true':
+        try:
+            return task_function(cumulus_message, context, **taskargs)
+        except Exception as exception:
+            name = exception.args[0]
+            if isinstance(name, str) and 'WorkflowError' in name:
+                cumulus_message['payload'] = None
+                cumulus_message['exception'] = name
+                logger.error('WorkflowError')
+                return cumulus_message
+            logger.error(exception)
+            raise
+
+    adapter = MessageAdapter(schemas)
+    full_event = adapter.load_and_update_remote_event(cumulus_message, context_dict)
+    nested_event = adapter.load_nested_event(full_event, context_dict)
+    message_config = nested_event.get('messageConfig', {})
+
+    try:
+        task_response = task_function(nested_event, context, **taskargs)
+    except Exception as exception:
+        name = exception.args[0]
+        if isinstance(name, str) and 'WorkflowError' in name:
+            cumulus_message['payload'] = None
+            cumulus_message['exception'] = name
+            logger.error('WorkflowError')
+            return cumulus_message
+        logger.error(exception)
+        raise
+
+    return adapter.create_next_event(task_response, full_event, message_config)
---
+++ packages/cumulus-message-adapter-python/cumulus_message_adapter_python-1.2.1.tar.gz/cumulus_message_adapter_python-1.2.1/setup.py
@@ -0,0 +1,58 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+import imp
+
+
+here = path.abspath(path.dirname(__file__))
+
+# get the dependencies and installs
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    all_reqs = f.read().split('\n')
+install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
+dependency_links = [x.strip().replace('git+', '') for x in all_reqs if 'git+' in x]
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+__version__ = imp.load_source('version', 'version.py').__version__
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+setup(
+    name='cumulus_message_adapter_python',  # Required
+
+    # Versions should comply with PEP 440:
+    # https://www.python.org/dev/peps/pep-0440/
+    version=__version__,  # Required
+
+    # This is a one-line description or tagline of what your project does. This
+    # corresponds to the "Summary" metadata field:
+    # https://packaging.python.org/specifications/core-metadata/#summary
+    description='A handler library for cumulus tasks written in python',  # Required
+    long_description=long_description,  # Optional
+    long_description_content_type='text/markdown',
+    url='https://github.com/cumulus-nasa/cumulus-message-adapter-python',  # Optional
+    author='Cumulus Authors',  # Optional
+    author_email='info@developmentseed.org',  # Optional
+    classifiers=[  # Optional
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        # Indicate who your project is intended for
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+    ],
+    keywords='nasa cumulus',  # Optional
+    packages=find_packages(exclude=['.circleci', 'contrib', 'docs', 'tests']),
+    py_modules=['run_cumulus_task', 'cumulus_logger'],
+    install_requires=install_requires,
+    dependency_links=dependency_links
+)
