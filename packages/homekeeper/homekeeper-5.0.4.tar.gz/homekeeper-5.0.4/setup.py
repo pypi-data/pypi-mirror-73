5a7b5d75a76fe67adda9b2926f328d0591d3f1ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:41 +0000

    Add package homekeeper/5.0.4/homekeeper-5.0.4.tar.gz

---
+++ packages/homekeeper/homekeeper-5.0.4.tar.gz/homekeeper-5.0.4/setup.py
@@ -0,0 +1,30 @@
+import homekeeper
+import setuptools
+
+
+with open('README.txt', encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setuptools.setup(
+    author='Min Huang',
+    author_email='min.huang@alumni.usc.edu',
+    description='Symlinks your dotfiles from anywhere to your home directory.',
+    install_requires=['click >= 7.1.2'],
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    name='homekeeper',
+    packages=['homekeeper'],
+    platforms=['any'],
+    url='https://github.com/retiman/homekeeper',
+    version=homekeeper.__version__,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Topic :: Utilities',
+    ],
+    entry_points="""
+        [console_scripts]
+            homekeeper = homekeeper.cli:main
+    """,
+)
