8eb48c20393887f1460f225a8557814804adb513
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:48 +0000

    Add package yeyenino/1.0/yeyenino-1.0.tar.gz

---
+++ packages/yeyenino/yeyenino-1.0.tar.gz/yeyenino-1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='yeyenino',   #模块名字
+      version='1.0',   #版本号
+      description='字段判断', #描述
+      author='yeyenino',     #作者
+      author_email='714537539@qq.com',
+      py_modules=['yeyenino.autoIncrement']  #要发布的模块
+      )
---
+++ packages/yeyenino/yeyenino-1.0.tar.gz/yeyenino-1.0/yeyenino/autoIncrement.py
@@ -0,0 +1,7 @@
+def a(DLMC,Id):
+    if DLMC == "180" and Id == 1:
+        return "0101"
+    elif DLMC == "135":
+        return "0102"
+    else:
+        return "0103"
