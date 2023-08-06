950f053094400f5d1da3d43209c4ad7a4786bbff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:06 +0000

    Add package filoc/0.0.8/filoc-0.0.8-py3-none-any.whl

---
+++ packages/filoc/filoc-0.0.8-py3-none-any.whl/filoc/__init__.py
@@ -0,0 +1,12 @@
+from .rawfiloc import RawFiloc
+from .filoc import Filoc
+from .multiloc import MultilocBase, Multiloc, PandasMultiloc
+
+__version__ = '0.0.6'
+__all__     = [
+    'RawFiloc',
+    'Filoc',
+    'MultilocBase',
+    'Multiloc',
+    'PandasMultiloc',
+]
