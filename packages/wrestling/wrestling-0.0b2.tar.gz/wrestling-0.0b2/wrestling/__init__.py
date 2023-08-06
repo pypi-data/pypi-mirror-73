4260c394e69c50146c57c1adae790003645c5a5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:34 +0000

    Add package wrestling/0.0b2/wrestling-0.0b2.tar.gz

---
+++ packages/wrestling/wrestling-0.0b2.tar.gz/wrestling-0.0b2/wrestling/__init__.py
@@ -0,0 +1,4 @@
+from .events import Event
+from .wrestlers import CollegeWrestler, HighSchoolWrestler
+from .scoring import HighSchoolScoring, CollegeScoring
+from .matches import HighSchoolMatch, CollegeMatch
