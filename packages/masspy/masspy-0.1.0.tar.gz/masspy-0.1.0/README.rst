ffc37c9cf3bd7c16eef6d4abbdb4299ffcfea304
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:19 +0000

    Add package masspy/0.1.0/masspy-0.1.0.tar.gz

---
+++ packages/masspy/masspy-0.1.0.tar.gz/masspy-0.1.0/README.rst
@@ -0,0 +1,76 @@
+MASSpy - Mass Action Stoichiometric Simulation in Python
+========================================================
+
+|PyPI|
+
+What is MASSpy?
+~~~~~~~~~~~~~~~
+The **M**\ass **A**\ction **S**\toichiometric **S**\imulation **py**\thon
+(**MASSpy**) package contains modules for the construction, simulation, and
+analysis of kinetic models of biochemical reaction systems.
+
+**MASSpy** is built to integrate seemlessly with **COBRApy**, a widely used
+modeling software package for constraint-based reconstruction and analysis of
+biochemical reaction systems. Therefore **MASSpy**  can be used seperately from
+or in conjuction with **COBRApy**, thereby providing a wide range of modeling
+workflows and techniques.
+
+Additional information about **COBRApy** can be found in its
+`documentation <https://cobrapy.readthedocs.io/en/latest/index.html>`_ or
+`github page <https://github.com/opencobra/cobrapy>`_.
+
+Cite
+----
+A manuscript is in preparation for publication and will be the proper reference
+for citing the **MASSpy** in the future.
+
+Installation
+~~~~~~~~~~~~
+
+The recommended method is to install **MASSpy** is to use ``pip`` to
+`install masspy from PyPI <https://pypi.python.org/pypi/masspy>`_. It is
+recommended to do this inside a `virtual environment
+<http://docs.python-guide.org/en/latest/dev/virtualenvs/>`_)::
+
+	pip install masspy
+
+You can install all packages, including optional dependencies, directly by::
+
+    pip install masspy[all]
+
+If you downloaded the source code, run::
+
+	pip install -e .
+
+in the ``masspy`` source directory. For additional information, please refer to the
+the `detailed installation instructions <INSTALL.rst>`_.
+
+Contributing
+~~~~~~~~~~~~
+
+Contributions are always welcome! Please read the `contributions
+guideline <.github/CONTRIBUTING.rst>`_
+to get started.
+
+License
+-------
+
+The **MASSpy** source is released under both the GPL and LGPL licenses. You
+may choose which license you choose to use the software under. However,
+please note that binary packages which include GLPK (such as the binary
+wheels distributed at https://pypi.python.org/pypi/cobra) and
+`libRoadRunner <https://pypi.org/project/libroadrunner/>`_ will be bound
+by their licenses as well.
+
+This program is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License or the Lesser GNU
+General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+Public License for more details.
+
+.. |PyPI| image:: https://badge.fury.io/py/masspy.svg
+    :target: https://pypi.python.org/pypi/masspy
---
+++ packages/masspy/masspy-0.1.0.tar.gz/masspy-0.1.0/setup.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+"""This module contains the setup for the installation of MASSpy."""
+from sys import argv
+
+from setuptools import find_packages, setup
+
+
+setup_kwargs = dict()
+setup_requirements = []
+# prevent pytest-runner from being installed on every invocation
+if {'pytest', 'test', 'ptr'}.intersection(argv):
+    setup_requirements.append("pytest-runner")
+
+extras = {
+    "escher": "escher",
+    "json": "simplejson",
+}
+extras["all"] = sorted(extras.values())
+
+try:
+    with open('README.rst') as handle:
+        readme = handle.read()
+    with open('INSTALL.rst') as handle:
+        install = handle.read()
+    setup_kwargs["long_description"] = readme + "\n\n" + install
+except IOError:
+    setup_kwargs["long_description"] = ''
+
+if __name__ == "__main__":
+    setup(
+        name="masspy",
+        version="0.1.0",
+        packages=find_packages(),
+        setup_requires=setup_requirements,
+        install_requires=[
+            "cobra>=0.15.0",
+            "depinfo",
+            "libroadrunner>=1.5",
+            "numpy>=1.13.0",
+            "pandas>=0.17.0",
+            "optlang>=1.4.2",
+            "scipy>=1.2.0",
+            "six>=1.11.0",
+            "sympy>=1.0.0",
+            "tabulate>=0.8.1",
+            "matplotlib>=3.2.0",
+        ],
+        tests_require=[
+            "pytest",
+            "pytest-benchmark"
+        ],
+        extras_require=extras,
+        package_data={
+            '': [
+                'test/data/*',
+            ]
+        },
+        author="Zachary B. Haiman",
+        author_email="zhaiman@eng.ucsd.edu",
+        maintainer="Zachary B. Haiman",
+        maintainer_email="zhaiman@eng.ucsd.edu",
+        description="MASSpy is a package for kinetic modeling and simulation "
+                    "of biological networks",
+        license="LGPL/GPL v3+",
+        keywords=("metabolism biology kinetic modeling simulation programming"
+                  "cobra"),
+        url="https://github.com/SBRG/masspy",
+        # FIXME test_suite="mass.test.suite", Add when implemented
+        download_url="https://pypi.python.org/pypi/masspy",
+        classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Environment :: Console',
+            'Intended Audience :: Developers',
+            'Topic :: Software Development :: Build Tools',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: GNU Lesser General Public License v3'
+                ' or later (LGPLv3+)',
+            'License :: OSI Approved :: GNU General Public License v3'
+                ' or later (GPLv3+)',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: Implementation :: CPython',
+            'Topic :: Scientific/Engineering',
+            'Topic :: Scientific/Engineering :: Bio-Informatics'
+        ],
+        python_requires='>=3.6',
+        include_package_data=True,
+        platforms="GNU/Linux, Mac OS X >= 10.7, Microsoft Windows >= 7",
+        **setup_kwargs
+    )
