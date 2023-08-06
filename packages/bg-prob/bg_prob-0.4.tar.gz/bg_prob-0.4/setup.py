03e69443adb44f183ea64c1ef8fc5b89b4729fac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:07 +0000

    Add package bg-prob/0.4/bg_prob-0.4.tar.gz

---
+++ packages/bg-prob/bg_prob-0.4.tar.gz/bg_prob-0.4/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='bg_prob',
+      version='0.4',
+      description='Gaussian distributions',
+      long_description=open('bg_prob/README.md').read(),
+      packages=['bg_prob'],
+      zip_safe=False)
