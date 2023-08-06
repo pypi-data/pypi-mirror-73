883e137095bfb8e61f916ca798d5c679e769325b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:13 +0000

    Add package Physcraper/0.1/Physcraper-0.1.tar.gz

---
+++ packages/Physcraper/Physcraper-0.1.tar.gz/Physcraper-0.1/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='Physcraper',
+      version='0.1',
+      description='Physcraper',
+      author='Emily Jane McTavish',
+      author_email='ejmctavish@gmail.com',
+      packages=['physcraper'],
+      scripts=['bin/physcraper_run.py',
+               'bin/tree_comparison.py',
+               'bin/find_trees.py'],
+      data_files=['taxonomy/ott_ncbi',
+                  'taxonomy/ottids_in_synth.txt'],
+      install_requires=[]
+     )
