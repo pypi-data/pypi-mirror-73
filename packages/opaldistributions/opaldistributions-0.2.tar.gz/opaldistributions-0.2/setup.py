61dc9ee08029b4714af2cbcc1ab81bd7f2d35306
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:18 +0000

    Add package opaldistributions/0.2/opaldistributions-0.2.tar.gz

---
+++ packages/opaldistributions/opaldistributions-0.2.tar.gz/opaldistributions-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='opaldistributions',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['opaldistributions'],
+      zip_safe=False)
