116cdc596f0d8bf72317199df94ef438951ca025
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:24 +0000

    Add package pyTextMiner/1.1.116b2/pyTextMiner-1.1.116b2.tar.gz

---
+++ packages/pyTextMiner/pyTextMiner-1.1.116b2.tar.gz/pyTextMiner-1.1.116b2/lda2vec/run_20newsgroups.py
@@ -0,0 +1,53 @@
+from lda2vec import utils, model
+
+# Path to preprocessed data
+data_path  = "data/clean_data"
+# Whether or not to load saved embeddings file
+load_embeds = True
+
+import tensorflow as tf
+print(tf.__version__)
+
+# Load data from files
+(idx_to_word, word_to_idx, freqs, pivot_ids,
+ target_ids, doc_ids, embed_matrix) = utils.load_preprocessed_data(data_path, load_embed_matrix=load_embeds)
+
+# Number of unique documents
+num_docs = doc_ids.max() + 1
+# Number of unique words in vocabulary (int)
+vocab_size = len(freqs)
+# Embed layer dimension size
+# If not loading embeds, change 128 to whatever size you want.
+embed_size = embed_matrix.shape[1] -1 if load_embeds else 128
+# Number of topics to cluster into
+num_topics = 20
+# Amount of iterations over entire dataset
+num_epochs = 200
+# Batch size - Increase/decrease depending on memory usage
+batch_size = 4096
+# Epoch that we want to "switch on" LDA loss
+switch_loss_epoch = 0
+# Pretrained embeddings value
+pretrained_embeddings = embed_matrix if load_embeds else None
+# If True, save logdir, otherwise don't
+save_graph = True
+
+
+# Initialize the model
+m = model(num_docs,
+          vocab_size,
+          num_topics,
+          embedding_size=embed_size,
+          pretrained_embeddings=pretrained_embeddings,
+          freqs=freqs,
+          batch_size = batch_size,
+          save_graph_def=save_graph)
+
+# Train the model
+m.train(pivot_ids,
+        target_ids,
+        doc_ids,
+        len(pivot_ids),
+        num_epochs,
+        idx_to_word=idx_to_word,
+        switch_loss_epoch=switch_loss_epoch)
