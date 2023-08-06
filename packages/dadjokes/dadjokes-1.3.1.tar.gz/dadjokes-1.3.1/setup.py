12c3ef3aa08cc2590b80201ce1840eb4312819eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:59 +0000

    Add package dadjokes/1.3.1/dadjokes-1.3.1.tar.gz

---
+++ packages/dadjokes/dadjokes-1.3.1.tar.gz/dadjokes-1.3.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+from dadjokes.constants import VERSION
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(
+    name='dadjokes',
+    version=VERSION,
+    description='icanhazdadjoke api wrapper',
+    long_description=readme(),
+    url='https://github.com/crossnox/dadjokes',
+    author='CrossNox',
+    packages=['dadjokes'],
+    scripts=['script/dadjokes'],
+    install_requires=['requests'],
+    extras_require={'dev': ['bump']},
+    classifiers=['Programming Language :: Python :: 3'],
+)
