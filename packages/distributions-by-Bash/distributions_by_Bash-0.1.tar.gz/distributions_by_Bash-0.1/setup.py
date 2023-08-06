fb6e890464c014ce5e46fd87f3ea8d3b0129adf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:12 +0000

    Add package distributions-by-Bash/0.1/distributions_by_Bash-0.1.tar.gz

---
+++ packages/distributions-by-Bash/distributions_by_Bash-0.1.tar.gz/distributions_by_Bash-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_by_Bash',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_by_Bash'],
+      zip_safe=False)
