47847a9f7b9c6aa60cb5fd5f25120aef575b48f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:00 +0000

    Add package shevcdim-dsnd-distributions/0.1/shevcdim_dsnd_distributions-0.1.tar.gz

---
+++ packages/shevcdim-dsnd-distributions/shevcdim_dsnd_distributions-0.1.tar.gz/shevcdim_dsnd_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='shevcdim_dsnd_distributions',
+      version='0.1',
+      description='Gaussian and binomial distributions classes',
+      packages=['shevcdim_dsnd_distributions'],
+      author = 'shevchuk dimitri',
+      zip_safe=False)
