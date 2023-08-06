f100c1755915b30b63de7e9ca9fa192542a1344a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:13 +0000

    Add package spsn-probability/0.1/spsn_probability-0.1.tar.gz

---
+++ packages/spsn-probability/spsn_probability-0.1.tar.gz/spsn_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='spsn_probability',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['spsn_probability'],
+      author = 'Sanmeet Pradhan',
+      
+      zip_safe=False)
