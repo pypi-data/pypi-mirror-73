2bf401d7e4fbb7d61100447aa1711582987dc78e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:37 +0000

    Add package mve-probability-distributions/0.1/mve_probability_distributions-0.1.tar.gz

---
+++ packages/mve-probability-distributions/mve_probability_distributions-0.1.tar.gz/mve_probability_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='mve_probability_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
