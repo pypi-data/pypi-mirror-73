b4125409473958620ccffa46fbb2c8dc9dbfea9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:45 +0000

    Add package BiGauss/0.1/BiGauss-0.1.tar.gz

---
+++ packages/BiGauss/BiGauss-0.1.tar.gz/BiGauss-0.1/BiGauss/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  3 17:26:46 2020
+
+@author: Fadjia Leila
+"""
+
+from .Gaussiandistribution import Gaussian
+from .Binomialdistribution import Binomial
---
+++ packages/BiGauss/BiGauss-0.1.tar.gz/BiGauss-0.1/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  3 17:21:48 2020
+
+@author: Fadjia Leila
+"""
+
+from setuptools import setup 
+setup(name ='BiGauss',
+      version ='0.1',
+      description ='Gaussian and Binomial distributions',
+      packages = ['BiGauss'],
+      author = 'Rose Fadjia Leila Joseph',
+      auhor_email = 'fadjia20@gmail.com',
+      zip_safe=False)
