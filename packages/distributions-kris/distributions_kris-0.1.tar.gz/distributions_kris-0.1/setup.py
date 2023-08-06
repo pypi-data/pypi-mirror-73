5f2c120207b0b409e03ecb293dea5573b8e9dd92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:06 +0000

    Add package distributions-kris/0.1/distributions_kris-0.1.tar.gz

---
+++ packages/distributions-kris/distributions_kris-0.1.tar.gz/distributions_kris-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_kris',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_kris'],
+      author='Kris',
+      author_email='example@example.com',
+      zip_safe=False)
