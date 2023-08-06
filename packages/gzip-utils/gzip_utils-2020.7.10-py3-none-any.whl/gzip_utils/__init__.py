644858eecae10a95df9663b5fd3fe1bf3a4e78e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:49 +0000

    Add package gzip-utils/2020.7.10/gzip_utils-2020.7.10-py3-none-any.whl

---
+++ packages/gzip-utils/gzip_utils-2020.7.10-py3-none-any.whl/gzip_utils/__init__.py
@@ -0,0 +1,20 @@
+#
+#
+#
+try:
+    from importlib.metadata import PackageNotFoundError
+    from importlib.metadata import version
+except ImportError:  # pragma: no cover
+    from importlib_metadata import PackageNotFoundError  # type: ignore
+    from importlib_metadata import version  # type: ignore
+
+try:
+    __version__ = version(__package__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "0.0.0"
+
+
+from ._compressed_json_list import CompressedJsonList
+
+
+__all__ = ["CompressedJsonList"]
