7bc120e50e6a50ba7f600987610e1d018777e864
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:24 +0000

    Add package probability-distribution-func/0.2/probability_distribution_func-0.2.tar.gz

---
+++ packages/probability-distribution-func/probability_distribution_func-0.2.tar.gz/probability_distribution_func-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='probability_distribution_func',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
