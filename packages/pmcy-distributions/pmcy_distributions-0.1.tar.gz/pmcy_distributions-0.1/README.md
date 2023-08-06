a1443cd0719d52a6a4a2cd05fc586fe8776a9064
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:00 +0000

    Add package pmcy-distributions/0.1/pmcy_distributions-0.1.tar.gz

---
+++ packages/pmcy-distributions/pmcy_distributions-0.1.tar.gz/pmcy_distributions-0.1/README.md
@@ -0,0 +1,11 @@
+#pmcy_distributions package
+
+some detail/sumary of the package goes here
+
+# Files
+
+give explanations of the files here
+
+# Installations
+
+give installation instructions here
---
+++ packages/pmcy-distributions/pmcy_distributions-0.1.tar.gz/pmcy_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pmcy_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['pmcy_distributions'],
+      author = 'Petros Mina',
+      author_email = 'petros.mina@gmail.com',
+      zip_safe=False)
