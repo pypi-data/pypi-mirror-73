6a81385457f165d141d57bb867223da15f066fba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:58 +0000

    Add package swiftpath/0.0.1.dev0/swiftpath-0.0.1.dev0-py2.py3-none-any.whl

---
+++ packages/swiftpath/swiftpath-0.0.1.dev0-py2.py3-none-any.whl/swiftpath/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding=utf-8 -*-
+"""swiftpath is a :mod:`pathlib` API compliant library for interacting with.
+
+**Swift**, an object storage backend written for **Openstack**.
+
+Submodules
+============
+
+.. autosummary::
+   :toctree: _autosummary
+
+   swiftpath
+"""
+from .swiftpath import SwiftPath
+
+__version__ = "0.0.1.dev0"
