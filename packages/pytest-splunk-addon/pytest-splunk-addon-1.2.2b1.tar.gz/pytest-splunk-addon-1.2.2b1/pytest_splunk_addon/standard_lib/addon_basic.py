246051284b11f24a0f7933097e83a7f68f0aac9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:41 +0000

    Add package pytest-splunk-addon/1.2.2b1/pytest-splunk-addon-1.2.2b1.tar.gz

---
+++ packages/pytest-splunk-addon/pytest-splunk-addon-1.2.2b1.tar.gz/pytest-splunk-addon-1.2.2b1/pytest_splunk_addon/standard_lib/addon_basic.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Base class for test cases. Provides test cases to verify
+field extractions and CIM compatibility.
+"""
+
+from .fields_tests import FieldTestTemplates
+from .cim_tests import CIMTestTemplates
+from .index_tests import IndexTimeTestTemplate
+
+
+class Basic(FieldTestTemplates, CIMTestTemplates, IndexTimeTestTemplate):
+    """
+    Base class for test cases. Inherit this class to include the test 
+    cases for an Add-on. Only implement the common tests here, all the other 
+    specific test case should be implemented in a TestTemplate class and Basic 
+    should inherit it.
+    """
+    pass
+
