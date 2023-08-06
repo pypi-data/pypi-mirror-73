6960c04713735d5536f1c770275010e6972571ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:30 +0000

    Add package query-factory/0.0.6/query_factory-0.0.6.tar.gz

---
+++ packages/query-factory/query_factory-0.0.6.tar.gz/query_factory-0.0.6/query_factory/exceptions.py
@@ -0,0 +1,14 @@
+class NoSpecsForVariable(Exception):
+    """Raise when attempting to access specs of an unknown variable in template."""
+
+
+class MalformedTemplate(Exception):
+    """Raise when yaml template does not meet requirements."""
+
+
+class NoOrEmptyQueryException(Exception):
+    """Raise when templator has no query or empty query."""
+
+
+class MissingOrExtraVariableException(Exception):
+    """Raise when templator is given more or less variables than explicitly specified in template."""
