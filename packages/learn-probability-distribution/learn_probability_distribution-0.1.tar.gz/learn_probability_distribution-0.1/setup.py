400577eda9ee43912fdd8285ac4c633c8d3923d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:04 +0000

    Add package learn-probability-distribution/0.1/learn_probability_distribution-0.1.tar.gz

---
+++ packages/learn-probability-distribution/learn_probability_distribution-0.1.tar.gz/learn_probability_distribution-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='learn_probability_distribution',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['learn_probability_distribution'],
+      author= 'Ankit Hans',
+      author_email = 'ankithans1947@gmail.com',
+      zip_safe=False)
