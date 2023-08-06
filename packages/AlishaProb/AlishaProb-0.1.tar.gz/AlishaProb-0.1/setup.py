b634e4b3f3e142fe9038aa5837cd5c2b0a2ef10b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:56 +0000

    Add package AlishaProb/0.1/AlishaProb-0.1.tar.gz

---
+++ packages/AlishaProb/AlishaProb-0.1.tar.gz/AlishaProb-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='AlishaProb',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['AlishaProb'],
+      author= 'Alisha Gupta',
+      author_email= 'guptaalisha0198@gmail.com',
+      zip_safe=False)
