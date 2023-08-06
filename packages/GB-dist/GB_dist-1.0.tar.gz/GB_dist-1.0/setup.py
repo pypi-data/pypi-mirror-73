ccdb7fa8defd3c82c4d9582b66d570b1cda5c3d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:01 +0000

    Add package GB-dist/1.0/GB_dist-1.0.tar.gz

---
+++ packages/GB-dist/GB_dist-1.0.tar.gz/GB_dist-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='GB_dist',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['GB_dist'],
+      author = 'Manish kumar thota',
+      author_email = 'manish.thota1999@gmail.com',
+      
+      zip_safe=False)
