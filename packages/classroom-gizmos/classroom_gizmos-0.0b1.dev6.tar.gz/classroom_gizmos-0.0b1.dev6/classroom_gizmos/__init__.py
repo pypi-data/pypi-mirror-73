1142e7b62f05b01bb30af7d13f00ba10fa0e8adf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:56 +0000

    Add package classroom-gizmos/0.0b1.dev6/classroom_gizmos-0.0b1.dev6.tar.gz

---
+++ packages/classroom-gizmos/classroom_gizmos-0.0b1.dev6.tar.gz/classroom_gizmos-0.0b1.dev6/classroom_gizmos/__init__.py
@@ -0,0 +1,4 @@
+## See https://packaging.python.org/guides/single-sourcing-package-version/
+## ----- Time-stamp: <2020-07-12 19:27:02 hedfp> -----
+
+__version__ = "0.0b1.dev6", # X.YaN.devM format
