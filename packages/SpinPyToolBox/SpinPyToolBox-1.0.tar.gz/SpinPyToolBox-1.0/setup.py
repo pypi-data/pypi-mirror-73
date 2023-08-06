73b1cfdcc17415db93106380c09be118d80f14a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:33 +0000

    Add package SpinPyToolBox/1.0/SpinPyToolBox-1.0.tar.gz

---
+++ packages/SpinPyToolBox/SpinPyToolBox-1.0.tar.gz/SpinPyToolBox-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='SpinPyToolBox',
+      version='1.0',
+      description='SpinPyToolBox',
+      url='http://github.com/storborg/SpinPyToolBox',
+      author='HeavenMu',
+      author_email='muheaven@outlook.com',
+      license='MIT',
+      packages=['SpinPyToolBox'],
+      zip_safe=False)
