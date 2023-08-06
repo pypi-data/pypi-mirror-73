3ceed6a4324419717982229c45b4be64b7c385b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:10 +0000

    Add package salemGaussianBinomial/0.1/salemGaussianBinomial-0.1.tar.gz

---
+++ packages/salemGaussianBinomial/salemGaussianBinomial-0.1.tar.gz/salemGaussianBinomial-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='salemGaussianBinomial',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['salemGaussianBinomial'],
+      zip_safe=False)
