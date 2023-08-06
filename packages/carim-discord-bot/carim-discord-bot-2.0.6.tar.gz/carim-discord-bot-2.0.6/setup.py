ff6e9bdc7da1a57bea9cefb31b07529bce560a4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:21 +0000

    Add package carim-discord-bot/2.0.6/carim-discord-bot-2.0.6.tar.gz

---
+++ packages/carim-discord-bot/carim-discord-bot-2.0.6.tar.gz/carim-discord-bot-2.0.6/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    install_requires=[
+        'discord.py>=1.3.4,<1.4.0a0'
+    ],
+    extras_require={
+        'tests': [
+            'pytest',
+            'pytest-timeout',
+            'pytest-asyncio'
+        ]
+    }
+)
