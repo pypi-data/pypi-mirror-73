60f91178854dd03e28a33ad0ace317efe0b3a7fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:17 +0000

    Add package prettyqt/0.85.1/prettyqt-0.85.1.tar.gz

---
+++ packages/prettyqt/prettyqt-0.85.1.tar.gz/prettyqt-0.85.1/tests/test_custom_widgets.py
@@ -0,0 +1,297 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `prettyqt` package."""
+
+import pickle
+import re
+
+from qtpy import QtCore, QtGui
+
+import prettyqt.custom_widgets.dataset as fo
+from prettyqt import core, custom_widgets, gui, widgets
+
+
+def test_booldicttoolbutton(qtbot):
+    dct = dict(a="test", b="test2")
+    w = custom_widgets.BoolDictToolButton("Title", None, dct)
+    w["a"] = True
+    assert w["a"] is True
+    assert w.as_dict() == dict(a=True, b=False)
+
+
+def test_buttondelegate(qtbot):
+    widget = custom_widgets.ButtonDelegate(parent=None)
+    widget.setEditorData(widgets.Widget(), None)
+    widget.createEditor(None, None, QtCore.QModelIndex())
+    widget.currentIndexChanged()
+
+
+def test_radiodelegate(qtbot):
+    widget = widgets.TableWidget()
+    widget.setColumnCount(3)
+    widget.insertRow(0)
+    widget.setHorizontalHeaderLabels(["LIB", "CELL", "area"])
+    item = widgets.TableWidgetItem("test")
+    widget.setItem(0, 0, item)
+    widget.setItem(1, 1, widgets.TableWidgetItem("test"))
+    delegate = custom_widgets.RadioDelegate(widget, ["a", "b"])
+    widget.setItemDelegateForColumn(0, delegate)
+    widget.openPersistentEditor(item)
+
+
+def test_colorchooserbutton(qtbot):
+    btn = custom_widgets.ColorChooserButton()
+    btn.set_color("green")
+    with open("data.pkl", "wb") as jar:
+        pickle.dump(btn, jar)
+    with open("data.pkl", "rb") as jar:
+        btn = pickle.load(jar)
+    repr(btn)
+    assert btn.get_value() == gui.Color("green")
+    btn.set_value("blue")
+    assert btn.is_valid()
+
+
+def test_regexeditor(qtbot):
+    widget = custom_widgets.RegexEditorWidget()
+    widget.regex = "[0-9]"
+    assert widget.regex == "[0-9]"
+    widget.string = "test123"
+    assert widget.string == "test123"
+    widget.compile_flags = re.IGNORECASE
+
+
+def test_dataset(qtbot):
+    class Test(fo.DataSet):
+        i1 = fo.Bool(label="My first one")
+        string1 = fo.String(label="My first one", regex="[0-9]")
+        string2 = fo.String(label="My second one", notempty=True)
+        choiceitem = fo.Enum(label="A", choices=["A", "B"], disabled_on="i1")
+        mchoiceitem = fo.MultipleChoice(label="A", choices=["A", "B"])
+        floatitem = fo.Float(label="FloatItem", enabled_on="i1")
+        intitem = fo.Int(label="IntItem", enabled_on="i1")
+        coloritem = fo.Color(label="ColorItem", value="green")
+        fileitem = fo.File(label="File")
+        folderitem = fo.Folder(label="File", optional="Test")
+        stringornumber = fo.StringOrNumber(label="Test")
+        buttonitem = fo.Button(label="Button", callback=print)
+        intlist = fo.IntList(label="Button", value=[1, 2, 3])
+        intlist = fo.FloatList(label="Button", value=[1.1, 2, 3])
+        code = fo.Code(label="Test", value="class Test")
+
+    settings = Test(icon="mdi.timer")
+    settings.create_dialog()
+    settings.to_dict()
+
+
+def test_flagselectionwidget(qtbot):
+    widget = custom_widgets.FlagSelectionWidget()
+    items = {"MultiLine": 0, "Ignore case": 2}
+    widget.add_items(items)
+    assert widget.get_value() == 0
+
+
+def test_stringornumberwidget(qtbot):
+    widget = custom_widgets.StringOrNumberWidget()
+    widget.get_value()
+    widget.on_value_change()
+
+
+def test_optionalwidget(qtbot):
+    w = widgets.RadioButton()
+    container = custom_widgets.OptionalWidget(w, "Test")
+    container.get_value()
+    container.enabled = False
+    assert container.enabled is False
+
+
+def test_singlelinetextedit(qtbot):
+    w = custom_widgets.SingleLineTextEdit()
+    w.set_text("test")
+
+
+def test_mappedcheckbox(qtbot):
+    widget = custom_widgets.MappedCheckBox(true_value=0, false_value=1)
+    widget.set_value(0)
+    assert widget.get_value() == 0
+    widget.setChecked(False)
+    assert widget.get_value() == 1
+
+
+def test_filechooserbutton(qtbot):
+    btn = custom_widgets.FileChooserButton()
+    with open("data.pkl", "wb") as jar:
+        pickle.dump(btn, jar)
+    with open("data.pkl", "rb") as jar:
+        btn = pickle.load(jar)
+    btn.set_value("/")
+    btn.get_value()
+
+
+def test_fontchooserbutton(qtbot):
+    btn = custom_widgets.FontChooserButton()
+    with open("data.pkl", "wb") as jar:
+        pickle.dump(btn, jar)
+    with open("data.pkl", "rb") as jar:
+        btn = pickle.load(jar)
+    btn.set_font("Consolas")
+    repr(btn)
+
+
+def test_iconlabel(qtbot):
+    w = custom_widgets.IconLabel()
+    w.set_text("test")
+    assert w.text() == "test"
+    repr(w)
+
+
+def test_inputandslider(qtbot):
+    w = custom_widgets.InputAndSlider()
+    w.set_range(0, 10)
+    w.set_value(5)
+    assert w.get_value() == 5
+    w.set_step_size(2)
+    assert w.is_valid()
+
+
+def test_codeeditor(qtbot):
+    editor = custom_widgets.CodeEditor()
+    assert editor.text() == ""
+    editor.line_area_width()
+    editor.set_syntaxhighlighter("python")
+    event = QtGui.QResizeEvent(core.Size(10, 10), core.Size(20, 20))
+    editor.resizeEvent(event)
+    event = QtGui.QPaintEvent(core.Rect(0, 0, 20, 20))
+    editor.line_area_paintevent(event)
+
+
+def test_imageviewer(qtbot):
+    custom_widgets.ImageViewer()
+
+
+def test_flowlayout(qtbot):
+    widget = widgets.Widget()
+    layout = custom_widgets.FlowLayout(margin=1)
+    btn = widgets.PushButton("Short")
+    layout += btn
+    layout += widgets.PushButton("Longer")
+    layout += widgets.PushButton("Different text")
+    layout += widgets.PushButton("More text")
+    layout += widgets.PushButton("Even longer button text")
+    layout.do_layout(core.Rect(), False)
+    layout.sizeHint()
+    widget.set_layout(layout)
+    assert layout[0] == btn
+    for i in layout:
+        pass
+    assert len(layout) == 5
+    layout.get_children()
+    with open("data.pkl", "wb") as jar:
+        pickle.dump(layout, jar)
+    with open("data.pkl", "rb") as jar:
+        layout = pickle.load(jar)
+
+
+def test_labeledslider(qtbot):
+    slider = custom_widgets.LabeledSlider(["test1", "test2"], "vertical")
+    slider = custom_widgets.LabeledSlider(["test1", "test2"])
+    qtbot.addWidget(slider)
+    qtbot.mouseClick(slider.sl, QtCore.Qt.LeftButton)
+    qtbot.mouseMove(slider.sl, core.Point(20, 20))
+    slider.paintEvent(None)
+
+
+def test_markdownwidget(qtbot):
+    custom_widgets.MarkdownWindow()
+
+
+def test_popupinfo(qtbot):
+    popup = custom_widgets.PopupInfo()
+    popup.show_popup("test")
+
+
+def test_promptlineedit(qtbot):
+    widget = custom_widgets.PromptLineEdit()
+    widget.prompt_text = "test"
+    assert widget.prompt_text == "test"
+    widget.set_button_visible(True)
+    widget.set_button_visible(False)
+    widget.set_text("test")
+
+
+def test_selectionwidget(qtbot):
+    widget = custom_widgets.SelectionWidget()
+
+    class Test(object):
+        pass
+
+    test = Test()
+    items = {"Semicolon": ";", "Tab": "tab", "Comma": ",", "class": test}
+    widget.add_items(items)
+    widget.add_items(("a", "b"))
+    widget.add_tooltip_icon("test")
+    widget.add_custom(label="test", regex=r"\S{1}")
+    radiobuttons = [k for k, v in widget.buttons.items()]
+    radiobuttons[1].click()
+    assert widget.get_value() == "tab"
+    radiobuttons[3].click()
+    assert widget.current_choice() == test
+    for i in widget:
+        pass
+    widget.select_radio_by_data(";")
+    choice = widget.current_choice()
+    assert choice == ";"
+    widget.set_value(",")
+    assert widget.get_value() == ","
+    widget.update_choice(True)
+
+
+def test_spanslider(qtbot):
+    slider = custom_widgets.SpanSlider()
+    qtbot.addWidget(slider)
+    slider.set_lower_value(10)
+    slider.set_upper_value(20)
+    slider.set_lower_pos(15)
+    slider.set_upper_pos(25)
+    assert slider.lower_value == 15
+    assert slider.upper_value == 25
+    slider.set_lower_value(12)
+    slider.set_upper_pos(20)
+    color = gui.Color("blue")
+    slider.set_left_color(color)
+    slider.set_right_color(color)
+    slider.swap_controls()
+    slider.trigger_action(slider.SliderNoAction, True)
+    slider.trigger_action(slider.SliderSingleStepAdd, True)
+    slider.paintEvent(None)
+    slider.pixel_pos_to_value(100)
+    slider.draw_span(gui.Painter(), core.Rect())
+    slider.move_pressed_handle()
+    qtbot.mouseClick(slider, QtCore.Qt.LeftButton)
+    qtbot.mouseMove(slider, core.Point(20, 20))
+    qtbot.mouseMove(slider, core.Point(0, 0), delay=10)
+    assert slider.movement_mode == "free"
+    slider.set_movement_mode("no_overlap")
+
+
+def test_waitingspinner(qtbot):
+    test_widget = widgets.Widget()
+    spinner = custom_widgets.WaitingSpinner(parent=test_widget)
+    spinner.paintEvent(None)
+    spinner.set_line_num(2)
+    assert spinner.line_num() == 2
+    spinner.set_line_length(2)
+    assert spinner.line_length() == 2
+    spinner.set_line_width(2)
+    assert spinner.line_width() == 2
+    spinner.set_inner_radius(2)
+    assert spinner.inner_radius() == 2
+    spinner.set_color("black")
+    spinner.set_revolutions_per_second(2)
+    spinner.set_trail_fade_percentage(2)
+    spinner.set_minimum_trail_opacity(2)
+    spinner.rotate()
+    spinner.start()
+    spinner.stop()
+    spinner.update_position()
---
+++ packages/prettyqt/prettyqt-0.85.1.tar.gz/prettyqt-0.85.1/tests/test_gui.py
@@ -0,0 +1,221 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `prettyqt` package."""
+
+import pickle
+import pytest
+
+from qtpy import QtCore
+import qtpy
+
+from prettyqt import core, gui
+
+
+def test_brush():
+    gui.Brush()
+
+
+def test_color():
+    color = gui.Color()
+    color.set_color("gray")
+    with open("data.pkl", "wb") as jar:
+        pickle.dump(color, jar)
+    with open("data.pkl", "rb") as jar:
+        color = pickle.load(jar)
+    assert str(color) == "#808080"
+    repr(color)
+    # color.as_qt()
+
+
+def test_cursor():
+    cursor = gui.Cursor()
+    cursor.set_shape("arrow")
+    with pytest.raises(ValueError):
+        cursor.set_shape("test")
+    assert cursor.get_shape() == "arrow"
+
+
+def test_doublevalidator():
+    val = gui.DoubleValidator()
+    val.setRange(0, 9)
+    with open("data.pkl", "wb") as jar:
+        pickle.dump(val, jar)
+    with open("data.pkl", "rb") as jar:
+        val = pickle.load(jar)
+    assert val.is_valid_value("4")
+    assert not val.is_valid_value("10")
+    repr(val)
+
+
+def test_font():
+    font = gui.Font("Consolas")
+    repr(font)
+    font.metrics
+    font = gui.Font.mono()
+    with pytest.raises(ValueError):
+        font.set_style_hint("test")
+    font.set_style_hint("monospace")
+    font.set_weight("thin")
+    with pytest.raises(ValueError):
+        font.set_weight("test")
+
+
+def test_fontmetrics():
+    font = gui.Font("Consolas")
+    fontmetrics = gui.FontMetrics(font)
+    val = fontmetrics.elided_text("This is a test", mode="right", width=40)
+    assert len(val) < 5
+
+
+def test_icon():
+    icon = gui.Icon()
+    icon.for_color("black")
+    with open("data.pkl", "wb") as jar:
+        pickle.dump(icon, jar)
+    with open("data.pkl", "rb") as jar:
+        icon = pickle.load(jar)
+
+
+def test_intvalidator():
+    val = gui.IntValidator()
+    val.setRange(0, 9)
+    with open("data.pkl", "wb") as jar:
+        pickle.dump(val, jar)
+    with open("data.pkl", "rb") as jar:
+        val = pickle.load(jar)
+    assert val.is_valid_value("4")
+    assert not val.is_valid_value("10")
+    repr(val)
+
+
+def test_keysequence():
+    assert gui.KeySequence.to_shortcut_str(0x41, QtCore.Qt.ShiftModifier) == "Shift+A"
+
+
+def test_standarditem():
+    s = gui.StandardItem()
+    with open("data.pkl", "wb") as jar:
+        pickle.dump(s, jar)
+    with open("data.pkl", "rb") as jar:
+        s = pickle.load(jar)
+    repr(s)
+    s.set_icon("mdi.timer")
+    s.clone()
+
+
+def test_standarditemmodel():
+    model = gui.StandardItemModel()
+    model.add("test")
+    for item in model:
+        pass
+    with open("data.pkl", "wb") as jar:
+        pickle.dump(model, jar)
+    with open("data.pkl", "rb") as jar:
+        model = pickle.load(jar)
+    model += gui.StandardItem("Item")
+    model[0]
+    assert len(model.find_items("test")) == 1
+    with pytest.raises(ValueError):
+        model.find_items("test", mode="wrong_mode")
+
+
+def test_textcursor():
+    cursor = gui.TextCursor()
+    cursor.set_position(1, "move")
+    cursor.move_position("start", "move", 1)
+    cursor.select("document")
+    cursor.replace_text(0, 2, "test")
+    cursor.select_text(1, 3)
+    cursor.span()
+    with cursor.edit_block():
+        pass
+
+
+def test_painter():
+    painter = gui.Painter(gui.Image())
+    painter.use_antialiasing()
+    painter.set_pen("none")
+    painter.fill_rect((0, 1, 3, 5), "transparent")
+    painter.fill_rect(core.Rect(), "transparent")
+    with pytest.raises(ValueError):
+        painter.fill_rect(core.Rect(), "testus")
+    painter.set_color("black")
+    painter.set_composition_mode("source_atop")
+    with pytest.raises(ValueError):
+        painter.set_composition_mode("test")
+    with pytest.raises(ValueError):
+        painter.set_pen("test")
+    # assert painter.get_composition_mode() == "source_atop"
+
+
+def test_pdfwriter():
+    writer = gui.PdfWriter("test")
+    writer.setup(core.RectF())
+
+
+def test_pen():
+    pen = gui.Pen()
+    pen.set_color("blue")
+
+
+def test_picture():
+    gui.Picture()
+
+
+def test_pixmap():
+    gui.Pixmap()
+
+
+def test_polygonf():
+    poly = gui.PolygonF()
+    with open("data.pkl", "wb") as jar:
+        pickle.dump(poly, jar)
+    with open("data.pkl", "rb") as jar:
+        poly = pickle.load(jar)
+
+
+def test_regexpvalidator():
+    val = gui.RegExpValidator()
+    val.set_regex("[0-9]")
+    with open("data.pkl", "wb") as jar:
+        pickle.dump(val, jar)
+    with open("data.pkl", "rb") as jar:
+        val = pickle.load(jar)
+    assert val.get_regex() == "[0-9]"
+    assert val.is_valid_value("0")
+    repr(val)
+
+
+@pytest.mark.skipif(qtpy.API == "pyside2", reason="Only supported in PyQt5")
+def test_regularexpressionvalidator():
+    val = gui.RegularExpressionValidator()
+    val.set_regex("[0-9]")
+    with open("data.pkl", "wb") as jar:
+        pickle.dump(val, jar)
+    with open("data.pkl", "rb") as jar:
+        val = pickle.load(jar)
+    assert val.get_regex() == "[0-9]"
+    assert val.is_valid_value("0")
+    repr(val)
+
+
+def test_syntaxhighlighter():
+    gui.SyntaxHighlighter(None)
+
+
+def test_textcharformat():
+    fmt = gui.TextCharFormat()
+    fmt.set_font_weight("bold")
+    assert fmt.get_font_weight() == "bold"
+    fmt.set_foreground_color("yellow")
+    fmt.set_background_color("yellow")
+    with pytest.raises(ValueError):
+        fmt.set_font_weight("test")
+    fmt = gui.TextCharFormat(bold=True)
+    assert fmt.get_font_weight() == "bold"
+    fmt.select_full_width()
+
+
+def test_validator():
+    gui.Validator()
