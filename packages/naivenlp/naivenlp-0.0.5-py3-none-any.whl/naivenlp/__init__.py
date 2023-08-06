6508d9280ae8a68cb44b9b4337830f79588c6898
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:09 +0000

    Add package naivenlp/0.0.5/naivenlp-0.0.5-py3-none-any.whl

---
+++ packages/naivenlp/naivenlp-0.0.5-py3-none-any.whl/naivenlp/__init__.py
@@ -0,0 +1,34 @@
+import logging
+
+from naivenlp.similarity import (
+    cosine_distance,
+    cosine_similarity,
+    damerau_distance,
+    jaccard_distance,
+    jaccard_similarity,
+    lcs_distance,
+    lcs_length,
+    levenshtein_distance,
+    levenshtein_distance_normalized,
+    levenshtein_similarity,
+    longest_common_subsequence_distance,
+    longest_common_subsequence_length,
+    optimal_string_alignment_distance,
+    osa_distance,
+    sorense_dice_distance,
+    sorense_dice_similarity,
+    weighted_levenshtein_distance,
+)
+from naivenlp.tokenizers.abstract_tokenizer import AbstractTokenizer, CustomTokenizer, VocabBasedTokenizer
+from naivenlp.tokenizers.bert_tokenizer import BertTokenizer
+from naivenlp.tokenizers.tokenizer import BasicTokenizer, WordpieceTokenizer
+from naivenlp.tokenizers.transformer_tokenizer import TransformerTokenizer
+
+__version__ = "0.0.5"
+__name__ = "naivenlp"
+
+
+logging.basicConfig(
+    format="%(asctime)s %(levelname)7s %(filename)20s %(lineno)4d] %(message)s",
+    level=logging.INFO
+)
