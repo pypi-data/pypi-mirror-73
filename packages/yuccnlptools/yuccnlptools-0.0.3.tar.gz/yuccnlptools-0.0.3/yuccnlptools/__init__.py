7ccc421d80f3651b2649f1e478ca339b992d9a8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:12 +0000

    Add package yuccnlptools/0.0.3/yuccnlptools-0.0.3.tar.gz

---
+++ packages/yuccnlptools/yuccnlptools-0.0.3.tar.gz/yuccnlptools-0.0.3/yuccnlptools/__init__.py
@@ -0,0 +1,15 @@
+
+from .hello import hello
+
+from .data.datasets import GenernalDataset, GenernalDataTrainingArguments
+
+from .data import (
+    genernal_convert_examples_to_features,
+    genernal_output_modes,
+    genernal_processors,
+    SmpRankProcessor,
+    genernal_tasks_num_labels,
+    genernal_compute_metrics,
+)
+
+from .textclassification import TextClassificationModel
