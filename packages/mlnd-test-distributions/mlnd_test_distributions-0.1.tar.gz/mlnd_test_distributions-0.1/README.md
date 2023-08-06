52102d1d58ee9deeb8ca8ab3c20d6cf735a43c67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:21 +0000

    Add package mlnd-test-distributions/0.1/mlnd_test_distributions-0.1.tar.gz

---
+++ packages/mlnd-test-distributions/mlnd_test_distributions-0.1.tar.gz/mlnd_test_distributions-0.1/README.md
@@ -0,0 +1,10 @@
+# mlnd-distributions package
+
+Summary of the package
+
+# Files
+
+Explanation of files in the package
+
+# Installation
+
---
+++ packages/mlnd-test-distributions/mlnd_test_distributions-0.1.tar.gz/mlnd_test_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='mlnd_test_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['mlnd_test_distributions'],
+      author_email='bousejin@gmail.com',
+      zip_safe=False)
