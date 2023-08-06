9dbf9e4154926e12ff7695150f4ab4bdec97bba0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:24 +0000

    Add package dsndpythonpro/1.2/dsndpythonpro-1.2.tar.gz

---
+++ packages/dsndpythonpro/dsndpythonpro-1.2.tar.gz/dsndpythonpro-1.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dsndpythonpro',
+      version='1.2',
+      description='Gaussian distributions',
+      packages=['dsndpythonpro'],
+      
+      zip_safe=False)
