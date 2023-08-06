4d613f108a18e50fb4dd05382e7eabc4f513f217
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:08 +0000

    Add package rbac_builder/1.0.0/rbac_builder-1.0.0.tar.gz

---
+++ packages/rbac_builder/rbac_builder-1.0.0.tar.gz/rbac_builder-1.0.0/rbac_builder/__init__.py
@@ -0,0 +1,16 @@
+"""Role Base Access Control Builder
+Import the `RBACBuilder` module to work with the RBAC builder:
+    >>> from rbac_builder import RBACBuilder
+    >>> rbac_builder = RBACBuilder()
+See https://github.com/tukida/rbac_builder for more information
+"""
+
+__author__ = "Kidataek"
+__version__ = "1.0.0"
+
+from .base import RBACBuilder
+from .baseview import BaseView
+from .utils import generate_uuid
+from .models import Model, SQLA
+from .security.decorators import has_access, permission_name
+from .security.manager import BaseSecurityManager
