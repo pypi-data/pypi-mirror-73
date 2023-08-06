c2ce97f6002c548e001dfb4f0b65a129bc9c0e14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:02 +0000

    Add package super-distributions/0.1.1/super_distributions-0.1.1.tar.gz

---
+++ packages/super-distributions/super_distributions-0.1.1.tar.gz/super_distributions-0.1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='super_distributions',
+      version='0.1.1',
+      author= 'Narayan Sharma',
+      author_email= 'narayansharma275@gmail.com',
+      description= 'Gaussian and Binomial distributions',
+      packages=['super_distributions'],
+      zip_safe=False)
