c5545a5392b86881295786ab599bd817f2d1e729
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:47 +0000

    Add package pgevents/0.0.1/pgevents-0.0.1-py3-none-any.whl

---
+++ packages/pgevents/pgevents-0.0.1-py3-none-any.whl/pgevents/cli.py
@@ -0,0 +1,34 @@
+import logging
+
+import click
+
+from pgevents.utils import app_loader
+
+LOGGER = logging.getLogger(__name__)
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument("path")
+def init_db(path):
+    LOGGER.info("Initialising database for app: %s", path)
+    app = app_loader.load(path)
+    app.init_db()
+    LOGGER.info("Initialised database for app: %s", path)
+
+
+@cli.command()
+@click.argument("path")
+def run(path):
+    LOGGER.info("Running app: %s", path)
+    app = app_loader.load(path)
+    app.run()
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    cli()
