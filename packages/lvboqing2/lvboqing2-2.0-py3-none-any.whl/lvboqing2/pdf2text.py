6c17c563b960dd3b0b3d7d18e64005d789e23a09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:15 +0000

    Add package lvboqing2/2.0/lvboqing2-2.0-py3-none-any.whl

---
+++ packages/lvboqing2/lvboqing2-2.0-py3-none-any.whl/lvboqing2/pdf2text.py
@@ -0,0 +1,20 @@
+""" One line description.单行描述
+
+    A more detailed explanation.更详尽的解释
+"""
+
+""" This module provides functions to convert a PDF to text  实例"""
+
+def convert():
+    """
+        Convert the give PDF to text.
+        Parameters:                             参数
+        path(str):The path to a PDF file.
+        ...
+
+
+        Returns:                                返回值
+        str:The content of the PDF file as text.
+    """
+
+    print("吕柏青写的第一个发布包")
