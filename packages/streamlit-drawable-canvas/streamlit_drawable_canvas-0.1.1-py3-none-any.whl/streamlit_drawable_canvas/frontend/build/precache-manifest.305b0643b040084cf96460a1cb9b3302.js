6e4f79a747dc3e973c366c060e193db4220787f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:58 +0000

    Add package streamlit-drawable-canvas/0.1.1/streamlit_drawable_canvas-0.1.1-py3-none-any.whl

---
+++ packages/streamlit-drawable-canvas/streamlit_drawable_canvas-0.1.1-py3-none-any.whl/streamlit_drawable_canvas/frontend/build/precache-manifest.305b0643b040084cf96460a1cb9b3302.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1db9f4c0b1009a042374f046bfa50ce7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "de3b6a1711bd4fd60856",
+    "url": "./static/css/main.704bfeb6.chunk.css"
+  },
+  {
+    "revision": "9b7c837d01ea9c39ad3e",
+    "url": "./static/js/2.f7d7016b.chunk.js"
+  },
+  {
+    "revision": "d6226ead92df551047503e21c60dd32c",
+    "url": "./static/js/2.f7d7016b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de3b6a1711bd4fd60856",
+    "url": "./static/js/main.00c99b43.chunk.js"
+  },
+  {
+    "revision": "6515c66d2a8747a146d578e1c038a822",
+    "url": "./static/js/main.00c99b43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77e61c67eb4f95e15891",
+    "url": "./static/js/runtime-main.ff285346.js"
+  }
+]);
