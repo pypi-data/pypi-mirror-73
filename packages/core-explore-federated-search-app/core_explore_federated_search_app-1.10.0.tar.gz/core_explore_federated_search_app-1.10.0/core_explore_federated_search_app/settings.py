1d497f6dcc889af94eb110a0b35d3e2be666fcb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:51 +0000

    Add package core-explore-federated-search-app/1.10.0/core_explore_federated_search_app-1.10.0.tar.gz

---
+++ packages/core-explore-federated-search-app/core_explore_federated_search_app-1.10.0.tar.gz/core_explore_federated_search_app-1.10.0/core_explore_federated_search_app/settings.py
@@ -0,0 +1,9 @@
+""" Settings
+"""
+from django.conf import settings
+
+if not settings.configured:
+    settings.configure()
+
+INSTALLED_APPS = getattr(settings, "INSTALLED_APPS", [])
+INSTALLED_APPS.append("oauth2_provider")
