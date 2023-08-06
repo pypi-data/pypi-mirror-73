18c30bb7fd23f3622641bc7db649d8ccfa4a5216
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:50 +0000

    Add package ddvc-distributions/1.0/ddvc_distributions-1.0.tar.gz

---
+++ packages/ddvc-distributions/ddvc_distributions-1.0.tar.gz/ddvc_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ddvc_distributions',
+      version='1.0',
+      description=' Package to calculate and plot Gaussian and Binomial distribution',
+      packages=['ddvc_distributions'],
+      author='David De Vargas',
+      author_email='dvccueter@gmail.com',
+      zip_safe=False)
