e7fb9dd2167dfb259c893c1b9eadd7be50f2d631
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:13 +0000

    Add package ambrose-distributions/0.1/ambrose_distributions-0.1.tar.gz

---
+++ packages/ambrose-distributions/ambrose_distributions-0.1.tar.gz/ambrose_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='ambrose_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['ambrose_distributions'],
+      zip_safe=False)
