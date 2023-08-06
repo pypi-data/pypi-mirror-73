1266fa360cc51ddb0dd5039b8e168fffac49f690
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:21 +0000

    Add package pyramid-oereb/1.7.6/pyramid_oereb-1.7.6-py2.py3-none-any.whl

---
+++ packages/pyramid-oereb/pyramid_oereb-1.7.6-py2.py3-none-any.whl/tests/records/test_exclusion_of_liability.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import pytest
+
+from pyramid_oereb.lib.records.exclusion_of_liability import ExclusionOfLiabilityRecord
+
+
+def test_mandatory_fields():
+    with pytest.raises(TypeError):
+        ExclusionOfLiabilityRecord()
+
+
+def test_init():
+    record = ExclusionOfLiabilityRecord({'en': 'Disclaimer'}, {'en': u'No warranty on nothing.'})
+    assert record.title.get('en') == 'Disclaimer'
+    assert record.content is not None
+    if sys.version_info.major == 2:
+        assert isinstance(record.content.get('en'), unicode)  # noqa
+    else:
+        assert isinstance(record.content.get('en'), str)
