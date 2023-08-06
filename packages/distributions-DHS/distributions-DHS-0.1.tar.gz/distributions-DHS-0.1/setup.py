3f4cba4efedd20be52c0e79e6474d318db3b8d1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:29 +0000

    Add package distributions-DHS/0.1/distributions-DHS-0.1.tar.gz

---
+++ packages/distributions-DHS/distributions-DHS-0.1.tar.gz/distributions-DHS-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions-DHS',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author = 'Danny Huanca ejercicio Udacity',
+      author_email = 'dannyhuancasevilla@gmail.com',
+      zip_safe=False)
