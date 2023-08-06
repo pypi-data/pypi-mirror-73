5c6c867c73728d82bb3868b96b24b2b72a38c7b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:04 +0000

    Add package random-var-distro/0.1/random_var_distro-0.1.tar.gz

---
+++ packages/random-var-distro/random_var_distro-0.1.tar.gz/random_var_distro-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='random_var_distro',
+      version='0.1',
+      description='Probability distributions for continuous and discrete random variables',
+      packages=['random_var_distro'],
+      zip_safe=False)
