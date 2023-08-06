33b61c683c1a11123338f9a6229520ba704476d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:24 +0000

    Add package gbinom-dist/0.1/gbinom_dist-0.1.tar.gz

---
+++ packages/gbinom-dist/gbinom_dist-0.1.tar.gz/gbinom_dist-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gbinom_dist',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      authour='Mihir',
+      author_email='mihirsanchaniya@yahoo.com',
+      zip_safe=False)
