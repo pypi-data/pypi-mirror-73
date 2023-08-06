a3cb11087faa9c9b974a827f206cc1649dc45526
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:13 +0000

    Add package blockapi/0.0.70/blockapi-0.0.70-py3-none-any.whl

---
+++ packages/blockapi/blockapi-0.0.70-py3-none-any.whl/blockapi/api/stellar.py
@@ -0,0 +1,43 @@
+from blockapi.services import (
+    BlockchainAPI,
+    on_failure_return_none
+)
+
+
+class StellarAPI(BlockchainAPI):
+    """
+    coins: stellar
+    API docs: https://www.stellar.org/developers/horizon/reference/index.html
+    Explorer: https://stellarchain.io/
+    """
+
+    active = True
+
+    symbol = 'XLM'
+    base_url = 'https://horizon.stellar.org'
+    rate_limit = 0
+    coef = 1
+    max_items_per_page = None
+    page_offset_step = None
+    confirmed_num = None
+
+    supported_requests = {
+        'get_balance': '/accounts/{address}',
+    }
+
+    @on_failure_return_none()
+    def get_balance(self):
+        response = self.request('get_balance',
+                                address=self.address)
+        if not response:
+            return None
+
+        #return response.get('balances')
+        balances = [{ 'symbol': bal['asset_code'],
+                      'amount': float(bal['balance']),
+                    } for bal in response['balances'] if 'asset_code' in bal ]
+        balance_XLM = [{ 'symbol': 'XLM',
+                         'amount': float(bal['balance']),
+                       } for bal in response['balances'] if bal['asset_type'] == 'native' ]
+
+        return balances + balance_XLM
---
+++ packages/blockapi/blockapi-0.0.70-py3-none-any.whl/blockapi/api/zchain.py
@@ -0,0 +1,36 @@
+from blockapi.services import (
+    BlockchainAPI,
+    on_failure_return_none
+)
+
+
+class ZchainAPI(BlockchainAPI):
+    """
+    coins: zcash
+    API docs: https://explorer.zcha.in/api
+    Explorer: https://explorer.zcha.in/
+    """
+
+    active = True
+
+    symbol = 'ZEC'
+    base_url = 'https://api.zcha.in'
+    rate_limit = 0
+    coef = 1
+    max_items_per_page = None
+    page_offset_step = None
+    confirmed_num = None
+
+    supported_requests = {
+        'get_balance': '/v2/mainnet/accounts/{address}',
+    }
+
+    @on_failure_return_none()
+    def get_balance(self):
+        response = self.request('get_balance',
+                                address=self.address)
+        if not response:
+            return None
+
+        retval = response.get('balance') * self.coef
+        return [{'symbol': self.symbol, 'amount': retval}]
