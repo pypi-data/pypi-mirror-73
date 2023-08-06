93e7b90bb4e8ba1911fb637edf765d08c7084a8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:20 +0000

    Add package soso-distributions/0.1/soso_distributions-0.1.tar.gz

---
+++ packages/soso-distributions/soso_distributions-0.1.tar.gz/soso_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='soso_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['soso_distributions'],
+      author= 'Sogbeye Oloju',
+      author_email = 'soclassique@yahoo.com',
+      zip_safe=False)
