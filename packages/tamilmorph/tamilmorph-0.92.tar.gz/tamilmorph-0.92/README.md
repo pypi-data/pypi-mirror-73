ccf6ea240e261c43bfd032357c0259ba196c2aa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:30 +0000

    Add package tamilmorph/0.92/tamilmorph-0.92.tar.gz

---
+++ packages/tamilmorph/tamilmorph-0.92.tar.gz/tamilmorph-0.92/README.md
@@ -0,0 +1,3 @@
+Find more on
+[Github-flavored Markdown](https://guides.github.com/features/mastering-markdown/)
+
---
+++ packages/tamilmorph/tamilmorph-0.92.tar.gz/tamilmorph-0.92/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tamilmorph", # Replace with your own username
+    version="0.92",
+    author="Kengatharaiyer Sarveswaran",
+    author_email="sarvesk@uom.lk",
+    description="A FST based Morphological analyser and generator for Tamil",
+    long_description="Thamizhi morph (Tamil morph) is a rule based morphological analyser and generator for Tamil nouns and verbs",
+    long_description_content_type="text/markdown",
+    url="https://github.com/sarves/thamizhi-morph",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    package_data={
+        'tamilmorph': ['fsts/*.fst']},
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Natural Language :: Tamil"
+    ],
+    python_requires='>=3.6',
+)
