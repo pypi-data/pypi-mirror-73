dffbe8cb2ef1571e7288b25dfa3ebc43dc6fdc0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:57 +0000

    Add package youtube-search-python/1.2.5/youtube-search-python-1.2.5.tar.gz

---
+++ packages/youtube-search-python/youtube-search-python-1.2.5.tar.gz/youtube-search-python-1.2.5/youtubesearchpython/__init__.py
@@ -0,0 +1,8 @@
+#########v1.2.5#########
+
+from youtubesearchpython.videos__search import SearchVideos
+from youtubesearchpython.playlist__search import SearchPlaylists
+
+#########(Legacy)#########
+
+from youtubesearchpython.videos__search import SearchVideos as searchYoutube 
