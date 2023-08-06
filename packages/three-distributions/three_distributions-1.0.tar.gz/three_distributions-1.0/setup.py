6e66f96f9c4cf9c36214823f15855a8d3aa5cd3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:44 +0000

    Add package three-distributions/1.0/three_distributions-1.0.tar.gz

---
+++ packages/three-distributions/three_distributions-1.0.tar.gz/three_distributions-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='three_distributions',
+      version='1.0',
+      description='Generic, Gaussian and Binomial distributions',
+      packages=['three_distributions'],
+      author = 'Riya Lodha',
+      zip_safe=False)
