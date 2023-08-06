0b347abe62735b8ddd186cf8a4f09c1f7a42dcfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:58 +0000

    Add package NG-distributions/0.1/NG_distributions-0.1.tar.gz

---
+++ packages/NG-distributions/NG_distributions-0.1.tar.gz/NG_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='NG_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distribution',
+      packages=['NG_distributions'],
+      author = 'Shivakant Vishwakarma',
+      author_email = 'shivakant7621@gmail.com',
+      zip_safe=False)
