2cf7aa0f7e59540dd13c50e8843133bbe8a493bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:19 +0000

    Add package python-oop/0.2/python_oop-0.2.tar.gz

---
+++ packages/python-oop/python_oop-0.2.tar.gz/python_oop-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='python_oop',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['python_oop'],
+      author = 'Chetan Bhangale',
+      author_email = 'cbhangale4@gmail.com',
+      zip_safe=False)
