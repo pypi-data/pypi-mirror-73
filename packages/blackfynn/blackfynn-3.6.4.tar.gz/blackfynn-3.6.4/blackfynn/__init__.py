34c7897f310d1edef7750fb98dac751f4d6ab876
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:20 +0000

    Add package blackfynn/3.6.4/blackfynn-3.6.4.tar.gz

---
+++ packages/blackfynn/blackfynn-3.6.4.tar.gz/blackfynn-3.6.4/blackfynn/__init__.py
@@ -0,0 +1,32 @@
+from .client import Blackfynn
+from .config import DEFAULTS as DEFAULT_SETTINGS
+from .config import Settings
+from .models import (
+    BaseNode,
+    Collection,
+    DataPackage,
+    Dataset,
+    File,
+    LinkedModelProperty,
+    Model,
+    ModelFilter,
+    ModelJoin,
+    ModelProperty,
+    ModelSelect,
+    ModelTemplate,
+    Organization,
+    Property,
+    Record,
+    RecordSet,
+    Relationship,
+    RelationshipSet,
+    RelationshipType,
+    Tabular,
+    TabularSchema,
+    TimeSeries,
+    TimeSeriesAnnotation,
+    TimeSeriesChannel,
+)
+
+__title__ = "blackfynn"
+__version__ = "3.6.4"
