95bd5d33b900d7cf182dd69f291ce74aa1c677d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:55 +0000

    Add package core-module-local-id-registry-app/1.10.0/core_module_local_id_registry_app-1.10.0.tar.gz

---
+++ packages/core-module-local-id-registry-app/core_module_local_id_registry_app-1.10.0.tar.gz/core_module_local_id_registry_app-1.10.0/core_module_local_id_registry_app/settings.py
@@ -0,0 +1,14 @@
+""" Core module loval id registry settings
+"""
+from django.conf import settings
+
+if not settings.configured:
+    settings.configure()
+
+LOCAL_ID_LENGTH = getattr(settings, "LOCAL_ID_LENGTH", 20)
+""" int: length used to generate the unique local id.
+"""
+
+INSTALLED_APPS = getattr(settings, "INSTALLED_APPS", [])
+""" :py:class:`list`: List of apps installed.
+"""
