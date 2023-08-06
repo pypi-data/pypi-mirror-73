55863e55f65228906d8aedc11f688eb57c20ea23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:17 +0000

    Add package python-dynamodb-lock-whatnick/0.9.2/python_dynamodb_lock_whatnick-0.9.2.tar.gz

---
+++ packages/python-dynamodb-lock-whatnick/python_dynamodb_lock_whatnick-0.9.2.tar.gz/python_dynamodb_lock_whatnick-0.9.2/python_dynamodb_lock/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+"""The package contains a single module - with the same name i.e. python_dynamodb_lock"""
+
+__author__ = """Mohan Kishore, Tisham Dhar"""
+__email__ = 'mohankishore@yahoo.com, whatnickd@gmail.com'
+__version__ = '0.9.2'
+__copyright__ = 'Copyright (C) 2018 Mohan Kishore, 2020 Tisham Dhar'
