4fc7104e6815f8d2b8fb01c3217989c52ea14ec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:54 +0000

    Add package hepkit/1.1.1/hepkit-1.1.1.tar.gz

---
+++ packages/hepkit/hepkit-1.1.1.tar.gz/hepkit-1.1.1/hepkit/hepFormat.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def DefaultSetup(mc=999,mr=10):
+  '''
+  Personal preferences for plt.pyplot
+  '''
+  plt.rcParams['font.family'] = 'serif'
+  plt.rcParams['font.weight'] = 'light'
+  plt.rcParams['font.size'] = 16
+  plt.rcParams['figure.figsize'] = [12,8]
+  pd.options.display.max_columns = mc
+  pd.options.display.max_rows = mr
