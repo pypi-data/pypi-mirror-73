174375eb67da745bec2f965841148fe5b748ba8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:22 +0000

    Add package sumon/0.1/sumon-0.1.tar.gz

---
+++ packages/sumon/sumon-0.1.tar.gz/sumon-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='sumon',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['sumon'],
+      author = 'Sumon Bhattacharya',
+      author_email = 'sumonbhattacharjee599@gmail.com',
+      zip_safe=False)
