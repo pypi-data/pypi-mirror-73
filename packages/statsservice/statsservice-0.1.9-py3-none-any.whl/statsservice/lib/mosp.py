aae3464c9d73b90ae09e3c2a4657252ba2eff79a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:53 +0000

    Add package statsservice/0.1.9/statsservice-0.1.9-py3-none-any.whl

---
+++ packages/statsservice/statsservice-0.1.9-py3-none-any.whl/statsservice/lib/mosp.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+# Bridge with a MOSP platform. By default: https://objects.monarc.lu
+#
+
+import requests
+
+from urllib.parse import urljoin
+from statsservice.bootstrap import application
+
+
+MOSP_API_OBJECT_ENDPOINT = urljoin(application.config["MOSP_URL"], "/object/")
+
+
+def is_object_published(uuid):
+    """Check if an object has been published on MOSP."""
+    r = requests.get(urljoin(MOSP_API_OBJECT_ENDPOINT, uuid))
+    if r.status_code == 200:
+        return True
+    return False
