d99ef954ec369dd0fd742268d68560e711bd6961
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:03 +0000

    Add package huh/0.0.1/huh-0.0.1.tar.gz

---
+++ packages/huh/huh-0.0.1.tar.gz/huh-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='huh',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://www.chopraaa.com',
+    author='me',
+    author_email='v@chopraaa.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
