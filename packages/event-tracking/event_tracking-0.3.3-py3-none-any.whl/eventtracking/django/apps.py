4a860dba3463ade8c5164e5d1b3674669f99d4fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:37 +0000

    Add package event-tracking/0.3.3/event_tracking-0.3.3-py3-none-any.whl

---
+++ packages/event-tracking/event_tracking-0.3.3-py3-none-any.whl/eventtracking/django/apps.py
@@ -0,0 +1,15 @@
+"""
+New AppConfig for Django 1.8
+"""
+from __future__ import absolute_import
+from django.apps import AppConfig
+
+
+class EventTrackingConfig(AppConfig):
+    """
+    Django 1.8 requires unique app labels and only uses the characters to the right of the
+    last period in the string. .django was not specific enough.
+    """
+
+    name = 'eventtracking.django'
+    label = 'eventtracking_django'
