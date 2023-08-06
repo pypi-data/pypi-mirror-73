947b1c3ec4ce77ea630fda34521414ad1066952d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:52 +0000

    Add package my-distributions-381/0.1/my-distributions-381-0.1.tar.gz

---
+++ packages/my-distributions-381/my-distributions-381-0.1.tar.gz/my-distributions-381-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='my-distributions-381',
+      version='0.1',
+      auther_name='Bilal Ahmed',
+      auther_email='bilalmehar381@gmail.com',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
