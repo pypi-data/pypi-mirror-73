5e2f9fc305a70e53eddb19f4a753115ef9fb04a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:10 +0000

    Add package synacell/0.0.6/synacell-0.0.6-py3-none-any.whl

---
+++ packages/synacell/synacell-0.0.6-py3-none-any.whl/synacell/test/__init__.py
@@ -0,0 +1,15 @@
+from .testfunc import Test
+from .test_simple import *
+from .test_plot import *
+from .test_simple import *
+
+
+def run_all():
+    fail = 0
+    fail += run_all_simple()
+    fail += run_all_plot()
+    if fail:
+        print(f"FAIL: Running all tests!")
+    else:
+        print(f"SUCCESS: Running all tests!")
+    print(Test.test_string)
