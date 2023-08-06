3e5077aabf3a520afb674e03105ef307831c3f08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:10 +0000

    Add package ljd-distributions/0.1/ljd_distributions-0.1.tar.gz

---
+++ packages/ljd-distributions/ljd_distributions-0.1.tar.gz/ljd_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ljd_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['ljd_distributions'],
+      author = 'Lucky Jeagun Daniel',
+      author_email = 'luckyjeagun1@gmail.com',
+      zip_safe=False)
