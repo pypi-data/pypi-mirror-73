76903693f0f6b0a3b248afcaa85162180616c9a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:10 +0000

    Add package dsnd-probability-sos-o3/0.1/dsnd_probability_sos_o3-0.1.tar.gz

---
+++ packages/dsnd-probability-sos-o3/dsnd_probability_sos_o3-0.1.tar.gz/dsnd_probability_sos_o3-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_sos_o3',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dsnd_probability_sos_o3'],
+      zip_safe=False)
