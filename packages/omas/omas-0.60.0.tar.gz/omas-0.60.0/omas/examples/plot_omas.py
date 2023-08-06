0754a1445c5a4c17af245f699ad256efb8b6316a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:14 +0000

    Add package omas/0.60.0/omas-0.60.0.tar.gz

---
+++ packages/omas/omas-0.60.0.tar.gz/omas-0.60.0/omas/examples/plot_omas.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+OMAS plot examples
+==================
+This example loads some data from S3, augments the ODS with pressure information, and generates some plots
+"""
+
+from matplotlib import pyplot
+from omas import *
+
+# load some data from S3
+ods = load_omas_s3('OMFITprofiles_sample', user='omas_shared')
+
+# augment ODS with pressure information
+ods.consistency_check = False  # NOTE: some extra pressures are calculated if IMAS consistency_check = False
+ods.physics_core_profiles_pressures()
+
+# omas plot for pressures
+ods.plot_core_profiles_pressures()
+pyplot.show()
+
+# omas plot for core profiles
+ods.plot_core_profiles_summary()
+pyplot.show()
+
+# omas plot for equilibrium
+omas_plot.equilibrium_summary(ods, linewidth=1, label='my equilibrium')
+pyplot.show()
