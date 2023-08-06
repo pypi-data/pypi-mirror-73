c26abec2b050fd95a316469cf856a9f1ff4dce22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:18 +0000

    Add package distributions-lee-4135/0.1/distributions_lee_4135-0.1.tar.gz

---
+++ packages/distributions-lee-4135/distributions_lee_4135-0.1.tar.gz/distributions_lee_4135-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_lee_4135',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_lee_4135'],
+      zip_safe=False)
