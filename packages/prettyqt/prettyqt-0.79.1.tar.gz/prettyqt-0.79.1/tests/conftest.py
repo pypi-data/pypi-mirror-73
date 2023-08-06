bf5f59bb8f161a16c8dd6c7ccecb48a28171aa2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:01 +0000

    Add package prettyqt/0.79.1/prettyqt-0.79.1.tar.gz

---
+++ packages/prettyqt/prettyqt-0.79.1.tar.gz/prettyqt-0.79.1/tests/conftest.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `datacook` package."""
+
+# import pytest
+
+import pytest
+
+from prettyqt import widgets
+
+
+@pytest.fixture(scope="session")
+def qapp():
+    app = widgets.Application([])
+    app.set_metadata(
+        app_name="test", app_version="1.0.0", org_name="test", org_domain="test"
+    )
+    yield app
