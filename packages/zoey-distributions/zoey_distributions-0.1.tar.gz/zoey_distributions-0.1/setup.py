dadbcd5cfaa32b00fd73922769637b45110e2b7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:13 +0000

    Add package zoey-distributions/0.1/zoey_distributions-0.1.tar.gz

---
+++ packages/zoey-distributions/zoey_distributions-0.1.tar.gz/zoey_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='zoey_distributions',
+      version='0.1',
+      description='Gaussian distributions and Binomial distributions',
+      packages=['zoey_distributions'],
+      author = 'Jadesola Adedokun',
+      author_email = 'jadesolaadedokun@gmail.com',
+      zip_safe=False)
