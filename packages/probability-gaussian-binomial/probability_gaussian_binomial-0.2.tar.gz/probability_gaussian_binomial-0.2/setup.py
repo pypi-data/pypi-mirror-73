4ac1f25d79068ad2522d9adb89d330552687f608
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:03 +0000

    Add package probability-gaussian-binomial/0.2/probability_gaussian_binomial-0.2.tar.gz

---
+++ packages/probability-gaussian-binomial/probability_gaussian_binomial-0.2.tar.gz/probability_gaussian_binomial-0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+import sys
+
+with open('./probability_gaussian_binomial/README.md') as f:
+    long_description = f.read()
+
+setup(name='probability_gaussian_binomial',
+      version='0.2',
+      description='Gaussian and binomial distributions',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      packages=['probability_gaussian_binomial'],
+      author= 'Gopi Mehta',
+      author_email= 'gopimehta60@gmail.com',
+    zip_safe=False)
