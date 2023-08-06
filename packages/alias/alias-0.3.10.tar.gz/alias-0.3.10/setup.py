210aa45397774abcefaaf977d249c6ba3badea1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:40 +0000

    Add package alias/0.3.10/alias-0.3.10.tar.gz

---
+++ packages/alias/alias-0.3.10.tar.gz/alias-0.3.10/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+# NOT 
+setup(
+    name='alias', # Needed to silence warnings (and to be a worthwhile package)
+    url='https://github.com/psytron/alias',
+    author='Mico Malecki',
+    author_email='m@psytron.com',
+    include_package_data=True,
+    packages=['alias'], 
+    package_data={'alias':['*']},# Needed to actually package
+    install_requires=[
+        'keycache',
+        'pandas',
+        'alpaca_trade_api',
+        'v20',
+        'ccxt'],# DEPENDENCIES 
+    version='0.3.10',
+    license='PSYTRON', # Can be anything
+    description='Alias is the container.',
+    long_description=open('PYPI.md').read(),
+    long_description_content_type='text/markdown'
+)
