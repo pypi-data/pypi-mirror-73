38e91df0d5f496fbce03ede055fc8367a220d94d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:34 +0000

    Add package pyobjc-framework-CloudKit/6.2.2/pyobjc-framework-CloudKit-6.2.2.tar.gz

---
+++ packages/pyobjc-framework-CloudKit/pyobjc-framework-CloudKit-6.2.2.tar.gz/pyobjc-framework-CloudKit-6.2.2/PyObjCTest/test_ckreference.py
@@ -0,0 +1,17 @@
+import sys
+
+if sys.maxsize > 2 ** 32:
+    from PyObjCTools.TestSupport import TestCase, min_os_level
+    import CloudKit
+    import objc
+
+    class TestCKReference(TestCase):
+        @min_os_level("10.10")
+        def testClasses(self):
+            self.assertHasAttr(CloudKit, "CKReference")
+            self.assertIsInstance(CloudKit.CKReference, objc.objc_class)
+
+        @min_os_level("10.10")
+        def testConstants(self):
+            self.assertEqual(CloudKit.CKReferenceActionNone, 0)
+            self.assertEqual(CloudKit.CKReferenceActionDeleteSelf, 1)
