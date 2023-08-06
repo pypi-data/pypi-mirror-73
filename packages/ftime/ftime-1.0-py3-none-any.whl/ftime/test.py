448cb52ddf8d21160d363fdc31bb66cce880d9d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:29 +0000

    Add package ftime/1.0/ftime-1.0-py3-none-any.whl

---
+++ packages/ftime/ftime-1.0-py3-none-any.whl/ftime/test.py
@@ -0,0 +1,18 @@
+from ftime import timed
+
+@timed
+def f():
+    return [i*j for i in range(1000) for j in range(1000)]
+    
+
+@timed
+def g():
+    f()
+    return [i**i for i in range(1000)]
+
+    
+f()
+g()
+
+# print the profiler results to the screen
+timed.results()
