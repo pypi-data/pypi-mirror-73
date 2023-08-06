7f349409ef89ffc81e7613bda18659c5e4e3bf08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:03 +0000

    Add package nfstream/5.1.6/nfstream-5.1.6-cp36-cp36m-macosx_10_15_x86_64.whl

---
+++ packages/nfstream/nfstream-5.1.6-cp36-cp36m-macosx_10_15_x86_64.whl/nfstream-5.1.6.data/purelib/nfstream/__init__.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+file: __init__.py
+This file is part of nfstream.
+
+Copyright (C) 2019-20 - Zied Aouini <aouinizied@gmail.com>
+
+nfstream is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+nfstream is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with nfstream.
+If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
+from .streamer import NFStreamer
+from .plugin import NFPlugin
+from .observer import NFObserver
+
+"""
+    streamer module is the core module of nfstream package.
+"""
+
+__author__ = """Zied Aouini"""
+__email__ = 'aouinizied@gmail.com'
+__version__ = '5.1.6'
