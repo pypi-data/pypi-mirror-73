d1d6dd3daabe41c0e43defcf861da6788cda8d63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:24 +0000

    Add package get-pop/1.3/get_pop-1.3.tar.gz

---
+++ packages/get-pop/get_pop-1.3.tar.gz/get_pop-1.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="get_pop",
+    version="1.3",
+    author="DSR",
+    description="Command line tool to generates CSVs of population data for specified US states.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # list folders, not files
+    # package_dir={"": "get_pop"},
+    packages=["get_pop", "get_pop/modules"],
+    scripts=["get_pop/bin/getpop.py"],
+    package_data={"get_pop": ["static/usa_pop_counties_2019.csv"]},
+)
