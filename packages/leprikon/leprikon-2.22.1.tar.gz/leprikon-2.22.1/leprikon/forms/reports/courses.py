7d69755958c8966da88f3d4ee90b2e25ba40002b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:37 +0000

    Add package leprikon/2.22.1/leprikon-2.22.1.tar.gz

---
+++ packages/leprikon/leprikon-2.22.1.tar.gz/leprikon-2.22.1/leprikon/forms/reports/courses.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+
+from ..form import GetFormMixin
+
+
+class CoursePaymentsForm(GetFormMixin, forms.Form):
+    date_start = forms.DateField(label=_('Start date'))
+    date_end = forms.DateField(label=_('End date'))
+
+
+class CoursePaymentsStatusForm(GetFormMixin, forms.Form):
+    date = forms.DateField(label=_('Status for date'))
+
+
+class CourseStatsForm(GetFormMixin, forms.Form):
+    date = forms.DateField(label=_('Statistics for date'))
+    paid_only = forms.BooleanField(label=_('Count only paid registrations'), required=False)
---
+++ packages/leprikon/leprikon-2.22.1.tar.gz/leprikon-2.22.1/leprikon/forms/reports/events.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+
+from ..form import GetFormMixin
+
+
+class EventPaymentsForm(GetFormMixin, forms.Form):
+    date_start = forms.DateField(label=_('Start date'))
+    date_end = forms.DateField(label=_('End date'))
+
+
+class EventPaymentsStatusForm(GetFormMixin, forms.Form):
+    date = forms.DateField(label=_('Status for date'))
+
+
+class EventStatsForm(GetFormMixin, forms.Form):
+    date = forms.DateField(label=_('Statistics for date'))
+    paid_only = forms.BooleanField(label=_('Count only paid registrations'), required=False)
