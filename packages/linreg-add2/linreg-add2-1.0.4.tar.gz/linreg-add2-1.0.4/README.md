8c9b05092d99cbdfbf9a1f9c18e92ef5b1606b2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:44 +0000

    Add package linreg-add2/1.0.4/linreg-add2-1.0.4.tar.gz

---
+++ packages/linreg-add2/linreg-add2-1.0.4.tar.gz/linreg-add2-1.0.4/README.md
@@ -0,0 +1,6 @@
+#LINEAR REGRESSION ADDMISSION
+A Python package to predict addmission chances using gre score tofel score and other details
+https://github.com/DexDand/linreg-add/blob/master/README.md
+##USAGE
+
+main(337,118,4,4.5,4.5,9.65,1)
---
+++ packages/linreg-add2/linreg-add2-1.0.4.tar.gz/linreg-add2-1.0.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="linreg-add2",
+    version="1.0.4",
+    description="A Python package to predict addmission chances using gre score tofel score and other details",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/DexDand/linreg-add",
+    author="Abhishek Dand",
+    author_email="abhi080497@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["linreg_add2"],
+    include_package_data=True,
+)
