41d35feed1bc747931a39022bb0fd80edb61feee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:10 +0000

    Add package ebdalia/0.1.0/ebdalia-0.1.0.tar.gz

---
+++ packages/ebdalia/ebdalia-0.1.0.tar.gz/ebdalia-0.1.0/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import setuptools
+
+setuptools.setup(
+	name='ebdalia',
+	version='0.1.0',
+	scripts=['ebdalia'],
+	author="Jonathas Hortense",
+	author_email="jonathas.hortense@daliaresearch.com",
+	description="Elastic Beanstalk ssh cli using the internal IP address",
+	# long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/DaliaResearch/ebdalia",
+	packages=setuptools.find_packages(),
+	install_requires=[
+		'awsebcli',
+		'boto3'
+	],
+)
