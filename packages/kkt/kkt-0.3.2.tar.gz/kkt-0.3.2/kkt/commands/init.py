7b04fa391c17f9df61d82a307185c65827aeee59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:52 +0000

    Add package kkt/0.3.2/kkt-0.3.2.tar.gz

---
+++ packages/kkt/kkt-0.3.2.tar.gz/kkt-0.3.2/kkt/commands/init.py
@@ -0,0 +1,96 @@
+from enum import Enum
+from pathlib import Path
+from typing import Any, Dict, List
+
+import click
+from kaggle import KaggleApi
+from kaggle.models.kaggle_models_extended import KernelPushResponse
+from kaggle.models.kernel_push_request import KernelPushRequest
+
+from ..exception import KktSectionNotFound
+from ..parser import DEFAULT_KKT_CONFIG, KktParser
+from .kkt_command import kkt_command
+
+
+class KernelType(Enum):
+    script = "script"
+    notebook = "notebook"
+
+    def __str__(self):
+        return self.value
+
+
+def competition_prompt(api: KaggleApi) -> str:
+    competition_query = click.prompt("competition", default="", show_default=False)
+    competitions = api.competitions_list(search=competition_query)
+    for i, c in enumerate(competitions):
+        click.echo(f"{i} {c}")
+    competition_index = click.prompt(
+        ">", type=int, show_choices=False, prompt_suffix=" "
+    )
+    return str(competitions[competition_index])
+
+
+def dataset_prompt() -> List:
+    dataset_sources = []
+    while click.confirm("Would you like to add dataset sources?"):
+        dataset_source = click.prompt("dataset source")
+        dataset_sources.append(dataset_source)
+    return dataset_sources
+
+
+def init_impl(api: KaggleApi) -> Dict:
+    meta_data: Dict[str, Any] = {}
+    meta_data["competition"] = competition_prompt(api)
+    meta_data["slug"] = click.prompt("slug", type=str)
+    meta_data["code_file"] = click.prompt("code_file", default="script.py")
+    meta_data["kernel_type"] = str(
+        click.prompt(
+            "kernel_type",
+            default=DEFAULT_KKT_CONFIG["meta_data"]["kernel_type"],
+            type=KernelType,
+        )
+    )
+    meta_data["is_private"] = click.confirm(
+        "is_private", default=DEFAULT_KKT_CONFIG["meta_data"]["is_private"]
+    )
+    meta_data["enable_gpu"] = click.confirm(
+        "enable_gpu", default=DEFAULT_KKT_CONFIG["meta_data"]["enable_gpu"]
+    )
+    meta_data["enable_internet"] = click.confirm(
+        "enable_internet", default=DEFAULT_KKT_CONFIG["meta_data"]["enable_internet"]
+    )
+    meta_data["dataset_sources"] = dataset_prompt()
+    meta_data["competition_sources"] = [meta_data["competition"]]
+    meta_data["enable_constraint"] = click.confirm(
+        "enable_constraint",
+        default=DEFAULT_KKT_CONFIG["meta_data"]["enable_constraint"],
+    )
+
+    kkt_config: Dict[str, Any] = {
+        "meta_data": meta_data,
+    }
+    kkt_config["enable_git_tag"] = click.confirm(
+        "enable_git_tag", default=DEFAULT_KKT_CONFIG["enable_git_tag"]
+    )
+    return kkt_config
+
+
+def confirm_initialize() -> bool:
+    return click.confirm(
+        "Kkt section is found in pyproject.yml. Do you want to continue?"
+    )
+
+
+@kkt_command(init=True)
+def init(
+    api: KaggleApi, kkt: Dict, pyproject_path: Path, *args: List, **kwargs: Dict
+) -> None:
+    if kkt is not None and not confirm_initialize():
+        return
+    click.echo("Appending Kkt section into your pyproject.toml config.")
+
+    kkt = init_impl(api)
+
+    parser = KktParser(pyproject_path)
+    parser.write(kkt)
