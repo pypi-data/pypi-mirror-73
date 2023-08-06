695ead0de62f62229f95b6417b0f9039942b7458
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:10 +0000

    Add package flightcontrol/0.1.0/flightcontrol-0.1.0.tar.gz

---
+++ packages/flightcontrol/flightcontrol-0.1.0.tar.gz/flightcontrol-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='flightcontrol',
+      version='0.1.0',
+      description='Flight tracking resources',
+      url='https://github.com/TimHanewich/FlightControl',
+      author='Tim Hanewich',
+      author_email='tahanewich@live.com',
+      license='MIT',
+      packages=['flightcontrol'],
+      zip_safe=False)
