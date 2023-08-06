308ff6ba39ba6eb5293be5dd1e2d085ea585ffde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:00 +0000

    Add package dsnd-probability-v2/0.1/dsnd_probability_v2-0.1.tar.gz

---
+++ packages/dsnd-probability-v2/dsnd_probability_v2-0.1.tar.gz/dsnd_probability_v2-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_v2',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_probability_v2'],
+      author='Ashvinee',
+      author_email='ashvineek9@gmail.com',
+      zip_safe=False)
