498a0848c0a4704a1f76c40fa6a7db5153203558
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:42 +0000

    Add package victor-distributions/1.11/victor_distributions-1.11.tar.gz

---
+++ packages/victor-distributions/victor_distributions-1.11.tar.gz/victor_distributions-1.11/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='victor_distributions',
+      version='1.011',
+      description='Gaussian distributions',
+      packages=['victor_distributions'],
+      author = 'professor_olufemi',
+      author_email = 'femiosinkolu@gmail.com',
+      zip_safe=False)
