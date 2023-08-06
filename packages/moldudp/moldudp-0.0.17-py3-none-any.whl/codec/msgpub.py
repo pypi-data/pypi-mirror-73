86e180a3ba1f98b70808b2a29a4c18d7722496ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:18 +0000

    Add package moldudp/0.0.17/moldudp-0.0.17-py3-none-any.whl

---
+++ packages/moldudp/moldudp-0.0.17-py3-none-any.whl/codec/msgpub.py
@@ -0,0 +1,10 @@
+# Copyright 2020 All right reserved
+# Author: Chester Chee <chester.chee@gmail.com>
+#
+# Message publisher interface
+#
+class MsgPublisher:
+
+    # publish message to bus
+    def publish(self, buffer):
+        pass
