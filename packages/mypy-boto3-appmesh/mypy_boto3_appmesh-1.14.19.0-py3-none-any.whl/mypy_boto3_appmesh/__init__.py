fce90ae95c6fafed44b1406d504e2ea8d2db5109
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:45 +0000

    Add package mypy-boto3-appmesh/1.14.19.0/mypy_boto3_appmesh-1.14.19.0-py3-none-any.whl

---
+++ packages/mypy-boto3-appmesh/mypy_boto3_appmesh-1.14.19.0-py3-none-any.whl/mypy_boto3_appmesh/__init__.py
@@ -0,0 +1,54 @@
+"""
+Main interface for appmesh service.
+
+Usage::
+
+    ```python
+    import boto3
+    from mypy_boto3_appmesh import (
+        AppMeshClient,
+        Client,
+        ListMeshesPaginator,
+        ListRoutesPaginator,
+        ListTagsForResourcePaginator,
+        ListVirtualNodesPaginator,
+        ListVirtualRoutersPaginator,
+        ListVirtualServicesPaginator,
+    )
+
+    session = boto3.Session()
+
+    client: AppMeshClient = boto3.client("appmesh")
+    session_client: AppMeshClient = session.client("appmesh")
+
+    list_meshes_paginator: ListMeshesPaginator = client.get_paginator("list_meshes")
+    list_routes_paginator: ListRoutesPaginator = client.get_paginator("list_routes")
+    list_tags_for_resource_paginator: ListTagsForResourcePaginator = client.get_paginator("list_tags_for_resource")
+    list_virtual_nodes_paginator: ListVirtualNodesPaginator = client.get_paginator("list_virtual_nodes")
+    list_virtual_routers_paginator: ListVirtualRoutersPaginator = client.get_paginator("list_virtual_routers")
+    list_virtual_services_paginator: ListVirtualServicesPaginator = client.get_paginator("list_virtual_services")
+    ```
+"""
+from mypy_boto3_appmesh.client import AppMeshClient
+from mypy_boto3_appmesh.paginator import (
+    ListMeshesPaginator,
+    ListRoutesPaginator,
+    ListTagsForResourcePaginator,
+    ListVirtualNodesPaginator,
+    ListVirtualRoutersPaginator,
+    ListVirtualServicesPaginator,
+)
+
+Client = AppMeshClient
+
+
+__all__ = (
+    "AppMeshClient",
+    "Client",
+    "ListMeshesPaginator",
+    "ListRoutesPaginator",
+    "ListTagsForResourcePaginator",
+    "ListVirtualNodesPaginator",
+    "ListVirtualRoutersPaginator",
+    "ListVirtualServicesPaginator",
+)
