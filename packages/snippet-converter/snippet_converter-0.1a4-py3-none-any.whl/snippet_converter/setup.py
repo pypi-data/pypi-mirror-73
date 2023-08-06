dc46b28fb1dc828e1f3dffb4bae261588fed6d71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:29 +0000

    Add package snippet-converter/0.1a4/snippet_converter-0.1a4-py3-none-any.whl

---
+++ packages/snippet-converter/snippet_converter-0.1a4-py3-none-any.whl/snippet_converter/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import os
+import json
+
+
+def run() -> None:
+    """Set up variables file with defaults."""
+
+    variables = {}
+    variables['SUBLIME_SNIPPET_PATH'] = os.path.expanduser(
+        '~') + '/.config/sublime-text-3/Packages/User/Snippets'
+    variables['CODE_SNIPPET_PATH'] = os.path.expanduser(
+        '~') + '/.config/Code/User/snippets'
+    variables['scopes'] = {
+        'source.c++': 'cpp',
+        'source.python': 'python',
+        'source.java': 'java'
+    }
+
+    variables_file = open(os.path.expanduser(
+        '~') + '/.config/snippet-converter-variables.json', 'w')
+    json.dump(variables, variables_file, indent=2)
+    variables_file.close()
+
+
+if __name__ == '__main__':
+    run()
