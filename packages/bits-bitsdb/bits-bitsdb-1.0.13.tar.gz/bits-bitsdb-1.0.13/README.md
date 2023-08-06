7be6cbe3fd8d3abc4e1970e71252ff0ca9f22a5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:24 +0000

    Add package bits-bitsdb/1.0.13/bits-bitsdb-1.0.13.tar.gz

---
+++ packages/bits-bitsdb/bits-bitsdb-1.0.13.tar.gz/bits-bitsdb-1.0.13/README.md
@@ -0,0 +1,3 @@
+# bits-bitsdb
+
+BITSdb Python Package
---
+++ packages/bits-bitsdb/bits-bitsdb-1.0.13.tar.gz/bits-bitsdb-1.0.13/bits/bitsdb/bitsdbapi.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""BITSdbAPI class file."""
+
+# from bits.google import Google
+
+
+class BITSdbAPI(object):
+    """BITSdbAPI class."""
