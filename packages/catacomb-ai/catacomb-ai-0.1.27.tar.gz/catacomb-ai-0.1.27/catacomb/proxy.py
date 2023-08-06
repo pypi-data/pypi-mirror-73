61bbe62caaed56bf62d8f890dc1f02ef7025e7ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:57 +0000

    Add package catacomb-ai/0.1.27/catacomb-ai-0.1.27.tar.gz

---
+++ packages/catacomb-ai/catacomb-ai-0.1.27.tar.gz/catacomb-ai-0.1.27/catacomb/proxy.py
@@ -0,0 +1,38 @@
+import asyncio
+import websockets
+import json
+
+
+def connect(System, input_type="JSON", output_type="JSON", name="Local Model", examples=[]):
+    system = System()
+    asyncio.get_event_loop().run_until_complete(
+        proxy(system, input_type, output_type, name, examples)
+    )
+
+
+async def proxy(system, input_type, output_type, name, examples):
+    uri = "wss://beta.catacomb.ai/ws/proxy/"
+    async with websockets.connect(uri) as websocket:
+        model_id = ""
+        await websocket.send(json.dumps({
+            "type": "types",
+            "input_type": input_type,
+            "output_type": output_type,
+            "name": name,
+            "examples": examples
+        }))
+        while True:
+            text_data = await websocket.recv()
+            data = json.loads(text_data)
+            if data["type"] == "connect":
+                model_id = data["id"]
+                print(f"🤖 Success! View your system at https://view.catacomb.ai/local/{model_id}/")
+            elif data["type"] == "request":
+                req = data["request"]["input"]
+                req_id = data["req_id"]
+                output = system.output(req)
+                await websocket.send(json.dumps({
+                    "type": "response",
+                    "response": output,
+                    "req_id": req_id,
+                }))
