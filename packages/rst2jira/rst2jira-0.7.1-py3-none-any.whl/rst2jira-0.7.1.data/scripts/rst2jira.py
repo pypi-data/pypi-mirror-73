331627461816bb3f9ce27d20a34fdba14d399f76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:19 +0000

    Add package rst2jira/0.7.1/rst2jira-0.7.1-py3-none-any.whl

---
+++ packages/rst2jira/rst2jira-0.7.1-py3-none-any.whl/rst2jira-0.7.1.data/scripts/rst2jira.py
@@ -0,0 +1,21 @@
+#!python
+
+"""
+Confluence Wiki output generator for the Docutils Publisher.
+"""
+
+try:
+    import locale
+    locale.setlocale(locale.LC_ALL, '')
+except:
+    pass
+
+from docutils.core import publish_cmdline, default_description
+
+from rst2confluence import confluence
+
+
+description = ('Generates documents in Confluence Wiki format from standalone '
+               'reStructuredText sources.  ' + default_description)
+
+publish_cmdline(writer=confluence.Writer(), description=description)
