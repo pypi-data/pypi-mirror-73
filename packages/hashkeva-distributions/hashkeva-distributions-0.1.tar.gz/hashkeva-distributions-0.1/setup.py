9739a2eb61ba05477063b6a09b5beca057d3dc6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:41 +0000

    Add package hashkeva-distributions/0.1/hashkeva-distributions-0.1.tar.gz

---
+++ packages/hashkeva-distributions/hashkeva-distributions-0.1.tar.gz/hashkeva-distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='hashkeva-distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['hashkeva-distributions'],
+      zip_safe=False)
