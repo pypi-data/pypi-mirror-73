ec8f3353b1eaab463fb997c1e4611e431c8b6bfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:57 +0000

    Add package python-mpv/0.4.7/python-mpv-0.4.7.tar.gz

---
+++ packages/python-mpv/python-mpv-0.4.7.tar.gz/python-mpv-0.4.7/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+setup(
+    name = 'python-mpv',
+    version = '0.4.7',
+    py_modules = ['mpv'],
+    description = 'A python interface to the mpv media player',
+    url = 'https://github.com/jaseg/python-mpv',
+    author = 'jaseg',
+    author_email = 'github@jaseg.net',
+    license = 'AGPLv3+',
+    extras_require = {
+        'screenshot_raw': ['Pillow']
+        },
+    tests_require = ['xvfbwrapper'],
+    test_suite = 'mpv-test',
+    keywords = ['mpv', 'library', 'video', 'audio', 'player', 'display',
+        'multimedia'],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: X11 Applications',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Programming Language :: C',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Multimedia :: Sound/Audio :: Players',
+        'Topic :: Multimedia :: Video :: Display']
+)
