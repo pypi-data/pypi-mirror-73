3e7773fe4428dbb425a1d209b1fa7c5f007b63e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:14 +0000

    Add package textattack/0.2.0/textattack-0.2.0.tar.gz

---
+++ packages/textattack/textattack-0.2.0.tar.gz/textattack-0.2.0/textattack/__init__.py
@@ -0,0 +1,17 @@
+name = "textattack"
+
+from . import (
+    attack_recipes,
+    attack_results,
+    augmentation,
+    commands,
+    constraints,
+    datasets,
+    goal_function_results,
+    goal_functions,
+    loggers,
+    models,
+    search_methods,
+    shared,
+    transformations,
+)
