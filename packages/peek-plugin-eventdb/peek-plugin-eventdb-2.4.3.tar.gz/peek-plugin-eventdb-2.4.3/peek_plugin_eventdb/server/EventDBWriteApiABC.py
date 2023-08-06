d5fdb09675bedbe709a93508483938180a0bf3bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:45 +0000

    Add package peek-plugin-eventdb/2.4.3/peek-plugin-eventdb-2.4.3.tar.gz

---
+++ packages/peek-plugin-eventdb/peek-plugin-eventdb-2.4.3.tar.gz/peek-plugin-eventdb-2.4.3/peek_plugin_eventdb/server/EventDBWriteApiABC.py
@@ -0,0 +1,53 @@
+from abc import ABCMeta, abstractmethod
+from typing import List
+
+from twisted.internet.defer import Deferred
+
+
+class EventDBWriteApiABC(metaclass=ABCMeta):
+    @abstractmethod
+    def addEvents(self, modelSetKey: str,
+                  eventsEncodedPayload: str) -> Deferred:
+        """ Add Events
+
+        Add events to the EventDB
+
+        :param modelSetKey:  The name of the model set for the EventDB
+        :param eventsEncodedPayload: An encoded Payload containing a
+         list of events to insert.
+
+        :return: A deferred that fires when the insert is complete.
+        :rtype: None
+
+        """
+
+    @abstractmethod
+    def removeEvents(self, modelSetKey: str, eventKeys: List[str]) -> Deferred:
+        """ Remove Events
+
+        Remove events from the EventDB
+
+        :param modelSetKey:  The name of the model set for the EventDB
+        :param eventKeys: An list of event keys to remove.
+
+        :return: A deferred that fires when the removal is complete.
+        :rtype: None
+
+        """
+
+    @abstractmethod
+    def replaceProperties(self, modelSetKey: str,
+                          propertiesEncodedPayload: str) -> Deferred:
+        """ Replace Properties
+
+        Create or Replace the properties for a model set of events.
+
+        :param modelSetKey:  The name of the model set for the EventDB
+        :param propertiesEncodedPayload: An encoded Payload containing a
+                payload.tuples=List[EventDBPropertyTuple]
+         list of events to insert.
+
+        :return: A deferred that fires when the create / update is complete.
+        :rtype: None
+
+        """
