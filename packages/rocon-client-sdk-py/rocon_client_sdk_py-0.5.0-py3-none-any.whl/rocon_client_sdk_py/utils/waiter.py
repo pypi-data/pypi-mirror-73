7332072e8e0b6397cb51ca48c0bef027c0ba4f08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:11 +0000

    Add package rocon-client-sdk-py/0.5.0/rocon_client_sdk_py-0.5.0-py3-none-any.whl

---
+++ packages/rocon-client-sdk-py/rocon_client_sdk_py-0.5.0-py3-none-any.whl/rocon_client_sdk_py/utils/waiter.py
@@ -0,0 +1,63 @@
+import threading
+import asyncio
+from rx.subject import Subject
+from rx.scheduler.eventloop import AsyncIOScheduler
+
+
+class Waiter():
+    def __init__(self, event_loop, timeout_msec = -1):
+        self._timeout_msec = timeout_msec
+        self._event = Subject()
+        self._wait_exit = False
+        self.result_ob = None
+        self._event_loop = event_loop
+        self._timer = None
+
+        self.scheduler = AsyncIOScheduler(loop=event_loop)
+        self.done = asyncio.Future()
+
+
+    def set_timeout(self, timeout_msec):
+        self._timeout_msec = timeout_msec
+
+    def _timeout_callback(self):
+
+        self._event.on_next('timeout')
+
+    async def wait(self):
+        if self._timeout_msec > 0:
+            self._timer = threading.Timer(self._timeout_msec/1000, self._timeout_callback)
+            self._timer.start()
+
+        self._wait_exit = False
+
+        def cb_next(data):
+            if self._timer:
+                self._timer.cancel()
+
+            self._wait_exit = True
+            self.done.set_result(data)
+
+        def cb_completed():
+
+            self.done.set_result('done')
+
+        #self._event.subscribe(on_next=cb_next, on_completed=cb_completed, scheduler=self.scheduler)
+        self._event.subscribe(on_next=cb_next, scheduler=self.scheduler)
+        result = await self.done
+
+
+        return result
+
+    def end(self, msg):
+        self._event.on_next(msg or 'success')
+        self._event.on_completed()
+        #self.result_ob.dispose()
+
+    def set_next(self, msg):
+        self._event.on_next(msg)
+
+
+    def cancel(self):
+        #TODO
+        pass
