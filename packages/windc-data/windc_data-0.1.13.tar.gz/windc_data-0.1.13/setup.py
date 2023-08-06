e2e5bc381d67cc014da25a0403956391dcd54c36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:42 +0000

    Add package windc-data/0.1.13/windc_data-0.1.13.tar.gz

---
+++ packages/windc-data/windc_data-0.1.13.tar.gz/windc_data-0.1.13/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="windc_data",
+    version="0.1.13",
+    author="Adam Christensen",
+    author_email="achristensen@gams.com",
+    description="A helper package to read/write WiNDC Data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://windc.wisc.edu",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    install_requires=[
+        "gams",
+        "ipython",
+        "networkx",
+        "pandas",
+        "pip",
+        "tqdm",
+        "xlrd",
+        "argparse",
+        "schema",
+        "terminaltables",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
